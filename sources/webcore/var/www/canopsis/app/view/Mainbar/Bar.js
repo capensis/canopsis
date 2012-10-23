875edbbd314e53ebf4b8d0d4bceddfc495a22329
William Pain <wpain@capensis.fr>
2012-10-23 17:10:02 +0200

    fix: empty commit

--- sources/webcore/var/www/canopsis/app/view/Mainbar/Bar.js
+++ sources/webcore/var/www/canopsis/app/view/Mainbar/Bar.js
@@ -321,7 +321,8 @@ Ext.define('canopsis.view.Mainbar.Bar' , {
 				iconCls: 'icon-mainbar-sources',
 				text: '<b>commit</b>: '+global.commit.substr(0,10),
 				onClick: function(){
-					window.open("https://github.com/capensis/canopsis/commit/"+global.commit,'_blank');
+					if (global.commit)
+						window.open("https://github.com/capensis/canopsis/commit/"+global.commit,'_blank');
 				}
 			},'-', {
 				iconCls: 'icon-logout',
