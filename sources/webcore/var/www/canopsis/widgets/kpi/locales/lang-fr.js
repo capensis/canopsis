f51867576906f59b2c2e944c086da7d885a5193b
Tristan Carranante <tcarranante@capensis.fr>
2012-10-30 15:59:47 +0100

    change: pie become kpi

---
+++ sources/webcore/var/www/canopsis/widgets/kpi/locales/lang-fr.js
@@ -0,0 +1,35 @@
+/*
+#--------------------------------
+# Copyright (c) 2011 "Capensis" [http://www.capensis.com]
+#
+# This file is part of Canopsis.
+#
+# Canopsis is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Canopsis is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with Canopsis.  If not, see <http://www.gnu.org/licenses/>.
+# ---------------------------------
+*/
+
+i18n['kpi.on'] = 'sur';
+i18n['kpi.Auto title (if one node)'] = 'Titre automatique (si un noeud)';
+i18n["kpi.Total (by default use 'max' field of perfdata)"] = "Total (par défaut utilise le champ 'max' des données de performance";
+i18n['kpi.Time window'] = 'Fenêtre de temps';
+i18n['kpi.Series Type'] = 'Type de séries';
+i18n['kpi.Line Width'] = 'Largeur de la ligne';
+i18n['kpi.Enable legend'] = 'Afficher la légende';
+i18n['kpi.Legend vertical alignement'] = 'Alignement vertical de la légende';
+i18n['kpi.Legend horizontal alignement'] = 'Alignement horizontal de la légende';
+i18n['kpi.Legend layout'] = 'Disposition de la légende';
+i18n['kpi.Pie Size %'] = 'Taille en %';
+i18n['kpi.Pie'] = 'Camembert';
+i18n['kpi.Pie Style'] = 'Style du Camembert';
+i18n['kpi.Lgend Style'] = 'Style de la légende';
