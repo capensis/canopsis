43104f74e7ae2b63fe86274ec4a2106b634e4f11
William Pain <wpain@capensis.fr>
2012-11-22 17:10:42 +0100

    fix: include order

--- tools/webcore_minifier.py
+++ tools/webcore_minifier.py
@@ -32,6 +32,7 @@ first_files = [
 	os.path.join(webui_path, "app/lib/renderers.js"),
 	os.path.join(webui_path, "app/lib/form/cfield.js"),
 	os.path.join(webui_path, "app/lib/form/field/cdate.js"),
+	os.path.join(webui_path, "app/lib/view/cpopup.js"),
 	os.path.join(webui_path, "app/lib/view/cgrid.js"),
 	os.path.join(webui_path, "app/lib/view/cgrid_state.js"),
 	os.path.join(webui_path, "app/view/Tabs/Content.js")
