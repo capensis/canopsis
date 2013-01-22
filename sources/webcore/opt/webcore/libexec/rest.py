7b3a84760fa45f0571845f617318705a9dbbb38d
William Pain <wpain@capensis.fr>
2013-01-22 15:51:29 +0100

    fix: remove previous commit

--- sources/webcore/opt/webcore/libexec/rest.py
+++ sources/webcore/opt/webcore/libexec/rest.py
@@ -84,12 +84,6 @@ def rest_get_media(namespace, _id):
 @get('/rest/:namespace/:ctype')
 @get('/rest/:namespace')
 def rest_get(namespace, ctype=None, _id=None):
-	
-	## parse '/rest/:namespace/:_id'
-	if ctype and not _id:
-		_id = ctype
-		ctype = None
-
 	#get the session (security)
 	account = get_account()
 
