ee3981d8e607c5eed906511b42d5dd0fb252f8dd
William Pain <wpain@capensis.fr>
2013-02-07 16:43:44 +0100

    add: download on build process

--- sources/webcore/opt/webcore/webcore_update_locales.sh
+++ sources/webcore/opt/webcore/webcore_update_locales.sh
@@ -1,16 +1,28 @@
 #!/bin/bash -e
 
-DST="~/var/www/canopsis/resources/locales/"
+DEFAULT_DST=~/var/www/canopsis/resources/locales/
+
+# For dev only, add DST on cli
+if [ "x$1" != "x" ]; then
+	if [ -e $1 ]; then
+		echo "Use: $1"
+		cd $1
+	else
+		cd $DEFAULT_DST
+	fi
+else
+	cd $DEFAULT_DST
+fi
+
+
 URL="http://repo.canopsis.org/locales/"
 LOCALES="fr"
-
-cd $DST
 
 for LOCALE in $LOCALES; do
 	FILE="lang-$LOCALE.po"
 	echo "Update '$LOCALE':"
 	if [ -e $FILE ]; then
-		rm $FILE
+		rm -f $FILE
 	fi
 	wget -q $URL/$FILE -O $FILE
 	echo " + $FILE: Done"
