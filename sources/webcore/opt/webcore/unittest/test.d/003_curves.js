7f049953c2fb8bb0acc6a36eaab1c2d89934dde6
Tristan Carranante <tcarranante@capensis.fr>
2013-03-07 16:31:43 +0100

    add: end of curve unit_test + add record id to remove to curve store

--- sources/webcore/opt/webcore/unittest/test.d/003_curves.js
+++ sources/webcore/opt/webcore/unittest/test.d/003_curves.js
@@ -36,9 +36,61 @@ casper.then(function() {
 	selectComboValue('dashStyle','ShortDashDotDot');
 	fill_field(selector_curve_form, 'line_color', 'C79F4B');
 	fill_field(selector_curve_form, 'area_color', 'C79F4B');
-
 	click("span.icon-save");
 	waitWhile("span.icon-save");
 	waitText("Casper");
 	casper.waitUntilVisible("div.ui-pnotify-container");
+});
+
+casper.then(function() {
+	casper.echo('> Select created curve', 'COMMENT');
+	clickRowLabel('CasperLabel');
+});
+
+casper.then(function() {
+	casper.echo('> Edit curve', 'COMMENT');
+	clickRowLabel('CasperLabel', true);
+	wait("span.icon-save");
+});
+
+casper.then(function() {
+	fill_field(selector_curve_form, 'label', 'CasperLabelModified');
+	click("span.icon-save");
+	waitWhile("span.icon-save");
+	waitText("CasperLabelModified");
+	casper.waitUntilVisible("div.ui-pnotify-container");
+});
+
+casper.then(function() {
+	casper.echo('> Select created curve', 'COMMENT');
+	clickRowLabel('CasperLabelModified');
+});
+
+casper.then(function() {
+	casper.echo('> Remove created Curve', 'COMMENT');
+	click("span.icon-delete");
+	clickLabel('Yes');
+	casper.waitUntilVisible("div.ui-pnotify-container");
+});
+
+casper.then(function() {
+	casper.echo('> Reload curve', 'COMMENT');
+	click("span.icon-reload");
+	casper.waitUntilVisible("div.ui-pnotify-container");
+});
+
+casper.then(function() {
+	casper.echo('> Check if curve is really deleted', 'COMMENT');
+
+	casper.waitForText("Casper", function() {
+		casper.test.fail("curve not deleted");
+	}, function(){
+		casper.test.pass("Ok");
+	}, 500);
+
+});
+
+casper.then(function() {
+	casper.echo('> Close Tab', 'COMMENT');
+	closeTab();
 });
