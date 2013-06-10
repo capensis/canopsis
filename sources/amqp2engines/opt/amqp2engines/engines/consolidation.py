62b2c877b3c472f7696388aa79c2cfd68e46f2e8
William Pain <wpain@capensis.fr>
2013-06-10 17:59:43 +0200

    tweak: rewrite consolidation engine

--- sources/amqp2engines/opt/amqp2engines/engines/consolidation.py
+++ sources/amqp2engines/opt/amqp2engines/engines/consolidation.py
@@ -45,60 +45,67 @@ class engine(cengine):
 		self.thd_warn_sec_per_evt = 8
 		self.thd_crit_sec_per_evt = 10
 
-		self.manager = pyperfstore2.manager(logging_level=logging.INFO)	
 		cengine.__init__(self, name=NAME, *args, **kargs)
 
 	def pre_run(self):
 		self.storage = get_storage(namespace='object', account=caccount(user="root", group="root"))
-		self.manager = pyperfstore2.manager(logging_level=self.logging_level)
-		self.load_consolidation()
+		self.manager = pyperfstore2.manager(logging_level=logging.INFO)
+				
 		self.beat()
 
 	def beat(self):
-		beat_start = time.time()
+		self.logger.debug("Consolidate metrics:")
 
-		self.clean_consolidations()
+		now = time.time()
+		beat_elapsed = 0
 
-		non_loaded_records = self.storage.find({ '$and' : [{ 'crecord_type': 'consolidation' },{'enable': True}, {'loaded': { '$ne' : True} } ] }, namespace="object" )
-
-		if len(non_loaded_records) > 0  :
-			for item in non_loaded_records :
-				self.logger.info("New consolidation found '%s', load" % item.name)
-				self.load(item)
-
-		for _id in self.records.keys() :
-			exists = self.storage.find_one({ '_id': _id } )
-			if not exists:
-				self.logger.info("%s deleted, remove from record list" % self.records[_id]['crecord_name'])
-				del(self.records[_id])
+		self.load_consolidation()
 
 		for record in self.records.values():
-			consolidation_last_timestamp = self.timestamps[record.get('_id')]
+			
+			#self.logger.debug("Raw: %s" % record)
 
-			aggregation_interval = record.get('aggregation_interval', self.default_interval)
-			current_interval = int(time.time()) - consolidation_last_timestamp
+			_id = record.get('_id')
+			name = record.get('crecord_name')
 
-			self.logger.debug('current interval: %s , consolidation interval: %s' % (current_interval,aggregation_interval))
-			if  current_interval >= aggregation_interval:
-				self.logger.debug('Compute new consolidation for: %s' % record.get('crecord_name','No name found'))
+			aggregation_interval = record.get('aggregation_interval')
 
-				output_message = None
+			self.logger.debug("'%s':" % name)
+			self.logger.debug(" + interval: %s" % aggregation_interval)
+
+			last_run = record.get('consolidation_ts', now)
+
+			elapsed = now - last_run
+
+			self.logger.debug(" + elapsed: %s" % elapsed)
+
+			if elapsed == 0 or elapsed >= aggregation_interval:
+				self.logger.debug("Step 1: Select metrics")
+
 				mfilter = json.loads(record.get('mfilter'))
-				mfilter = {'$and': [mfilter, {'me': {'$nin':internal_metrics}}]}
-				#self.logger.debug('the mongo filter is: %s' % mfilter)
-				metric_list = self.manager.store.find(mfilter=mfilter)
-				self.logger.debug('length of matching metric list is: %i' % metric_list.count())
-				
-				aggregation_method = record.get('aggregation_method', False)
-				consolidation_methods = record.get('consolidation_method', False)
+				self.logger.debug(' + mfilter: %s' % mfilter)
 
+				# Exclude internal metrics
+				mfilter = {'$and': [mfilter, {'me': {'$nin':internal_metrics}}]}
+
+				metric_list = self.manager.store.find(mfilter=mfilter)
+
+				self.logger.debug(" + %s metrics found" % metric_list.count())
+
+				aggregation_method = record.get('aggregation_method')
+				self.logger.debug(" + aggregation_method: %s" % aggregation_method)
+
+				consolidation_methods = record.get('consolidation_method')
 				if not isinstance(consolidation_methods, list):
-					consolidation_methods = [ consolidation_methods ] 
+					consolidation_methods = [ consolidation_methods ]
+
+				self.logger.debug(" + consolidation_methods: %s" % consolidation_methods)
 
 				mType = mUnit = mMin = mMax = None
-				values = []
 
-				for index,metric in enumerate(metric_list) :
+				# Get metrics
+				metrics = []
+				for index, metric in enumerate(metric_list):
 					if  index == 0 :
 						#mType = metric.get('t')
 						mMin = metric.get('mi')
@@ -111,81 +118,69 @@ class engine(cengine):
 							mMin = metric.get('mi')
 						if metric.get('ma') > mMax :
 							mMax = metric.get('ma')
-						if 'sum' in consolidation_methods:
+						if 'sum' in consolidation_methods and mMax:
 							maxSum += metric.get('ma')
 						if metric.get('u') != mUnit :
+							self.logger.warning("%s: too many units" % name)
 							output_message = "warning : too many units"
 
-					self.logger.debug(' + Get points for: %s , %s , %s, %s' % (metric.get('_id'),metric.get('co'),metric.get('re',''),metric.get('me')))
+					self.logger.debug(' + %s , %s , %s, %s' % (
+						metric.get('_id'),
+						metric.get('co'),
+						metric.get('re',''),
+						metric.get('me'))
+					)
 
-					if int(time.time()) - aggregation_interval <= consolidation_last_timestamp + 60:
-						tstart = consolidation_last_timestamp
-						#self.logger.debug('   +   Use original tstart: %i' % consolidation_last_timestamp)
-					else:
-						tstart = int(time.time()) - aggregation_interval
-						#self.logger.debug('   +   new tstart: %i' % tstart)
+					metrics.append(metric.get('_id'))
 
-					self.logger.debug(
-										'   +   from: %s to %s' % 
-										(datetime.fromtimestamp(tstart).strftime('%Y-%m-%d %H:%M:%S'),
-										datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S'))
-									)
+				self.logger.debug(' + mMin: %s' % mMin)
+				self.logger.debug(' + mMax: %s' % mMax)
+				self.logger.debug(' + mUnit: %s' % mUnit)
 
-					list_points = self.manager.get_points(tstart=tstart,tstop=time.time(), _id=metric.get('_id'))
-					self.logger.debug('   +   Values on interval: %s' % ' '.join([str(value[1]) for value in list_points]))
+				self.logger.debug("Step 2: Aggregate (%s)" % aggregation_method)
 
-					if list_points:
-						fn = self.get_math_function(aggregation_method)
-						if fn:
-							point_value = fn([value[1] for value in list_points])
-						else:
-							point_value = list_points[len(list_points)-1][1]
-						values.append(point_value)
+				# Set time range
+				tstart = last_run
 
-				self.logger.debug('   +   Summary of horizontal aggregation "%s":' % aggregation_method)
-				self.logger.debug(values)
+				if elapsed == 0 or last_run < (now - 2 * aggregation_interval):
+					tstart = now - aggregation_interval
 
-				if not consolidation_methods:
-					self.storage.update(record.get('_id'), {'output_engine': "No second aggregation function given"  } )
-					return
+				self.logger.debug(
+					" + From: %s To %s "% 
+					(datetime.fromtimestamp(tstart).strftime('%Y-%m-%d %H:%M:%S'),
+					datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S'))
+				)
 
-				if len(values) == 0 :
-					self.logger.debug('  +  No values')
-					self.storage.update(record.get('_id'), {
-															'output_engine': "No input values",
-															'consolidation_ts':int(time.time())
-															})
-					self.timestamps[record.get('_id')] = int(time.time())
-					return
+				values = []
+				for mid in metrics:
+					points = self.manager.get_points(tstart=tstart, tstop=now, _id=mid)
+					fn = self.get_math_function(aggregation_method)
 
-				list_perf_data = []
-				for function_name in consolidation_methods :
-					fn = self.get_math_function(function_name)
+					pValues = [point[1] for point in points]
+					values.append(fn(pValues))
 
-					if not fn:
-						self.logger.debug('No function given for second aggregation')
-						self.storage.update(record.get('_id'), {'output_engine': "No function given for second aggregation"})
-						return
+				self.logger.debug(" + %s values" % len(values))
 
-					if len(values) == 0 :
-						if not output_message:
-							self.storage.update(record.get('_id'), {'output_engine': "No result"  } )
-						else:
-							self.storage.update(record.get('_id'), {'output_engine': "there are issues : %s warning : No result" % output_message } )
+				self.logger.debug("Step 3: Consolidate (%s)" % consolidation_methods)
 
+				perf_data_array = []
+				
+				for consolidation_method in consolidation_methods:
+					fn = self.get_math_function(consolidation_method)
 					value = fn(values)
 
-					self.logger.debug(' + Result of aggregation for "%s": %f' % (function_name,value))
+					self.logger.debug(" + %s: %s %s" % (consolidation_method, value, mUnit))
 
-					list_perf_data.append({ 
-											'metric' : function_name, 
-											'value' : roundSignifiantDigit(value,3), 
-											"unit": mUnit, 
-											'max': maxSum if function_name == 'sum' else mMax, 
-											'min': mMin, 
-											'type': 'GAUGE' } ) 
+					perf_data_array.append({
+						'metric' : consolidation_method,
+						'value' : roundSignifiantDigit(value,3),
+						"unit": mUnit,
+						'max': maxSum if consolidation_method == 'sum' else mMax,
+						'min': mMin,
+						'type': 'GAUGE'
+					}) 
 
-				point_timestamp = int(time.time()) - current_interval/2
+				self.logger.debug("Step 4: Send event")
 
 				event = cevent.forger(
 					connector ="consolidation",
@@ -195,102 +190,67 @@ class engine(cengine):
 					component = record['component'],
 					resource=record['resource'],
 					state=0,
-					timestamp=point_timestamp,
+					timestamp=now,
 					state_type=1,
-					output="Consolidation: '%s' successfully computed" % record.get('crecord_name','No name'),
+					output="Consolidation: '%s' successfully computed" % record.get('crecord_name', 'No name'),
 					long_output="",
 					perf_data=None,
-					perf_data_array=list_perf_data,
-					display_name=record['crecord_name']
-				)	
+					perf_data_array=perf_data_array,
+					display_name=name
+				)
 				rk = cevent.get_routingkey(event)
 				self.counter_event += 1
 				self.amqp.publish(event, rk, self.amqp.exchange_name_events)
 
-				self.logger.debug('The following event was sent:')
-				self.logger.debug(event)
+				self.timestamps[_id] = now
 
-				if not output_message:
-					engine_output = '%s : Computation done. Next Computation in %s s' % (datetime.now().strftime('%Y-%m-%d %H:%M:%S'),str(aggregation_interval))
-					self.storage.update(record.get('_id'),{'output_engine':engine_output} )
-				else:
-					engine_output = '%s : Computation done but there are issues : "%s" . Next Computation in %s s' % (datetime.now().strftime('%Y-%m-%d %H:%M:%S'),output_message,str(aggregation_interval))
-					self.storage.update(record.get('_id'), {'output_engine': engine_output} )
+				self.logger.debug("Step 5: Update configuration")
 
-				self.storage.update(record.get('_id'), {'consolidation_ts':int(time.time())})
-				self.timestamps[record.get('_id')] = int(time.time())
-		
-		self.counter_worktime += time.time() - beat_start
-		
-	def load (self, rec ) :
-		record = rec.dump()
-		rec.loaded = True
-		self.storage.update(record.get('_id'), {'loaded': True })
-		if record.get('mfilter', False) :
-			self.timestamps[record.get('_id')] = record.get('consolidation_ts',int(time.time()))
-			mfilter = json.loads(record.get('mfilter'))
-			metric_list = self.manager.store.find(mfilter=mfilter )
-			nb_items = metric_list.count()
-			self.storage.update(record.get('_id'), {
-													'output_engine': "Correctly Loaded",
-													'nb_items': nb_items
-													} )
-			# event = cevent.forger(
-			# 		connector = "consolidation",
-			# 		connector_name = "engine",
-			# 		event_type = "check",
-			# 		source_type="resource",
-			# 		component=record['component'],
-			# 		resource=record['resource'],
-			# 		state=0,
-			# 		state_type=1,
-			# 		output="Consolidation %s successfully loaded" % record.get('crecord_name','No name'),
-			# 		long_output="",
-			# 		perf_data=None,
-			# 		perf_data_array=None,
-			# 		display_name=record['crecord_name']
-			# )
-			#rk = cevent.get_routingkey(event)
-			self.records[record.get('_id')] = record
-			#self.amqp.publish(event, rk, self.amqp.exchange_name_events)
-		else:
-			self.storage.update(record.get('_id'), {'output_engine': "Impossible to load : no filter defined"  } )
+				beat_elapsed = time.time() - now
 
-	def clean_consolidations(self):
-		id_to_clean = []
-		ids = [_id for _id in self.records]
+				#if not output_message:
+				#	engine_output = '%s: Computation done in %.2f s. Next Computation in %s s' % (datetime.now().strftime('%Y-%m-%d %H:%M:%S'), elapsed, str(aggregation_interval))
+				#	self.storage.update(record.get('_id'),{'output_engine':engine_output} )
+				#else:
+				#	engine_output = '%s: Computation done in %.2f s but there are issues : "%s" . Next Computation in %s s' % (datetime.now().strftime('%Y-%m-%d %H:%M:%S'), elapsed, output_message,str(aggregation_interval))
+				#	self.storage.update(record.get('_id'), {'output_engine': engine_output} )
 
-		count = self.storage.count({'_id': {"$in": ids}}, namespace="object")
-		if count != len(ids):
-			for _id in self.records:
-				if not self.storage.count({'_id': _id, 'enable': True}, namespace="object"):
-					id_to_clean.append(_id)
-				
-			for _id in id_to_clean:
-				self.logger.debug("Clean consolidation %s" % _id)
-				try:
-					self.storage.update(_id, {'loaded': False})
-				except:
-					self.logger.debug(" + Record are deleted.")
-					
-				del self.records[_id]
+				self.storage.update(_id, {
+					'consolidation_ts': int(now),
+					'nb_items': len(metrics),
+					'output_engine': "Computation done in %.2fs" % (beat_elapsed)
+				})
 
-	def load_consolidation(self) :
-		records = self.storage.find({ '$and' :[ {'crecord_type': 'consolidation'},{'enable': True}] }, namespace="object")
-		for item in records :
-			self.load(item)
+			else:
+				self.logger.debug("Not the moment")
 
-		self.logger.info('Load %i consolidations' % len(records))
+		if not beat_elapsed:
+			beat_elapsed = time.time() - now
+
+		self.counter_worktime += beat_elapsed
+
+	
+	def load_consolidation(self):
+		self.logger.info('Load configurations:')
+
+		self.records = {}
+
+		records = self.storage.find({ '$and' :[ {'crecord_type': 'consolidation'}, {'enable': True}] }, namespace="object")
+
+		for item in records:
+			self.storage.update(item._id, {'loaded': True})
+			self.records[item._id] = item.dump()
+
+		self.logger.info(' + %i loaded' % len(records))
 				
 	def unload_consolidation(self):
 		records = self.storage.find({ '$and': [{'crecord_type': 'consolidation' }, {'loaded':True}]}, namespace="object")
 		for item in records :
 			self.storage.update(item._id, {
-										'output_engine': "Correctly Unload",
-										'loaded': False
-										} )
+				'loaded': False
+			} )
 
-		self.logger.info('Unload %i consolidations' % len(records))
+		self.logger.info('%i configuration unloaded' % len(records))
 
 	def get_math_function(self, name):
 		if name == 'mean':
@@ -303,6 +263,8 @@ class engine(cengine):
 			return lambda x: sum(x)
 		elif name == 'delta':
 			return lambda x: x[0] - x[-1]
+		elif name == 'last':
+			return lambda x: x[len(x)-1]
 		else:
 			return None
 
