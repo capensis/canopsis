ccbd1cbb3a3e07cefd632922ee73c143688c6e27
David Delassus <david.jose.delassus@gmail.com>
2013-09-09 13:58:57 +0200

    fix: filter syntax in testsuite of cmfilter

--- sources/canolibs/lib/canolibs/unittest/cmfilter-Myunittest.py
+++ sources/canolibs/lib/canolibs/unittest/cmfilter-Myunittest.py
@@ -82,11 +82,11 @@ class KnownValues(unittest.TestCase):
 		match = cmfilter.check(filter1, event)	
 		self.assertTrue(match, msg='Filter: %s' % filter1)
 
-		filter1 = {'timestamp': { '$and': [ {'$gt': 0} , {'$lt': 2378713357}] } }
+		filter1 = { '$and': [ {'timestamp': {'$gt': 0} } , {'timestamp': {'$lt': 2378713357} }] }
 		match = cmfilter.check(filter1, event)	
 		self.assertTrue(match, msg='Filter: %s' % filter1)
 
-		filter1 = { 'connector': { '$eq': 'cengine' },  'timestamp': { '$gt': 1378713357 }}
+		filter1 = { 'connector': { '$eq': 'cengine' },  'timestamp': { '$gt': 137871335 }}
 		match = cmfilter.check(filter1, event)	
 		self.assertTrue(match, msg='Filter: %s' % filter1)
 
