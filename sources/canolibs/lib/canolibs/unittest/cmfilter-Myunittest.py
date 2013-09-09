8be1aa2cd409e8d8957a6dfcd7ad9c7047d4ec02
David Delassus <david.jose.delassus@gmail.com>
2013-09-09 13:57:06 +0200

    fix: wrong timestamp in testsuite of cmfilter

--- sources/canolibs/lib/canolibs/unittest/cmfilter-Myunittest.py
+++ sources/canolibs/lib/canolibs/unittest/cmfilter-Myunittest.py
@@ -78,15 +78,15 @@ class KnownValues(unittest.TestCase):
 		match = cmfilter.check(filter1, event)	
 		self.assertTrue(match, msg='Filter: %s' % filter1)
 
-		filter1 = {'timestamp': { '$gt': 0, '$lt': 237871335 } }
+		filter1 = {'timestamp': { '$gt': 0, '$lt': 2378713357 } }
 		match = cmfilter.check(filter1, event)	
 		self.assertTrue(match, msg='Filter: %s' % filter1)
 
-		filter1 = {'timestamp': { '$and': [ {'$gt': 0} , {'$lt': 237871335}] } }
+		filter1 = {'timestamp': { '$and': [ {'$gt': 0} , {'$lt': 2378713357}] } }
 		match = cmfilter.check(filter1, event)	
 		self.assertTrue(match, msg='Filter: %s' % filter1)
 
-		filter1 = { 'connector': { '$eq': 'cengine' },  'timestamp': { '$gt': 137871335 }}
+		filter1 = { 'connector': { '$eq': 'cengine' },  'timestamp': { '$gt': 1378713357 }}
 		match = cmfilter.check(filter1, event)	
 		self.assertTrue(match, msg='Filter: %s' % filter1)
 
