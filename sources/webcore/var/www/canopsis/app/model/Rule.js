2a03e430d555759e99e9ade00a07c2e2d5a033da
Gwenaël Pluchon <gpluchon@capensis.fr>
2013-09-20 17:05:29 +0200

    fix: group permissions in filter rule interface model

--- sources/webcore/var/www/canopsis/app/model/Rule.js
+++ sources/webcore/var/www/canopsis/app/model/Rule.js
@@ -32,8 +32,8 @@ Ext.define('canopsis.model.Rule', {
 
 		{name: 'aaa_access_group', defaultValue: undefined},
 		{name: 'aaa_access_other', defaultValue: undefined},
-		{name: 'aaa_access_owner', defaultValue: undefined},
-		{name: 'aaa_admin_group', defaultValue: undefined},
+		{name: 'aaa_access_owner', defaultValue: ['r', 'w']},
+		{name: 'aaa_admin_group', defaultValue: ['r']},
 		{name: 'aaa_group', defaultValue: 'group.CPS_rule_admin'},
 		{name: 'aaa_owner', defaultValue: undefined}
 	]
