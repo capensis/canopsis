9c5c99b337d95755fda6aab9af851f4709cf3e04
David Delassus <david.jose.delassus@gmail.com>
2013-10-25 10:05:03 +0200

    fix: Typo in webcore.unittest

--- sources/webcore/opt/webcore/unittest/test.d/999_logout.js
+++ sources/webcore/opt/webcore/unittest/test.d/999_logout.js
@@ -7,5 +7,5 @@ var selector_logoutBtn =	'button[data-qtip="Logout"]';
 casper.then(function() {
 	casper.echo('> Logout', 'COMMENT');
 	click(selector_logoutBtn);
-	wait(selector_form, timeout*2, "Logout Ok");
+	wait(selector_form, timeout * 2, "Logout Ok");
 });
