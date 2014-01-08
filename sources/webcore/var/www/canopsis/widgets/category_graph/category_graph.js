ef6fcad2e1ace24d15041ec2f1a4fca9ea28ee4c
mrbozzo <jlabejof@yahoo.fr>
2014-01-08 17:51:17 +0100

    remove unused fields in category_graph

--- sources/webcore/var/www/canopsis/widgets/category_graph/category_graph.js
+++ sources/webcore/var/www/canopsis/widgets/category_graph/category_graph.js
@@ -30,7 +30,6 @@ Ext.define('widgets.timegraph.category_graph', {
 	graph_type: 'pie',
 	aggregate_max_points: 1,
 	aggregate_method: 'LAST',
-	aggregate_round_time: true,
 
 	//Default Options
 	max: 0,
