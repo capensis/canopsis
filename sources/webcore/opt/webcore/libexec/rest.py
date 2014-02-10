8a0498ae4bc745a9e1e84b769a8578b0127a530f
eregnier <eregnier@capensis.fr>
2014-02-10 11:46:07 +0100

    ha call modification

--- sources/webcore/opt/webcore/libexec/rest.py
+++ sources/webcore/opt/webcore/libexec/rest.py
@@ -36,6 +36,12 @@ from libexec.auth import get_account ,check_group_rights
 
 logger = logging.getLogger("rest")
 
+# dirty hack for now
+try:
+	from ha import *
+except:
+	logger.debug('unable to load ha module')
+
 ctype_to_group_access = {
 							'schedule' : 'group.CPS_schedule_admin',
 							'curve' : 'group.CPS_curve_admin',
@@ -47,7 +53,6 @@ ctype_to_group_access = {
 						}
 
 #########################################################################
-from restextend import ha
 #### GET Media
 @get('/rest/media/:namespace/:_id')
 def rest_get_media(namespace, _id):
