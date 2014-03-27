38a0689f7fa2b99acc1597dc7af6d5dad230f825
mrbozzo <jlabejof@yahoo.fr>
2014-03-27 12:57:46 +0100

    add: human raedable value in mini chart

--- sources/webcore/var/www/canopsis/widgets/mini_chart/mini_chart.js
+++ sources/webcore/var/www/canopsis/widgets/mini_chart/mini_chart.js
@@ -146,20 +146,25 @@ Ext.define('widgets.mini_chart.mini_chart', {
 
 				$(document).find('.tooltip-minichart').css('border', '2px solid ' + curve_color);
 
+				var result = '';
+
 				if(options.userOptions.chart_type === 'line_graph') {
-					result = '<b>' + rdr_tstodate(Math.round(fields['x']/1000)) + '</b><br>' + options.userOptions.metric + ': ';
-					var value = undefined;
-					if(me.humanReadable) {
-						value = rdr_humanreadable_value(fields['y'], options.userOptions.unit);
-					} else if (options.userOptions.unit) {
-						value = fields['y'] + ' ' + options.userOptions.unit;
-					} else {
-						value = fields['y'];
-					}
-					return result + value;
+					result = '<b>' + rdr_tstodate(Math.round(fields['x']/1000)) + '</b><br>';
+				} else {
+					result = '<b>' + rdr_tstodate(Math.round(options.userOptions.original_values[fields[0].offset][0] / 1000)) + '</b><br/>';
 				}
 
-				return '<b>' + rdr_tstodate(Math.round(options.userOptions.original_values[fields[0].offset][0] / 1000)) + '</b><br/>' + options.userOptions.metric + ' : ' + fields[0].value + ' ' + options.userOptions.unit;
+				result += options.userOptions.metric + ' : ';
+
+				if(me.humanReadable) {
+					result += rdr_humanreadable_value(fields['y'], options.userOptions.unit);
+				} else if (options.userOptions.unit) {
+					result += fields['y'] + ' ' + options.userOptions.unit;
+				} else {
+					result += fields['y'];
+				}
+
+				return result;
 			}
 		};
 
