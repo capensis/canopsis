f7958b157f3f8eeeade1fff6474ebd55d794deef
utopman <eregnier@capensis.fr>
2014-06-06 14:38:52 +0200

    unit testinf environement allows manage mockgin

---
+++ sources/canolibs/lib/canolibs/unittest/managermock.py
@@ -0,0 +1,13 @@
+import logging
+
+class ManagerMock(object):
+	def __init__(self, logging_level=logging.INFO):
+
+		self.exchange_name_events = 'managerMock'
+		self.logger = logging.getLogger(self.exchange_name_events)
+		self.data = []
+
+	def push(self, name=None, value=None, meta_data=None):
+		self.data.append({'name': name, 'value': value, 'meta_data': 'meta_data'})
+
+	#TODO some other mock methods
