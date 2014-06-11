2cac3ba4fe8f117c6621d545151fbb1a1ac6cb4d
mrbozzo <jlabejof@yahoo.fr>
2014-06-11 23:20:46 +0200

    add: Configuration, Category and Parameter classes in order to ease python configuration management

--- sources/canopsis/cconfiguration/cconfiguration/__init__.py
+++ sources/canopsis/cconfiguration/cconfiguration/__init__.py
@@ -23,7 +23,7 @@ __version__ = "0.1"
 
 __all__ = ('Configurable')
 
-from os.path import expanduser
+from os.path import expanduser, exists
 
 import logging
 
@@ -31,18 +31,114 @@ from cconfiguration.manager import ConfigurationManager
 from cconfiguration.watcher import add_configurable, remove_configurable
 
 
+class Configuration(object):
+    """
+    Manage configuration thanks to a list of categories which contain
+    properties.
+
+    The order of categories permit to ensure parameter overriding.
+    """
+
+    def __init__(self, *categories, **kwargs):
+        """
+        :param categories: categories to configure.
+        :type categories: list of Category.
+        """
+
+        super(Configuration, self).__init__(**kwargs)
+        self.categories = categories
+
+    def get_parameters(self, *args, **kwargs):
+        """
+        Get values and errors of parameters in respecting parameter overriding.
+
+        :return: two dictionaries respectively with values and errors by name.
+        :rtype: dict, dict
+        """
+
+        values = dict()
+        errors = dict()
+
+        result = values, errors
+
+        for category in self.categories:
+
+            for name, parameter in category.iteritems():
+
+                if parameter.value is not None:
+
+                    to_update, to_delete = errors, values if \
+                        isinstance(parameter.value, Exception) \
+                        else values, errors
+
+                    to_update[name] = parameter.value
+
+                    if name in to_delete:
+                        del to_delete[name]
+
+        return result
+
+
+class Category(dict):
+    """
+    Parameter category which contains a dictionary of parameters.
+    """
+
+    def __init__(self, name, *parameters, **kwargs):
+        """
+        :param name: unique in a configuration.
+        :type name: str
+
+        :param parameters: Parameters
+        :type parameters: list of Parameter
+        """
+        super(Category, self).__init__(name=name, **kwargs)
+        # set parameter by names.
+        self.update({parameter.name: parameter for parameter in parameters})
+
+
+class Parameter(object):
+    """
+    Parameter identified among a category by its name.
+    Provide a value and a parser (str by default).
+    """
+
+    def __init__(self, name, value=None, parser=str, *args, **kwargs):
+        """
+        :param name: unique by category
+        :type name: str
+
+        :param value: parameter value. None if not given.
+        :type value: object
+
+        :param parser: parameter test deserializer which takes in parameter
+            a str.
+        :type parser: callable
+        """
+
+        super(Parameter, self).__init__(*args, **kwargs)
+        self.name = name
+
+
 class Configurable(object):
     """
     Manages class configuration synchronisation with configuration files.
     """
 
-    DEFAULT_CONFIGURATION_FILE = '~/etc/conf.conf'
+    DEFAULT_CONF_FILE = '~/etc/conf.conf'
 
     CONF = 'CONF'
 
     AUTO_CONF = 'auto_conf'
     MANAGERS = 'conf_managers'
     LOGGING_LEVEL = 'logging_level'
+
+    CONFIGURATION = Configuration(
+        Category(
+            name=CONF,
+            Parameter(name=AUTO_CONF, parser=bool),
+            Parameter(name=MANAGERS),
+            Parameter(name=LOGGING_LEVEL)))
 
     PARSING_RULES = {
         CONF: {
@@ -54,12 +150,12 @@ class Configurable(object):
 
     def __init__(
         self,
-        configuration_files=None, auto_conf=True, logging_level=logging.INFO,
+        conf_files=None, auto_conf=True, logging_level=logging.INFO,
         managers=None, parsing_rules=None, _ready_to_conf=True, *args, **kwargs
     ):
         """
-        :param configuration_files: configuration_files to parse.
-        :type configuration_files: Iterable or str
+        :param conf_files: conf_files to parse.
+        :type conf_files: Iterable or str
 
         :param auto_conf: true force auto conf as soon as parameter change
         :type auto_conf: bool
@@ -78,10 +174,10 @@ class Configurable(object):
 
         super(Configurable, self).__init__(*args, **kwargs)
 
-        if configuration_files is None:
-            configuration_files = [Configurable.DEFAULT_CONFIGURATION_FILE]
+        if conf_files is None:
+            conf_files = [Configurable.DEFAULT_CONF_FILE]
 
-        self.configuration_files = configuration_files
+        self.conf_files = conf_files
 
         self.auto_conf = auto_conf
 
@@ -92,32 +188,32 @@ class Configurable(object):
             if parsing_rules is None else parsing_rules
 
         # set managers
-        self.managers = ConfigurationManager.get_managers() \
-            if managers is None else managers
+        self.managers = set(ConfigurationManager.get_managers()) \
+            if managers is None else set(managers)
 
         if _ready_to_conf and self.auto_conf:
             self.apply_configuration()
 
     @property
-    def configuration_files(self):
+    def conf_files(self):
         """
         :return: self configuration files
         :rtype: tuple
         """
 
-        result = tuple(getattr(self, '_configuration_files', list()))
+        result = tuple(getattr(self, '_conf_files', list()))
 
         return result
 
-    @configuration_files.setter
-    def configuration_files(self, value):
+    @conf_files.setter
+    def conf_files(self, value):
         """
-        Change of configuration_files in adding it in watching list.
+        Change of conf_files in adding it in watching list.
         """
 
         # remove previous watching
         remove_configurable(self)
-        self._configuration_files = tuple(value)
+        self._conf_files = tuple(value)
         # add new watching
         add_configurable(self)
 
@@ -143,31 +239,31 @@ class Configurable(object):
         self._logger.setLevel(value)
 
     def apply_configuration(
-        self, parsing_rules=None, configuration_files=None,
+        self, parsing_rules=None, conf_files=None,
         managers=None, *args, **kwargs
     ):
         """
         Apply configuration on a destination in 5 phases:
 
-        1. identify the right manager to use with configuration_files to parse.
-        2. for all configuration_files, get parameters which match
+        1. identify the right manager to use with conf_files to parse.
+        2. for all conf_files, get parameters which match
             with input parsing_rules.
-        3. apply parsing rules on configuration_file parameters.
+        3. apply parsing rules on conf_file parameters.
         4. put values and parsing errors in two different dictionaries.
         5. returns both dictionaries of parameter values and errors.
 
         :param parsing_rules: Iterable of parsing_rule.
         :type parsing_rules: Iterable
 
-        :param configuration_files: configuration files to parse. If
-            configuration_files is a str, it is automatically putted into an
+        :param conf_files: configuration files to parse. If
+            conf_files is a str, it is automatically putted into an
             Iterable.
-        :type configuration_files: Iterable(str) or str.
+        :type conf_files: Iterable(str) or str.
         """
 
         parameters, error_parameters = self.get_parameters(
             parsing_rules=parsing_rules,
-            configuration_files=configuration_files,
+            conf_files=conf_files,
             managers=managers,
             *args, **kwargs)
 
@@ -177,20 +273,20 @@ class Configurable(object):
 
     def get_parameters(
         self,
-        parsing_rules=None, configuration_files=None, logger=None,
+        parsing_rules=None, conf_files=None, logger=None,
         managers=None, *args, **kwargs
     ):
         """
         Get a dictionary of parameters by name from parsing_rules,
-        configuration_files and conf_managers.
+        conf_files and conf_managers.
 
         :param parsing_rules: Iterable of parsing_rule. If None, use
             self.parsing_rules.
         :type parsing_rules:
             Iterable(dict(category, dict(parameter name, parser)))
 
-        :param configuration_files: Iterable of configuration file path.
-        :type configuration_files: Iterable(str)
+        :param conf_files: Iterable of configuration file path.
+        :type conf_files: Iterable(str)
         """
 
         # start to initialize input parameters
@@ -200,36 +296,39 @@ class Configurable(object):
         if parsing_rules is None:
             parsing_rules = self.parsing_rules
 
-        if configuration_files is None:
-            configuration_files = self._configuration_files
+        if conf_files is None:
+            conf_files = self._conf_files
 
-        if isinstance(configuration_files, str):
-            configuration_files = [configuration_files]
+        if isinstance(conf_files, str):
+            conf_files = [conf_files]
 
         # clean configuration file list
-        configuration_files = [
-            expanduser(configuration_file) for configuration_file
-            in configuration_files]
+        conf_files = [
+            expanduser(conf_file) for conf_file
+            in conf_files]
 
         parameters, error_parameters = dict(), dict()
 
         if managers is None:
             managers = self.managers
 
-        # iterate on all configuration_files
-        for configuration_file in configuration_files:
+        # iterate on all conf_files
+        for conf_file in conf_files:
 
-            config_manager = self._get_manager(
-                configuration_file=configuration_file,
+            if not exists(conf_file):
+                continue
+
+            conf_manager = self._get_manager(
+                conf_file=conf_file,
                 logger=logger, managers=managers)
 
             # if a config_resource is not None
-            if config_manager is not None:
+            if conf_manager is not None:
 
-                # get parameters from a good config_manager
-                _parameters, _error_parameters = config_manager.get_parameters(
+                # get parameters from a good conf_manager
+                _parameters, _error_parameters = conf_manager.get_parameters(
                     parsing_rules=parsing_rules,
-                    configuration_file=configuration_file,
+                    conf_file=conf_file,
                     logger=logger)
 
                 # update parameters and error_parameters
@@ -239,17 +338,17 @@ class Configurable(object):
                 # clean parameters and error_parameters in order to be
                 # consistent with _parameters and _error_parameters
                 for name in _error_parameters:
-                    if name not in parameters:
+                    if name in parameters:
                         del parameters[name]
 
                 for name in _parameters:
-                    if name not in error_parameters:
+                    if name in error_parameters:
                         del error_parameters[name]
 
             else:
-                # if no config_manager, display a warning log message
+                # if no conf_manager, display a warning log message
                 logger.warning('No manager found among {0} for {1}'.format(
-                    configuration_file))
+                    conf_file))
 
         result = parameters, error_parameters
 
@@ -267,14 +366,14 @@ class Configurable(object):
         return result
 
     def set_parameters(
-        self, configuration_file, parameter_by_categories, config_manager=None,
+        self, conf_file, parameter_by_categories, conf_manager=None,
         logger=None, *args, **kwargs
     ):
         """
-        Set parameters on input configuration_file.
+        Set parameters on input conf_file.
 
         Args:
-            - configuration_files (str): configuration_file to udate with
+            - conf_files (str): conf_file to udate with
                 parameters
             - parameter_by_categories (dict(str: dict(str: object)):
             - logger (logging.Logger): logger to use to set parameters.
@@ -286,15 +385,18 @@ class Configurable(object):
             logger = self._logger
 
         # try to find a good conf_manager if conf_manager is None
-        if config_manager is None:
-            config_manager = self._get_manager(
-                configuration_file=configuration_file,
+        if conf_manager is None:
+            conf_manager = self._get_manager(
+                conf_file=conf_file,
                 logger=logger,
                 managers=self.managers)
 
-        if config_manager is not None:
-            config_manager.set_parameters(
-                configuration_file=configuration_file,
+        elif issubclass(conf_manager, ConfigurationManager):
+            conf_manager = conf_manager()
+
+        if conf_manager is not None:
+            conf_manager.set_parameters(
+                conf_file=conf_file,
                 parameter_by_categories=parameter_by_categories,
                 logger=logger)
 
@@ -302,7 +404,7 @@ class Configurable(object):
             logger.error(
                 'No ConfigurationManager found for \
                 configuration file {0}'.format(
-                    configuration_file))
+                    conf_file))
 
         return result
 
@@ -327,17 +429,18 @@ class Configurable(object):
             self.conf_managers = list()
             conf_managers = conf_managers.split(',')
             for manager in conf_managers:
-                ConfigurationManager.add_manager(manager)
+                manager = ConfigurationManager.add_manager(manager)
+                self.managers.append(manager)
 
     @staticmethod
     def _get_manager(
-        configuration_file, logger, managers
+        conf_file, logger, managers
     ):
         """
-        Get the first manager able to handle input configuration_file.
-        None if no manager is able to handle input configuration_file.
+        Get the first manager able to handle input conf_file.
+        None if no manager is able to handle input conf_file.
 
-        :return: first ConfigurationManager able to handle configuration_file.
+        :return: first ConfigurationManager able to handle conf_file.
         :rtype: ConfigurationManager
         """
 
@@ -346,8 +449,7 @@ class Configurable(object):
         for manager in managers:
             manager = manager()
 
-            handle = manager.handle(
-                configuration_file=configuration_file, logger=logger)
+            handle = manager.handle(conf_file=conf_file, logger=logger)
 
             if handle:
                 result = manager
