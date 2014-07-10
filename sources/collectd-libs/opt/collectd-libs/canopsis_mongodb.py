1038c48fa0adcb8105303d07478056dfd8a27595
mrbozzo <jlabejof@yahoo.fr>
2014-07-11 01:27:56 +0200

    :update: fix import issues in collectd-libs with new canolibs projects

--- sources/collectd-libs/opt/collectd-libs/canopsis_mongodb.py
+++ sources/collectd-libs/opt/collectd-libs/canopsis_mongodb.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 #--------------------------------
 # Copyright (c) 2011 "Capensis" [http://www.capensis.com]
 #
@@ -25,62 +26,62 @@ plugin_name = "canopsis_mongodb"
 storage = None
 
 namespaces = ['object', 'cache', 'events', 'events_log', 'ack', 'entities',
-	'perfdata3', 'perfdata2', 'perfdata2_daily']
+    'perfdata3', 'perfdata2', 'perfdata2_daily']
 
 ### Functions
 def put_value(metric, value, type='gauge'):
-	metric = collectd.Values(
-		plugin = plugin_name,
-		type = type,
-		values = [value],
-		type_instance = metric
-	)
-	metric.dispatch()
+    metric = collectd.Values(
+        plugin = plugin_name,
+        type = type,
+        values = [value],
+        type_instance = metric
+    )
+    metric.dispatch()
 
 def log(msg):
-	collectd.info("%s: %s" % (plugin_name, msg))
+    collectd.info("%s: %s" % (plugin_name, msg))
 
 ### Callbacks
 def init_callback():
-	log('Init plugin')
-	
-	from cstorage import get_storage
-	from caccount import caccount
+    log('Init plugin')
 
-	global storage
-	root = caccount(user="root", group="root")
-	storage = get_storage(account=root, namespace='object')
+    from cstorage import get_storage
+    from caccount import caccount
+
+    global storage
+    root = caccount(user="root", group="root")
+    storage = get_storage(account=root, namespace='object')
 
 def config_callback(config):
-	log('Config plugin')
+    log('Config plugin')
 
 def read_callback(data=None):
-	for namespace in namespaces:
-		put_value(namespace+"_size", storage.get_namespace_size(namespace))		
-		
-	## Pyperfstore
-	perfdata2_size = storage.get_namespace_size("perfdata2_bin.chunks") 
-	perfdata2_size += storage.get_namespace_size("perfdata2_bin.files")
-	perfdata2_size += storage.get_namespace_size("perfdata2")
-	perfdata2_size += storage.get_namespace_size("perfdata2_daily")
-	put_value("perfdata2_size", perfdata2_size)
+    for namespace in namespaces:
+        put_value(namespace+"_size", storage.get_namespace_size(namespace))
 
-	perfdata3_size = storage.get_namespace_size("perfdata3_bin.chunks") 
-	perfdata3_size += storage.get_namespace_size("perfdata3_bin.files")
-	perfdata3_size += storage.get_namespace_size("perfdata3")
-	put_value("perfdata3_size", perfdata3_size)
+    ## Pyperfstore
+    perfdata2_size = storage.get_namespace_size("perfdata2_bin.chunks")
+    perfdata2_size += storage.get_namespace_size("perfdata2_bin.files")
+    perfdata2_size += storage.get_namespace_size("perfdata2")
+    perfdata2_size += storage.get_namespace_size("perfdata2_daily")
+    put_value("perfdata2_size", perfdata2_size)
 
-	## Briefcase
-	files_size = storage.get_namespace_size("binaries.chunks") 
-	files_size += storage.get_namespace_size("binaries.files")
-	files_size += storage.get_namespace_size("files")
-	put_value("files_size", files_size)
+    perfdata3_size = storage.get_namespace_size("perfdata3_bin.chunks")
+    perfdata3_size += storage.get_namespace_size("perfdata3_bin.files")
+    perfdata3_size += storage.get_namespace_size("perfdata3")
+    put_value("perfdata3_size", perfdata3_size)
 
-	size = perfdata2_size + perfdata3_size + files_size
+    ## Briefcase
+    files_size = storage.get_namespace_size("binaries.chunks")
+    files_size += storage.get_namespace_size("binaries.files")
+    files_size += storage.get_namespace_size("files")
+    put_value("files_size", files_size)
 
-	for namespace in namespaces:
-		size += storage.get_namespace_size(namespace)
-	put_value("size", size)
+    size = perfdata2_size + perfdata3_size + files_size
+
+    for namespace in namespaces:
+        size += storage.get_namespace_size(namespace)
+    put_value("size", size)
 
 ### MAIN ###
 collectd.register_config(config_callback)
--- sources/collectd-libs/opt/collectd-libs/canopsis_rabbitmq.py
+++ sources/collectd-libs/opt/collectd-libs/canopsis_rabbitmq.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 #--------------------------------
 # Copyright (c) 2011 "Capensis" [http://www.capensis.com]
 #
@@ -25,14 +26,11 @@ import urllib2
 import os
 import httplib, base64, urlparse, socket
 
-
-
-
 plugin_name = "canopsis_rabbitmq"
 
 url = "None"
 
-canopsis_exchanges = ['canopsis.events','canopsis.alerts']
+canopsis_exchanges = ['canopsis.events', 'canopsis.alerts']
 
 #opener = None
 
@@ -42,73 +40,76 @@ filename = os.path.expanduser(filename)
 config = ConfigParser.RawConfigParser()
 
 try:
-	config.read(filename)
-	section = 'master'
-	amqp_host = config.get(section, "host")
-	amqp_userid = config.get(section, "userid")
-	amqp_password = config.get(section, "password")
+    config.read(filename)
+    section = 'master'
+    amqp_host = config.get(section, "host")
+    amqp_userid = config.get(section, "userid")
+    amqp_password = config.get(section, "password")
 
-	url = "http://%s:55672/api" % amqp_host
-except Exception, err:
-	log("Impossible to load configurations (%s) !" % err)
+    url = "http://%s:55672/api" % amqp_host
+except Exception as err:
+    log("Impossible to load configurations (%s) !" % err)
 
 
 ### Functions
 def put_value(metric, value, type='gauge'):
-	metric = collectd.Values(
-		plugin = plugin_name,
-		type = type,
-		values = [value],
-		type_instance = metric
-	)
-	metric.dispatch()
+    metric = collectd.Values(
+        plugin = plugin_name,
+        type = type,
+        values = [value],
+        type_instance = metric
+    )
+    metric.dispatch()
+
 
 def log(msg):
-	collectd.info("%s: %s" % (plugin_name, msg))
+    collectd.info("%s: %s" % (plugin_name, msg))
+
 
 ### Callbacks
 def init_callback():
-	log('Init plugin')
+    log('Init plugin')
 
-	if not url:
-		log(' + url is not defined !')
-		return
+    if not url:
+        log(' + url is not defined !')
+        return
+
 
 def config_callback(config):
-	log('Config plugin')
+    log('Config plugin')
+
 
 def read_callback(data=None):
 
-	#f = opener.open(url+"/exchanges")
-	exchanges = api("GET", "/api/exchanges", "", 'localhost', '15672', amqp_userid, amqp_password)
-	
-	try:
-		exchanges = json.loads(exchanges)
-		for exchange in exchanges:
-			name = exchange['name']
-			if name in canopsis_exchanges:
-				name = name.split('.')[1]
-				
-				try:
-					message_stats_out = exchange['message_stats_out']				
-					put_value('%s_msg_out' % name, message_stats_out['publish'], type='derive')
-					
-				except Exception, err:
-					#log("Impossible to put OUT values of %s (%s)" % (name, err))
-					pass
-					
-				try:
-					message_stats_in  = exchange['message_stats_in']
-					put_value('%s_msg_in' % name, message_stats_in['publish'], type='derive')
-					
-				except Exception, err:
-					#log("Impossible to put IN values of %s (%s)" % (name, err))
-					pass
-				
-	except Exception, err:
-		log("Impossible to read json data (%s)" % err)
-		pass
-		
+    #f = opener.open(url+"/exchanges")
+    exchanges = api("GET", "/api/exchanges", "", 'localhost', '15672', amqp_userid, amqp_password)
+
+    try:
+        exchanges = json.loads(exchanges)
+        for exchange in exchanges:
+            name = exchange['name']
+            if name in canopsis_exchanges:
+                name = name.split('.')[1]
+
+                try:
+                    message_stats_out = exchange['message_stats_out']
+                    put_value('%s_msg_out' % name, message_stats_out['publish'], type='derive')
+
+                except Exception as err:
+                    #log("Impossible to put OUT values of %s (%s)" % (name, err))
+                    pass
+
+                try:
+                    message_stats_in  = exchange['message_stats_in']
+                    put_value('%s_msg_in' % name, message_stats_in['publish'], type='derive')
+
+                except Exception, err:
+                    #log("Impossible to put IN values of %s (%s)" % (name, err))
+                    pass
+
+    except Exception as err:
+        log("Impossible to read json data (%s)" % err)
+        pass
 
 
 def api(method, path, body, hostname, port, username, password):
@@ -142,8 +143,6 @@ def api(method, path, body, hostname, port, username, password):
             raise Exception("Received %d %s for path %s\n%s"
                             % (resp.status, resp.reason, path, resp.read()))
         return resp.read()
-
-
 
 ### MAIN ###
 collectd.register_config(config_callback)
