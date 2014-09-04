18ab54545e6028364b51b2683fc6645884ae84b6
David Delassus <david.jose.delassus@gmail.com>
2014-09-04 13:26:23 +0200

    fix: move service URL fix to the bottle decorator

--- sources/python/auth/canopsis/auth/cas.py
+++ sources/python/auth/canopsis/auth/cas.py
@@ -72,6 +72,12 @@ class CASBackend(BaseBackend):
             server = config.get('server')
             service = config.get('service')
 
+            if service[-1] == '/':
+                service += 'logged_in'
+
+            else:
+                service += '/logged_in'
+
             if not s.get('auth_on', False):
                 res = self.do_auth(s, server, service)
 
@@ -147,12 +153,6 @@ class CASBackend(BaseBackend):
 
         else:
             self.logger.info('Redirecting user to CAS server: {0} --> {1}'.format(cas_server, service_url))
-
-            if service_url[-1] == '/':
-                service_url += 'logged_in'
-
-            else:
-                service_url += '/logged_in'
 
             url = '{0}/login?service={1}'.format(
                 cas_server,
