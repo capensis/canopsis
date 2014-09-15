f54f1801dd39d8d253df0702bbc593059c2c54b0
mrbozzo <jlabejof@yahoo.fr>
2014-09-15 11:08:25 +0200

    :add: check project

--- sources/python/event/canopsis/event/__init__.py
+++ sources/python/event/canopsis/event/__init__.py
@@ -21,14 +21,45 @@
 __version__ = "0.1"
 
 
-class Event(dict):
+class Event(object):
+    """
+    Manage event content
 
-    EVENT_TYPE = 'event_type'
-    SOURCE_TYPE = 'source_type'
+    An event contains information and require a type and source_type.
+    """
 
-    def __init__(self, event_type, source_type, *content):
+    TYPE = 'type'
+    SOURCE = 'source'
+    DATA = 'data'
+    META = 'meta'
 
-        super(Event, self).__init__(**content)
+    __slots__ = (TYPE, SOURCE, DATA, META)
 
-        self[Event.EVENT_TYPE] = event_type
-        self[Event.SOURCE_TYPE] = source_type
+    def __init__(self, source, data, meta, _type=None):
+
+        super(Event, self).__init__()
+
+        self.type = type(self).__name__.lower() if _type is None else _type
+        self.source = source
+        self.data = data
+        self.meta = meta
+
+    @classmethod
+    def new_event(event_class, **old_event):
+        """
+        Create an Event from an old event (ficus and older version).
+        """
+
+        _type = event_class.__name__.lower()
+        _type = old_event.pop(Event.EVENT_TYPE, _type)
+        source = old_event.pop(Event.SOURCE)
+        data = old_event.pop(Event.DATA, None)
+        meta = old_event.pop(Event.META, None)
+
+        result = Event(
+            _type=_type,
+            source=source,
+            data=data,
+            meta=meta)
+
+        return result
