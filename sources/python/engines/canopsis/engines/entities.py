3dc6bc41003ccd0134c49e36e7f96d95019bd84c
utopman <eregnier@capensis.fr>
2014-10-23 16:06:49 +0200

    udpate canopsis date to 2014

--- sources/python/engines/canopsis/engines/entities.py
+++ sources/python/engines/canopsis/engines/entities.py
@@ -1,6 +1,6 @@
 # -*- coding: UTF-8 -*-
 #--------------------------------
-# Copyright (c) 2011 "Capensis" [http://www.capensis.com]
+# Copyright (c) 2014 "Capensis" [http://www.capensis.com]
 #
 # This file is part of Canopsis.
 #
