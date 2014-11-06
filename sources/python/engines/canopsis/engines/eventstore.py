79177848a69d0aa0eb0cbccef126b660bd4a6ea5
Zack <dibe.zack@gmail.com>
2014-11-06 11:46:40 +0100

    fix: get archiver conf from correct collection

--- sources/python/engines/canopsis/engines/eventstore.py
+++ sources/python/engines/canopsis/engines/eventstore.py
@@ -33,7 +33,8 @@ class engine(Engine):
         super(engine, self).__init__(*args, **kargs)
 
         self.archiver = Archiver(
-            namespace='events', autolog=False, logging_level=self.logging_level)
+            namespace='events', confnamespace='object',
+            autolog=False, logging_level=self.logging_level)
 
         self.event_types = reader([CONFIG.get('events', 'types')]).next()
         self.check_types = reader([CONFIG.get('events', 'checks')]).next()
