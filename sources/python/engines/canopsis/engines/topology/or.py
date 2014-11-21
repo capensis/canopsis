f2a396fe6d9c06528c479c25b268c252a2a8dbb1
utopman <eregnier@capensis.fr>
2014-11-21 11:55:34 +0100

    pep 8 for all !

--- sources/python/engines/canopsis/engines/topology/or.py
+++ sources/python/engines/canopsis/engines/topology/or.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-#--------------------------------
+# --------------------------------
 # Copyright (c) 2014 "Capensis" [http://www.capensis.com]
 #
 # This file is part of Canopsis.
--- sources/python/old/test/tf_archiver.py
+++ sources/python/old/test/tf_archiver.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-#--------------------------------
+# --------------------------------
 # Copyright (c) 2014 'Capensis' [http://www.capensis.com]
 #
 # This file is part of Canopsis.
