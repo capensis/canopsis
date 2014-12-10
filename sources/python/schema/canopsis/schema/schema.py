ce6c91392a1fa4b137113d8b567db8d25273dc4b
jbb <jbb@jbb-hp>
2014-12-10 17:02:02 +0100

    Schema API functions (implementation, but no integration /!\)
    get_schema, push_schema, validate_data, validate_schema, transform
    functions available
    
    Also added utils.py, common routines for internal processing

--- sources/python/schema/canopsis/schema/schema.py
+++ sources/python/schema/canopsis/schema/schema.py
@@ -18,6 +18,16 @@
 # along with Canopsis.  If not, see <http://www.gnu.org/licenses/>.
 # ---------------------------------
 
+from sys import prefix
+from os.path import join
+from StringIO import StringIO
+from lxml.etree import parse, tostring, XMLSchema, XSLT
+
+from canopsis.schema.utils import is_name_available, get_unique_key, get_xml, \
+    is_unique_key_existing
+
+SCHEMA_PATH = join(prefix, 'etc/schema.d/xml')
+
 
 class Schema(object):
 
@@ -26,20 +36,157 @@ class Schema(object):
         pass
 
     def get_schema(self, schema):
-        raise NotImplementedError
+        """
+        Return an available schema
+
+        :param str schema: unique_key or filename
+        :return: schema in xml
+        :rtype: str
+        :raises ValueError: if schema param is neither a unique_key nor
+          a file
+        """
+
+        # Xml Schema. It will be set to a filename if a match is found.
+        xschema = get_xml(schema)
+
+        # xschema not set <=> no matches found
+        if xschema is None:
+            raise ValueError(
+                '{} neither matches with a schema filename nor with a '
+                'schema unique_key (targetNamespace)'.format(schema))
+
+        return tostring(xschema.getroot())
 
     def get_data_type_schemas(self, data_type):
         raise NotImplementedError
 
-    def push_schema(self, data_type, schema, name, schema_type):
-        raise NotImplementedError
+    def push_schema(self, name, schema):
+        """
+        Store a schema on server
+
+        :param str name: name of schema file
+        :param str schema: xml content in a string
+        :return: True if insertion has been performed
+        :rtype: bool
+        :raises ValueError: if schema name already exists
+        :raises SyntaxError: if xml cannot be read by lxml
+        :raises AttributeError: if unique_key does not exist
+        :raises ValueError: if unique_key exists in an other schema
+        """
+
+        if is_name_available(name):
+            raise ValueError(
+                'Provided schema name : \'{}\' already exists'.format(name))
+
+        if self.validate_schema(schema)[0] is False:
+            raise SyntaxError('Provided xml can neither be interpreted as '
+                              'XMLSchema nor as XSLT')
+
+        xschema = parse(StringIO(schema))
+        unique_key = get_unique_key(xschema)
+        if unique_key is None:
+            raise AttributeError('Unique key (targetNamespace attribute of '
+                                 'root element) does not exist')
+
+        if is_unique_key_existing(unique_key):
+            raise ValueError('Unique key (\'{}\') already exists'.
+                             format(unique_key))
+
+        # if no exception has been raised until here, we can write the
+        # schema
+        with open(join(SCHEMA_PATH, name), 'w') as schema_file:
+            schema_file.write(schema)
+
+        return True
 
     def validate_schema(self, schema):
-        raise NotImplementedError
+        """
+        Make sure provided schema's syntax/grammar are correct
+
+        :param str schema: xml (schema itself) or unique_key or
+          filename
+        :return: [True, <schema_type>] if schema is correct and
+          [False, None] otherwise
+        :rtype: list
+
+        .. info:: <schema_type> can be either 'XMLSchema' or 'XSLT'
+        """
+
+        xschema = get_xml(schema)
+
+        if xschema is None:
+            try:
+                xschema = parse(StringIO(schema))
+            except:
+                return [False, None]
+
+        try:
+            XMLSchema(xschema)
+            return [True, 'XMLSchema']
+        except:
+            pass
+
+        try:
+            XSLT(xschema)
+            return [True, 'XSLT']
+        except:
+            pass
+
+        return [False, None]
+
+    def assert_structural_schema(self, schema):
+        """
+        Raise an error if a schema is not a structural one
+
+        :param str schema: xml (schema itself) or unique_key or
+          filename
+        :raises AssertionError: if schema is not a structural one
+        """
+
+        assert(self.validate_schema(schema) == [True, 'XMLSchema']), \
+            'Schema \'{}\' has not been validated as a structural schema.'
+
+    def assert_metamorphic_schema(self, schema):
+        """
+        Raise an error if a schema is not a metamorphic one
+
+        :param str schema: xml (schema itself) or unique_key or
+          filename
+        :raises AssertionError: if schema is not a metamorphic one
+        """
+
+        assert(self.validate_schema(schema) == [True, 'XLST']), \
+            'Schema \'{}\' has not been validated as a metamorphic schema.'
 
     def validate_data(self, data, structural_schema, metamorphic_schema=None,
                       validate_schemas=False):
-        raise NotImplementedError
+        """
+        Ensure that a data structure matches a schema (xml schema)
+
+        :param str data: data to check
+        :param str structural_schema: unique_key or filename
+        :param str metamorphic_schema: unique_key or filename
+        :param bool validate_schemas: Any provided schema will be
+          valided before use if set to True. They are used as is
+          otherwise. This option is suited for better perfs. Use it at
+          your own risk.
+        :return: True if data is valid, False otherwise
+        :rtype: bool
+        """
+
+        if validate_schemas is True:
+            self.assert_structural_schema(structural_schema)
+
+            if metamorphic_schema is not None:
+                self.assert_metamorphic_schema(metamorphic_schema)
+
+        if metamorphic_schema is not None:
+            self.transform(data, metamorphic_schema)
+
+        xmlschema = XMLSchema(get_xml(structural_schema))
+        xml = parse(StringIO(data))
+
+        return xmlschema.validate(xml)
 
     def transform(self,
                   data,
@@ -49,5 +196,53 @@ class Schema(object):
                   validate_data=False,
                   validate_schemas=False,
                   ):
-        raise NotImplementedError
+        """
+        Transform provided data with a metamorphic schema (xslt)
+
+        :param str data: data to transform
+        :param str metamorphic_schema: unique_key or filename
+        :param from_structure: Structural schema (unique_key or
+          filename) to check data. Check is not performed if None.
+        :type from_structure: str or None
+        :param to_structure: Structural schema (unique_key or filename)
+          to check transformed data. Check is not performed if None.
+        :type to_structure: str or None
+        :param bool validate_data: Structural schemas are used if True,
+          they are ignored otherwise. This option is suited for better
+          perfs. Use it at your own risk.
+        :param bool validate_schemas: Any provided schema will be
+          valided before use if set to True. They are used as is
+          otherwise. This option is suited for better perfs. Use it at
+          your own risk.
+        :return: transformed data
+        :rtype: str (xml)
+        """
+
+        if validate_schemas is True:
+            if validate_data is True:
+
+                if from_structure is not None:
+                    self.assert_structural_schema(from_structure)
+
+                if to_structure is not None:
+                    self.assert_structural_schema(to_structure)
+
+            self.assert_metamorphic_schema(metamorphic_schema)
+
+        if validate_data is True and from_structure is not None:
+            if self.validate_data(data, from_structure) is False:
+                raise ValueError('Original data has not been validated '
+                                 'according to \'{}\''.format(from_structure))
+
+        xslt = XSLT(get_xml(metamorphic_schema))
+        xml = parse(StringIO(data))
+
+        transformed_data = xslt(xml)
+
+        if validate_data is True and to_structure is not None:
+            if self.validate_data(transformed_data, to_structure) is False:
+                raise ValueError('Transformed data has not been validated '
+                                 'according to \'{}\''.format(to_structure))
+
+        return transformed_data
 
---
+++ sources/python/schema/canopsis/schema/utils.py
@@ -0,0 +1,161 @@
+# -*- coding: utf-8 -*-
+# --------------------------------
+# Copyright (c) 2014 "Capensis" [http://www.capensis.com]
+#
+# This file is part of Canopsis.
+#
+# Canopsis is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Canopsis is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with Canopsis.  If not, see <http://www.gnu.org/licenses/>.
+# ---------------------------------
+
+from sys import prefix
+from os import listdir
+from os.path import isfile, join
+from lxml.etree import parse
+
+SCHEMA_PATH = join(prefix, 'etc/schema.d/xml')
+
+
+def get_unique_key(schema):
+    """
+    Standardize the way to determine a schema unique_key
+
+    :param _ElementTree schema: lxml _ElementTree object
+    :return: unique key or None if not found
+    :rtype: str or None
+    """
+
+    try:
+        return schema.xpath('/*/@targetNamespace')[0]
+    except IndexError:
+        return None
+
+
+def get_existing_unique_keys():
+    """
+    Return all unique keys from all available schemas for the API
+
+    :return: {unique_key: schema_filename+}
+    :rtype: dict
+    """
+
+    unique_keys = {}
+
+    # At the moment only one location is allowed for schemas : in
+    # sys.prefix/etc/schema.d/xml
+    for schema_file in listdir(SCHEMA_PATH):
+        try:
+            schema = parse(join(SCHEMA_PATH, schema_file))
+        except:
+            # If we don't manage to parse it, it may be a non-xml
+            # file that we ignore
+            continue
+
+        unique_key = get_unique_key(schema)
+        # unique_key is None when an xml parsable ressource does not
+        # fit the unique_key requirement
+        if unique_key is not None:
+            unique_keys[unique_key] = schema_file
+
+    return unique_keys
+
+
+def get_xml(schema):
+    """
+    Convert a schema identifier to a schema itself. A schema can be
+    identified by :
+
+       - its filename
+       - its entire unique_key
+       - its unique_key without version part
+
+    .. info:: unique_keys have the following expression :
+       <key>:<version>
+
+    :param schema: unique_key or filename
+    :return: schema or None if identifier did not lead to any schema
+    :rtype: _ElementTree or None
+    """
+
+    # First, we check for the filename
+    if is_name_available(schema):
+        return get_xml_from_name(schema)
+
+    # If no matches are found, we check for the unique_key, both with
+    # and without version. A full match (with the version) is
+    # prioritary on all partial matches.
+    unique_keys = get_existing_unique_keys()
+    match_without_version = None
+
+    # The following algorithm anticipates the fact that the research
+    # will not perform a full match. match_without_version records the
+    # first partial match. In worst case we do not need to iterate over
+    # unique_keys a second time.
+    for key, schema_file in unique_keys.items():
+        if key == schema:
+            return get_xml_from_name(schema_file)
+        if (key.split(':')[0] == schema and match_without_version is None):
+            match_without_version = schema_file
+    else:
+        if match_without_version is not None:
+            return get_xml_from_name(match_without_version)
+
+    # Eventually, if nothing has been returned yet...
+    return None
+
+
+def get_xml_from_name(name):
+    """
+    Return an xml tree from a filename that can be used by lxml
+
+    :param str name: filename
+    :return: xml element tree
+    :rtype: _ElementTree (lxml)
+    """
+
+    return parse(join(SCHEMA_PATH, name))
+
+
+def is_name_available(name):
+    """
+    Assert if a filename exists at the schemas location
+
+    :param str name: schema filename
+    :return: True is the schema is present, False otherwise
+    :rtype: bool
+    """
+
+    return isfile(join(SCHEMA_PATH, name))
+
+
+def is_unique_key_existing(unique_key):
+    """
+    Assert if a unique_key already exists
+
+    :param str unique_key: unique key token
+    :return: True if a match is found, False otherwise
+    :rtype: bool
+    """
+
+    unique_keys = get_existing_unique_keys()
+
+    # Processing this function can be optimized depending on how it is
+    # used. We assume here that calls will be made by giving entire
+    # unique_keys (with version).
+    uk_noversion = [key.split(':')[0] for key, schema in unique_keys.items()]
+
+    if unique_key in list(unique_keys.keys()) + uk_noversion:
+        return True
+    else:
+        return False
+
