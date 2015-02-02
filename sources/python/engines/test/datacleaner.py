beb81ee90d5d52d3a5037a9a79d6c55fd237ad23
utopman <eregnier@capensis.fr>
2015-02-02 11:56:16 +0100

    only hypcontrol start in unitests.sh and datacleaner tests updated

--- sources/python/engines/test/datacleaner.py
+++ sources/python/engines/test/datacleaner.py
@@ -36,10 +36,6 @@ class DataCleanerTest(TestCase):
         self.assertIn('events', self.engine.clean_collection)
         self.assertIn('events_log', self.engine.clean_collection)
 
-    def test_beat_interval(self):
-        one_day = 3600 * 24
-        self.assertEqual(self.engine.beat_interval, one_day)
-
     def test_db_connection(self):
 
         # event collection
