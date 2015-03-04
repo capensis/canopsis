b383683739aedb9e257a4a187f1b34483bc60050
utopman <eregnier@capensis.fr>
2015-03-04 15:57:50 +0100

    remove not working precondition on component_name

--- sources/python/old/canopsis/old/mfilter.py
+++ sources/python/old/canopsis/old/mfilter.py
@@ -92,11 +92,6 @@ def field_check(mfilter, event, key):
 
 def check(mfilter, event):
 
-    # Check connector_name
-    if 'connector_name' in mfilter and 'connector_name' in event:
-        if mfilter['connector_name'] != event['connector_name']:
-            return False
-
     # For each key of filter
     for key in mfilter:
         if key == '$and':
