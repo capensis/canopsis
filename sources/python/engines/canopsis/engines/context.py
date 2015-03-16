eb42b8dc42442f143862d298ce0b9be81e89d669
b3j0f <jlabejof@yahoo.fr>
2015-03-16 14:44:01 +0100

    :fix: bug while attempt to save an entity in the engine context when resource is empty

--- sources/python/engines/canopsis/engines/context.py
+++ sources/python/engines/canopsis/engines/context.py
@@ -143,6 +143,11 @@ class engine(Engine):
 
         context, name = self.context.get_entity_context_and_name(entity)
 
+        if 'resource' in context and not context['resource']:
+            del context['resource']
+        if 'resource' in entity and not entity['resource']:
+            del entity['resource']
+
         # put the status entity in the context
         self.context.put(
             _type=entity[Context.TYPE], entity=entity, context=context,
@@ -153,8 +158,6 @@ class engine(Engine):
         if resource:
             context['resource'] = name
         else:
-            if 'resource' in context:
-                del context['resource']
             context['component'] = name
 
         # remove type from context because type will be metric
