cea71308363b207bd53e1ef8172610bedeb46715
utopman <eregnier@capensis.fr>
2015-04-16 11:17:34 +0200

    remove wrong init code

--- sources/python/linklist/canopsis/linklist/__init__.py
+++ sources/python/linklist/canopsis/linklist/__init__.py
@@ -18,49 +18,6 @@
 # along with Canopsis.  If not, see <http://www.gnu.org/licenses/>.
 # ---------------------------------
 
-__version__ = "0.1"
-
-
-from canopsis.event import Event
-
-
-class Check(Event):
-    """
-    Manage checking event with state and status information
-    """
-
-    STATE = 'state'  #: event state field name
-    STATUS = 'status'  #: event status field name
-    EVENT_TYPE = 'check'  #: check event type
-
-    OK = 0  #: ok state value
-    MINOR = 1  #: minor state value
-    MAJOR = 2  #: major state value
-    CRITICAL = 3  #: critical state value
-
-    def __init__(self, source, state, status, meta):
-
-        super(Event, self).__init__(
-            source=source,
-            data={
-                Check.STATE: state,
-                Check.STATUS: status
-            },
-            meta=meta
-        )
-
-    @property
-    def state(self):
-        return self.data[Check.STATE]
-
-    @state.setter
-    def state(self, value):
-        self.data[Check.STATE] = value
-
-    @property
-    def status(self):
-        return self.data[Check.STATUS]
-
-    @status.setter
-    def status(self, value):
-        self.data[Check.STATUS] = value
+# attach this project to canopsis package
+from pkgutil import extend_path
+__path__ = extend_path(__path__, __name__)
