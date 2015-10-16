6b5f8e57d21e7363ff56dacb45a8d9aabd0fd622
David Delassus <david.jose.delassus@gmail.com>
2015-10-16 22:32:14 +0200

    :add: serie engine

--- sources/python/serie/canopsis/serie/manager.py
+++ sources/python/serie/canopsis/serie/manager.py
@@ -18,9 +18,10 @@
 # along with Canopsis.  If not, see <http://www.gnu.org/licenses/>.
 # ---------------------------------
 
-from canopsis.configuration.configurable.registry import ConfigurableRegistry
+from canopsis.middleware.registry import MiddlewareRegistry
 from canopsis.configuration.configurable.decorator import add_category
 from canopsis.configuration.configurable.decorator import conf_paths
+from canopsis.configuration.parameters import Parameter
 
 from canopsis.timeserie.timewindow import Period
 from canopsis.timeserie.core import TimeSerie
@@ -34,18 +35,48 @@ from RestrictedPython.Guards import safe_builtins
 
 CONF_PATH = 'serie/manager.conf'
 CATEGORY = 'SERIE'
-CONTENT = []
+CONTENT = [
+    Parameter('points_per_interval', int)
+]
 
 
 @conf_paths(CONF_PATH)
 @add_category(CATEGORY, content=CONTENT)
-class Serie(ConfigurableRegistry):
+class Serie(MiddlewareRegistry):
 
+    SERIE_STORAGE = 'serie_storage'
     CONTEXT_MANAGER = 'context'
     PERFDATA_MANAGER = 'perfdata'
 
-    def __init__(self, context=None, perfdata=None, *args, **kwargs):
+    @property
+    def points_per_interval(self):
+        if not hasattr(self, '_points_per_interval'):
+            self.points_per_interval = None
+
+        return self._points_per_interval
+
+    @points_per_interval.setter
+    def points_per_interval(self, value):
+        if value is None:
+            value = 10
+
+        self._points_per_interval = value
+    
+    def __init__(
+        self,
+        points_per_interval=None,
+        serie_storage=None,
+        context=None,
+        perfdata=None,
+        *args, **kwargs
+    ):
         super(Serie, self).__init__(*args, **kwargs)
+
+        if points_per_interval is not None:
+            self.points_per_interval = points_per_interval
+
+        if serie_storage is not None:
+            self[Serie.SERIE_STORAGE] = serie_storage
 
         if context is not None:
             self[Serie.CONTEXT_MANAGER] = context
@@ -185,3 +216,15 @@ class Serie(ConfigurableRegistry):
     def calculate(self, serieconf, timewindow=None):
         perfdatas = self.aggregation(serieconf, timewindow)
         return self.consolidation(serieconf, perfdatas)
+
+    def get_series(self, timestamp):
+        storage = self[Serie.SERIE_STORAGE]
+
+        javascript_condition = '({0} - {1}) >= ({2} / {3})'.format(
+            timestamp,
+            'this.last_computation',
+            'this.aggregation_interval',
+            self.points_per_interval
+        )
+
+        return storage.find_elements(query={'$where': javascript_condition})
---
+++ sources/python/serie/canopsis/serie/process.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+# --------------------------------
+# Copyright (c) 2015 "Capensis" [http://www.capensis.com]
+#
+# This file is part of Canopsis.
+#
+# Canopsis is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Canopsis is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with Canopsis.  If not, see <http://www.gnu.org/licenses/>.
+# ---------------------------------
+
+from canopsis.common.utils import singleton_per_scope
+from canopsis.task.core import register_task
+from canopsis.engines.core import publish
+
+from canopsis.timeserie.timewindow import TimeWindow
+from canopsis.serie.manager import Serie
+
+from time import time
+
+
+@register_task
+def beat_processing(engine, manager=None, logger=None, **kwargs):
+    if manager is None:
+        manager = singleton_per_scope(Serie)
+
+    for serie in manager.get_series(time()):
+        publish(
+            publisher=engine.amqp,
+            event=serie,
+            rk=engine.amqp_queue,
+            exchange='amq.direct',
+            logger=logger
+        )
+
+
+@register_task
+def serie_processing(engine, serieconf, manager=None, logger=None, **kwargs):
+    if manager is None:
+        manager = singleton_per_scope(Serie)
+
+    timewin = TimeWindow(
+        start=serieconf['last_computation'],
+        stop=time()
+    )
+
+    points = manager.calculate(serieconf, timewin)
+
+    for point in points:
+        metric = {
+            'metric': serieconf['crecord_name'],
+            'value': point[1],
+            'type': 'GAUGE'
+        }
+
+        for meta in ['unit', 'min', 'max', 'warn', 'crit']:
+            if serieconf.get(meta, None) is not None:
+                metric[meta] = serieconf[meta]
+
+        event = {
+            'timestamp': point[0],
+            'connector': 'canopsis',
+            'connector_name': engine.name,
+            'event_type': 'perf',
+            'source_type': 'component',
+            'component': 'serie',
+            'perf_data_array': [metric]
+        }
+
+        publish(publisher=engine.amp, event=event, logger=logger)
