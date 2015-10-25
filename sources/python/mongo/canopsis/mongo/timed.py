eb0aa68c2e1e24fd591460c948a790aa874fb1eb
b3j0f <jlabejof@yahoo.fr>
2015-10-25 18:37:00 +0100

    :add: _filter in the method TimedStorage.get

--- sources/python/mongo/canopsis/mongo/timed.py
+++ sources/python/mongo/canopsis/mongo/timed.py
@@ -27,16 +27,19 @@ from canopsis.timeserie.timewindow import get_offset_timewindow
 class MongoTimedStorage(MongoStorage, TimedStorage):
 
     class Key:
+        """Index key names."""
 
-        DATA_ID = 'd'
-        VALUE = 'v'
-        TIMESTAMP = 't'
+        DATA_ID = 'd'  #: data id index name.
+        VALUE = 'v'  #: value index name.
+        TIMESTAMP = 't'  #: timestamp index name.
 
-    TIMESTAMP_BY_ID = \
-        [(Key.DATA_ID, MongoStorage.ASC), (Key.TIMESTAMP, MongoStorage.DESC)]
+    TIMESTAMP_BY_ID = [  #: storage indexes.
+        (Key.DATA_ID, MongoStorage.ASC), (Key.TIMESTAMP, MongoStorage.DESC)
+    ]
 
     def get(
-        self, data_ids, timewindow=None, limit=0, skip=0, sort=None,
+        self, data_ids, timewindow=None, _filter=None,
+        limit=0, skip=0, sort=None,
         *args, **kwargs
     ):
 
@@ -47,8 +50,18 @@ class MongoTimedStorage(MongoStorage, TimedStorage):
 
         one_element = False
 
+        if _filter is not None:  # add value filtering
+            if isinstance(_filter, dict):
+                for name in _filter:
+                    vname = '{0}.{1}'.format(MongoTimedStorage.Key.VALUE, name)
+                    where[vname] = _filter[name]
+
+            else:
+                where[MongoTimedStorage.Key.VALUE] = _filter
+
         if isiterable(data_ids, is_str=False):
             where[MongoTimedStorage.Key.DATA_ID] = {'$in': data_ids}
+
         else:
             where[MongoTimedStorage.Key.DATA_ID] = data_ids
             one_element = True
@@ -58,7 +71,8 @@ class MongoTimedStorage(MongoStorage, TimedStorage):
         if timewindow is not None:
             timestamp = timewindow.stop()
             where[MongoTimedStorage.Key.TIMESTAMP] = {
-                '$lte': timewindow.stop()}
+                '$lte': timewindow.stop()
+            }
 
         # do the query
         cursor = self._find(document=where)
@@ -122,21 +136,39 @@ class MongoTimedStorage(MongoStorage, TimedStorage):
         timewindow = get_offset_timewindow(offset=timestamp)
 
         data = self.get(
-            data_ids=[data_id], timewindow=timewindow, limit=1)
+            data_ids=data_id, timewindow=timewindow, limit=1
+        )
 
         data_value = None
 
         if data:
-            data_value = data[data_id][0][TimedStorage.VALUE]
+            data = data[0]
+            data_value = data[TimedStorage.VALUE]
 
         if value != data_value:  # new entry to insert
 
-            values_to_insert = {
+            document = {
                 MongoTimedStorage.Key.DATA_ID: data_id,
                 MongoTimedStorage.Key.TIMESTAMP: timestamp,
                 MongoTimedStorage.Key.VALUE: value
             }
-            self._insert(document=values_to_insert, cache=cache)
+
+            if data and data[TimedStorage.TIMESTAMP] == timestamp:
+                spec = {
+                    MongoTimedStorage.Key.DATA_ID: data_id,
+                    MongoTimedStorage.Key.TIMESTAMP: timestamp
+                }
+
+                self._update(
+                    spec=spec,
+                    document=document,
+                    cache=cache,
+                    multi=False,
+                    upsert=False
+                )
+
+            else:
+                self._insert(document=document, cache=cache)
 
     def remove(self, data_ids, timewindow=None, cache=False, *args, **kwargs):
 
