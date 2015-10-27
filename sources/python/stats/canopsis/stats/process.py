e154bd27976cf42b40177b89641681fdff854cc1
David Delassus <david.jose.delassus@gmail.com>
2015-10-27 11:23:54 +0100

    :update: compute event related stats

--- sources/python/stats/canopsis/stats/process.py
+++ sources/python/stats/canopsis/stats/process.py
@@ -25,60 +25,92 @@ from canopsis.engines.core import publish
 from canopsis.stats.producers.user import UserMetricProducer
 from canopsis.stats.producers.event import EventMetricProducer
 from canopsis.session.manager import Session
+from canopsis.alerts.status import get_previous_step
+from canopsis.alerts.manager import Alerts
 
 
 @register_task
-def event_processing(
-    engine,
-    event,
-    usermgr=None,
-    eventmgr=None,
-    logger=None,
-    **kwargs
-):
+def event_processing(engine, event, usermgr=None, logger=None, **kwargs):
     if usermgr is None:
         usermgr = singleton_per_scope(UserMetricProducer)
-
-    if eventmgr is None:
-        eventmgr = singleton_per_scope(EventMetricProducer)
-
-    if event['type'] in ['ack', 'check']:
-        for manager in [usermgr, eventmgr]:
-            manager.cache(event)
 
     events = []
 
     if event['type'] == 'ack':
         events.append(usermgr.alarm_ack(event, event['ack']['author']))
-        events.append(eventmgr.alarm_ack(event))
-
-    elif event['type'] == 'check':
-        if event['state'] == 0:
-            events.append(eventmgr.alarm_solved(event))
-
-            involved_events = eventmgr.get_cache(event)
-
-            for involved_event in involved_events:
-                if involved_event['type'] == 'ack':
-                    events.append(eventmgr.alarm_ack_solved(event))
-                    break
-
-            eventmgr.clear_cache(event)
-
-        elif event.get('ack', {}).get('isAck', False):
-            events.append(eventmgr.alarm_ack(event))
-
-        else:
-            events.append(eventmgr.alarm(event))
 
     for event in events:
         publish(publisher=engine.amp, event=event, logger=logger)
 
 
 @register_task
-def beat_processing(engine, sessionmgr=None, logger=None, **kwargs):
+def beat_processing(
+    engine,
+    sessionmgr=None,
+    eventmgr=None,
+    alertsmgr=None,
+    logger=None,
+    **kwargs
+):
     if sessionmgr is None:
         sessionmgr = singleton_per_scope(Session)
 
-    for event in sessionmgr.duration():
+    if eventmgr is None:
+        eventmgr = singleton_per_scope(EventMetricProducer)
+
+    if alertsmgr is None:
+        alertsmgr = singleton_per_scope(Alerts)
+
+    storage = alertsmgr[alertsmgr.ALARM_STORAGE]
+
+    events = sessionmgr.duration()
+
+    with engine.Lock(engine, 'alarm_stats_computation') as l:
+        if l.own():
+            resolved_alarms = alertsmgr.get_alarms(
+                resolved=True,
+                exclude_tags='stats'
+            )
+
+            for docalarm in resolved_alarms:
+                alarm = docalarm[storage.VALUE]
+                alarm_ts = docalarm[storage.TIMESTAMP]
+                alarm_events = alertsmgr.get_events(docalarm)
+
+                solved_delay = alarm['resolved'] - alarm_ts
+                events.append(eventmgr.alarm_solved_delay(solved_delay))
+
+                if alarm['ack'] is not None:
+                    ack_ts = alarm['ack']['t']
+                    ackremove = get_previous_step(
+                        alarm,
+                        'ackremove',
+                        ts=ack_ts
+                    )
+                    ts = alarm_ts if ackremove is None else ackremove['t']
+                    ack_delay = ack_ts - ts
+
+                    events.append(eventmgr.alarm_ack_delay(ack_delay))
+                    events.append(
+                        eventmgr.alarm_ack_solved_delay(
+                            solved_delay - ack_delay
+                        )
+                    )
+
+                if len(alarm_events) > 0:
+                    events.append(eventmgr.alarm(alarm_events[0]))
+
+                for event in alarm_events:
+                    if event['event_type'] == 'ack':
+                        events.append(eventmgr.alarm_ack(event))
+
+                    elif event['timestamp'] == alarm['resolved']:
+                        events.append(eventmgr.alarm_solved(event))
+
+                        if alarm['ack'] is not None:
+                            events.append(eventmgr.alarm_ack_solved(event))
+
+                alertsmgr.update_current_alarm(docalarm, alarm, tags='stats')
+
+    for event in events:
         publish(publisher=engine.amqp, event=event, logger=logger)
