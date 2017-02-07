9c3680847575a0627ae4d438b43f37c8bc39e8f4
tgosselin <contact@dowan.beer>
2017-02-07 17:47:30 +0100

    create methods

--- sources/python/context_graph/canopsis/context_graph/manager.py
+++ sources/python/context_graph/canopsis/context_graph/manager.py
@@ -27,5 +27,62 @@ class ContextGraph(MiddlewareRegistry):
         """
         super(ContextGraph, self).__init__(*args, **kwargs)
 
+    def check_comp(self, comp_id):
+        """_check_comp
 
+        check if the component exists in database
 
+        :param comp_id: id of component
+        :return type: boolean
+        """
+
+    def check_re(self, re_id):
+        """_check_re
+
+        :param re_id:
+        """
+
+    def check_conn(self, conn_id):
+        """_check_conn
+
+        :param conn_id:
+        """
+
+    def check_links(self, conn_id, comp_id, re_id):
+        """_check_links
+
+        :param conn_id:
+        :param comp_id:
+        :param re_id:
+        """
+
+    def _checks_conn_re_link(self, conn_id, re_id):
+        """_checks_conn_re_link
+
+        :param conn_id:
+        :param re_id:
+        """
+    def _check_conn_comp_link(self, conn_id, comp_id):
+        """_check_conn_comp_link
+
+        :param conn_id:
+        :param comp_id:
+        """
+
+    def add_comp(self, comp):
+        """add_comp
+
+        :param comp:
+        """
+
+    def add_re(self, re):
+        """add_re
+
+        :param re:
+        """
+
+    def add_conn(self, conn):
+        """add_conn
+
+        :param conn:
+        """
--- sources/python/context_graph/canopsis/context_graph/process.py
+++ sources/python/context_graph/canopsis/context_graph/process.py
@@ -19,4 +19,23 @@ def event_processing(
         engine, event, manager=None, logger=None, ctx=None, tm=None, cm=None,
         **kwargs
 ):
-    logger.error(event)
+    """event_processing
+
+    :param engine:
+    :param event:
+    :param manager:
+    :param logger:
+    :param ctx:
+    :param tm:
+    :param cm:
+    :param **kwargs:
+    """
+    #generate id
+
+    if context_graph_manager.check_comp(comp_id):
+        pass
+    if context_graph_manager.check_re(re_id):
+        pass
+    if context_graph_manager.check_conn(comm_id):
+        pass
+
