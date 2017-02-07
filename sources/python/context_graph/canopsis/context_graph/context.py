90248732c711a9ba840e5fdc2aa712b1366353eb
Arthur Dewarumez <arthur.dewarumez@gmail.com>
2017-02-07 14:28:27 +0100

    Start implementation of the new context.

---
+++ sources/python/context_graph/canopsis/context_graph/context.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+"""Module in chage of defining the graph context and updating
+ it when it's needed."""
+
+from __future__ import unicode_literals
+
+from canopsis.task.core import register_task
+from canopsis.context_graph.manager import ContextGraph
+from canopsis.common.utils import singleton_per_scope
+
+@register_task
+def event_processing(
+        engine, event, manager=None, logger=None, ctx=None, tm=None, cm=None,
+        **kwargs
+):
+    logger.error(event)
