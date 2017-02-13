6c65e08dc018924be5c0706a521791bad199be81
Arthur Dewarumez <arthur.dewarumez@gmail.com>
2017-02-13 15:12:35 +0100

    Optimisation.

--- sources/python/context_graph/canopsis/context_graph/process.py
+++ sources/python/context_graph/canopsis/context_graph/process.py
@@ -14,6 +14,49 @@ from canopsis.context_graph.manager import ContextGraph
 
 context_graph_manager = ContextGraph()
 
+
+def create_entity(id, name, etype, depends=[], impact=[], measurements=[], infos={}):
+    return {'_id': id,
+            'type': etype,
+            'name': name,
+            'depends': depends,
+            'impact': impact,
+            'measurements': measurements,
+            'infos': infos
+            }
+
+
+def update_depends_link(ent1_id, ent2_id, context):
+    """
+    Update depends link between from entity ent1 to ent2 in the context.
+    """
+    ent_from = context[ent1_id]
+
+    # for ent in ent_from["depends"]:
+    #     if not ent in ent_to["depends"]:
+    #         ent_to["depends"].append(ent)
+
+    if ent2_id not in ent_from["depends"]:
+        ent_from.append(ent2_id)
+
+
+def update_link(ent1_id, ent2_id, context):
+    """
+    Update depends link from entity ent1 to ent2 in the context.
+    Basicaly, add ent2_id in the field "impact" of the entity
+    identified by ent1_id the then add ent1_id in the field "depends"
+    of ent2_id.
+    """
+    ent1 = context[ent1_id]
+    ent2 = context[ent2_id]
+
+    if ent1_id not in ent2["impact"]:
+        ent2["impact"].append(ent1_id)
+
+    if ent2_id not in ent1["depends"]:
+        ent1["depends"].append(ent2_id)
+
+
 @register_task
 def event_processing(
         engine, event, manager=None, logger=None, ctx=None, tm=None, cm=None,
@@ -30,62 +73,88 @@ def event_processing(
     :param cm:
     :param **kwargs:
     """
+
+    # Retreive id from id
     comp_id = event['component']
+
     re_id = None
     if 'resource' in event.keys():
         re_id = '{0}/{1}'.format(event['resource'], comp_id)
+
     conn_id = '{0}/{1}'.format(event['connector'], event['connector_name'])
 
+    related_ctx = context_graph_manager.get_entity([comp_id, re_id, conn_id])
 
-    if not context_graph_manager.check_comp(comp_id):
+    comp_there = False
+    re_there = False
+    conn_there = False
+
+    for entity in related_ctx:
+        if entity["_id"] == comp_id:
+            comp_there = True
+
+        if entity["_id"] == re_id:
+            re_there = True
+
+        if entity["_id"] == conn_id:
+            conn_there = True
+
+    context = {}
+
+    if not comp_there:
         depends = [conn_id]
         if 'resource' in event.keys():
             depends.append(re_id)
-            context_graph_manager.add_re({
-                '_id': re_id,
-                'name': event['resource'],
-                'type': 'resource',
-                'depends': [conn_id],
-                'impact': [comp_id],
-                'measurements': [],
-                'infos': {}
-            })
-        context_graph_manager.add_comp({
-            '_id': comp_id,
-            'name': comp_id,
-            'type': 'component',
-            'depends': depends,
-            'impact': [],
-            'measurements': [],
-            'infos': {}
-        })
-    if re_id is not None:
-        if not context_graph_manager.check_re(re_id):
-            context_graph_manager.add_re({
-                    '_id': re_id,
-                    'name': event['resource'],
-                    'type': 'resource',
-                    'depends': [conn_id],
-                    'impact': [comp_id],
-                    'measurements': [],
-                    'infos': {}
-            })
-            context_graph_manager.manage_comp_to_re_link(re_id, comp_id)
-    if not context_graph_manager.check_conn(conn_id):
+            re = create_entity(re_id,
+                               event['resource'],
+                               'resource',
+                               [conn_id],
+                               [comp_id],
+                               [],
+                               {})  # FIXME handles info if needed
+            context[re_id] = re
+        # FIXME handles info if needed
+        comp = create_entity(
+            comp_id, comp_id, 'component', depends, [], [], {})
+        context[comp_id] = comp
+
+    # If comp did not exist, a resource is created above
+    if re_id is not None and comp_there is True:
+        if not re_there:
+            re = create_entity(re_id,
+                               event['resource'],
+                               'resource',
+                               [conn_id],
+                               [comp_id],
+                               [],
+                               {})
+
+        # update link between re_id and comp_id if needed
+        update_depends_link(re_id, comp_id, context)
+
+    if not conn_there:
         impact = [comp_id]
         if re_id is not None:
             impact.append(re_id)
-        context_graph_manager.add_conn({
-            '_id': conn_id,
-            'name': event['connector_name'],
-            'type': 'connector',
-            'depends': [],
-            'impact': impact,
-            'measurements': [],
-            'infos': {}
-        })
-        context_graph_manager.manage_re_to_conn_link(conn_id, re_id)
-        context_graph_manager.manage_comp_to_conn_link(conn_id, comp_id)
-    context_graph_manager._check_conn_comp_link(conn_id, comp_id)
+        conn = create_entity(conn_id,
+                             event['connector_name'],
+                             'connector',
+                             [],
+                             impact,
+                             [],
+                             {})
+
+        context[conn_id] = conn
+
+        # update link from re to conn
+        update_depends_link(re_id, conn_id, context)
+
+        # update link from conn to comp
+        update_depends_link(comp_id, conn_id, context)
+
+    update_link(comp_id, conn_id)
+
     if re_id is not None:
-        context_graph_manager._check_conn_re_link(conn_id, re_id)
+        update_link(re_id, conn_id)
+
+    context_graph_manager.put_elements(context.values)
