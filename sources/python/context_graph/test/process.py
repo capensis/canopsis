acc65d0d8330612f62a4394ab572e69452ecf92a
tgosselin <contact@dowan.beer>
2017-02-24 15:50:29 +0100

    test logger

--- sources/python/context_graph/test/process.py
+++ sources/python/context_graph/test/process.py
@@ -1,7 +1,14 @@
 from unittest import main, TestCase
-import canopsis.context_graph.process
+from canopsis.context_graph.process import *
+
+class Logger(object):
+    def debug(self, log):
+        print(log)
 
 class Test(TestCase):
+
+    def setUp(self):
+        LOGGER = Logger()
 
     def test_update_case_6(self):
         entities_t1 = [{'_id': 'comp_1','type': 'component', 'impact': [], 'depends': []}, {'_id': 're_1','type':
@@ -9,8 +16,8 @@ class Test(TestCase):
                                                                                             'depends':[]}] 
         entities_t2 = [{'_id': 'conn_1','type': 'connector'}, {'_id': 'comp_1','type': 'component'}, {'_id': 're_1','type': 'resource'}]
         ids = {'re_id': 're_1', 'comp_id': 'comp_1', 'conn_id': 'conn_1'}
-        self.assertEquals(process.update_case_6(entities_t2, ids), 1)
-        self.assertEquals(process.update_case_6(entities_t2, ids), 0)
+        self.assertEquals(update_case6(entities_t2, ids), 1)
+        self.assertEquals(update_case6(entities_t2, ids), 0)
 
 
 if __name__ == '__main__':
