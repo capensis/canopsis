dbf9786713cf59ac89b11bbc160109a0cb5dfc1d
Arthur Dewarumez <adewarumez@capensis.fr>
2017-04-10 10:19:39 +0200

    Remove method to check the schema of the json and update action methods prototype.
    Plop

--- sources/python/context_graph/canopsis/context_graph/import_ctx.py
+++ sources/python/context_graph/canopsis/context_graph/import_ctx.py
@@ -24,22 +24,6 @@ class ContextGraphImport(ContextGraph):
     DISABLE = "disable"
     ENABLE = "enable"
 
-    JSON_FIELDS = [CIS, LINKS]
-
-    __REQUIRED = 0
-    __OPTIONAL = 1
-
-    CIS_DICT = {ID: (__REQUIRED, str),
-                NAME: (__REQUIRED, str),
-                TYPE: (__REQUIRED, str),
-                EXTRAS: (__REQUIRED, dict),
-                ACTION: (__REQUIRED, str)}
-
-    LINKS_DICT = {FROM: (__REQUIRED, str),
-                  TO: (__REQUIRED, str),
-                  EXTRAS: (__REQUIRED, dict),
-                  ACTION: (__REQUIRED, str)}
-
     def __get_entities_to_update(self, json):
         # a set so no duplicate ids without effort and low time complexity
         ids = set()
@@ -59,67 +43,36 @@ class ContextGraphImport(ContextGraph):
 
         return ctx
 
-    def a_delete_entity(self, ci, entities_to_update, update):
+    def __a_delete_entity(self, ci):
         pass
 
-    def a_update_entity(self, ci, entities_to_update, update):
+    def __a_update_entity(self, ci):
         pass
 
-    def a_create_entity(self, ci, entities_to_update, update):
+    def __a_create_entity(self, ci):
         pass
 
-    def a_disable_entity(self, ci, entities_to_update, update):
+    def __a_disable_entity(self, ci):
         pass
 
-    def a_enable_entity(self, ci, entities_to_update, update):
+    def __a_enable_entity(self, ci):
         pass
 
-    def a_delete_link(self, link, entities_to_update, update):
+    def __a_delete_link(self, link):
         pass
 
-    def a_update_link(self, link, entities_to_update, update):
+    def __a_update_link(self, link):
         pass
 
-    def a_create_link(self, link, entities_to_update, update):
+    def __a_create_link(self, link):
         pass
 
-    def a_disable_link(self, link, entities_to_update, update):
+    def __a_disable_link(self, link):
         pass
 
-    def a_enable_link(self, link, entities_to_update, update):
+    def __a_enable_link(self, link):
         pass
 
-    def _check_schema(self, json):
-        # TODO add better description in the exception.
-        # TODO add checks for list of expected values.
-        # TODO add check for optional/required fields
-
-        def check_sub_elt(schema, dict_):
-            if len(schema.keys()) > len(dict_.keys()):
-                raise ValueError(
-                    "Some key/keys are missing in a sub element of the json.")
-            if len(schema.keys()) > len(dict_.keys()):
-                raise ValueError(
-                    "Too many key/lkeys in a sub element of the json.")
-
-            for key in schema.keys():
-                if not schema[key][1] == type(dict_[key][1]):
-                    raise ValueError(
-                        "A value did not match the expected value.")
-
-        if not self.JSON_FIELDS == json.keys():
-            if len(self.JSON_FIELDS) > len(json.keys()):
-                raise ValueError("Some keys are missing in the json.")
-            if len(self.JSON_FIELDS) < len(json.keys()):
-                raise ValueError("Too many keys are present in the json.")
-
-        # check CIS fields
-        for ci in json[self.CIS]:
-            check_sub_elt(self.CIS_DICT, ci)
-
-        # check CIS fields
-        for link in json[self.LINKS]:
-            check_sub_elt(self.LINK_DICT, link)
 
     def import_context(self, json):
         if (not isinstance(json, dict) or isinstance(json, str)):
@@ -130,38 +83,38 @@ class ContextGraphImport(ContextGraph):
 
         self._check_schema(json)
 
-        entities_to_update = self.__get_entities_to_update(json)
+        self.entities_to_update = self.__get_entities_to_update(json)
 
-        update = {}
+        self.update = {}
 
         for ci in json[self.CIS]:
             if ci[self.ACTION] == self.DELETE:
-                self.a_delete_entity(ci, entities_to_update, update)
+                self.__a_delete_entity(ci)
             if ci[self.ACTION] == self.CREATE:
-                self.a_delete_entity(ci, entities_to_update, update)
+                self.__a_delete_entity(ci)
             elif ci[self.ACTION] == self.UPDATE:
-                self.a_delete_entity(ci, entities_to_update, update)
+                self.__a_delete_entity(ci)
             elif ci[self.ACTION] == self.DISABLE:
-                self.a_delete_entity(ci, entities_to_update, update)
+                self.__a_delete_entity(ci)
             elif ci[self.ACTION] == self.ENABLE:
-                self.a_delete_entity(ci, entities_to_update, update)
+                self.__a_delete_entity(ci)
             else:
                 raise ValueError("The action {0} is not recognized\n".format(
                     ci[self.ACTION]))
 
         for link in json[self.LINKS]:
             if link[self.ACTION] == self.DELETE:
-                self.a_delete_link(link, entities_to_update, update)
+                self.__a_delete_link(link)
             if link[self.ACTION] == self.CREATE:
-                self.a_delete_link(link, entities_to_update, update)
+                self.__a_delete_link(link)
             elif link[self.ACTION] == self.UPDATE:
-                self.a_delete_link(link, entities_to_update, update)
+                self.__a_delete_link(link)
             elif link[self.ACTION] == self.DISABLE:
-                self.a_delete_link(link, entities_to_update, update)
+                self.__a_delete_link(link)
             elif link[self.ACTION] == self.ENABLE:
-                self.a_delete_link(link, entities_to_update, update)
+                self.__a_delete_link(link)
             else:
                 raise ValueError("The action {0} is not recognized\n".format(
                     link[self.ACTION]))
 
-        self.put_entities(update.values())
+        self.put_entities(self.update.values())
