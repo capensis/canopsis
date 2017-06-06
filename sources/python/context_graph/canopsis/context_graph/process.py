241f768459d4c909c0f4a641528387ead94fa28c
Arthur Dewarumez <adewarumez@capensis.fr>
2017-06-06 14:59:51 +0200

    Remove useless logger.
    Remove useless logger.

--- sources/python/context_graph/canopsis/context_graph/process.py
+++ sources/python/context_graph/canopsis/context_graph/process.py
@@ -70,9 +70,6 @@ def create_entity(id,
 
     context_graph_manager._enable_entity(ent)
 
-    import pprint
-    LOGGER.critical("CREATE ENT : {0}\n".format(pprint.pformat(ent)))
-
     return ent
 
 
@@ -493,15 +490,8 @@ def update_context(presence, ids, in_db, event):
     for key in extra_infos:
         evt_entity['infos'][key] = extra_infos[key]
 
-    import pprint
-    for ent in to_update:
-        LOGGER.critical("UPDATE CTX BFR : {0}\n".format(pprint.pformat(ent)))
-
     for key in event_info:
         evt_entity["infos"][key] = event_info[key]
-
-    for ent in to_update:
-        LOGGER.critical("UPDATE CTX AFR : {0}\n".format(pprint.pformat(ent)))
 
     context_graph_manager.keys_info_filter(evt_entity["infos"])
     context_graph_manager._put_entities(to_update)
