6c1bb3dd318ea960666649c7b95ff75f98057fb6
Arthur Dewarumez <adewarumez@capensis.fr>
2017-06-15 10:47:56 +0200

    Add several constante, corrects several BaseTest.methods.

--- sources/python/pbehavior/test/tf_pbehavior.py
+++ sources/python/pbehavior/test/tf_pbehavior.py
@@ -19,25 +19,31 @@
 # along with Canopsis.  If not, see <http://www.gnu.org/licenses/>.
 # ---------------------------------
 
+import dateutil
 import unittest
 import re
 import json
 import requests
 from ast import literal_eval
-from time import sleep
+from time import sleep, time
 from urllib import urlencode
 from pymongo import MongoClient
+from canopsis.pbhevior.manager import PBehaviorManager
 
-# TODO specify the entity use for the test and add it in the PBEHAVIOR
-# and FILTER
+PBH_RESOURCE = "pbh_resource"
+PBH_CONNECTOR = "pbh_connector",
+PBH_CON_NAME = PBH_CONNECTOR + "_name"
+PBH_COMPONENT= "pbh_component",
+
+RESOURCE_ID = "{0}/{1}".format(PBH_RESOURCE, PBH_COMPONENT)
 
 WEB_HOST = "localhost"
 AUTH_KEY = "b71786a6-4c4c-11e7-8da1-0800279471b5"
 MONGO_HOST = "localhost"
 
 URL_BASE = "http://{0}:8082".format(WEB_HOST)
-URL_AUTH = "{0}/?authkey={1}".format(URL_BASE, AUTH_KEY)
-URL_PBEH = "{0}/pbehavior/create?{1}".format(URL_BASE)
+URL_AUTH = "{0}/?authkey={1}"
+URL_PBEH = "{0}/pbehavior/create?{1}".format(URL_BASE, None)
 URL_MONGO = 'mongodb://cpsmongo:canopsis@{0}:27017/canopsis'.format(MONGO_HOST)
 
 ENTITIES_COL = "default_entities"
@@ -57,7 +63,8 @@ FILTER = {
     "priority": 1,
     "run_once": True,
     "crecord_type": "filter",
-    "mfilter": "{\"$or\":[{\"connector\":{\"$eq\":\"i_am_a_connector\"}}]}",
+    "mfilter": "{\"$or\":[{\"connector\":{\"$eq\":\"{0}\"}}]}".format(
+        PBH_CONNECTOR),
     "crecord_creation_time": 1497367809,
     "crecord_name": None,
     "description": "I am a nice filter"
@@ -78,31 +85,41 @@ DEL_FILTER = {
 }
 
 PBEHAVIOR = {
-    "filter": None,
+    "name": "A name",
+    "filter": {"_id": RESOURCE_ID},
     "author": "Functionnal_test",
     "tstart": None,
     "tstop": None,
     "rrule": None,
-    "enabled": "true",
-    "connector": None,
-    "connector_name": None
+    "enabled": True,
+    "connector": PBH_CONNECTOR,
+    "connector_name": PBH_CON_NAME
 }
 
-BEAT = 60
+BEAT = 2
 
 
 class BaseTest(unittest.TestCase):
-    def _insert_filter(self):
-        self.obj_col.insert(FILTER)
+
+    def _insert_filter(self, **kwargs):
+        filter_ = FILTER.copy()
+
+        for key in kwargs:
+            filter_[key] = kwargs[key]
+
+        self.obj_col.insert(filter_)
         print("Waiting {0}s for the beat".format(BEAT))
         sleep(BEAT)
 
     def _remove_filter(self):
-        self.obj_col.delete_one()
+        # self.obj_col.delete_many(DEL_FILTER)
+        pass
 
     def _authenticate(self):
         session = requests.Session()
-        response = session.get(URL_AUTH)
+        response = session.get(URL_AUTH.format(URL_BASE, AUTH_KEY))
+        print("Attempting login on {0}".format(
+            URL_AUTH.format(URL_BASE, AUTH_KEY)))
 
         if re.search("<title>Canopsis | Login</title>", response.text)\
            is not None:
@@ -110,7 +127,29 @@ class BaseTest(unittest.TestCase):
 
         return session
 
-    def _create_pbehavior(self, pbehavior):
+    def _create_pbehavior(self, in_=True, **kwargs):
+        pb = PBEHAVIOR.copy()
+        middle = time()
+        pb["tstart"] = middle - 5 * 60
+        pb["tstop"] = middle + 5 * 60
+
+        if in_ is False:
+            pb["tstart"] += 3600
+            pb["tstop"] += 3600
+        elif in_ is not True:
+            self.fail("Nope")
+
+        key_to_add = PBEHAVIOR.keys()
+        key_to_add.remove("tstart")
+        key_to_add.remove("tstop")
+
+        for key in key_to_add:
+            if key in kwargs:
+                pb[key] = kwargs[key]
+
+        return pb
+
+    def _push_pbehavior(self, pbehavior):
         response = self.session.post(URL_PBEH.format(urlencode(pbehavior)))
 
         response = literal_eval(response.text)
@@ -120,21 +159,24 @@ class BaseTest(unittest.TestCase):
         else:
             self.fail("Impossible to insert the pbehavior.")
 
+    def _create_rrule(self):
+        pass
+
     def setUp(self):
         client = MongoClient(URL_MONGO)
         self.ent_col = client.canopsis[ENTITIES_COL]
         self.obj_col = client.canopsis[OBJECT_COL]
 
-        self._insert_filter()
         self.session = self._authenticate()
 
         self.pbehavior_ids = []
+
+        self.pbm = PBehaviorManager()
 
     def tearDown(self):
         self._remove_filter()
         # TODO remove entity created in the context
 
 
-class InFullOutFull(BaseTest):
-
-    pass
+if __name__ == "__main__":
+    unittest.main()
