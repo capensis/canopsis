dd4bc4a82401d4c3a06946c0c1dc3873e78038f9
Arthur Dewarumez <adewarumez@capensis.fr>
2017-06-16 16:31:05 +0200

    PEP8 Add 404 on route /weather/selectors/<selector_id> if selector_id does not mach a selector. Convert the list of json of weatherselectors on string in order to be send.

--- sources/python/webcore/canopsis/webcore/services/weather.py
+++ sources/python/webcore/canopsis/webcore/services/weather.py
@@ -20,75 +20,82 @@
 
 from __future__ import unicode_literals
 
-from canopsis.common.ws import RouteLeRetour as route
 from canopsis.context_graph.manager import ContextGraph
 from canopsis.alerts.reader import AlertsReader
 
 context_manager = ContextGraph()
 alarm_manager = AlertsReader()
 
-from bottle import abort
+from bottle import abort, response
+import json
+
 
 def exports(ws):
-
     @ws.application.route('weather/selectors/<selector_filter>')
-    def get_selector(
-            selector_filter
-    ):
+    def get_selector(selector_filter):
+        """
+        Return the list of the selectors that match the filter
+        Every selector will be return the corresponding alarm, sla, pbehavior
+        and linklist.
+        :param dict selector_filter: a mongo filter used to select selector
+        :return list: the selectors that match selector_filter
+        """
         selector_filter['type'] = 'selector'
         selector_list = context_manager.get_entities(query=selector_filter)
-        
+
         ret_val = []
         for i in selector_list:
             tmp = {}
-            tmp_alarm = alarm_manager.get(filter_={'d' : i['_id']})['alarms']
+            tmp_alarm = alarm_manager.get(filter_={'d': i['_id']})['alarms']
 
             tmp['entity_id'] = i['_id']
-            tmp['criticity'] = i['infos'].get('criticity','')
+            tmp['criticity'] = i['infos'].get('criticity', '')
             tmp['org'] = i['infos'].get('org', '')
-            tmp['sla_text'] = '' # when sla
+            tmp['sla_text'] = ''  # when sla
             tmp['display_name'] = i['name']
             if tmp_alarm != []:
                 tmp['state'] = tmp_alarm[0]['v']['state']
                 tmp['status'] = tmp_alarm[0]['v']['status']
                 tmp['snooze'] = tmp_alarm[0]['v']['snooze']
                 tmp['ack'] = tmp_alarm[0]['v']['ack']
-            tmp['pbehavior'] = [] # add this when it's ready
-            tmp['linklist'] = [] # add this when it's ready
+            tmp['pbehavior'] = []  # add this when it's ready
+            tmp['linklist'] = []  # add this when it's ready
             ret_val.append(tmp)
         return ret_val
 
     @ws.application.route("/weather/selectors/<selector_id>")
-    def weatherselectors(
-        selector_id
-    ):
+    def weatherselectors(selector_id):
         """
             get selector and entities for the second part.
         """
-        selector_entity = context_manager.get_entities(
-            query={'_id': selector_id}
-        )[0]
+        try:
+            selector_entity = context_manager.get_entities(
+                query={'_id': selector_id})[0]
+        except IndexError:
+            abort(404, "No selfuch selector")
+
         entities = context_manager.get_entities(
-            query=json.loads(selector_entity['infos']['mfilter'])
-        )
+            query=json.loads(selector_entity['infos']['mfilter']))
+
         ret_val = []
         for i in entities:
             tmp_val = {}
-            tmp_alarm = alarm_manager.get(filter_={'d':i['_id']})['alarms']
-            
+            tmp_alarm = alarm_manager.get(filter_={'d': i['_id']})['alarms']
+
             tmp_val['entity_id'] = i['_id']
-            tmp_val['sla_text'] = '' # when sla
+            tmp_val['sla_text'] = ''  # TODO when sla, use it
             tmp_val['org'] = i['infos'].get('org', '')
-            tmp_val['display_name'] = selector_entity['display_name'] # check if we need selector here
+            tmp_val['display_name'] = selector_entity[
+                'name']  # check if we need selector here
             tmp_val['name'] = i['name']
             if tmp_alarm != []:
                 tmp_val['state'] = tmp_alarm[0]['v']['state']
                 tmp_val['status'] = tmp_alarm[0]['v']['status']
                 tmp_val['snooze'] = tmp_alarm[0]['v']['snooze']
                 tmp_val['ack'] = tmp_alarm[0]['v']['ack']
-            tmp_val['pbehavior'] = [] # wait for pbehavior
-            tmp_val['linklist'] = [] # wait for linklist
-        return ret_val
+            tmp_val['pbehavior'] = []  # TODO wait for pbehavior
+            tmp_val['linklist'] = []  # TODO wait for linklist
+            ret_val.append(tmp_val)
 
-
-        
+        response.content_type = "application/json"
+        return json.dumps(ret_val)
