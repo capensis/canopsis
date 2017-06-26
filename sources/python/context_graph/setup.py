27ab5baac46cb40627e6230473ff5a68aa97f1d9
Arthur Dewarumez <adewarumez@capensis.fr>
2017-06-26 15:56:48 +0200

    Correct typo in the setup.py file.

--- sources/python/context_graph/setup.py
+++ sources/python/context_graph/setup.py
@@ -5,5 +5,5 @@ install_requires = ['canopsis.common']
 setup(
     description='A context as a graph',
     install_requires=install_requires,
-    keyword="context_graph"
+    keywords="context_graph"
 )
