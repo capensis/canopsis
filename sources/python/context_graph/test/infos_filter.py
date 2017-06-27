d609d40c0bcbec6beff88bc27e6600353222871d
Arthur Dewarumez <adewarumez@capensis.fr>
2017-06-27 11:44:08 +0200

    Add BaseTest

---
+++ sources/python/context_graph/test/infos_filter.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env/python
+# -*- coding: utf-8 -*-
+
+from unittest import main, TestCase
+from canopsis.context_graph.manager import InfosFilter
+from enum import Enum
+
+class Keys(Enum):
+
+    K_ID = "_id"
+    K_TYPE = "type"
+    K_REQUIRED = "required"
+    K_PROPERTIES = "properties"
+    K_ITEMS = "items"
+
+    T_OBJECT = "object"
+    T_BOOL = "boolean"
+    T_ARRAY = "array"
+    T_NUMBER = "number"
+    T_STRING = "string"
+
+SCHEMA = {
+    Keys.K_ID.value: "schema_infos",
+    Keys.K_TYPE.value: Keys.T_OBJECT.value,
+    Keys.K_REQUIRED.value: ["enabled", "enabled_history", "disabled_history"],
+    Keys.K_PROPERTIES.value: {
+        "enabled": {
+            Keys.K_TYPE.value: Keys.T_BOOL.value
+        },
+        "enabled_history": {
+            Keys.K_TYPE.value: Keys.T_ARRAY.value,
+            Keys.K_ITEMS.value: {
+                Keys.K_TYPE.value: Keys.T_NUMBER.value
+            }
+        },
+        "disabled_history": {
+            Keys.K_TYPE.value: Keys.T_ARRAY.value,
+            Keys.K_ITEMS.value: {
+                Keys.K_TYPE.value: Keys.T_NUMBER.value
+            }
+        }
+    }
+}
+
+
+
+class MockLogger:
+
+    def __init__(self, test_instance):
+        self.test_instance = test_instance
+
+    def warning(self, _):
+        self.test_instance.asertTrue(True)
+
+class BaseTest(TestCase):
+
+    def setUp(self):
+        self.infosfilter = InfosFilter(MockLogger(self))
+        self.infosfilter._schema = SCHEMA
+
+class TestReloadSchema(BaseTest):
+    pass
+
+
+if __name__ == '__main__':
+    main()
