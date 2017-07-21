be4c599a016d569524a9c6c9325965940e3670df
tgosselin <contact@dowan.beer>
2017-07-21 15:26:53 +0200

    test return links

--- sources/python/webcore/canopsis/webcore/services/weather.py
+++ sources/python/webcore/canopsis/webcore/services/weather.py
@@ -404,7 +404,7 @@ def exports(ws):
             tmp_alarm = alarmreader_manager.get(filter_={'d': entity['_id']})
             tmp_alarm = tmp_alarm['alarms']
             tmp_linklist = []
-            for k, v in entity['links'].items:
+            for k, v in entity['links'].items():
                 tmp_linklist.append({'cat_name': k, 'links': v})
 
             enriched_entity['entity_id'] = entity['_id']
