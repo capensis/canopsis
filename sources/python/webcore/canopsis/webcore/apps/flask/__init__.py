6b4136ad8456da77a828d08324e600307089fa9d
Florent Peterschmitt <fpeterschmitt@capensis.fr>
2017-08-03 14:45:35 +0200

    doc manual install

--- sources/python/webcore/canopsis/webcore/apps/flask/__init__.py
+++ sources/python/webcore/canopsis/webcore/apps/flask/__init__.py
@@ -1,2 +1,10 @@
 from pkgutil import extend_path
 __path__ = extend_path(__path__, __name__)
+
+"""
+FIXIT: flask dependencies not yes integrated. Manual installation required:
+
+su - canopsis
+easy_install pip
+pip install flask flask_restful
+"""
