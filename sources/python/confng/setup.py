5814770196cc805d51b86754f61ff2fbe846896f
Florent Peterschmitt <fpeterschmitt@capensis.fr>
2017-08-04 11:28:26 +0200

    add confng

---
+++ sources/python/confng/setup.py
@@ -0,0 +1,8 @@
+from canopsis.common.setup import setup
+
+install_requires = ['canopsis.common', 'leryan.types==0.0.17']
+
+setup(
+    description='Canopsis NG Configuration',
+    install_requires=install_requires,
+    keywords='configuration')
