d89b85153351902561ba8214dc37aed84413d1f8
Florent Peterschmitt <florent@peterschmitt.fr>
2017-08-05 15:41:59 +0200

    fix tests, test file logger

--- sources/python/logger/test/test_logger.py
+++ sources/python/logger/test/test_logger.py
@@ -1,4 +1,6 @@
 import unittest
+import tempfile
+import os
 
 try:
     from io import StringIO
@@ -9,10 +11,10 @@ from canopsis.logger import Logger, OutputFile, OutputStream
 
 class TestLogger(unittest.TestCase):
 
-    def test_logger(self):
+    def test_logger_stream(self):
 
         output = StringIO()
-        logger = Logger.get('single', output, OutputStream)
+        logger = Logger.get('stream', output, OutputStream)
 
         logger.info(u'first_line')
         logger.info(u'second_line')
@@ -38,3 +40,19 @@ class TestLogger(unittest.TestCase):
         log_lines = output.readlines()
 
         self.assertEqual(len(log_lines), 4)
+
+    def test_logger_file(self):
+
+        tmpf = tempfile.NamedTemporaryFile(delete=False)
+        tmpf.close()
+
+        logger = Logger.get('file', tmpf.name, OutputFile)
+
+        logger.info(u'first_line')
+
+        with open(tmpf.name, 'r') as fh:
+            content = fh.readlines()
+            self.assertEqual(len(content), 1)
+            self.assertTrue(content[0][:-1].endswith(u'first_line'))
+
+        os.unlink(tmpf.name)
