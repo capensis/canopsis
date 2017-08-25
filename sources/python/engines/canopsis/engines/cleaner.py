f85156f63dd6f05cf4d77f9d83e215c038fa7fb3
Romain Hennuyer <romainhk@gmail.com>
2017-08-25 14:44:16 +0200

    message more precise

--- sources/python/engines/canopsis/engines/cleaner.py
+++ sources/python/engines/canopsis/engines/cleaner.py
@@ -99,7 +99,8 @@ class engine(Engine):
         try:
             event['state'] = int(event.get('state', 0))
         except:
-            self.logger.error('state convertion failed ({})'.format(event))
+            self.logger.error('state convertion failed: must be an int ({})'
+                              .format(event))
             event['state'] = 0
         event['state_type'] = event.get('state_type', 1)
         event['event_type'] = event.get('event_type', 'check')
