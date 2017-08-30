b4cfd4df68d9d5a7599f9c94ef05ec3854958d2e
Florent Peterschmitt <fpeterschmitt@capensis.fr>
2017-08-30 17:14:21 +0200

    doc(webcore:pb): fix docstring

--- sources/python/pbehavior/canopsis/pbehavior/utils.py
+++ sources/python/pbehavior/canopsis/pbehavior/utils.py
@@ -12,8 +12,8 @@ def check_valid_rrule(rrule):
     Raises error when invalid with a more explicit message.
     :param str rrule: rrule as string
     :raises ValueError: rrule is invalid
-    :returns: True|False, (None|'reason')|Exception
-    :rtype bool: True if ok, raises exception if invalid
+    :returns: True if ok, raises exception if invalid
+    :rtype bool:
     """
     if rrule == '' or rrule == None:
         return True
