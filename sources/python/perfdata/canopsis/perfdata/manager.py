948a7649e4c9231b6842650dba9389a66943c0ce
Arthur Dewarumez <adewarumez@capensis.fr>
2017-08-31 10:26:33 +0200

    Refactor:
    Make the condition more explicit.

--- sources/python/perfdata/canopsis/perfdata/manager.py
+++ sources/python/perfdata/canopsis/perfdata/manager.py
@@ -205,7 +205,7 @@ class PerfData(MiddlewareRegistry):
         """
 
         # do something only if there are points to put
-        if points:
+        if len(points) > 0:
             first_point = points[0]
             # if first_point is a timestamp, points is one point
             if isinstance(first_point, (Number, basestring)):
