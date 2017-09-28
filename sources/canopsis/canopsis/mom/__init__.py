db10a4ae73da8ab2a6b4acaa1a5bd324e64e8cfb
Florent Peterschmitt <fpeterschmitt@capensis.fr>
2017-09-28 11:39:50 +0200

    fix(monopackage): move mom

--- sources/canopsis/canopsis/mom/__init__.py
+++ sources/canopsis/canopsis/mom/__init__.py
@@ -18,8 +18,6 @@
 # along with Canopsis.  If not, see <http://www.gnu.org/licenses/>.
 # ---------------------------------
 
-__version__ = "0.1"
-
 
 from canopsis.middleware.core import Middleware
 from canopsis.configuration.model import Category, Parameter
