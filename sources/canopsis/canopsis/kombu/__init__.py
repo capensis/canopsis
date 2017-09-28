3de0fed6a451d6be7899b091ceee28a9d9ec7b04
Florent Peterschmitt <fpeterschmitt@capensis.fr>
2017-09-28 11:38:50 +0200

    fix(monopackage): move migration

--- sources/canopsis/canopsis/kombu/__init__.py
+++ sources/canopsis/canopsis/kombu/__init__.py
@@ -18,8 +18,6 @@
 # along with Canopsis.  If not, see <http://www.gnu.org/licenses/>.
 # ---------------------------------
 
-__version__ = "0.1"
-
 from kombu import \
     Connection, Exchange, Queue, Producer, Consumer
 
