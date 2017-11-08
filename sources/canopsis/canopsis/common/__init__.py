0c3db39f65c7ba4914a126cff5a6c8d23ca18784
Florent Peterschmitt <florent@peterschmitt.fr>
2017-11-08 20:28:16 +0100

    feat: pyinstaller spec file

--- sources/canopsis/canopsis/common/__init__.py
+++ sources/canopsis/canopsis/common/__init__.py
@@ -27,18 +27,23 @@ class CanopsisUnsupportedEnvironment(Exception):
 
 
 def _root_path():
-    root = None
+    root = os.environ.get('CPS_ROOT_PATH', '')
 
     if os.path.isdir(os.path.join(sys.prefix, 'etc')):
         root = sys.prefix
+
+    elif os.path.isdir(os.path.join(root, 'etc')):
+        pass
 
     elif os.path.isdir(os.path.join('opt', 'canopsis', 'etc')):
         root = os.path.join(os.path.sep, 'opt', 'canopsis')
 
     else:
-        msg = 'unsupported environment: cannot safely detect canopsis root path.'
+        msg = 'unsupported environment: '
+        'cannot safely detect canopsis root path.'
         raise CanopsisUnsupportedEnvironment(msg)
 
     return root
+
 
 root_path = _root_path()
