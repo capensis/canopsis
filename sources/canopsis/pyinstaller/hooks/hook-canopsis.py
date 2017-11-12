d674597be58657ca03ddff5e2ebbc9aaacae1b65
Florent Peterschmitt <florent@peterschmitt.fr>
2017-11-12 14:21:59 +0100

    fix: hiddenimports

--- sources/canopsis/pyinstaller/hooks/hook-canopsis.py
+++ sources/canopsis/pyinstaller/hooks/hook-canopsis.py
@@ -2,6 +2,7 @@ import os
 
 from PyInstaller.utils.hooks import collect_data_files
 
+
 def _find_canopsis_pyfiles(srcdir):
     canopsis_files = []
     for dirpath, subdirs, files in os.walk(srcdir):
@@ -9,6 +10,7 @@ def _find_canopsis_pyfiles(srcdir):
             canopsis_files.append(os.path.join(dirpath, file_))
 
     return canopsis_files
+
 
 def _find_canopsis_imports(srcdir):
     pyfiles = _find_canopsis_pyfiles(srcdir)
@@ -26,20 +28,22 @@ def _find_canopsis_imports(srcdir):
 
     return imports
 
+
 def get_static_hidden_imports():
     imports = [
-        "gunicorn.workers.ggevent",
-        "gunicorn.glogging",
-        "kombu.transport.pyamqp",
-        "validictory"
+        'gunicorn.workers.ggevent',
+        'gunicorn.glogging',
+        'kombu.transport.pyamqp',
+        'validictory'
     ]
 
     return imports
 
+
 def get_additional_data():
     return collect_data_files('jsonschema')
 
-def hook(hook_api):
-    hook_api.add_datas(_get_additional_data())
-    hook_api.add_imports(*_get_static_hidden_imports())
-    hook_api.add_imports(*_find_canopsis_imports('canopsis'))
+
+#datas = get_additional_data()
+#hiddenimports = get_static_hidden_imports()
+hiddenimports = _find_canopsis_imports('canopsis')
