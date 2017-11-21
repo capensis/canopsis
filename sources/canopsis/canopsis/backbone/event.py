af48dcae8470e6ebf05cec98825e2a0db473f91f
Romain Hennuyer <romainhk@gmail.com>
2017-11-21 14:24:58 +0100

    test(backbone): TU for Event class

--- sources/canopsis/canopsis/backbone/event.py
+++ sources/canopsis/canopsis/backbone/event.py
@@ -1,8 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Event standardization.
+"""
+
 from __future__ import unicode_literals
 
-from json import loads
+from json import dumps
 from six import string_types
 from time import time
+
+DEFAULT_EVENT_TYPE = 'check'
+DEFAULT_OUTPUT = ''
+DEFAULT_STATE = 0
+DEFAULT_STATE_TYPE = 1
 
 
 class EventUTF8Error(Exception):
@@ -11,12 +23,19 @@ class EventUTF8Error(Exception):
 
 class Event(object):
 
+    """
+    Event representation.
+    """
+
     _present_keys = {
         'connector',
         'connector_name',
         'component',
         'source_type',
-        'event_type'
+        'event_type',
+        'state',
+        'state_type',
+        'output'
     }
 
     def __init__(
@@ -25,7 +44,10 @@ class Event(object):
             connector_name,
             component,
             source_type,
-            event_type,
+            event_type=DEFAULT_EVENT_TYPE,
+            state=DEFAULT_STATE,
+            state_type=DEFAULT_STATE_TYPE,
+            output=DEFAULT_OUTPUT,
             **kwargs
     ):
         self.connector = connector
@@ -33,29 +55,29 @@ class Event(object):
         self.component = component
         self.source_type = source_type
         self.event_type = event_type
+        self.state = state
+        self.state_type = state_type
+        self.output = output
 
         if 'timestamp' not in kwargs:
             kwargs['timestamp'] = int(time())
-
-        if 'state' not in kwargs:
-            kwargs['state'] = 0
-
-        if 'state_type' not in kwargs:
-            kwargs['state_type'] = 1
-
-        if 'event_type' not in kwargs:
-            kwargs['event_type'] = 'check'
-
-        if 'output' not in kwargs:
-            kwargs['output'] = ''
 
         for key, val in kwargs.items():
             self._present_keys.add(key)
             setattr(self, key, val)
 
     def is_valid(self):
-        if self.event_type == 'resource' and not hasattr(self, 'resource'):
+        """
+        Verify that the event is valid.
+
+        :rtype: bool
+        """
+        if self.source_type not in ['component', 'resource']:
             return False
+
+        if self.source_type == 'resource' and not hasattr(self, 'resource'):
+            return False
+
         if (self.event_type == 'perf'
                 and ('perfdata' not in self._present_keys
                      or 'perf_data_array' not in self._present_keys)):
@@ -64,6 +86,11 @@ class Event(object):
         return True
 
     def ensure_utf8_format(self):
+        """
+        Force string attributes to be encoded in UTF-8.
+
+        :raises: EventUTF8Error
+        """
         for key in self._present_keys:
             attr = getattr(self, key)
             if isinstance(attr, string_types):
@@ -73,8 +100,13 @@ class Event(object):
                     raise EventUTF8Error
 
     def to_json(self):
+        """
+        Convert the Event to a json format.
+
+        :rtype: str
+        """
         try:
-            return loads({k: getattr(self, k) for k in self._present_keys})
+            return dumps({k: getattr(self, k) for k in self._present_keys})
         except ValueError:
             pass
 
---
+++ sources/canopsis/test/backbone/test_event.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# --------------------------------
+# Copyright (c) 2016 "Capensis" [http://www.capensis.com]
+#
+# This file is part of Canopsis.
+#
+# Canopsis is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Canopsis is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with Canopsis.  If not, see <http://www.gnu.org/licenses/>.
+# ---------------------------------
+
+from __future__ import unicode_literals
+
+from json import loads
+import logging
+from unittest import TestCase, main
+
+from canopsis.backbone.event import Event
+
+
+class BackboneEventTest(TestCase):
+
+    def setUp(self):
+        self.logger = logging.getLogger('alarms')
+
+        self.event = Event(
+            connector='cleese',
+            connector_name='chapman',
+            component='idle',
+            resource='palin',
+            source_type='resource',
+            output='How Not to be seen',
+        )
+
+    def test_is_valid(self):
+        self.assertTrue(self.event.is_valid())
+
+        bad_event = Event(
+            connector='cleese',
+            connector_name='chapman',
+            component='idle',
+            source_type='Dinsdale'
+        )
+        self.assertFalse(bad_event.is_valid())
+
+        bad_event = Event(
+            connector='cleese',
+            connector_name='chapman',
+            component='idle',
+            source_type='resource'
+        )
+        self.assertFalse(bad_event.is_valid())
+
+    def test_ensure_utf8_format(self):
+        try:
+            self.event.ensure_utf8_format()
+        except Exception:
+            self.fail('ensure_utf8_format has failed but it shouldn\'t')
+
+    def test_to_json(self):
+        res = self.event.to_json()
+        self.assertIsInstance(res, str)
+
+        json = loads(res)
+        del json['timestamp']
+        self.assertDictEqual(json, {
+            "connector": "cleese",
+            "resource": "palin",
+            "event_type": "check",
+            "component": "idle",
+            "state_type": 1,
+            "source_type": "resource",
+            "state": 0,
+            "connector_name": "chapman",
+            "output": "How Not to be seen"
+        })
+
+if __name__ == '__main__':
+    main()
