d5aa43c5675b454bf372979a7897b0248f847f35
Romain Hennuyer <romainhk@gmail.com>
2017-12-06 16:53:31 +0100

    chore: upgrade runner to permit to choose a specific test module

--- sources/functional_testing/runner.py
+++ sources/functional_testing/runner.py
@@ -21,13 +21,31 @@
 
 from __future__ import unicode_literals
 
+import argparse
 import importlib
 from os import listdir
 from os.path import isfile, join
 import re
+import sys
 import unittest
 
 pyfile = re.compile('[^_].*\.py$')
+
+
+def import_module(suite, name):
+    """
+    Import a module in a suite from his name.
+
+    :param TestSuite suite: suite object
+    :param str name: module name (ex. apis.weather)
+    """
+    try:
+        mod = importlib.import_module(name)
+    except ImportError as exc:
+        print('Cannot import {} ({})'.format(name, exc))
+        return
+
+    suite.addTests(loader.loadTestsFromModule(mod))
 
 
 def add_modules_from_folder(suite, folder):
@@ -41,18 +59,24 @@ def add_modules_from_folder(suite, folder):
 
     for f in files:
         mod_name = 'apis.{}'.format('.'.join(f.split('.')[:-1]))
-        try:
-            mod = importlib.import_module(mod_name)
-        except ImportError as exc:
-            print('Cannot import {} ({})'.format(mod_name, exc))
-            continue
+        import_module(suite, mod_name)
 
-        suite.addTests(loader.loadTestsFromModule(mod))
 
-loader = unittest.TestLoader()
-suite = unittest.TestSuite()
+if __name__ == "__main__":
 
-add_modules_from_folder(suite=suite, folder='apis')
+    parser = argparse.ArgumentParser(prog='functionnal testing runner')
+    parser.add_argument('module_names', nargs='*',
+                        help='Namespace of modules to test (ex. apis.weather)')
+    args = parser.parse_args(sys.argv[1:])
 
-runner = unittest.TextTestRunner(verbosity=3)
-result = runner.run(suite)
+    loader = unittest.TestLoader()
+    suite = unittest.TestSuite()
+
+    if args.module_names == []:
+        add_modules_from_folder(suite=suite, folder='apis')
+    else:
+        for mod_name in args.module_names:
+            import_module(suite, mod_name)
+
+    runner = unittest.TextTestRunner(verbosity=3)
+    result = runner.run(suite)
