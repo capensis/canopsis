d8b98a0e7ddeca4f07a62c45d11c561681e3c5b3
Arthur Dewarumez <adewarumez@capensis.fr>
2018-01-16 11:48:14 +0100

    fix: the script is run as canopsis, no need to use `su - canopsis`.

--- files/entrypoint-engine.sh
+++ files/entrypoint-engine.sh
@@ -3,5 +3,5 @@
 if [ "x$1" == "xshell" ]; then
   exec /bin/bash
 else
-  su - canopsis -c "engine-launcher -e $ENGINE_MODULE -n $ENGINE_NAME -w 1 -l info"
+  /opt/canopsis/bin/engine-launcher -e $ENGINE_MODULE -n $ENGINE_NAME -w 1 -l info
 fi
