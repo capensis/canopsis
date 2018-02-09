ec6c069d08c11b98c263342dc98352f8eac092e2
Florent Peterschmitt <fpeterschmitt@capensis.fr>
2018-02-09 23:49:35 +0100

    build(docker): fix webserver start by setting HOME var

--- docker/files/entrypoint.sh
+++ docker/files/entrypoint.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+export HOME=/opt/canopsis
 
 if [ "x$1" == "xshell" ]; then
   exec /bin/bash
 else
     if [ "${CPS_WEBSERVER}" = "1" ]; then
-        GEVENT_RESOLVER=\"ares\" /opt/canopsis/bin/webserver -t 240 --access-logfile ~/var/log/webserver-access.log -k gevent -w 1 -b 0.0.0.0:8082
+        /opt/canopsis/bin/webserver --access-logfile /opt/canopsis/var/log/webserver-access.log -k gevent -w 1 -b 0.0.0.0:8082
     else
         /opt/canopsis/bin/engine-launcher -e $ENGINE_MODULE -n $ENGINE_NAME -w 1 -l info
     fi
