026564b8efaf661336353d005e68403e7a03b7ce
Florent Peterschmitt <florent@peterschmitt.fr>
2018-02-11 22:32:38 +0100

    fix(docker): reduce image size ~80MB by not chown-ing everything. also more secure

--- build-docker.sh
+++ build-docker.sh
@@ -7,9 +7,10 @@ if [ "${1}" = "" ]; then
     exit 1
 fi
 
+opt_squash=""
 tag="${1}"
 workdir=$(dirname $(readlink -e $0))
 cd $workdir
 
-docker build --build-arg PROXY=$http_proxy --build-arg TAG=${tag} -f docker/Dockerfile.sysbase -t canopsis/canopsis-sysbase:${tag} .
-docker build --build-arg PROXY=$http_proxy --build-arg TAG=${tag} -f docker/Dockerfile -t canopsis/canopsis-core:${tag} .
+docker build ${opt_squash} --build-arg PROXY=$http_proxy --build-arg TAG=${tag} -f docker/Dockerfile.sysbase -t canopsis/canopsis-sysbase:${tag} .
+docker build ${opt_squash} --build-arg PROXY=$http_proxy --build-arg TAG=${tag} -f docker/Dockerfile -t canopsis/canopsis-core:${tag} .
--- docker/build/install.sh
+++ docker/build/install.sh
@@ -2,9 +2,13 @@
 set -e
 set -o pipefail
 
-echo "[easy_install]\nallow_hosts = ''\nfind_links = file:///sources/python-libs/" > ${CPS_HOME}/.pydistutils.cfg
+virtualenv ${CPS_HOME}
+
+echo "[easy_install]\nallow_hosts = ''\nfind_links = file:///sources/python-libs/" > /root/.pydistutils.cfg
 
 source ${CPS_HOME}/bin/activate
 
-pip install --no-index --find-links=file:///sources/python-libs --upgrade setuptools distribute pip
-pip install --no-index --find-links=file:///sources/python-libs /sources/canopsis/
+pip install -b /tmp/pipbuild --no-index --find-links=file:///sources/python-libs --upgrade setuptools distribute pip
+pip install -b /tmp/pipbuild --no-index --find-links=file:///sources/python-libs /sources/canopsis/
+
+rm -rf /tmp/pipbuild
