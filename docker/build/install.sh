4de52a431bab6a1099b7148c24c1dd4dd7c95e1f
Florent Peterschmitt <fpeterschmitt@capensis.fr>
2018-02-14 12:36:48 +0100

    feat(docker): use debian 9 (stretch)

--- docker/build/install.sh
+++ docker/build/install.sh
@@ -2,13 +2,15 @@
 set -e
 set -o pipefail
 
-virtualenv ${CPS_HOME}
+virtualenv --no-download ${CPS_HOME}
 
-echo -e "[easy_install]\nallow_hosts = ''\nfind_links = file:///sources/python-libs/" > /root/.pydistutils.cfg
+find_links="file:///sources/python-libs/"
+
+echo -e "[easy_install]\nallow_hosts = ''\nfind_links = ${find_links}" > /root/.pydistutils.cfg
 
 source ${CPS_HOME}/bin/activate
 
-pip install -b /tmp/pipbuild --no-index --find-links=file:///sources/python-libs --upgrade setuptools distribute pip
-pip install -b /tmp/pipbuild --no-index --find-links=file:///sources/python-libs /sources/canopsis/
+pip install -b /tmp/pipbuild --no-index -f ${find_links} --upgrade setuptools pip
+pip install -b /tmp/pipbuild --no-index -f ${find_links} /sources/canopsis/
 
 rm -rf /tmp/pipbuild
