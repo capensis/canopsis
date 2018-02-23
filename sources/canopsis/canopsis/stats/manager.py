7a0c5e23aa826ff6ccf7e206c6a99d4ce9673e85
Arthur Dewarumez <adewarumez@capensis.fr>
2018-02-23 18:26:55 +0100

    feat: add method to get the number of OK and KO check.

--- sources/canopsis/canopsis/stats/manager.py
+++ sources/canopsis/canopsis/stats/manager.py
@@ -699,3 +699,35 @@ class Stats(MiddlewareRegistry):
 
             else:
                 return 0
+
+    def get_ok_ko(self, connector, component, resource=None):
+        """For an entity defined by component, connector, resource return
+        the number of OK check and KO check.
+        :param connector: the connector of the entity
+        :param component: the component of the entity
+        :param resource: the resource of the entity
+        :return: a dict with two key ok and ko or none if no data are found for
+        the given entity."""
+        template = "SELECT  SUM(ok) as ok, SUM(ko) as ko FROM " \
+                   "event_state_history WHERE connector='{0}' and component=" \
+                   "'{1}' {2} GROUP BY component, connector {3}" \
+
+        if resource is not None:
+            query = template.format(connector,
+                                    component,
+                                    resource,
+                                    ", resource")
+        else:
+            query = template.format(connector, component, "", "")
+
+        with open("/tmp/plop.log", 'a') as fd:
+            fd.write("{}\n".format(query))
+
+        result = self.influxdbstg.raw_query(query)
+        data = list(result.get_points())
+        if len(data) > 0:
+            data = data[0]
+            data.pop("time")
+            return data
+
+        return None
