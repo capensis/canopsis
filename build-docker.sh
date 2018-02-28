f4d52756d7cf66030bb21435db9a38e70ac318ad
Florent Peterschmitt <fpeterschmitt@capensis.fr>
2018-02-28 09:59:04 +0100

    refacto(docker): bricks-tag parameter

--- build-docker.sh
+++ build-docker.sh
@@ -3,17 +3,17 @@ set -e
 set -o pipefail
 
 if [ "${2}" = "" ]; then
-    echo "Usage: $0 <tag> <brick_branch>"
+    echo "Usage: $0 <tag> <bricks-tag>"
     exit 1
 fi
 
 opt_squash=""
 tag="${1}"
-brick_branch="${2}"
+bricks_tag="${2}"
 workdir=$(dirname $(readlink -e $0))
 cd $workdir
 
-./docker/build/bricks.sh "${brick_branch}"
+./docker/build/bricks.sh "${bricks_tag}"
 
 docker build ${opt_squash} --build-arg PROXY=$http_proxy --build-arg TAG=${tag} -f docker/Dockerfile.sysbase -t canopsis/canopsis-sysbase:${tag} .
 docker build ${opt_squash} --build-arg PROXY=$http_proxy --build-arg TAG=${tag} -f docker/Dockerfile -t canopsis/canopsis-core:${tag} .
