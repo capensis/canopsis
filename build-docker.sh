a935984d9544e6a81c217a15192e2964480ea7c1
Florent Peterschmitt <florent@peterschmitt.fr>
2018-03-06 23:24:47 +0100

    fix(docker): fix ownership through runtime arg

--- build-docker.sh
+++ build-docker.sh
@@ -13,11 +13,15 @@ bricks_tag="${2}"
 workdir=$(dirname $(readlink -e $0))
 cd $workdir
 
+fix_ownership="$(id -u):$(id -g)"
+
 ./docker/build/bricks.sh "${bricks_tag}"
 
 docker build ${opt_squash} --build-arg PROXY=$http_proxy --build-arg TAG=${tag} -f docker/Dockerfile.sysbase -t canopsis/canopsis-sysbase:${tag} .
+
 docker build ${opt_squash} --build-arg PROXY=$http_proxy -f docker/Dockerfile.wheel -t canopsis/debian-9-wheel:latest .
-docker run -v $(pwd)/docker/wheels/:/root/wheelrep/ canopsis/debian-9-wheel:latest
+docker run -v $(pwd)/docker/wheels/:/root/wheelrep/ canopsis/debian-9-wheel:latest "${fix_ownership}"
+
 docker build ${opt_squash} --build-arg PROXY=$http_proxy --build-arg TAG=${tag} -f docker/Dockerfile -t canopsis/canopsis-core:${tag} .
 
 if [ "${3}" == "test" ]; then
