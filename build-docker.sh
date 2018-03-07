4898a3878bc122d1b4406da65266efb57f69326b
Florent Peterschmitt <fpeterschmitt@capensis.fr>
2018-03-07 11:46:31 +0100

    fix(ci): wheel build dir and wheel repo dir

--- build-docker.sh
+++ build-docker.sh
@@ -14,14 +14,18 @@ workdir=$(dirname $(readlink -e $0))
 cd $workdir
 
 fix_ownership="$(id -u):$(id -g)"
-wheel_dir=${WHEEL_DIR:-${workdir}/docker/wheels/}
+wheel_dir=${WHEEL_DIR:-${workdir}/docker/wheelbuild/}
 
 ./docker/build/bricks.sh "${bricks_tag}"
 
 docker build ${opt_squash} --build-arg PROXY=$http_proxy --build-arg TAG=${tag} -f docker/Dockerfile.sysbase -t canopsis/canopsis-sysbase:${tag} .
 
 docker build ${opt_squash} --build-arg PROXY=$http_proxy -f docker/Dockerfile.wheel -t canopsis/debian-9-wheel:latest .
+mkdir -p ${wheel_dir}
 docker run -v ${wheel_dir}:/root/wheelrep/ canopsis/debian-9-wheel:latest "${fix_ownership}"
+
+rm -rf ${workdir}/docker/wheels/
+cp -ar ${wheel_dir} ${workdir}/docker/wheels
 
 docker build ${opt_squash} --build-arg PROXY=$http_proxy --build-arg TAG=${tag} -f docker/Dockerfile -t canopsis/canopsis-core:${tag} .
 
