f3d4d6121e24f173ad547e67d4da8a968596ad05
Florent Peterschmitt <fpeterschmitt@capensis.fr>
2018-03-07 09:50:30 +0100

    fix(ci): store wheels where gitlab doesn't need to clean them

--- build-docker.sh
+++ build-docker.sh
@@ -14,13 +14,14 @@ workdir=$(dirname $(readlink -e $0))
 cd $workdir
 
 fix_ownership="$(id -u):$(id -g)"
+wheel_dir=${WHEEL_DIR:-${workdir}/docker/wheels/}
 
 ./docker/build/bricks.sh "${bricks_tag}"
 
 docker build ${opt_squash} --build-arg PROXY=$http_proxy --build-arg TAG=${tag} -f docker/Dockerfile.sysbase -t canopsis/canopsis-sysbase:${tag} .
 
 docker build ${opt_squash} --build-arg PROXY=$http_proxy -f docker/Dockerfile.wheel -t canopsis/debian-9-wheel:latest .
-docker run -v $(pwd)/docker/wheels/:/root/wheelrep/ canopsis/debian-9-wheel:latest "${fix_ownership}"
+docker run -v ${wheel_dir}:/root/wheelrep/ canopsis/debian-9-wheel:latest "${fix_ownership}"
 
 docker build ${opt_squash} --build-arg PROXY=$http_proxy --build-arg TAG=${tag} -f docker/Dockerfile -t canopsis/canopsis-core:${tag} .
 
