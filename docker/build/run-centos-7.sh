5f648ddfd0fa079087aa46165fb690f0df5e0cd3
Florent Peterschmitt <fpeterschmitt@capensis.fr>
2018-03-09 17:47:13 +0100

    fix(docker): drop some build packages on run-centos-7, also configure locale to en_US.UFT-8

--- docker/build/run-centos-7.sh
+++ docker/build/run-centos-7.sh
@@ -7,11 +7,12 @@ yum clean all
 sed -i /etc/yum/pluginconf.d/fastestmirror.conf -e 's/enabled=.*/enabled=0/g'
 find /etc/yum.repos.d/ -name "CentOS*.repo" -exec sed -e '/^mirrorlist=/d' -i {} \;
 find /etc/yum.repos.d/ -name "CentOS*.repo" -exec sed -re 's/^#baseurl(.*)/baseurl\1/g' -i {} \;
+echo 'LANG=en_US.UTF-8' > /etc/locale.conf
 
 yum makecache
 yum install -y epel-release
 yum makecache
 
-yum install -y xz make gcc gcc-c++ zlib-devel libevent-devel libevent ncurses-devel libcurl-devel wget libtool openssl openssl-devel bzip2-devel cyrus-sasl-devel openldap-devel libcurl-devel python-devel openldap-devel libxml2-devel libxslt-devel perl-ExtUtils-MakeMaker git rsync librsync-devel uthash-devel.noarch libacl-devel libxslt-devel libffi-devel xmlsec1-devel xmlsec1-openssl-devel libtool-ltdl-devel python-virtualenv
+yum install -y xz zlib-devel libevent-devel libevent ncurses-devel libcurl-devel curl tmux htop libtool openssl openssl-devel bzip2-devel cyrus-sasl-devel openldap-devel libcurl-devel python-devel openldap-devel libxml2-devel libxslt-devel perl-ExtUtils-MakeMaker git rsync librsync-devel uthash-devel.noarch libacl-devel libxslt-devel libffi-devel xmlsec1-devel xmlsec1-openssl-devel libtool-ltdl-devel python-virtualenv
 
 rm -rf /var/cache/yum
