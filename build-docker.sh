a92c4cedfad7bd3acc402433dceb26db70e7910c
Florent Peterschmitt <fpeterschmitt@capensis.fr>
2018-03-09 13:15:58 +0100

    wip(multios): add centos 7 files

--- build-docker.sh
+++ build-docker.sh
@@ -22,6 +22,8 @@ sysbase=${SYSBASE:-"debian-9"}
 if [ ! "${3}" == "test-ci" ]; then
     echo "Wheeldir: ${wheel_dir}"
     ./docker/build/bricks.sh "${bricks_tag}"
+
+    echo "BUILDING SYSBASE ${sysbase}"
     docker build ${opt_squash} --build-arg PROXY=$http_proxy --build-arg TAG=${tag} --build-arg SYSBASE=${sysbase} -f docker/Dockerfile.sysbase-${sysbase} -t canopsis/canopsis-sysbase:${sysbase}-${tag} .
 
     if [ ! -d ${wheel_dir} ]; then
@@ -34,19 +36,23 @@ if [ ! "${3}" == "test-ci" ]; then
 
     current_requirements_control=$(md5sum sources/canopsis/requirements.txt | awk '{print $1}')
 
-    if [ "$(grep ${current_requirements_control} ${wheel_req_control})" = "" ]; then
+    if [ "$(grep ${current_requirements_control} ${wheel_req_control})" = "" ]||[ ! -d ${workdir}/docker/wheels/${sysbase} ]; then
         echo -n "${current_requirements_control}" > ${wheel_req_control}
-        docker build ${opt_squash} --build-arg PROXY=$http_proxy --build-arg SYSBASE=${sysbase} -f docker/Dockerfile.wheel -t canopsis/wheel-${sysbase}:latest .
+        echo "BUILDING WHEEL ${sysbase}"
+        docker build ${opt_squash} --build-arg PROXY=$http_proxy --build-arg SYSBASE=${sysbase} --build-arg TAG=${tag} -f docker/Dockerfile.wheel -t canopsis/wheel-${sysbase}:latest .
         mkdir -p ${wheel_dir}
+        echo "RUNNING WHEEL ${sysbase}"
         docker run -v ${wheel_dir}:/root/wheelrep/ canopsis/wheel-${sysbase}:latest "${fix_ownership}"
     fi
 
     rm -rf ${workdir}/docker/wheels/
     cp -ar ${wheel_dir} ${workdir}/docker/wheels
 
+    echo "BUILDING CORE ${sysbase}"
     docker build ${opt_squash} --build-arg PROXY=$http_proxy --build-arg SYSBASE=${sysbase} --build-arg TAG=${tag} -f docker/Dockerfile -t canopsis/canopsis-core:${sysbase}-${tag} .
 fi
 
 if [ "${3}" == "test" ]||[ "${3}" == "test-ci" ]; then
+    echo "BUILDING TEST ${sysbase}"
     docker build ${opt_squash} --build-arg PROXY=$http_proxy --build-arg SYSBASE=${sysbase} --build-arg TAG=${tag} -f docker/Dockerfile.tests -t canopsis/canopsis-core:${sysbase}-${tag}-test .
 fi
