3467a275512ec971e2bae04f14f9fbd1cb1194ea
Florent Peterschmitt <fpeterschmitt@capensis.fr>
2018-03-12 10:19:18 +0100

    feat(docker): debian-8

---
+++ docker/build/run-debian-8.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/bash
+set -e
+set -o pipefail
+
+echo "deb http://ftp.fr.debian.org/debian/ jessie main contrib non-free" > /etc/apt/sources.list
+echo "deb http://security.debian.org/ jessie/updates main" >> /etc/apt/sources.list
+
+rm -f /etc/localtime
+ln -s /usr/share/zoneinfo/UTC /etc/localtime
+
+apt-get update
+
+apt-get -y --no-install-recommends install locales
+
+echo 'en_US.UTF-8 UTF-8' > /etc/locale.gen
+locale-gen
+
+export LANG="en_US.UTF-8"
+
+apt-get -y --no-install-recommends install \
+    apt-transport-https \
+    base-files \
+    bash \
+    bash-completion \
+    ca-certificates \
+    curl \
+    libsasl2-2 \
+    libxml2 \
+    libxslt1.1 \
+    lsb-base \
+    lsb-release \
+    libffi6 \
+    libgmp10 \
+    libgnutlsxx28 \
+    libgnutls-openssl27 \
+    libhogweed2 \
+    libicu52 \
+    libidn11 \
+    libnettle4 \
+    libp11-kit0 \
+    libpsl0 \
+    libssl1.0.0 \
+    libtasn1-6 \
+    libxmlsec1 \
+    libxmlsec1-openssl \
+    libldap-2.4-2 \
+    pkgconf \
+    pkg-config \
+    python \
+    python2.7 \
+    python-pip \
+    python-pkg-resources \
+    python-virtualenv \
+    python-wheel \
+    rsync \
+    sudo \
+    tmux \
+    vim \
+    virtualenv \
+
+apt-get clean
