5307ea7f04f11c3921bf1efdd61c154aff5a243f
Florent Peterschmitt <fpeterschmitt@capensis.fr>
2018-03-15 10:44:44 +0100

    fix(docker): add listalarm and timeline ui bricks to core

--- docker/build/bricks.sh
+++ docker/build/bricks.sh
@@ -5,5 +5,9 @@ set -o pipefail
 rm -rf docker/bricks && mkdir -p docker/bricks
 
 git clone https://git.canopsis.net/canopsis-ui-bricks/brick-querybuilder.git -b ${1} docker/bricks/brick-querybuilder
+git clone https://git.canopsis.net/canopsis-ui-bricks/brick-listalarm.git -b ${1} docker/bricks/brick-listalarm
+git clone https://git.canopsis.net/canopsis-ui-bricks/brick-timeline.git -b ${1} docker/bricks/brick-timeline
 
 rm -rf /sources/bricks/brick-querybuilder/.git
+rm -rf /sources/bricks/brick-listalarm/.git
+rm -rf /sources/bricks/brick-timeline/.git
