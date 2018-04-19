8b4bac20f25fdc65876f8e6477d6b83f686f7fcc
Arthur Dewarumez <adewarumez@capensis.fr>
2018-04-19 16:54:01 +0200

    feat: wip

--- src/components/selectionactions/component.js
+++ src/components/selectionactions/component.js
@@ -21,35 +21,69 @@ Ember.Application.initializer({
                 {
                     class: '',
                     mixin_name: 'bulk_pbehavior',
-                    caption: 'Apply PBehavior'
+                    caption: 'Apply PBehavior',
+					rightName: "listalarm_pbehavior"
                 },
                 {
                     class: 'glyphicon glyphicon-ok',
                     mixin_name: 'fastack',
-                    caption: 'Fast Ack'
+                    caption: 'Fast Ack',
+					rightName: "listalarm_fastAck"
                 },
                 {
                     class: 'glyphicon glyphicon-saved',
                     mixin_name: 'ack',
-                    caption: 'Ack'
+                    caption: 'Ack',
+					rightName: "listalarm_ack"
                 },
                 {
                     class: 'glyphicon glyphicon-ban-circle',
                     mixin_name: 'ackremove',
-                    caption: 'Cancel ack'
+                    caption: 'Cancel ack',
+					rightName: "listalarm_cancelAck"
                 },
                 {
                     class: 'glyphicon glyphicon-share-alt',
                     mixin_name: 'recovery',
-                    caption: 'Restore alarm'
+                    caption: 'Restore alarm',
+					rightName: 'listalarm_restoreAlarm',
                 }
             ]),
+
+			availableAction: [],
+
+			canAction: function(rights, actionName){
+				console.error("Rights", rights)
+				console.error("ActionName", actionName)
+				if (rights.hasOwnProperty(actionName)) {
+					if (rights.get(actionName).checksum) {
+						return true
+					}
+				}
+				return false
+			},
+
+
+			genAvailableAction: function(rights) {
+				var actions = new Array()
+				for(i = 0; i < this.get("actionsMap").length; i++) {
+					if (this.get("canAction")(rights, this.actionsMap[i]["rightName"])) {
+						actions.push(this.actionsMap[i])
+					}
+				}
+				this.set("availableAction", actions)
+			},
 
             /**
              * @method init
              */
             init: function() {
                 this._super();
+				this.set("rights", this.get("_parentView._controller.login.rights"))
+				console.error("Rights ", this.get("rights"))
+				var func = this.get("genAvailableAction")
+				console.error("genAvailable ", func)
+				func(this.get("rights"))
             },
 
             actions: {
