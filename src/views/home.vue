955d2766180eed2c29246d1ddc76d7a8eaa50b85
Nina <nsanti1113@gmail.com>
2018-04-20 17:57:16 +0200

    Modifie home for testing

--- src/views/home.vue
+++ src/views/home.vue
@@ -2,15 +2,18 @@
   .home
     img(src="../assets/logo.png")
     hello-world(msg="Test props")
+    test
 </template>
 
 <script>
 import HelloWorld from '@/components/hello-world.vue';
+import Test from '@/components/Context/Test.vue';
 
 export default {
   name: 'home',
   components: {
     HelloWorld,
+    Test,
   },
 };
 </script>
