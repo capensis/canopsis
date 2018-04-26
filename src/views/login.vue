932f2c8578979f8f32ba1ce7a8f14c1150e11adc
VLaroye <vlaroye@cepensis.fr>
2018-04-26 15:12:16 +0200

    refactor(login): Implemented nameSpaceHelper in login component

--- src/views/login.vue
+++ src/views/login.vue
@@ -5,19 +5,12 @@
     d-flex
     align-center
   )
-    v-layout(
-      justify-center
-      row
-    )
-      v-flex(
-        xs11
-        md6
-        lg5
-      )
+    v-layout(justify-center row)
+      v-flex(xs11 md6 lg5)
         v-card
           v-layout(row wrap)
             v-flex(xs12)
-              v-toolbar(class='green darken-2 white--text')
+              v-toolbar(class="green darken-2 white--text")
                 v-toolbar-title {{ $t('common.login') }}
             v-flex(xs12 py-2)
               v-form
@@ -26,7 +19,7 @@
                     autofocus
                     clearable
                     color="blue darken-4"
-                    v-model='formData.username'
+                    v-model="formData.username"
                     name="username"
                     :label="$t('common.username')"
                   )
@@ -34,20 +27,24 @@
                   v-text-field(
                     clearable
                     color="blue darken-4"
-                    v-model='formData.password'
+                    v-model="formData.password"
                     type="password"
                     name="password"
                     :label="$t('common.password')"
                   )
                 v-flex(xs2 px-2)
                   v-btn(
-                    @click='handleSubmit'
+                    @click="handleSubmit"
                     depressed
                     color="blue darken-4 white--text"
                   ) {{ $t('common.submit') }}
 </template>
 
 <script>
+import { createNamespacedHelpers } from 'vuex';
+
+const { mapActions } = createNamespacedHelpers('auth');
+
 export default {
   name: 'login-page',
   data() {
@@ -59,8 +56,10 @@ export default {
     };
   },
   methods: {
+    ...mapActions(['login']),
+
     handleSubmit() {
-      this.$store.dispatch('auth/login', { username: this.formData.username, password: this.formData.password });
+      this.login({ username: this.formData.username, password: this.formData.password });
     },
   },
 };
