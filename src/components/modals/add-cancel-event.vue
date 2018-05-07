a1b2def526cbcf74c5ef47d4dc03426028c7728d
Aleksandr Bakautov <aleksandr.bakautov@noveogroup.com>
2018-05-07 20:25:33 +0700

    Revert "test yarn"
    This reverts commit bfcaba5

--- src/components/modals/add-cancel-event.vue
+++ src/components/modals/add-cancel-event.vue
@@ -25,13 +25,12 @@
 
 <script>
 import AlarmGeneralTable from '@/components/tables/alarm-general.vue';
+
 import ModalMixin from './modal-mixin';
 
 export default {
   name: 'add-cancel-event',
-  mixins: [
-    ModalMixin,
-  ],
+  mixins: [ModalMixin],
   components: {
     AlarmGeneralTable,
   },
@@ -47,19 +46,7 @@ export default {
       const isFormValid = await this.$validator.validateAll();
 
       if (isFormValid) {
-        console.log(this.$store);
-        const response = await this.$store.dispatch('alarm/cancelConfirmation', {
-          comment: this.form.output,
-          alarmData: {
-            connector: 'toto',
-            connector_name: 'toto',
-            component: 'localhost',
-            state: 0,
-            state_type: 1,
-            resource: 'res99',
-          },
-        });
-        console.log(response);
+        console.log('SUBMITTED');
       }
     },
   },
--- src/store/index.js
+++ src/store/index.js
@@ -3,16 +3,12 @@ import Vuex from 'vuex';
 
 import i18nModule from './modules/i18n';
 import modalModule from './modules/modal';
-import alarmsModule from './modules/alarm';
 
 Vue.use(Vuex);
 
-const store = new Vuex.Store({
+export default new Vuex.Store({
   modules: {
     i18n: i18nModule,
     modal: modalModule,
-    alarm: alarmsModule,
   },
 });
-
-export default store;
