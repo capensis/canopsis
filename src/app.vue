ea92cdf90c15b0009ee18c34a80c06547f54c725
VLaroye <vlaroye@cepensis.fr>
2018-05-07 15:10:45 +0200

    feature(login): Comas in pug attributes lists

--- src/app.vue
+++ src/app.vue
@@ -1,9 +1,5 @@
 <template lang="pug">
   v-app#app
-    //#nav
-      router-link(to="/") Home
-      router-link(to="/about") About
-    //router-view
     login
 </template>
 
--- src/views/login.vue
+++ src/views/login.vue
@@ -1,41 +1,40 @@
 <template lang="pug">
   v-container(
-    class="blue darken-4"
-    fluid
-    d-flex
+    class="blue darken-4",
+    fluid,
+    d-flex,
     align-center
   )
-    v-layout(justify-center row)
-      v-flex(xs11 md6 lg5)
+    v-layout(justify-center, row)
+      v-flex(xs11, md6, lg5)
         v-card
-          v-layout(row wrap)
+          v-layout(row, wrap)
             v-flex(xs12)
               v-toolbar(class="green darken-2 white--text")
                 v-toolbar-title {{ $t('common.login') }}
-            v-flex(xs12 py-2)
+            v-flex(xs12, py-2)
               v-form
                 v-flex(px-3)
                   v-text-field(
-                    autofocus
-                    clearable
-                    color="blue darken-4"
-                    v-model="formData.username"
-                    name="username"
+                    autofocus,
+                    clearable,
+                    color="blue darken-4",
+                    v-model="formData.username",
+                    name="username",
                     :label="$t('common.username')"
                   )
                 v-flex(px-3)
                   v-text-field(
-                    clearable
-                    color="blue darken-4"
-                    v-model="formData.password"
-                    type="password"
-                    name="password"
+                    clearable,
+                    color="blue darken-4",
+                    v-model="formData.password",
+                    type="password",
+                    name="password",
                     :label="$t('common.password')"
                   )
                 v-flex(xs2 px-2)
                   v-btn(
-                    @click="handleSubmit"
-                    depressed
+                    @click="handleSubmit",
                     color="blue darken-4 white--text"
                   ) {{ $t('common.submit') }}
 </template>
