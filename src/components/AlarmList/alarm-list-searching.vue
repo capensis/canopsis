a1222d747c35ed7c4c1c5114f789f06e68a753e1
Nina <nsanti1113@gmail.com>
2018-05-11 17:23:49 +0200

    feat(alarm-list-searching): add the alarm-list-searching component to the alarm-list

---
+++ src/components/AlarmList/alarm-list-searching.vue
@@ -0,0 +1,57 @@
+<template lang="pug">
+  v-toolbar.toolbar(dense)
+    v-text-field(
+    label="Search"
+    v-model="searchingText"
+    hide-details
+    single-line
+    )
+    v-btn(icon @click="submit")
+      v-icon search
+    v-btn(icon @click="clear")
+      v-icon clear
+    v-tooltip(bottom)
+      v-btn(icon slot="activator")
+        v-icon help_outline
+      span Aide sur la recherche avancée
+</template>
+
+<script>
+import omit from 'lodash/omit';
+
+export default {
+  name: 'alarm-list-searching',
+  data() {
+    return {
+      searchingText: '',
+    };
+  },
+  methods: {
+    clear() {
+      const query = omit(this.$route.query, ['search']);
+      this.$router.push({
+        query: {
+          ...query,
+        },
+
+      });
+    },
+    submit() {
+      const search = this.searchingText;
+      this.$router.push({
+        query: {
+          ...this.$route.query,
+          search,
+        },
+
+      });
+    },
+  },
+};
+</script>
+
+<style scoped>
+  .toolbar {
+    background-color: rgb(251,247,247);
+  }
+</style>
