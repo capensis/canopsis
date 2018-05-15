20a56a8b69a2c75f752aa75e3651e56bc7d0db80
Nina <nsanti1113@gmail.com>
2018-05-15 11:20:00 +0200

    feat(alarmConvention store): add the alarmConvention module the the alarm store

--- src/store/modules/entities/index.js
+++ src/store/modules/entities/index.js
@@ -1,8 +1,10 @@
 import alarmModule from './alarm';
+import alarmConventionModule from './alarmConvention';
 
 export default {
   namespaced: true,
   modules: {
     alarm: alarmModule,
+    alarmConvention: alarmConventionModule,
   },
 };
