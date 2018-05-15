a1beca5df9ca9fd0aa046ec6378bd546a8d1f20e
Nina <nsanti1113@gmail.com>
2018-05-15 11:14:35 +0200

    Feat(alarm-chips): Add alarm-chips, that regroup the state-chips and the status-chips for an alarm

---
+++ src/components/BasicComponent/alarm-chips.vue
@@ -0,0 +1,42 @@
+<template lang="pug">
+  v-chip(:color="color" text-color="white") {{ this.text }}
+</template>
+
+<script>
+import { createNamespacedHelpers } from 'vuex';
+
+const { mapGetters } = createNamespacedHelpers('entities/alarmConvention');
+
+export default {
+  name: 'state-chips',
+  props: {
+    val: {
+      type: Number,
+      default: 0,
+    },
+    isStatus: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  computed: {
+    ...mapGetters(['getStateAlarmConvention', 'getStatusAlarmConvention']),
+    color() {
+      if (this.isStatus) {
+        return this.getStatusAlarmConvention(this.val)('color');
+      }
+      return this.getStateAlarmConvention(this.val)('color');
+    },
+    text() {
+      if (this.isStatus) {
+        return this.getStatusAlarmConvention(this.val)('text');
+      }
+      return this.getStateAlarmConvention(this.val)('text');
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
