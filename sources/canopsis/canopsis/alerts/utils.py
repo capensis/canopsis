356af7fd056a2ecf8bd216873416c5d88618bfe4
Florent Peterschmitt <fpeterschmitt@capensis.fr>
2018-05-17 11:57:12 +0200

    doc: alerts utils docstring

--- sources/canopsis/canopsis/alerts/utils.py
+++ sources/canopsis/canopsis/alerts/utils.py
@@ -6,7 +6,10 @@ def compat_go_crop_states(alarm):
     """
     If a statecounter field exists in an alarm step,
     val key is replaced with the value of the statecounter field.
+
     :param dict alarm: dict with alarm data
+    :returns: the alarm with modified steps
+    :rtype dict:
     """
     steps = alarm.get('v', {}).get('steps', [])
     for i, step in enumerate(steps):
