ad4dd9db7b176607a35c2d4ab181d52a82c4ec14
Lucas Seguinot <lseguinot@capensis.fr>
2018-05-17 11:44:47 +0200

    feat(statsng): add basic engine structure

---
+++ sources/canopsis/canopsis/engines/statsng/__init__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+# --------------------------------
+# Copyright (c) 2015 "Capensis" [http://www.capensis.com]
+#
+# This file is part of Canopsis.
+#
+# Canopsis is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Canopsis is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with Canopsis.  If not, see <http://www.gnu.org/licenses/>.
+# ---------------------------------
+
+from __future__ import unicode_literals
+
+from canopsis.engines.core import Engine
+
+
+class engine(Engine):
+    etype = "statsng"
+
+    def work(self, event, *args, **kargs):
+        """
+        AMQP event processing.
+
+        :param dict event: event to process.
+        """
+
+        self.logger.info('received event!')
