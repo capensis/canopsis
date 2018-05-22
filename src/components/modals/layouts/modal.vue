fc120e6cf67a4e73e9150b67c8f3510fd57f7c8c
Benjamin Dubois <bdubois@capensis.fr>
2018-05-22 17:30:38 +0200

    restoring deleted files

---
+++ src/components/modals/layouts/modal.vue
@@ -0,0 +1,46 @@
+<template lang="pug">
+  v-dialog(v-model="isOpened", v-bind="dialogProps")
+    slot(v-if="isActive")
+</template>
+
+<script>
+import { createNamespacedHelpers } from 'vuex';
+
+const { mapGetters, mapActions } = createNamespacedHelpers('modal');
+
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    dialogProps: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+  computed: {
+    ...mapGetters({
+      modalName: 'name',
+      modalConfig: 'config',
+      modalAnimationPending: 'animationPending',
+    }),
+    isActive() {
+      return this.modalName === this.name;
+    },
+    isOpened: {
+      get() {
+        return this.modalName === this.name && !this.modalAnimationPending;
+      },
+      set() {
+        this.hideModal();
+      },
+    },
+  },
+  methods: {
+    ...mapActions({
+      hideModal: 'hide',
+    }),
+  },
+};
+</script>
