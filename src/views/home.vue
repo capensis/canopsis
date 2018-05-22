ffce1114c75b2db408ecb704fe82cc465cbd856b
Nina <nsanti1113@gmail.com>
2018-05-22 15:20:26 +0200

    refactor: delete hello world component

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,16 +1,12 @@
 <template lang="pug">
   .home
     img(src="../assets/logo.png")
-    hello-world(msg="AlarmList props")
 </template>
 
 <script>
-import HelloWorld from '@/components/hello-world.vue';
-
 export default {
   name: 'home',
   components: {
-    HelloWorld,
   },
 };
 </script>
