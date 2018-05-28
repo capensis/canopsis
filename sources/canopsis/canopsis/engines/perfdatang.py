e0006af00152708611b4ab1c4d9fd7ca5954cde4
Lucas Seguinot <lseguinot@capensis.fr>
2018-05-28 11:51:47 +0200

    feat(perfdatang): add perfdatang engine

---
+++ sources/canopsis/canopsis/engines/perfdatang.py
@@ -0,0 +1,34 @@
+# --------------------------------
+# Copyright (c) 2018 "Capensis" [http://www.capensis.com]
+#
+# This file is part of Canopsis.
+#
+# Canopsis is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Canopsis is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with Canopsis.  If not, see <http://www.gnu.org/licenses/>.
+# ---------------------------------
+
+from __future__ import unicode_literals
+
+from canopsis.engines.core import Engine
+
+
+class engine(Engine):
+    etype = "perfdatang"
+
+    def work(self, event, *args, **kwargs):
+        """
+        AMQP event processing.
+
+        :param dict event: event to process.
+        """
+        return
