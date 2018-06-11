d5005c683a74c5a0d0bd9e2ed318a4563161901d
Nina <nsanti1113@gmail.com>
2018-06-11 11:40:03 +0200

    fix(alarm-list): fix format date

--- src/components/alarm-list/alarm-column-value.vue
+++ src/components/alarm-list/alarm-column-value.vue
@@ -1,14 +1,13 @@
 <template lang="pug">
   span
-    template(v-if="attributeType === 'textable'") {{ getProp(alarm, pathToProperty) }}
     template(v-if="attributeType === 'state'")
-      state(:stateId="getProp(alarm, pathToProperty)", :showIcon="getProp(alarm, 'v.state._t') === 'changestate'")
+      state(:stateId="alarm | get(pathToProperty, filter)", :showIcon="showIcon")
     template(v-if="attributeType === 'status'")
-      status(:statusId="getProp(alarm, pathToProperty)")
+      status(:statusId="alarm | get(pathToProperty, filter)")
+    template(v-if="attributeType === 'textable'") {{ alarm | get(pathToProperty, filter) }}
 </template>
 
 <script>
-import getProp from 'lodash/get';
 import State from '@/components/alarm-list/alarm-state-column-value.vue';
 import Status from '@/components/alarm-list/alarm-status-column-value.vue';
 
@@ -26,22 +25,25 @@ export default {
       type: String,
       required: true,
     },
+    filter: {
+      type: Function,
+    },
   },
   computed: {
     attributeType() {
       if (this.pathToProperty === 'v.state.val') {
         return 'state';
       }
-
       if (this.pathToProperty === 'v.status.val') {
         return 'status';
       }
-
       return 'textable';
+    },
+    showIcon() {
+      return this.$options.filters.get(this.alarm, 'v.state._t') === 'changestate';
     },
   },
   methods: {
-    getProp,
   },
 };
 </script>
