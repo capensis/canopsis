e57f93649bd15518390b258fe729ed5cf00c915c
Nina <nsanti1113@gmail.com>
2018-06-15 15:31:55 +0200

    refactor(searching): translation on the advanced search help

--- src/i18n/messages/en.js
+++ src/i18n/messages/en.js
@@ -39,6 +39,21 @@ export default {
       year: 'year | years',
     },
   },
+  search: {
+    advancedSearch: '<span>Help on the advanced research :</span>\n' +
+    '<p>- [ NOT ] &lt;ColumnName&gt; &lt;Operator&gt; &lt;Value&gt;</p> [ AND|OR [ NOT ] &lt;ColumnName&gt; &lt;Operator&gt; &lt;Value&gt; ]\n' +
+    '<p>The "-" before the research is required</p>\n' +
+    '<p>Operators :\n' +
+    '    <=, <,=, !=,>=, >, LIKE (For MongoDB regular expression)</p>\n' +
+    '<p>Value\'s type : String between quote, Boolean ("TRUE", "FALSE"), Integer, Float, "NULL"</p>\n' +
+    '<dl><dt>Examples :</dt><dt>- Connector = "connector_1"</dt>\n' +
+    '    <dd>Alarms whose connectors are "connector_1"</dd><dt>- Connector="connector_1" AND Resource="resource_3"</dt>\n' +
+    '    <dd>Alarms whose connectors is "connector_1" and the ressources is "resource_3"</dd><dt>- Connector="connector_1" OR Resource="resource_3"</dt>\n' +
+    '    <dd>Alarms whose connectors is "connector_1" or the ressources is "resource_3"</dd><dt>- Connector LIKE 1 OR Connector LIKE 2</dt>\n' +
+    '    <dd>Alarms whose connectors contains 1 or 2</dd><dt>- NOT Connector = "connector_1"</dt>\n' +
+    '    <dd>Alarms whose connectors isn\'t "connector_1"</dd>\n' +
+    '</dl>',
+  },
   alarmList: {
     actions: {
       ack: 'ack',
