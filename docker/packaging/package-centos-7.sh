5d7a3fde5ea2c3b94ee4f576c6007adf4dd518b0
Florent Peterschmitt <fpeterschmitt@capensis.fr>
2018-07-02 11:34:13 +0200

    fix(packaging): fix centos 7 core packaging

--- docker/packaging/package-centos-7.sh
+++ docker/packaging/package-centos-7.sh
@@ -4,12 +4,12 @@ set -o pipefail
 
 echo "#!/bin/bash" > /usr/lib/rpm/brp-python-bytecompile
 
-sed -i /packaging/centos-7-canopsis-cat.spec -e "s/CPS_PKG_TAG/${CPS_PKG_TAG}/g"
-sed -i /packaging/centos-7-canopsis-cat.spec -e "s/CPS_PKG_REL/${CPS_PKG_REL}/g"
+sed -i /packaging/centos-7-canopsis-core.spec -e "s/CPS_PKG_TAG/${CPS_PKG_TAG}/g"
+sed -i /packaging/centos-7-canopsis-core.spec -e "s/CPS_PKG_REL/${CPS_PKG_REL}/g"
 
-yum-builddep -y /packaging/centos-7-canopsis-cat.spec
+yum-builddep -y /packaging/centos-7-canopsis-core.spec
 
-rpmbuild -bb /packaging/centos-7-canopsis-cat.spec
+rpmbuild -bb /packaging/centos-7-canopsis-core.spec
 
 rsync -vrc /root/rpmbuild/RPMS/* /packages/
 
