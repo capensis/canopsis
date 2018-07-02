ba91b416f3c6e10c5efeddfe84f7223e1419e898
Florent Peterschmitt <fpeterschmitt@capensis.fr>
2018-07-02 11:30:30 +0200

    feat: canopsis-core packaging

--- docker/packaging/setup-centos-7.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-set -o pipefail
-
-yum makecache
-yum groupinstall -y "Development tools"
-yum install -y yum-utils
-
-rm -rf /var/cache/yum
--- docker/packaging/setup-debian-9.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-set -o pipefail
-
-mkdir -p /root/canopsis-core-${CPS_PKG_TAG}/{DEBIAN,opt}
-mkdir -p /root/canopsis-core-${CPS_PKG_TAG}/lib/systemd/system/
-rsync -aKSH /opt/canopsis /root/canopsis-core-${CPS_PKG_TAG}/opt/
-cp -ar /lib/systemd/system/canopsis-* /root/canopsis-core-${CPS_PKG_TAG}/lib/systemd/system/
