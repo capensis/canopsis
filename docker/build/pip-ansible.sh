131f3e8ac26b1f44b2bc6b0c3996b30978000f90
Florent Peterschmitt <fpeterschmitt@capensis.fr>
2018-07-03 18:04:36 +0200

    fix(ansible): install jmespath in ansible venv

--- docker/build/pip-ansible.sh
+++ docker/build/pip-ansible.sh
@@ -10,7 +10,8 @@ repver="${ID}-${VERSION_ID}"
 source ${CPS_HOME}/venv-ansible/bin/activate
 find_links="file:///sources/wheels/${repver}"
 
-pip install -U setuptools pip wheel
+# jmespath is for ansible role repository adder
+pip install -U setuptools pip wheel jmespath
 pip install -b /tmp/pipbuild --no-index -f ${find_links} "ansible==2.4.4"
 
 rm -rf /tmp/pipbuild
