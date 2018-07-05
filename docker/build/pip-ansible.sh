c00b8de59d5771c5041f6dbb6df20de38803c867
Florent Peterschmitt <fpeterschmitt@capensis.fr>
2018-07-05 13:29:24 +0200

    fix(ansible): quick fix for influxdb role unability to stay inside ansible venv, doc

--- docker/build/pip-ansible.sh
+++ docker/build/pip-ansible.sh
@@ -22,7 +22,13 @@ if [ "${VERSION_ID}" = "8" ]&&[ "${ID}" = "debian" ]; then
 fi
 
 pip install -U setuptools pip
-pip install -U setuptools pip wheel jmespath "pymongo==3.6.1" "influxdb==5.1.0" "${pyopenssl}" "ansible==2.4.4"
+
+# jmespath is required by ansible role repository adder maat
+# pymongo 3.5.0 is required by ansible 2.4.x module mongodb
+# influxdb is required for the modified ansible role influxdb
+# pyopenssl is required to fix compatibility between systems
+# finally ansible 2.4 is required to avoid role compat problems
+pip install -U setuptools pip wheel jmespath "pymongo==3.5.0" "influxdb==5.1.0" "${pyopenssl}" "ansible==2.4.4"
 
 rm -rf /tmp/pipbuild
 rm -rf ~/.cache
