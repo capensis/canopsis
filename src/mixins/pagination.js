92decee82f6bcb96b0ba22279b88f723768e5674
VLaroye <vlaroye@cepensis.fr>
2018-07-17 10:03:14 +0200

    fix(pagination-bug): Moved 'last' and 'first' logic to pagination mixin

--- src/mixins/pagination.js
+++ src/mixins/pagination.js
@@ -15,6 +15,36 @@ export default {
     limit() {
       return parseInt(this.$route.query.limit, 10) || PAGINATION_LIMIT;
     },
+    /**
+     * Calculate first item nb to display on pagination, in case it's not given by the backend
+     */
+    first() {
+      const { page } = this.$route.query;
+      if (page === 1 || !this.$route.query.page) {
+        return 1;
+      }
+      if (this.$route.query.limit) {
+        return 1 + (this.$route.query.limit * (page - 1));
+      }
+
+      return 1 + (PAGINATION_LIMIT * (page - 1));
+    },
+    /**
+     * Calculate last item nb to display on pagination, in case it's not given by the backend
+     */
+    last() {
+      let last;
+
+      if (this.$route.query.page === 1 || !this.$route.query.page) {
+        last = this.$route.query.limit || PAGINATION_LIMIT;
+      } else if (this.$route.query.limit) {
+        last = this.$route.query.page * this.$route.query.limit;
+      } else {
+        last = this.$route.query.page * PAGINATION_LIMIT;
+      }
+
+      return last;
+    },
   },
   watch: {
     $route: {
