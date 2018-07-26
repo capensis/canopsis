33d3b649de1c0b7b09cc60c963f4c5f33cb192bb
VLaroye <vlaroye@cepensis.fr>
2018-07-26 10:56:39 +0200

    feature(weather-grid): Added basic layout + Changed icon sizes

--- src/components/other/service-weather/weather-grid.vue
+++ src/components/other/service-weather/weather-grid.vue
@@ -2,17 +2,20 @@
   v-container(fluid)
     v-layout(wrap)
       v-flex(xs3)
-        v-card.green.darken-1
-          v-card-text Watcher's name
-      v-flex(xs3)
-        v-card.green.darken-1
-          v-card-text Watcher's name
-      v-flex(xs3)
-        v-card.green.darken-1
-          v-card-text Watcher's name
-      v-flex(xs3)
-        v-card.green.darken-1
-          v-card-text Watcher's name
+        v-card(
+          class=['white--text', 'green darken-1']
+        )
+          v-layout(justify-start, align-center)
+            v-flex(xs2)
+              raining-cloud.ma-2
+            div Watcher Name
+          v-layout
+            v-flex(xs12)
+              div.moreInfos.py-1
+                v-layout(justify-center)
+                  div More infos
+                  v-icon.pl-1(color="white", small) arrow_forward
+
 </template>
 
 <script>
@@ -20,6 +23,38 @@ import RainingCloud from './icons/cloud.vue';
 
 export default {
   components: { RainingCloud },
+  data() {
+    return {
+      values: {
+        ok: {
+          color: 'green darken-1',
+          icon: 'sun',
+        },
+        minor: {
+          color: 'yellow darken-1',
+          icon: 'cloudy sun',
+        },
+        major: {
+          color: 'orange darken-1',
+          icon: 'cloud',
+        },
+        critical: {
+          color: 'red darken-1',
+          icon: 'raining cloud',
+        },
+        pbehavior: {
+          color: 'grey lighten-1',
+          icon: 'pause',
+        },
+      },
+    };
+  },
 };
 </script>
+
+<style lang="scss" scoped>
+  .moreInfos {
+    background-color: rgba(0,0,0,0.2);
+  }
+</style>
 
