f34c9d20d97cc78a6441212c738450879e4cf29b
Vladimir Erokhov <Vladimir.Erokhov@noveogroup.com>
2018-08-13 22:09:20 +0700

    Removed excess code

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,23 +1,7 @@
 <template lang="pug">
   div
-    v-btn(@click="openModal") Modal
     div#brand Canopsis Next
 </template>
-
-<script>
-export default {
-  methods: {
-    openModal() {
-      this.$store.dispatch('modal/show', {
-        name: 'confirmation',
-        config: {
-          number: 0,
-        },
-      });
-    },
-  },
-};
-</script>
 
 <style lang="scss" scoped>
   #brand {
