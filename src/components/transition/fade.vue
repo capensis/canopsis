b599e74455bf655459ef5a1705d616a160ffa2f0
Aleksandr Bakautov <bakautov.aleksandr@noveogroup.com>
2018-08-31 11:39:04 +0700

    Fixed fade transition component

--- src/components/transition/fade.vue
+++ src/components/transition/fade.vue
@@ -1,23 +1,13 @@
 <template lang="pug">
-  transition(name="fade", :duration="duration")
+  transition(name="fade")
     slot
 </template>
 
-<script>
-export default {
-  props: {
-    duration: {
-      type: Number,
-      default: 1000,
-    },
-  },
-};
-</script>
-
 <style scoped>
   .fade-enter-active, .fade-leave-active {
-    transition: opacity .5s;
+    transition: opacity .3s;
   }
+
   .fade-enter, .fade-leave-to {
     opacity: 0;
   }
