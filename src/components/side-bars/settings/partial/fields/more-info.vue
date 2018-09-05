813d9a18f35de732b2aac9a821e9ea514fb70894
VLaroye <vlaroye@cepensis.fr>
2018-09-05 10:13:21 +0200

    fix(more-infos-popup): Added more-info-popup template to request sent with widget settings

--- src/components/side-bars/settings/partial/fields/more-info.vue
+++ src/components/side-bars/settings/partial/fields/more-info.vue
@@ -2,5 +2,6 @@
   v-list-group
     v-list-tile(slot="activator") {{$t('settings.moreInfosModal')}}
     v-container
-      v-text-field(class="pa-0", textarea, hide-details)
+      v-text-field(class="pa-0", textarea, hide-details, @input="$emit('input', $event)")
 </template>
+
