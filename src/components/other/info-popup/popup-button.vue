9cd6fbd039a5992179a659055f8aa22d7a7536c3
VLaroye <vlaroye@cepensis.fr>
2018-09-14 17:46:59 +0200

    feature(alarm-list-settings-text-editor): Changed event on more-infos + Revert changes in info-popup

--- src/components/other/info-popup/popup-button.vue
+++ src/components/other/info-popup/popup-button.vue
@@ -42,7 +42,7 @@ export default {
     },
     textContent() {
       const template = Handlebars.compile(this.popupData.template);
-      const context = { alarm: this.alarm };
+      const context = { alarm: this.alarm.v };
 
       return template(context);
     },
