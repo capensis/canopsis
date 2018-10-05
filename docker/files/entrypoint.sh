f4c38f63df222f81b6895a42d446a44f5dd2b777
Lucas Seguinot <lseguinot@capensis.fr>
2018-10-05 10:50:26 +0200

    feat(docker): get logging level from CPS_LOGGING_LEVEL env variable

--- docker/files/entrypoint.sh
+++ docker/files/entrypoint.sh
@@ -11,6 +11,6 @@ else
     if [ "${CPS_WEBSERVER}" = "1" ]; then
         /opt/canopsis/bin/webserver --access-logfile /opt/canopsis/var/log/webserver-access.log -k gevent -w 1 -b 0.0.0.0:8082
     else
-        /opt/canopsis/bin/engine-launcher -e $ENGINE_MODULE -n $ENGINE_NAME -w 1 -l info
+        /opt/canopsis/bin/engine-launcher -e $ENGINE_MODULE -n $ENGINE_NAME -w 1 -l ${CPS_LOGGING_LEVEL:-info}
     fi
 fi
