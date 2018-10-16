74168b4ba634010dcd5636300d3fb1b9aba5deed
Clara Dewarumez <cdewarumez@capensis.fr>
2018-10-16 12:10:00 +0200

    fix(test): fix test alerts

--- sources/canopsis/test/alerts/test_manager.py
+++ sources/canopsis/test/alerts/test_manager.py
@@ -26,6 +26,7 @@ from operator import itemgetter
 from time import time, mktime, sleep
 from unittest import main
 
+from canopsis.alerts import DEFAULT_FILTER_AUTHOR
 from canopsis.alerts.enums import AlarmField, States, AlarmFilterField
 from canopsis.alerts.filter import AlarmFilter
 from canopsis.alerts.status import OFF, STEALTHY, CANCELED, is_keeped_state
@@ -351,7 +352,7 @@ class TestManager(BaseTest):
         }
 
         expected_status = {
-            'a': 'ut-connector.ut-connector0',
+            'a': DEFAULT_FILTER_AUTHOR,
             '_t': 'statusinc',
             'm': 'UT message',
             't': 0,
@@ -405,7 +406,7 @@ class TestManager(BaseTest):
         alarm = self.manager.change_of_status(alarm, 0, 1, event)
 
         expected_status = {
-            'a': 'ut-connector.ut-connector0',
+            'a': DEFAULT_FILTER_AUTHOR,
             '_t': 'statusinc',
             'm': 'UT message',
             't': 0,
@@ -442,7 +443,7 @@ class TestManager(BaseTest):
         alarm = self.manager.change_of_status(alarm, 0, CANCELED, event)
 
         expected_status = {
-            'a': 'ut-connector.ut-connector0',
+            'a': DEFAULT_FILTER_AUTHOR,
             '_t': 'statusinc',
             'm': 'UT message',
             't': 0,
@@ -479,7 +480,7 @@ class TestManager(BaseTest):
         alarm = self.manager.get_current_alarm(alarm_id)
 
         expected_state = {
-            'a': 'test.test0',
+            'a': DEFAULT_FILTER_AUTHOR,
             '_t': 'stateinc',
             'm': 'test message',
             't': 0,
@@ -538,7 +539,7 @@ class TestManager(BaseTest):
 
         expected_state = {
             'a': 'test.test0',
-            '_t': 'stateinc',
+            '_t': DEFAULT_FILTER_AUTHOR,
             'm': 'test message',
             't': 0,
             'val': 1,
@@ -659,7 +660,7 @@ class TestManager(BaseTest):
         alarm = self.manager.get_current_alarm(alarm_id)
 
         expected_status = {
-            'a': 'test.test0',
+            'a': DEFAULT_FILTER_AUTHOR,
             '_t': 'statusinc',
             'm': 'test message',
             't': 0,
@@ -718,7 +719,7 @@ class TestManager(BaseTest):
         alarm = self.manager.get_current_alarm(alarm_id)
 
         expected_status = {
-            'a': 'test.test0',
+            'a': DEFAULT_FILTER_AUTHOR,
             '_t': 'statusinc',
             'm': 'test message',
             't': 0,
--- sources/canopsis/test/alerts/test_tasks.py
+++ sources/canopsis/test/alerts/test_tasks.py
@@ -19,8 +19,7 @@
 # along with Canopsis.  If not, see <http://www.gnu.org/licenses/>.
 # ---------------------------------
 
-from unittest import main
-
+from canopsis.alerts import DEFAULT_FILTER_AUTHOR
 from canopsis.alerts.enums import AlarmField, States
 from canopsis.alerts.status import get_previous_step, CANCELED, is_keeped_state
 from canopsis.task.core import get_task
@@ -326,7 +325,7 @@ class TestTasks(BaseTest):
 
         self.assertTrue(alarm[AlarmField.state.value] is not None)
         self.assertEqual(alarm[AlarmField.state.value]['t'], 0)
-        self.assertEqual(alarm[AlarmField.state.value]['a'], 'test.test0')
+        self.assertEqual(alarm[AlarmField.state.value]['a'], DEFAULT_FILTER_AUTHOR)
         self.assertEqual(alarm[AlarmField.state.value]['m'], 'test message')
         self.assertEqual(alarm[AlarmField.state.value]['val'], state)
         self.assertTrue(
@@ -368,7 +367,7 @@ class TestTasks(BaseTest):
 
         self.assertTrue(alarm[AlarmField.status.value] is not None)
         self.assertEqual(alarm[AlarmField.status.value]['t'], 0)
-        self.assertEqual(alarm[AlarmField.status.value]['a'], 'test.test0')
+        self.assertEqual(alarm[AlarmField.status.value]['a'], DEFAULT_FILTER_AUTHOR)
         self.assertEqual(alarm[AlarmField.status.value]['m'], 'test message')
         self.assertEqual(alarm[AlarmField.status.value]['val'], statusval)
         self.assertTrue(
