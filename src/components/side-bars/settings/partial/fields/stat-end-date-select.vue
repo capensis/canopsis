3d8b4e602469cf95118f78d333d4915e73e005fa
VLaroye <vlaroye@cepensis.fr>
2018-10-16 17:33:58 +0200

    refactor(stats-end-date-setting): Added watcher to change the date when duration change to Month

--- src/components/side-bars/settings/partial/fields/stat-end-date-select.vue
+++ src/components/side-bars/settings/partial/fields/stat-end-date-select.vue
@@ -40,11 +40,26 @@ export default {
       return this.duration.slice(-1);
     },
   },
+  watch: {
+    // Each time the duration change on settings, we need to check if the duration unit is 'm' (for 'Month')
+    // If it's 'm', we need to put the date on the 1st day of the month, at 00:00 (UTC)
+    durationUnit(value, oldValue) {
+      if (value !== oldValue && value === 'm') {
+        const date = moment.tz(this.value * 1000, moment.tz.guess()).startOf('month');
+        this.$emit('input', date.add(date.utcOffset(), 'm').unix());
+      }
+    },
+  },
   methods: {
     updateValue(value) {
+      /**
+       * If the duration is 'm' (for 'Month'), we need to put the date on the first day of the month,
+       * at 00:00 (UTC)
+       */
       if (this.durationUnit === 'm') {
+        // Get the value's date object, and put it on the first day of the month, at 00:00 (local date)
         const date = moment.tz(value, moment.tz.guess()).startOf('month');
-
+        // Add the difference between the local date, and the UTC one.
         this.$emit('input', date.add(date.utcOffset(), 'm').unix());
       } else {
         this.$emit('input', moment(value).unix());
--- src/components/side-bars/settings/widgets/stats-histogram.vue
+++ src/components/side-bars/settings/widgets/stats-histogram.vue
@@ -62,8 +62,6 @@ export default {
       settings: {
         rowId,
         widget: cloneDeep(widget),
-        widget_preferences: {
-        },
       },
     };
   },
