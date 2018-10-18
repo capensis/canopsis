f6eea73a20490dbed6e91b227e7fffc2484dfec9
clara <arthur.dewarumez@gmail.com>
2018-10-18 22:18:25 +0200

    wip(docker): wip

--- docker/packaging/package-debian-9.sh
+++ docker/packaging/package-debian-9.sh
@@ -1,14 +1,29 @@
 #!/bin/bash
-set -e
-set -o pipefail
-set -u
 
 arch="amd64"
+
+apt-get -y --no-install-recommends install virtualenv python-pip
+virtualenv --system-site-packages ${CPS_HOME}/venv-ansible
+source ${CPS_HOME}/venv-ansible/bin/activate
 
 deb_version="${CANOPSIS_PACKAGE_TAG}"
 deb_release="${CANOPSIS_PACKAGE_REL}"
 deb_name="canopsis-core"
 deb_path="/tmp/${deb_name}-${CANOPSIS_PACKAGE_TAG}"
+
+
+source /etc/os-release
+repver="${ID}-${VERSION_ID}"
+
+source /etc/os-release
+
+pyopenssl="pyOpenSSL"
+if [ "${VERSION_ID}" = "8" ]&&[ "${ID}" = "debian" ]; then
+    pyopenssl="pyOpenSSL==17.5.0"
+fi
+
+pip install -U setuptools pip wheel jmespath "pymongo==3.5.0" "influxdb==5.1.0" "${pyopenssl}" "ansible==2.4.4"
+
 
 mkdir ${deb_path}/DEBIAN -p
 
@@ -16,9 +31,9 @@ cat > ${deb_path}/DEBIAN/control << EOF
 Package: ${deb_name}
 Architecture: ${arch}
 Maintainer: Capensis
-Depends: base-files, bash, ca-certificates, libsasl2-2, libxml2, libxslt1.1, lsb-base, libffi6, libgmp10, libgnutls30, libgnutlsxx28, libgnutls-openssl27, libhogweed4, libicu57, libidn11, libnettle6, libp11-kit0, libpsl5, libssl1.1, libtasn1-6, libxmlsec1, libxmlsec1-openssl, libldap-2.4-2, python, python2.7, rsync, sudo, snmp, smitools, sudo
+Depends: base-files, bash, ca-certificates, libsasl2-2, libxml2, libxslt1.1, lsb-base, libffi6, libgmp10, libgnutls30, libgnutlsxx28, libgnutls-openssl27, libhogweed4, libicu57, libidn11, libnettle6, libp11-kit0, libpsl5, libssl1.1, libtasn1-6, libxmlsec1, libxmlsec1-openssl, libldap-2.4-2, python, python2.7, sudo, smitools
 Version: ${deb_version}-${deb_release}
-Description: Canopsis with CAT package.
+Description: Canopsis core
 EOF
 
 cat > ${deb_path}/DEBIAN/preinst << EOF
