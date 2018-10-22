4b358b545d720e65a1a11c43afac81e5ecea281f
VLaroye <vlaroye@cepensis.fr>
2018-10-22 17:58:55 +0200

    feature(admin-views): Base for admin view

---
+++ src/views/admin.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+  div
+    v-tabs(
+    v-model="activeTab",
+    centered,
+    dark,
+    slider-color="green"
+    )
+      v-tab Rights
+      v-tab Users
+      v-tab Roles
+      v-tab Parameters
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      activeTab: 0,
+    };
+  },
+};
+</script>
+
