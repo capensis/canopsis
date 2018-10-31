ef2c8338e5a5280eca911162206ea7552bb041ef
VLaroye <vlaroye@cepensis.fr>
2018-10-31 11:01:19 +0100

    feature(theme): Code lint fix

--- src/components/modals/alarm/create-ack-event.vue
+++ src/components/modals/alarm/create-ack-event.vue
@@ -1,47 +1,47 @@
 <template lang="pug">
-v-card
-  v-card-title.primary.white--text
-    v-layout(justify-space-between, align-center)
-      span.headline {{ $t('modals.createAckEvent.title') }}
-  v-card-text
-    v-container
-      v-layout(row align-center)
-        v-flex.text-xs-center
-          alarm-general-table(:items="items")
-      v-layout(row)
-        v-divider.my-3
-      v-layout(row)
-        v-text-field(
-        :label="$t('modals.createAckEvent.fields.ticket')",
-        :error-messages="errors.collect('ticket')",
-        v-model="form.ticket",
-        v-validate="rules",
-        data-vv-name="ticket"
-        )
-      v-layout(row)
-        v-textarea(
-        :label="$t('modals.createAckEvent.fields.output')",
-        :error-messages="errors.collect('output')",
-        v-model="form.output",
-        v-validate="rules",
-        data-vv-name="output",
-        )
-      v-layout(row)
-        v-tooltip(top)
-          v-checkbox(
-          slot="activator",
-          v-model="ack_resources",
-          :label="$t('modals.createAckEvent.fields.ackResources')"
+  v-card
+    v-card-title.primary.white--text
+      v-layout(justify-space-between, align-center)
+        span.headline {{ $t('modals.createAckEvent.title') }}
+    v-card-text
+      v-container
+        v-layout(row align-center)
+          v-flex.text-xs-center
+            alarm-general-table(:items="items")
+        v-layout(row)
+          v-divider.my-3
+        v-layout(row)
+          v-text-field(
+          :label="$t('modals.createAckEvent.fields.ticket')",
+          :error-messages="errors.collect('ticket')",
+          v-model="form.ticket",
+          v-validate="rules",
+          data-vv-name="ticket"
           )
-            span(slot-name="label") {{  }}
-          span {{ $t('modals.createAckEvent.tooltips.ackResources') }}
-  v-divider
-  v-layout.py-1(justify-end)
-    v-btn(@click="hideModal", depressed, flat) {{ $t('common.cancel') }}
-    v-btn.primary(@click.prevent="submit", :disabled="errors.any()") {{ $t('common.actions.acknowledge') }}
-    v-btn.warning(
-    @click.prevent="submitWithDeclare",
-    ) {{ $t('common.actions.acknowledgeAndReport') }}
+        v-layout(row)
+          v-textarea(
+          :label="$t('modals.createAckEvent.fields.output')",
+          :error-messages="errors.collect('output')",
+          v-model="form.output",
+          v-validate="rules",
+          data-vv-name="output",
+          )
+        v-layout(row)
+          v-tooltip(top)
+            v-checkbox(
+            slot="activator",
+            v-model="ack_resources",
+            :label="$t('modals.createAckEvent.fields.ackResources')"
+            )
+              span(slot-name="label") {{  }}
+            span {{ $t('modals.createAckEvent.tooltips.ackResources') }}
+    v-divider
+    v-layout.py-1(justify-end)
+      v-btn(@click="hideModal", depressed, flat) {{ $t('common.cancel') }}
+      v-btn.primary(@click.prevent="submit", :disabled="errors.any()") {{ $t('common.actions.acknowledge') }}
+      v-btn.warning(
+      @click.prevent="submitWithDeclare",
+      ) {{ $t('common.actions.acknowledgeAndReport') }}
 </template>
 
 <script>
