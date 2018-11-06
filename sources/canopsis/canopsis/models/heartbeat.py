3f4c57f169b51044e0dded2bc3dedce28749e35b
Clara Dewarumez <cdewarumez@capensis.fr>
2018-11-06 10:55:24 +0100

    feat(heartbeat): update the validation method of the heartbeat. Change the field name

--- sources/canopsis/canopsis/models/heartbeat.py
+++ sources/canopsis/canopsis/models/heartbeat.py
@@ -2,15 +2,16 @@ from __future__ import unicode_literals
 
 import re
 
+
 class HeartBeat:
 
     MAX_DUR_REGEXP = "[0-9]*(s|m|h)"
-    MAPPING_KEY = "mapping"
+    MAPPINGS_KEY = "mappings"
     MAX_DUR_KEY = "maxduration"
 
-    def __init__(self, mapping, maxDuration):
-        self.mapping = mapping
-        self.maxDuration = maxDuration
+    def __init__(self, mappings, maxduration):
+        self.mappings = mappings
+        self.max_duration = maxduration
 
     @classmethod
     def isValid(cls, heartBeat):
@@ -23,24 +24,25 @@ class HeartBeat:
         `Mappings` is a list of item.
         An `item` is an ojects with at least one key. The key and the
         associated value are both string
-        `maxDuration` is a string that match the follow pattern: [0-9]*(s|m|h).
+        `maxduration` is a string that match the follow pattern: [0-9]*(s|m|h).
         """
         it = 0
-        mapping = heartBeat[cls.MAPPING_KEY]
-        for key in mapping:
-            if not isinstance(key, basestring):
-                return False, "{} must be a string.".format(key)
+        mappings = heartBeat[cls.MAPPINGS_KEY]
+        for mapping in mappings:
+            for key in mapping:
+                if not isinstance(key, basestring):
+                    return False, "{} must be a string.".format(key)
 
-            if not isinstance(mapping[key], basestring):
-                return False, "The value associated to {0} of the element"\
-                    " at index {1} must be a string.".format(key, it)
+                if not isinstance(mapping[key], basestring):
+                    return False, "The value of `{0}` of the mapping object"\
+                        " at index {1} must be a string.".format(key, it)
 
-            it += 1
+                it += 1
 
-        if re.match(cls.MAX_DUR_REGEXP, heartBeat[cls.MAX_DUR_KEY])is not None:
+        if re.match(cls.MAX_DUR_REGEXP, heartBeat[cls.MAX_DUR_KEY]) is not None:
             return True, ""
 
-        return False, "The maxduration fields does not match the" \
+        return False, "The maxDuration fields does not match the" \
             " regular expression {}.".format(cls.MAX_DUR_KEY)
 
     def to_dict(self):
@@ -48,6 +50,5 @@ class HeartBeat:
         Return the representation of the current instance as a dict.
 
         """
-
-        return {self.MAPPING_KEY: self.mapping,
-                self.MAX_DUR_KEY: self.maxDuration}
+        return {self.MAPPINGS_KEY: self.mappings,
+                self.MAX_DUR_KEY: self.max_duration}
