d5050fb025f375cb00459df669bf241db83c252f
Passiflorae <clara.dewarumez@gmail.com>
2018-11-30 23:12:15 +0100

    feat(build): clean the dockerfile.prov.template file
    feat(build): remove prov image
    
    feat(build): remove prov image

--- docker/files/entrypoint.sh
+++ docker/files/entrypoint.sh
@@ -8,9 +8,23 @@ if [ "x$1" == "xshell" ]; then
   exec /bin/bash
 else
     sudo /opt/canopsis/bin/env2cfg
-    if [ "${CPS_WEBSERVER}" = "1" ]; then
-        /opt/canopsis/bin/webserver --access-logfile /opt/canopsis/var/log/webserver-access.log -k gevent -w 1 -b 0.0.0.0:8082
-    else
-        /opt/canopsis/bin/engine-launcher -e $ENGINE_MODULE -n $ENGINE_NAME -w 1 -l ${CPS_LOGGING_LEVEL:-info}
-    fi
+	echo $CPS_COMPONENT
+    case $CPS_COMPONENT in
+        webserver )
+			echo "running webserver"
+            /opt/canopsis/bin/webserver --access-logfile /opt/canopsis/var/log/webserver-access.log -k gevent -w 1 -b 0.0.0.0:8082
+            ;;
+        engine )
+			echo "running engine $ENGINE_NAME"
+            /opt/canopsis/bin/engine-launcher -e $ENGINE_MODULE -n $ENGINE_NAME -w 1 -l ${CPS_LOGGING_LEVEL:-info}
+            ;;
+        provisionning )
+			echo "running provisionning"
+            /opt/canopsis/bin/canopsinit
+            ;;
+        * )
+            echo "unknow CPS_COMPONENT $CPS_COMPONENT"
+            exit 5
+    esac
+	exit $?
 fi
