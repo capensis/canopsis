a7b8799ceca08051bfe258e6166db7722efb3fe6
Vladimir Erokhov <Vladimir.Erokhov@noveogroup.com>
2019-01-18 16:05:39 +0700

    feature(missing-context-explorer-rights): removed testing code

--- sources/webcore/src/canopsis-next/src/helpers/right.js
+++ sources/webcore/src/canopsis-next/src/helpers/right.js
@@ -3,7 +3,7 @@ import get from 'lodash/get';
 export function checkUserAccess(user, rightId, rightMask) {
   const checksum = get(user, ['rights', rightId, 'checksum'], 0);
 
-  return true;
+  return (checksum & rightMask) === rightMask;
 }
 
 export default {
