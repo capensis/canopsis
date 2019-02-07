3d5917220e7ff83c18beedf064ab2771319d7c82
remy <rfremont@capensis.fr>
2019-02-07 16:45:34 +0100

    Add locale C for pip install cmd.

--- docker/build/pip-deps.sh
+++ docker/build/pip-deps.sh
@@ -7,7 +7,7 @@ repver="${ID}-${VERSION_ID}"
 
 source ${CPS_HOME}/bin/activate
 
-pip install -r /sources/canopsis/requirements.txt -b /tmp/pipbuild
+LC_ALL=C pip install -r /sources/canopsis/requirements.txt -b /tmp/pipbuild
 
 rm -rf /tmp/pipbuild
 rm -rf ~/.cache
