77a0d65562613fee21a0bb47fa0adf415a93b62c
Vladimir Erokhov <Vladimir.Erokhov@noveogroup.com>
2019-03-06 01:44:44 +0700

    refactor(entities-modules): Bugfix

--- sources/webcore/src/canopsis-next/src/helpers/store/index.js
+++ sources/webcore/src/canopsis-next/src/helpers/store/index.js
@@ -1,4 +1,4 @@
 export { default as mapActionsWith, createMapActionsWith } from './map-actions-with';
 export { default as mapActionsWithPopup, createMapActionsWithPopup } from './map-actions-with-popup';
 
-export { default as prepareEntitiesToDelete } from './map-actions-with-popup';
+export { default as prepareEntitiesToDelete } from './prepare-entities-to-delete';
