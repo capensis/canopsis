022421898c8f58b0acb6bed8ce59d84c1b7d8f08
Vladimir Erokhov <Vladimir.Erokhov@noveogroup.com>
2019-03-06 15:40:11 +0700

    refactor(entities-modules): Put correct relations

--- sources/webcore/src/canopsis-next/src/helpers/store/index.js
+++ sources/webcore/src/canopsis-next/src/helpers/store/index.js
@@ -1,4 +1,5 @@
 export { default as mapActionsWith, createMapActionsWith } from './map-actions-with';
 export { default as mapActionsWithPopup, createMapActionsWithPopup } from './map-actions-with-popup';
 
-export { default as prepareEntitiesToDelete } from './prepare-entities-to-delete';
+export { prepareEntitiesToDelete } from './prepare-entities-to-delete';
+export { cloneSchemaWithEmbedded } from './prepare-entities-to-delete';
