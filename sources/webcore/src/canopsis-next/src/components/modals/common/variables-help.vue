73f2bf96a426ae1ab32ad9e3e85a4c2650c4d5db
Vladimir Erokhov <Vladimir.Erokhov@noveogroup.com>
2019-03-11 14:21:56 +0700

    feature(ui-add-variable-values-in-variable-helpers): FInished issue

--- sources/webcore/src/canopsis-next/src/components/modals/common/variables-help.vue
+++ sources/webcore/src/canopsis-next/src/components/modals/common/variables-help.vue
@@ -10,7 +10,11 @@
       )
         template(slot="prepend", slot-scope="props", v-if="props.item.isArray")
           div.caption.font-italic (Array)
-        template(slot="append", slot-scope="props", v-if="!props.item.children")
+        template(slot="label", slot-scope="props")
+          div {{ props.item.name }}
+            span.pl-1(v-if="props.leaf") :
+              ellipsis.pl-1.d-inline-block.grey--text.darken-2--text.body-1(:text="String(props.item.value)")
+        template(slot="append", slot-scope="props", v-if="props.leaf")
           v-tooltip(left)
             v-btn(@click="copyPathToClipBoard(props.item.path)", slot="activator", small, icon)
               v-icon file_copy
@@ -20,12 +24,14 @@
 <script>
 import { MODALS } from '@/constants';
 
-import modalInnerItemsMixin from '@/mixins/modal/inner-items';
+import modalInnerMixin from '@/mixins/modal/inner';
 import popupMixin from '@/mixins/popup';
+import Ellipsis from '@/components/tables/ellipsis.vue';
 
 export default {
   name: MODALS.variablesHelp,
-  mixins: [modalInnerItemsMixin, popupMixin],
+  components: { Ellipsis },
+  mixins: [modalInnerMixin, popupMixin],
   methods: {
     async copyPathToClipBoard(itemPath) {
       try {
