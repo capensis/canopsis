a5a9b9fe88b92afca56bf947d76b313e726d95f4
Vladimir Erokhov <Vladimir.Erokhov@noveogroup.com>
2019-03-26 10:29:02 +0700

    feature(rights-on-sidebar-and-topbar): Finished issue

--- sources/webcore/src/canopsis-next/src/mixins/layout/navigation/group-menu.js
+++ sources/webcore/src/canopsis-next/src/mixins/layout/navigation/group-menu.js
@@ -22,6 +22,18 @@ export default {
     };
   },
   computed: {
+    availableGroups() {
+      return this.groups.reduce((acc, group) => {
+        const views = group.views.filter(view => this.checkReadAccess(view._id));
+
+        if (views.length) {
+          acc.push({ ...group, views });
+        }
+
+        return acc;
+      }, []);
+    },
+
     getViewLink() {
       return (view = {}) => {
         const link = {
