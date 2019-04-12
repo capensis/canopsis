141e14e42962a857d938ff9ad754368f4c2cd4ab
VLaroye <vlaroye@cepensis.fr>
2019-04-12 15:18:49 +0200

    bugfix(links): Minor refacto

--- sources/webcore/src/canopsis-next/src/components/other/alarm/columns-formatting/alarm-column-value-links.vue
+++ sources/webcore/src/canopsis-next/src/components/other/alarm/columns-formatting/alarm-column-value-links.vue
@@ -1,18 +1,17 @@
 <template lang="pug">
   v-menu(@click.native.stop, :disabled="isDisabled")
     v-btn(slot="activator", :disabled="isDisabled", depressed, small) {{ $t('common.links') }}
-    v-list(dark)
+    v-list(dark, dense)
       v-list-tile-content(
       v-for="(category, key) in linkList",
       :key="key"
       )
-        template(v-if="category.length")
-          v-list-tile-title.px-2.font-weight-bold.category {{ key }}
-          v-list-tile(
-          v-for="(link, index) in category",
-          :key="index"
-          )
-            alarm-column-value-link(:link="link")
+        v-list-tile-title.px-2.font-weight-bold.category {{ key }}
+        v-list-tile(
+        v-for="(link, index) in category",
+        :key="index"
+        )
+          alarm-column-value-link(:link="link")
 </template>
 
 <script>
@@ -37,7 +36,9 @@ export default {
   computed: {
     linkList() {
       return Object.entries(this.links).reduce((acc, [category, categoryLinks]) => {
-        acc[category] = this.harmonizeLinks(categoryLinks, category);
+        if (categoryLinks.length) {
+          acc[category] = this.harmonizeLinks(categoryLinks, category);
+        }
 
         return acc;
       }, {});
