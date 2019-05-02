4ad790be885bd639888b22f40dbbaf596e2833fd
VLaroye <vlaroye@cepensis.fr>
2019-05-02 11:42:07 +0200

    fix(ui-language): Fix redirection messages

--- sources/webcore/src/canopsis-next/src/i18n/messages/en.js
+++ sources/webcore/src/canopsis-next/src/i18n/messages/en.js
@@ -1097,7 +1097,6 @@ export default {
   home: {
     popups: {
       info: {
-        noDefaultViewSelected: 'No default view selected. Redirecting to role default view.',
         noAccessToDefaultView: 'Access to default view forbidden. Redirecting to role default view.',
         notSelectedRoleDefaultView: 'No role default view selected.',
         noAccessToRoleDefaultView: 'Access to role default view forbidden.',
