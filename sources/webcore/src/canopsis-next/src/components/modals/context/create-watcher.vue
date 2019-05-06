9f1522314f6203b90a580fdf65df759b384d635d
VLaroye <vlaroye@cepensis.fr>
2019-05-06 17:21:48 +0200

    feature(ui-watcherng): Style improvments + Create-watcher modal refacto

--- sources/webcore/src/canopsis-next/src/components/modals/context/create-watcher.vue
+++ sources/webcore/src/canopsis-next/src/components/modals/context/create-watcher.vue
@@ -7,11 +7,33 @@
       v-tab(
       v-for="tab in tabs",
       :key="tab.name",
-      @click.prevent="currentComponent = tab.component",
       ) {{ tab.name }}
       v-tab-item
-        keep-alive
-        create-form(v-model="form")
+        v-form
+          v-layout(wrap, justify-center)
+            v-flex(xs11)
+              v-text-field(
+              :label="$t('modals.createWatcher.displayName')",
+              v-model="form.name",
+              :error-messages="errors.collect('name')",
+              data-vv-name="name",
+              v-validate="'required'",
+              )
+          v-layout(wrap, justify-center)
+            v-flex(xs11)
+              template(v-if="stack === $constants.CANOPSIS_STACK.go")
+                v-textarea(
+                label="Output template",
+                v-model="form.output_template",
+                :error-messages="errors.collect('output_template')",
+                data-vv-name="output_template",
+                v-validate="'required'",
+                )
+      v-tab-item
+        v-card
+          v-card-text
+            patterns-list(v-if="stack === $constants.CANOPSIS_STACK.go", v-model="form.entities")
+            filter-editor(v-else, v-model="form.mfilter", required)
       v-tab-item
         manage-infos(v-model="form.infos")
     v-divider
@@ -31,7 +53,9 @@ import modalInnerMixin from '@/mixins/modal/inner';
 import entitiesContextEntityMixin from '@/mixins/entities/context-entity';
 import entitiesInfoMixin from '@/mixins/entities/info';
 
-import CreateForm from './partial/create-watcher-form.vue';
+import FilterEditor from '@/components/other/filter/editor/filter-editor.vue';
+import PatternsList from '@/components/other/shared/patterns-list/patterns-list.vue';
+
 import ManageInfos from './partial/manage-infos.vue';
 
 export default {
@@ -40,7 +64,8 @@ export default {
     validator: 'new',
   },
   components: {
-    CreateForm,
+    FilterEditor,
+    PatternsList,
     ManageInfos,
   },
   mixins: [modalInnerMixin, entitiesContextEntityMixin, entitiesInfoMixin],
@@ -64,11 +89,16 @@ export default {
     return {
       form,
       submitting: false,
-      tabs: [
-        { component: 'CreateForm', name: this.$t('modals.createEntity.fields.form') },
-        { component: 'ManageInfos', name: this.$t('modals.createEntity.fields.manageInfos') },
-      ],
     };
+  },
+  computed: {
+    tabs() {
+      return [
+        { name: this.$t('modals.createEntity.fields.form') },
+        { name: this.stack === CANOPSIS_STACK.go ? this.$t('eventFilter.pattern') : this.$t('common.filter') },
+        { name: this.$t('modals.createEntity.fields.manageInfos') },
+      ];
+    },
   },
   methods: {
     async submit() {
@@ -114,3 +144,7 @@ export default {
   },
 };
 </script>
+
+<style>
+
+</style>
