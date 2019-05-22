5ba3d3f3d3437705e6439c01eecdcf379923f607
Christophe Vachery <cvachery@capensis.fr>
2019-05-22 09:24:49 +0200

    Fix options concatenation

--- docker/files/entrypoint-prov.sh
+++ docker/files/entrypoint-prov.sh
@@ -10,7 +10,7 @@ else
     sudo /opt/canopsis/bin/env2cfg
     sudo /entrypoint-prov-sync.sh
     options=""
-    [[ "$CPS_STACK" ]] && options="$options --canopsis-stack $CPS_STACK --canopsis-edition $CPS_EDITION"
-    [[ "$CPS_EDITION" ]] && options="$options --canopsis-stack $CPS_STACK"
+    [[ "$CPS_STACK" ]] && options="$options --canopsis-stack $CPS_STACK"
+    [[ "$CPS_EDITION" ]] && options="$options --canopsis-edition $CPS_EDITION"
     /opt/canopsis/bin/canopsinit $options
 fi
