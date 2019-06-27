f80e873c1865f00d5ee4aadaf17f3b1c85066e83
DLartsev <dmitry.lartsev@noveogroup.com>
2019-06-27 13:24:56 +0300

    feature(links-on-weather-item-template): Adds handlebars helpers links

--- sources/webcore/src/canopsis-next/src/helpers/handlebars.js
+++ sources/webcore/src/canopsis-next/src/helpers/handlebars.js
@@ -2,6 +2,18 @@ import Handlebars from 'handlebars';
 
 import dateFilter from '@/filters/date';
 
+const attributes = ({ hash }) => {
+  const attrs = [];
+  Object.keys(hash).forEach((prop) => {
+    attrs.push(`${
+      Handlebars.escapeExpression(prop)
+    }="${
+      Handlebars.escapeExpression(hash[prop])
+    }"`);
+  });
+  return attrs.join(' ');
+};
+
 export function compile(template, context) {
   const handleBarFunction = Handlebars.compile(template);
 
@@ -23,6 +35,10 @@ registerHelper('timestamp', (date) => {
 
   return '';
 });
+
+registerHelper('external-link', (text, href, options) => new Handlebars.SafeString(`<a href="${href}" ${attributes(options)}>${text}</a>`));
+
+registerHelper('internal-link', (text, href, options) => new Handlebars.SafeString(`<router-link to="${href}" ${attributes(options)}>${text}</router-link>`));
 
 export default {
   compile,
