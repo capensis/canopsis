ad1ebe0f60d483532d70f0c288691312b4fe64ff
Vladimir Erokhov <Vladimir.Erokhov@noveogroup.com>
2019-07-01 10:05:40 +0700

    feature(ui-add-login-and-app-info-parameters): Started doing issue

--- sources/webcore/src/canopsis-next/src/views/admin/parameters.vue
+++ sources/webcore/src/canopsis-next/src/views/admin/parameters.vue
@@ -1,15 +1,34 @@
 <template lang="pug">
   v-container
     h2.text-xs-center.my-3.display-1.font-weight-medium {{ $t('common.parameters') }}
-    v-list
-      v-list-tile
-        v-list-tile-title {{ $t('parameters.groupsNavigationType.title') }}
-        v-list-tile-content
-          v-select(
-          :items="groupsNavigationOptions",
-          :value="groupsNavigationType",
-          @change="setGroupsNavigationType"
-          )
+    v-card.ma-2
+      v-card-text
+        v-list
+          v-list-tile
+            v-list-tile-title {{ $t('parameters.groupsNavigationType.title') }}
+            v-list-tile-content
+              v-select(
+              :items="groupsNavigationOptions",
+              :value="groupsNavigationType",
+              @change="setGroupsNavigationType"
+              )
+    v-card.ma-2
+      v-card-text
+        v-form
+          v-list
+            v-list-tile
+              v-list-tile-title App title
+              v-list-tile-content
+                v-text-field(
+                )
+            v-list-tile
+              v-list-tile-title App title
+              v-list-tile-content
+                v-text-field(
+                )
+      v-divider
+      v-card-actions
+        v-btn.primary(type="submit") Submit
 </template>
 
 <script>
