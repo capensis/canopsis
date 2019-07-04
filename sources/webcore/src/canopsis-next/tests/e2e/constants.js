49afe2b3a9993ae5152d854bbceb9d845cd7f1c4
DLartsev <dmitry.lartsev@noveogroup.com>
2019-07-04 15:11:48 +0300

    test(administration-users-list-page): Adds constans file

---
+++ sources/webcore/src/canopsis-next/tests/e2e/constants.js
@@ -0,0 +1,40 @@
+module.exports = {
+
+  // ////////////////////
+  // Start login data //
+  // ////////////////////
+
+  login: {
+    username: 'root',
+    password: 'root',
+  },
+  // //////////////////
+  // End login data //
+  // //////////////////
+
+  // ////////////////////
+  // Start users data //
+  // ////////////////////
+
+  users: {
+    create: {
+      username: 'TestCreateUserName',
+      firstname: 'TestCreateFirstName',
+      lastname: 'TestCreateLastName',
+      email: 'test.create@example.com',
+      password: 'TestCreatePassword',
+    },
+    edit: {
+      username: 'TestEditUserName',
+      firstname: 'TestEditFirstName',
+      lastname: 'TestEditLastName',
+      email: 'test.Edit@example.com',
+      password: 'TestEditPassword',
+    },
+  },
+
+  // //////////////////
+  // End users data //
+  // //////////////////
+
+};
