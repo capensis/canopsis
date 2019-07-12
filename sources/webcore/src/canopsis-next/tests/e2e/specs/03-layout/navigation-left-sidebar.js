b12e9a5a39ccc919f12bd889df04e2c2a93ccd9c
DLartsev <dmitry.lartsev@noveogroup.com>
2019-07-12 15:22:34 +0300

    test(e2e-navigation-left-sidebar): Adds test create view

--- sources/webcore/src/canopsis-next/tests/e2e/specs/03-layout/navigation-left-sidebar.js
+++ sources/webcore/src/canopsis-next/tests/e2e/specs/03-layout/navigation-left-sidebar.js
@@ -1,5 +1,19 @@
 // http://nightwatchjs.org/guide#usage
 
+const { NAVIGATION: { LEFT_SIDEBAR } } = require('../../constants');
+
+const TEMPORARY_DATA = {};
+
+const onCreateTemporaryObject = ({ prefix, text, index }) => {
+  const i = typeof index === 'number' ? `-${index}` : '';
+  return {
+    name: `${prefix}-${text}-name${i}`,
+    title: `${prefix}-${text}-title${i}`,
+    description: `${prefix}-${text}-description${i}`,
+    tags: `${prefix}-${text}-tags${i}`,
+  };
+};
+
 module.exports = {
   async before(browser, done) {
     await browser.maximizeWindow()
@@ -19,11 +33,30 @@ module.exports = {
   },
 
   'Add view with some name': (browser) => {
+    const { text, create: { prefix } } = LEFT_SIDEBAR;
+
+    TEMPORARY_DATA[prefix] = onCreateTemporaryObject({ prefix, text });
+
+    const {
+      name, title, description, tags,
+    } = TEMPORARY_DATA[prefix];
+
     browser.page.layout.leftSideBar()
       .verifySettingsWrapperBefore()
       .clickSettingsViewButton()
       .verifyControlsWrapperBefore()
       .clickAddViewButton()
       .defaultPause();
+
+    browser.page.modals.view.create()
+      .verifyModalOpened()
+      .setViewName(name)
+      .setViewTitle(title)
+      .setViewDescription(description)
+      .clickViewEnabled()
+      .setViewGroupTags(tags)
+      .setViewGroupIds(tags)
+      .clickViewSubmitButton()
+      .verifyModalClosed();
   },
 };
