07910e4ac45b1bb4f7e1d1377dfde52561124953
DLartsev <dmitry.lartsev@noveogroup.com>
2019-07-16 21:03:28 +0300

    test(e2e-navigation-left-sidebar): Adds modal object create group

--- sources/webcore/src/canopsis-next/tests/e2e/page-objects/layout/groupsSideBar.js
+++ sources/webcore/src/canopsis-next/tests/e2e/page-objects/layout/groupsSideBar.js
@@ -10,6 +10,14 @@ const commands = {
     return this.waitForElementVisible('@groupsSideBarButton');
   },
 
+  clickPanelHeader(id) {
+    return this.customClick(this.el('@groupsSideBarHeader', id));
+  },
+
+  verifyPanelBody(id) {
+    return this.waitForElementVisible(this.el('@groupsSideBarBody', id));
+  },
+
   clickGroupsSideBarButton() {
     this.api.element('css selector', this.el('@groupsSideBarClosed'), ({ status }) => {
       if (status === 0) {
@@ -21,15 +29,18 @@ const commands = {
   },
 
   clickEditGroupButton(id) {
-    return this.customClick(this.el('@editGroupButton', id));
+    return this.waitForElementVisible(this.el('@editGroupButton', id))
+      .customClick(this.el('@editGroupButton', id));
   },
 
   clickEditViewButton(id) {
-    return this.customClick(this.el('@editViewButton', id));
+    return this.waitForElementVisible(this.el('@editViewButton', id))
+      .customClick(this.el('@editViewButton', id));
   },
 
   clickCopyViewButton(id) {
-    return this.customClick(this.el('@copyViewButton', id));
+    return this.waitForElementVisible(this.el('@copyViewButton', id))
+      .customClick(this.el('@copyViewButton', id));
   },
 
   browseGroupById(id) {
@@ -46,13 +57,14 @@ const groupsSideBar = sel('groupsSideBar');
 module.exports = {
   elements: {
     groupsSideBar,
-
+    groupsSideBarHeader: `${sel('groupsSideBar-header-group-%s')} .v-expansion-panel__header`,
+    groupsSideBarBody: `${sel('groupsSideBar-header-group-%s')} .v-expansion-panel__body`,
     groupsSideBarButton: sel('groupsSideBarButton'),
     groupsSideBarClosed: `${groupsSideBar}.v-navigation-drawer--close`,
     groupSideBarSelectorById: `.v-expansion-panel__header ${sel('groupsSideBar-group-%s')}`,
-    editGroupButton: `.v-expansion-panel__header ${sel('groupsSideBar-editGroupButton-group-%s')}`,
-    editViewButton: `.v-expansion-panel__header ${sel('groupsSideBar-editViewButton-group-%s')}`,
-    copyViewButton: `.v-expansion-panel__header ${sel('groupsSideBar-copyViewButton-group-%s')}`,
+    editGroupButton: `.v-expansion-panel__header .v-btn${sel('groupsSideBar-editGroupButton-group-%s')}`,
+    editViewButton: `.v-expansion-panel__body .v-btn${sel('groupsSideBar-editViewButton-group-%s')}`,
+    copyViewButton: `.v-expansion-panel__body .v-btn${sel('groupsSideBar-copyViewButton-group-%s')}`,
     viewSideBarSelectorById: '.v-expansion-panel__body a[href^="/view/%s"]',
   },
   commands: [commands],
--- sources/webcore/src/canopsis-next/tests/e2e/page-objects/modals/view/create.js
+++ sources/webcore/src/canopsis-next/tests/e2e/page-objects/modals/view/create.js
@@ -1,8 +1,33 @@
 // https://nightwatchjs.org/guide/#working-with-page-objects
 
 const { elementsWrapperCreator, modalCreator } = require('../../../helpers/page-object-creators');
+const { DEFAULT_PAUSE } = require('../../../config');
 
 const commands = {
+  clearViewName() {
+    return this.customClearValue('@viewFieldName');
+  },
+  clearViewTitle() {
+    return this.customClearValue('@viewFieldTitle');
+  },
+  clearViewDescription() {
+    return this.customClearValue('@viewFieldDescription');
+  },
+  clearViewGroupTags() {
+    const { viewFieldGroupTagsChipsRemove } = this.elements;
+
+    this.api.elements(
+      viewFieldGroupTagsChipsRemove.locateStrategy,
+      viewFieldGroupTagsChipsRemove.selector,
+      ({ value = [] }) => {
+        value.forEach(item => this.api.elementIdClick(item.ELEMENT).pause(DEFAULT_PAUSE));
+      },
+    );
+    return this;
+  },
+  clearViewGroupIds() {
+    return this.customClearValue('@viewFieldGroupIds');
+  },
   setViewName(value) {
     return this.customSetValue('@viewFieldName', value);
   },
@@ -26,6 +51,9 @@ const commands = {
   clickViewSubmitButton() {
     return this.customClick('@viewSubmitButton');
   },
+  clickViewDeleteButton() {
+    return this.customClick('@viewDeleteButton');
+  },
 };
 
 const modalSelector = sel('createViewModal');
@@ -38,8 +66,10 @@ module.exports = modalCreator(modalSelector, {
       viewFieldDescription: sel('viewFieldDescription'),
       viewFieldEnabled: `.v-input${sel('viewFieldEnabled')} .v-input--selection-controls__ripple`,
       viewFieldGroupTags: sel('viewFieldGroupTags'),
+      viewFieldGroupTagsChipsRemove: sel('.v-input.v-select--chips .v-select__selections .v-chip .v-chip__close'),
       viewFieldGroupIds: sel('viewFieldGroupIds'),
       viewSubmitButton: sel('viewSubmitButton'),
+      viewDeleteButton: sel('viewDeleteButton'),
     }),
   },
   commands: [commands],
