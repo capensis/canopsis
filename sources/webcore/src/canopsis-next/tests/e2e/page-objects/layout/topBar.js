9ffe52164d579b8c179ef47355e655018214a580
DLartsev <dmitry.lartsev@noveogroup.com>
2019-07-18 17:30:39 +0300

    test(e2e-navigation-top-bar-views): Adds page objects for topBar

--- sources/webcore/src/canopsis-next/tests/e2e/page-objects/layout/topBar.js
+++ sources/webcore/src/canopsis-next/tests/e2e/page-objects/layout/topBar.js
@@ -1,6 +1,33 @@
 // https://nightwatchjs.org/guide/#working-with-page-objects
 
+const el = require('../../helpers/el');
+
 const commands = {
+  el,
+
+  clickDropdownButton(tags) {
+    return this.customClick(this.el('@dropdownButton', tags));
+  },
+
+  verifyDropdownZone(tags) {
+    return this.waitForElementVisible(this.el('@dropdownZone', tags));
+  },
+
+  clickEditGroupButton(tags) {
+    return this.waitForElementVisible(this.el('@editGroupButton', tags))
+      .customClick(this.el('@editGroupButton', tags));
+  },
+
+  clickEditViewButton(title) {
+    return this.waitForElementVisible(this.el('@editViewButton', title))
+      .customClick(this.el('@editViewButton', title));
+  },
+
+  clickCopyViewButton(title) {
+    return this.waitForElementVisible(this.el('@copyViewButton', title))
+      .customClick(this.el('@copyViewButton', title));
+  },
+
   clickUserDropdown() {
     return this.customClick('@userTopBarDropdownButton');
   },
@@ -19,6 +46,11 @@ module.exports = {
     userTopBarDropdownButton: sel('userTopBarDropdownButton'),
     userProfileButton: sel('userProfileButton'),
     logoutButton: sel('logoutButton'),
+    dropdownButton: sel('dropButton-groupName-%s'),
+    dropdownZone: `.v-menu__content ${sel('dropZone-groupName-%s')}`,
+    editGroupButton: `${sel('dropButton-groupName-%s')} .v-btn${sel('editGroupButton')}`,
+    editViewButton: `.v-btn${sel('editViewButton-viewTitle-%s')}`,
+    copyViewButton: `.v-btn${sel('copyViewButton-viewTitle-%s')}`,
   },
   commands: [commands],
 };
