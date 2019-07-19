46329c15347ca5e76a6ec9d4d8e4e6c3f6e82630
DLartsev <dmitry.lartsev@noveogroup.com>
2019-07-19 17:25:34 +0300

    test(e2e-view-base-functions): Adds tests

--- sources/webcore/src/canopsis-next/tests/e2e/specs/04-view/base.js
+++ sources/webcore/src/canopsis-next/tests/e2e/specs/04-view/base.js
@@ -1,5 +1,16 @@
 // http://nightwatchjs.org/guide#usage
 
+const faker = require('faker');
+
+const TEMPORARY_DATA = {};
+
+const createTemporaryObject = () => ({
+  name: faker.lorem.word(),
+  title: faker.lorem.words(),
+  description: faker.lorem.sentences(),
+  tags: faker.lorem.slug(),
+});
+
 module.exports = {
   async before(browser, done) {
     await browser.maximizeWindow()
@@ -13,7 +24,7 @@ module.exports = {
       .end(done);
   },
 
-  'Open current user modal': (browser) => {
+  'Create test view': (browser) => {
     const topBar = browser.page.layout.topBar();
     const createUserModal = browser.page.modals.admin.createUser();
 
@@ -21,8 +32,37 @@ module.exports = {
       .clickUserProfileButton();
 
     createUserModal.verifyModalOpened()
-      .selectNavigationType(2)
+      .selectNavigationType(1)
       .clickSubmitButton()
+      .verifyModalClosed();
+  },
+
+  'Add view with some name from constants': (browser) => {
+    TEMPORARY_DATA.create = createTemporaryObject();
+
+    const {
+      name, title, description, tags,
+    } = TEMPORARY_DATA.create;
+
+    browser.page.layout.groupsSideBar()
+      .clickGroupsSideBarButton();
+
+    browser.page.layout.leftSideBar()
+      .verifySettingsWrapperBefore()
+      .clickSettingsViewButton()
+      .verifyControlsWrapperBefore()
+      .clickAddViewButton()
+      .defaultPause();
+
+    browser.page.modals.view.create()
+      .verifyModalOpened()
+      .setViewName(name)
+      .setViewTitle(title)
+      .setViewDescription(description)
+      .clickViewEnabled()
+      .setViewGroupTags(tags)
+      .setViewGroupIds(tags)
+      .clickViewSubmitButton()
       .verifyModalClosed();
   },
 };
