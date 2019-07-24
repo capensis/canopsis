1c91725371ef426da122f54d3a93291dd3b30012
DLartsev <dmitry.lartsev@noveogroup.com>
2019-07-24 20:41:14 +0300

    test(e2e-custom-commands-view-user): Adds createView command

---
+++ sources/webcore/src/canopsis-next/tests/e2e/custom-commands/completed/createView.js
@@ -0,0 +1,68 @@
+// http://nightwatchjs.org/guide#usage
+
+module.exports.command = function createView(
+  view = {
+    name: 'test-name',
+    title: 'test-title',
+    description: 'test-description',
+    tags: 'test-tags',
+  },
+  callback = result => result,
+) {
+  const {
+    name = 'test-name',
+    title = 'test-title',
+    description = 'test-description',
+    tags = 'test-tags',
+  } = view;
+
+  const viewPath = `${process.env.VUE_DEV_SERVER_URL}view/`;
+
+  const topBar = this.page.layout.topBar();
+  const createUser = this.page.modals.admin.createUser();
+  const groupsSideBar = this.page.layout.groupsSideBar();
+  const leftSideBar = this.page.layout.leftSideBar();
+  const modalViewCreate = this.page.modals.view.create();
+
+  this.url(viewPath);
+
+  topBar.clickUserDropdown()
+    .clickUserProfileButton();
+
+  createUser.verifyModalOpened()
+    .selectNavigationType(1)
+    .clickSubmitButton()
+    .verifyModalClosed();
+
+  groupsSideBar.clickGroupsSideBarButton();
+
+  leftSideBar.verifySettingsWrapperBefore()
+    .clickSettingsViewButton()
+    .verifyControlsWrapperBefore()
+    .clickAddViewButton()
+    .defaultPause();
+
+  modalViewCreate.verifyModalOpened()
+    .setViewName(name)
+    .setViewTitle(title)
+    .setViewDescription(description)
+    .clickViewEnabled()
+    .setViewGroupTags(tags)
+    .setViewGroupIds(tags);
+
+  this.waitForFirstXHR(
+    '/api/v2/views',
+    1000,
+    () => {
+      modalViewCreate.clickViewSubmitButton();
+    },
+    (xhr) => {
+      const userResponseData = JSON.parse(xhr.responseData);
+      callback({ userResponseData, view });
+    },
+  );
+
+  modalViewCreate.verifyModalClosed();
+
+  return this;
+};
