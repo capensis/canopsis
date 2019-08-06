9d775b8b26b5f2e3f325e235db44ebd4abacde87
DLartsev <dmitry.lartsev@noveogroup.com>
2019-08-06 14:45:41 +0300

    test(e2e-administration-rights-list-page): Add entities right and role

--- sources/webcore/src/canopsis-next/tests/e2e/helpers/entities.js
+++ sources/webcore/src/canopsis-next/tests/e2e/helpers/entities.js
@@ -16,3 +16,13 @@ module.exports.generateTemporaryUser = (prefix = '', suffix = uid()) => ({
   email: `${prefix}email-${suffix}@example.com`,
   password: `${prefix}password-${suffix}`,
 });
+
+module.exports.generateTemporaryRight = (prefix = '', suffix = uid()) => ({
+  ID: `${prefix}ID-${suffix}`,
+  description: `${prefix}description-${suffix}`,
+});
+
+module.exports.generateTemporaryRole = (prefix = '', suffix = uid()) => ({
+  name: `${prefix}name-${suffix}`,
+  description: `${prefix}description-${suffix}`,
+});
