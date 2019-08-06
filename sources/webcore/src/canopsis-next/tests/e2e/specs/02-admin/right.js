037f6d6e70ab0dc116fab3fd49f449e1da7f2a7e
DLartsev <dmitry.lartsev@noveogroup.com>
2019-08-06 14:51:31 +0300

    test(e2e-administration-rights-list-page): Add admin right test

---
+++ sources/webcore/src/canopsis-next/tests/e2e/specs/02-admin/right.js
@@ -0,0 +1,168 @@
+// http://nightwatchjs.org/guide#usage
+
+const qs = require('qs');
+const {
+  generateTemporaryUser,
+  generateTemporaryRight,
+  generateTemporaryRole,
+} = require('../../helpers/entities');
+
+const { API_ROUTES } = require('../../../../src/config');
+
+module.exports = {
+  async before(browser, done) {
+    browser.globals.temporary = {};
+
+    await browser.maximizeWindow()
+      .completed.loginAsAdmin();
+
+    done();
+  },
+
+  async after(browser, done) {
+    delete browser.globals.temporary;
+
+    await browser.completed.logout()
+      .end(done);
+  },
+
+  'Check tabs': (browser) => {
+    browser.page.admin.right()
+      .navigate();
+
+    browser.page.admin.right()
+      .clickTab('view')
+      .api.pause(1000);
+
+    browser.page.admin.right()
+      .clickTab('technical')
+      .api.pause(1000);
+
+    browser.page.admin.right()
+      .clickTab('business')
+      .api.pause(1000);
+
+    browser.refresh();
+  },
+
+  'Create new right': (browser) => {
+    const { temporary } = browser.globals;
+
+    temporary.right = generateTemporaryRight();
+
+    const {
+      ID,
+      description,
+    } = temporary.right;
+
+    const adminCreateRight = browser.page.modals.admin.createRight();
+
+    browser.page.admin.right()
+      .clickAddButton()
+      .clickCreateRight();
+
+    adminCreateRight.verifyModalOpened()
+      .setRightID(ID)
+      .setRightDescription(description);
+
+    browser.waitForFirstXHR(
+      API_ROUTES.action,
+      1000,
+      () => adminCreateRight.clickSubmitButton(),
+      ({ responseData, requestData }) => {
+        const requestParsedData = qs.parse(requestData);
+
+        return temporary.right = {
+          ...temporary.right,
+          ...JSON.parse(requestParsedData),
+          ...JSON.parse(responseData),
+        };
+      },
+    );
+    adminCreateRight.verifyModalClosed();
+  },
+
+  'Create new user role (put special rights)': (browser) => {
+    const { temporary } = browser.globals;
+
+    temporary.role = generateTemporaryRole();
+
+    const {
+      name,
+      description,
+    } = temporary.role;
+
+    const adminCreateRole = browser.page.modals.admin.createRole();
+
+    browser.page.admin.right()
+      .clickCreateRole();
+
+    adminCreateRole.verifyModalOpened()
+      .setRoleName(name)
+      .setRoleDescription(description);
+
+    browser.waitForFirstXHR(
+      API_ROUTES.role.create,
+      1000,
+      () => adminCreateRole.clickSubmitButton(),
+      ({ responseData, requestData }) => {
+        const requestParsedData = qs.parse(requestData);
+
+        return temporary.role = {
+          ...temporary.role,
+          ...JSON.parse(requestParsedData),
+          ...JSON.parse(responseData),
+        };
+      },
+    );
+    adminCreateRole.verifyModalClosed();
+  },
+
+  'Create user with this new role': (browser) => {
+    const { temporary } = browser.globals;
+
+    temporary.user = generateTemporaryUser('user');
+
+    const {
+      username,
+      firstname,
+      lastname,
+      email,
+      password,
+    } = temporary.user;
+
+    const adminCreateUser = browser.page.modals.admin.createUser();
+
+
+    browser.page.admin.right()
+      .clickCreateUser();
+
+    adminCreateUser.verifyModalOpened()
+      .setUsername(username)
+      .setFirstName(firstname)
+      .setLastName(lastname)
+      .setEmail(email)
+      .setPassword(password)
+      .selectLastRole()
+      .selectLanguage()
+      .selectNavigationType();
+
+    browser.waitForFirstXHR(
+      API_ROUTES.user.create,
+      1000,
+      () => adminCreateUser.clickSubmitButton(),
+      ({ responseData, requestData }) => {
+        const requestParsedData = qs.parse(requestData);
+
+        return temporary.user = {
+          ...temporary.user,
+          ...JSON.parse(requestParsedData.user),
+          ...JSON.parse(responseData),
+        };
+      },
+    );
+    adminCreateUser.verifyModalClosed();
+  },
+
+
+};
