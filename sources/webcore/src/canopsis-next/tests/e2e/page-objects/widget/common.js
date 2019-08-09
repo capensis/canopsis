8b7880ae152f5221e3c90c5896ad8cbbb8c3ecfe
DLartsev <dmitry.lartsev@noveogroup.com>
2019-08-09 19:51:59 +0300

    test(e2e-weather-widget): Add common page objects widgets

--- sources/webcore/src/canopsis-next/tests/e2e/page-objects/widget/common.js
+++ sources/webcore/src/canopsis-next/tests/e2e/page-objects/widget/common.js
@@ -5,23 +5,80 @@ const el = require('../../helpers/el');
 const commands = {
   el,
 
+  clickPeriodicRefresh() {
+    return this.customClick('@periodicRefresh')
+      .defaultPause();
+  },
+
+  getPeriodicRefreshSwitchStatus() {
+    let status = false;
+    this.getAttribute('@periodicRefreshSwitchInput', 'aria-checked', ({ value }) => {
+      status = value === 'true';
+    });
+    return status;
+  },
+
+  clickPeriodicRefreshSwitch() {
+    return this.customClick('@periodicRefreshSwitch');
+  },
+
+  clearPeriodicRefreshField() {
+    return this.customClearValue('@periodicRefreshField');
+  },
+
+  setPeriodicRefreshField(value) {
+    return this.customSetValue('@periodicRefreshField', value);
+  },
+
+  clickWidgetTitle() {
+    return this.customClick('@widgetTitle')
+      .defaultPause();
+  },
+
+  setWidgetTitleField(value) {
+    return this.customSetValue('@widgetTitleField', value);
+  },
+
+  clearWidgetTitleField() {
+    return this.customClearValue('@widgetTitleField');
+  },
+
+  clickCloseWidget() {
+    return this.customSetValue('@closeWidget');
+  },
+
   clickRowGridSize() {
-    return this.customClick('@rowGridSize');
+    return this.customClick('@rowGridSize')
+      .defaultPause();
+  },
+  clearRow() {
+    return this.customClearValue('@rowGridSizeCombobox');
   },
   setRow(value) {
     return this.customSetValue('@rowGridSizeCombobox', value)
       .customKeyup('@rowGridSizeCombobox', 'ENTER');
   },
   setSlider(slider, value) {
-    return this.customSetValue(this.el('@slider', slider), value);
+    return this.dragAndDrop(
+      this.el('@sliderThumb', slider),
+      this.el('@sliderTicks', slider, value),
+    );
   },
 };
 
 module.exports = {
   elements: {
+    periodicRefresh: sel('periodicRefresh'),
+    periodicRefreshSwitchInput: `input${sel('periodicRefreshSwitch')}`,
+    periodicRefreshSwitch: `.v-input${sel('periodicRefreshSwitch')} .v-input--selection-controls__ripple`,
+    periodicRefreshField: sel('periodicRefreshField'),
+    widgetTitle: sel('widgetTitle'),
+    widgetTitleField: sel('widgetTitleField'),
+    closeWidget: sel('closeWidget'),
     rowGridSize: sel('rowGridSize'),
     rowGridSizeCombobox: sel('rowGridSizeCombobox'),
-    slider: sel('slider-%s'),
+    sliderThumb: `div${sel('slider-%s')} .v-slider__thumb`,
+    sliderTicks: `div${sel('slider-%s')} .v-slider__ticks:nth-child(%s)`,
   },
   commands: [commands],
 };
