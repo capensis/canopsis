008f331ac650f499a58af7287577386013fd92e6
Vladimir Erokhov <Vladimir.Erokhov@noveogroup.com>
2019-08-30 14:42:08 +0700

    test(e2e-top-bar-language-setting): Finished issue

--- sources/webcore/src/canopsis-next/tests/e2e/specs/03-layout/top-bar.js
+++ sources/webcore/src/canopsis-next/tests/e2e/specs/03-layout/top-bar.js
@@ -1,5 +1,6 @@
 // http://nightwatchjs.org/guide#usage
 
+const { LANGUAGES_POSITIONS } = require('../../constants');
 const { generateTemporaryView } = require('../../helpers/entities');
 
 module.exports = {
@@ -27,24 +28,24 @@ module.exports = {
   },
 
   'Open current user modal': (browser) => {
-    const topBar = browser.page.layout.topBar();
-    const popup = browser.page.layout.popup();
-    const createUserModal = browser.page.modals.admin.createUser();
+    browser.page.layout.popup()
+      .clickOnEveryPopupsCloseIcons();
 
-    popup.clickOnEveryPopupsCloseIcons();
-
-    topBar.clickUserDropdown()
+    browser.page.layout.topBar()
+      .clickUserDropdown()
       .clickUserProfileButton();
 
-    createUserModal.verifyModalOpened();
+    browser.page.modals.admin.createUser()
+      .verifyModalOpened();
   },
 
-  'Select current user default view': (browser) => {
+  'Select current user default view and interface language': (browser) => {
     const createUserModal = browser.page.modals.admin.createUser();
     const selectViewModal = browser.page.modals.view.selectView();
     const { viewId, groupId } = browser.globals.defaultViewData;
 
-    createUserModal.clickSelectDefaultViewButton();
+    createUserModal.selectLanguage(LANGUAGES_POSITIONS.fr)
+      .clickSelectDefaultViewButton();
 
     selectViewModal.verifyModalOpened()
       .browseGroupById(groupId)
@@ -61,6 +62,39 @@ module.exports = {
     browser.url(process.env.VUE_DEV_SERVER_URL)
       .page.view()
       .verifyPageElementsBeforeById(viewId);
+  },
+
+  'Caching of user first interface language': (browser) => {
+    browser.page.layout.topBar()
+      .clickUserDropdown()
+      .getLogoutButtonText(({ value }) => {
+        browser.globals.logoutButtonText = value;
+      });
+  },
+
+  'Change user interface language': (browser) => {
+    browser.page.layout.topBar()
+      .clickUserProfileButton();
+
+    browser.page.modals.admin.createUser()
+      .verifyModalOpened()
+      .selectLanguage(LANGUAGES_POSITIONS.en)
+      .clickSubmitButton()
+      .verifyModalClosed();
+
+    browser.page.layout.popup()
+      .clickOnEveryPopupsCloseIcons();
+  },
+
+  'Check user second interface language': (browser) => {
+    const { logoutButtonText } = browser.globals;
+
+    browser.page.layout.topBar()
+      .clickUserDropdown()
+      .getLogoutButtonText(({ value }) => {
+        browser.assert.notEqual(value, logoutButtonText);
+      })
+      .clickUserDropdown();
   },
 
   'Delete test view': (browser) => {
