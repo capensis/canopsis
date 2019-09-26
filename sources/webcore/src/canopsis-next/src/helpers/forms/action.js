99af3ce6c4d0ba981401fbd10456c8eb8fefc0d1
Vincent Laroye <vlaroye@capensis.fr>
2019-09-26 14:56:51 +0200

    feature(ui-crud-actions): Move empty action creation to entities mixin

--- sources/webcore/src/canopsis-next/src/helpers/forms/action.js
+++ sources/webcore/src/canopsis-next/src/helpers/forms/action.js
@@ -2,8 +2,8 @@ import moment from 'moment';
 import { omit } from 'lodash';
 import { ACTION_TYPES, DURATION_UNITS, ACTION_AUTHOR } from '@/constants';
 
-import uuid from '@/helpers/uuid';
 import { unsetInSeveralWithConditions } from '@/helpers/immutable';
+import { generateAction } from '@/helpers/entities';
 import {
   pbehaviorToForm,
   pbehaviorToComments,
@@ -14,42 +14,14 @@ import {
 } from '@/helpers/forms/pbehavior';
 
 export function actionToForm(action = {}) {
-  const defaultHook = {
-    event_patterns: [],
-    alarm_patterns: [],
-    entity_patterns: [],
-    triggers: [],
-  };
+  const data = generateAction();
 
-  // Default 'snooze' action parameters
-  const snoozeParameters = {
-    message: '',
-    duration: {
-      duration: 1,
-      durationType: DURATION_UNITS.minute.value,
-    },
-  };
-
-  // Default 'pbehavior' action parameters
-  const pbehaviorParameters = {
-    general: {
-      name: '',
-      tstart: new Date(),
-      tstop: new Date(),
-      rrule: null,
-      reason: '',
-      type_: '',
-    },
-    comments: [],
-    exdate: [],
-  };
-
-  // Get basic action parameters
-  const generalParameters = {
-    _id: action._id || uuid('action'),
-    type: action.type || ACTION_TYPES.snooze,
-    hook: action.hook || defaultHook,
-  };
+  if (action) {
+    data.generalParameters = {
+      ...data.generalParameters,
+      ...action.generalParameters,
+    };
+  }
 
   // If action's type is "snooze", get snooze parameters
   if (action.type === ACTION_TYPES.snooze) {
@@ -69,34 +41,30 @@ export function actionToForm(action = {}) {
         durationType: foundUnit,
       };
 
-      snoozeParameters.duration = duration;
+      data.snoozeParameters.duration = duration;
     }
 
     if (action.parameters && action.parameters.message) {
-      snoozeParameters.message = action.parameters.message;
+      data.snoozeParameters.message = action.parameters.message;
     }
   }
 
   // If action's type is "pbehavior", get pbehavior parameters
   if (action.type === ACTION_TYPES.pbehavior) {
     if (action.parameters) {
-      pbehaviorParameters.general = omit(pbehaviorToForm(action.parameters), ['filter']);
+      data.pbehaviorParameters.general = omit(pbehaviorToForm(action.parameters), ['filter']);
 
       if (action.parameters.comments) {
-        pbehaviorParameters.comments = pbehaviorToComments(action.parameters.comments);
+        data.pbehaviorParameters.comments = pbehaviorToComments(action.parameters.comments);
       }
 
       if (action.parameters.exdate) {
-        pbehaviorParameters.exdate = pbehaviorToExdates(action.parameters.exdate);
+        data.pbehaviorParameters.exdate = pbehaviorToExdates(action.parameters.exdate);
       }
     }
   }
 
-  return {
-    generalParameters,
-    snoozeParameters,
-    pbehaviorParameters,
-  };
+  return data;
 }
 
 export function formToAction({ generalParameters = {}, pbehaviorParameters = {}, snoozeParameters = {} }) {
