b505de16b8d3c462ad3195baf5f3a07d3a38d32d
Vincent Laroye <vlaroye@capensis.fr>
2019-09-27 10:07:43 +0200

    feature(ui-crud-actions): Add heartbeat and action crud to constants to show them only when Go stack

--- sources/webcore/src/canopsis-next/src/constants.js
+++ sources/webcore/src/canopsis-next/src/constants.js
@@ -997,10 +997,14 @@ export const EXPLOITATION_PAGES_RULES = {
   eventFilter: { stack: CANOPSIS_STACK.go },
   webhooks: { stack: CANOPSIS_STACK.go, edition: CANOPSIS_EDITION.cat },
   snmpRule: { stack: CANOPSIS_STACK.go, edition: CANOPSIS_EDITION.cat },
+  heartbeat: { stack: CANOPSIS_STACK.go },
+  action: { stack: CANOPSIS_STACK.go },
 };
 
 export const USER_RIGHTS_TO_EXPLOITATION_PAGES_RULES = {
   [USERS_RIGHTS.technical.exploitation.eventFilter]: EXPLOITATION_PAGES_RULES.eventFilter,
   [USERS_RIGHTS.technical.exploitation.webhook]: EXPLOITATION_PAGES_RULES.webhooks,
   [USERS_RIGHTS.technical.exploitation.snmpRule]: EXPLOITATION_PAGES_RULES.snmpRule,
+  [USERS_RIGHTS.technical.exploitation.heartbeat]: EXPLOITATION_PAGES_RULES.heartbeat,
+  [USERS_RIGHTS.technical.exploitation.action]: EXPLOITATION_PAGES_RULES.action,
 };
