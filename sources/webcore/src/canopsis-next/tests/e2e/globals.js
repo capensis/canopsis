aeaac5fd5e96ccc77698cfee4353b632d5f8ad24
dmikhaylishin <Dmitriy.Mikhaylishin@noveogroup.com>
2019-10-07 13:09:45 +0700

    test(api-creation-example): remove nightwatch record

--- sources/webcore/src/canopsis-next/tests/e2e/globals.js
+++ sources/webcore/src/canopsis-next/tests/e2e/globals.js
@@ -1,16 +1,6 @@
 // http://nightwatchjs.org/guide#external-globals
 
-const nightWatchRecord = require('nightwatch-record');
-
 module.exports = {
   asyncHookTimeout: 20000,
   waitForConditionTimeout: 5000,
-
-  beforeEach(browser, done) {
-    nightWatchRecord.start(browser, done);
-  },
-
-  afterEach(browser, done) {
-    nightWatchRecord.stop(browser, done);
-  },
 };
