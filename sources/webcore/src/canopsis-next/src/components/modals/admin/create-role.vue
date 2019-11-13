ec0ea38dea586408dad8f0cc61dbc2fc9e057152
Vladimir Erokhov <Vladimir.Erokhov@noveogroup.com>
2019-11-13 13:17:32 +0700

    feature(move-forms-from-modals): Added modal-wrapper

--- sources/webcore/src/canopsis-next/src/components/modals/admin/create-role.vue
+++ sources/webcore/src/canopsis-next/src/components/modals/admin/create-role.vue
@@ -1,15 +1,12 @@
 <template lang="pug">
-  v-card(data-test="createRoleModal")
-    v-card-title.primary.white--text
-      v-layout(justify-space-between, align-center)
-        span.headline {{ title }}
-    v-card-text.py-0
+  modal-wrapper(data-test="createRoleModal")
+    template(slot="title")
+      span {{ title }}
+    template(slot="text")
       role-form(v-model="form")
-    v-divider
-    v-card-actions
-      v-layout.py-1(justify-end)
-        v-btn(@click="$modals.hide", depressed, flat) {{ $t('common.cancel') }}
-        v-btn.primary.white--text(data-test="submitButton", @click="submit") {{ $t('common.submit') }}
+    template(slot="actions")
+      v-btn(@click="$modals.hide", depressed, flat) {{ $t('common.cancel') }}
+      v-btn.primary.white--text(data-test="submitButton", @click="submit") {{ $t('common.submit') }}
 </template>
 
 <script>
@@ -26,12 +23,14 @@ import entitiesViewGroupMixin from '@/mixins/entities/view/group';
 
 import RoleForm from '@/components/other/role/role-form.vue';
 
+import ModalWrapper from '../modal-wrapper.vue';
+
 export default {
   name: MODALS.createRole,
   $_veeValidate: {
     validator: 'new',
   },
-  components: { RoleForm },
+  components: { RoleForm, ModalWrapper },
   mixins: [
     modalInnerMixin,
     entitiesViewMixin,
---
+++ sources/webcore/src/canopsis-next/src/components/modals/modal-wrapper.vue
@@ -0,0 +1,14 @@
+<template lang="pug" functional>
+  v-card
+    v-card-title.primary.white--text
+      v-layout(justify-space-between, align-center)
+        span.headline
+          slot(name="title")
+    v-card-text
+      slot(name="text")
+    template(v-if="$slots.actions")
+      v-divider
+      v-card-actions
+        v-layout.py-1(justify-end)
+          slot(name="actions")
+</template>
