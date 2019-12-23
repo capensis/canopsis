d5f33a128eb7e10a243218286c9031ff38959443
Dmitry <Dmitriy.Mikhaylishin@noveogroup.com>
2019-12-23 19:26:20 +0700

    feature(customizable-pbehavior-reason): commit after self review

--- sources/webcore/src/canopsis-next/src/store/modules/entities/index.js
+++ sources/webcore/src/canopsis-next/src/store/modules/entities/index.js
@@ -17,7 +17,7 @@ import snmpMibModule from './snmp/mib';
 import actionModule from './action';
 import heartbeatModule from './heartbeat';
 import dynamicInfoModule from './dynamic-info';
-import pbehaviorReasons from './pbehavior-reasons';
+import pbehaviorReasonsModule from './pbehavior-reasons';
 
 export default {
   alarm: alarmModule,
@@ -39,5 +39,5 @@ export default {
   action: actionModule,
   heartbeat: heartbeatModule,
   dynamicInfo: dynamicInfoModule,
-  pbehaviorReasons,
+  pbehaviorReasons: pbehaviorReasonsModule,
 };
