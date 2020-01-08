bc847aba7464d01cf671c55466505514fdbe3f5a
Antoine Berthier <aberthier@capensis.fr>
2020-01-08 16:51:11 +0100

    Update config.js

--- sources/webcore/src/canopsis-next/src/config.js
+++ sources/webcore/src/canopsis-next/src/config.js
@@ -12,7 +12,7 @@ export const DEFAULT_WEATHER_LIMIT = 120;
 
 export const DEFAULT_LOCALE = 'en';
 
-export const DEFAULT_KEEP_ALIVE_INTERVAL = 10000;
+export const DEFAULT_KEEP_ALIVE_INTERVAL = 200000;
 
 
 export const LOCALE_PRIORITIES = {
