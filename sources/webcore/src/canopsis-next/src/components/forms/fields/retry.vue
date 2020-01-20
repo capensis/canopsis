8b532b0a9c69960a7b824e53dc22304e02720093
Dmitry <Dmitriy.Mikhaylishin@noveogroup.com>
2020-01-20 17:01:21 +0700

    feature(webhook-request-retry-delay): errors added

--- sources/webcore/src/canopsis-next/src/components/forms/fields/retry.vue
+++ sources/webcore/src/canopsis-next/src/components/forms/fields/retry.vue
@@ -1,19 +1,21 @@
 <template lang="pug">
   v-layout
-    v-flex
+    v-flex(xs6)
       v-text-field(
         v-field="value.count",
         v-validate="'required|numeric|min_value:0'",
         :label="$t('modals.createWebhook.fields.retryCount')",
+        :error-messages="errors.collect('retryCount')",
         :min="0",
         name="retryCount",
         type="number"
       )
-    v-flex
+    v-flex(xs6)
       v-text-field(
         v-field="value.delay",
         v-validate="'required|numeric|min_value:0'",
         :label="$t('modals.createWebhook.fields.retryDelay')",
+        :error-messages="errors.collect('retryDelay')",
         :min="0",
         name="retryDelay",
         type="number"
