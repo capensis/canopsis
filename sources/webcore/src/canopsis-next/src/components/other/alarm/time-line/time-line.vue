48e11c920c1a303767a3f6e58c3034b1564f73be
Dmitry <Dmitriy.Mikhaylishin@noveogroup.com>
2020-02-03 20:03:15 +0700

    bugfix(refresh-problem-with-alarm-timelint-first-solution): only in edit mode

--- sources/webcore/src/canopsis-next/src/components/other/alarm/time-line/time-line.vue
+++ sources/webcore/src/canopsis-next/src/components/other/alarm/time-line/time-line.vue
@@ -1,23 +1,32 @@
 <template lang="pug">
-  div.timeline(:data-test="`alarmTimeLine-${alarm._id}`")
-    ul(v-for="(steps, day) in groupedSteps", :key="day")
-      li(v-for="(step, index) in steps", :key="`step-${index}`")
-        .timeline-item(v-show="index === 0")
-          .date {{ day | date('short', true) }}
-        .timeline-item
-          .time {{ step.t | date('H:mm:SS', true) }}
-          time-line-flag.flag(:step="step")
-          time-line-card(:step="step", :isHTMLEnabled="isHTMLEnabled")
+  v-layout.pa-2(v-if="isShowLoader", justify-center)
+    v-progress-circular(
+      :size="30",
+      color="primary",
+      indeterminate
+    )
+  v-expand-transition(v-else)
+    div.timeline(:data-test="`alarmTimeLine-${alarm._id}`")
+      ul(v-for="(steps, day) in groupedSteps", :key="day")
+        li(v-for="(step, index) in steps", :key="`step-${index}`")
+          .timeline-item(v-show="index === 0")
+            .date {{ day | date('short', true) }}
+          .timeline-item
+            .time {{ step.t | date('H:mm:SS', true) }}
+            time-line-flag.flag(:step="step")
+            time-line-card(:step="step", :isHTMLEnabled="isHTMLEnabled")
 </template>
 
 <script>
 import moment from 'moment';
-import { orderBy, groupBy } from 'lodash';
+import { orderBy, groupBy, get, debounce } from 'lodash';
 
 import TimeLineFlag from '@/components/other/alarm/time-line/time-line-flag.vue';
 import TimeLineCard from '@/components/other/alarm/time-line/time-line-card.vue';
 
 import entitiesAlarmMixin from '@/mixins/entities/alarm';
+
+const PENDING_SET_DELAY = 500;
 
 export default {
   components: { TimeLineFlag, TimeLineCard },
@@ -32,26 +41,49 @@ export default {
       default: false,
     },
   },
+  data() {
+    return {
+      pending: true,
+    };
+  },
+
   computed: {
     steps() {
       return this.alarm.v.steps || [];
     },
 
-    groupedSteps() {
-      if (this.alarm && this.alarm.v.steps) {
-        const orderedSteps = orderBy(this.alarm.v.steps, ['t'], 'desc');
+    hasAlarmSteps() {
+      return get(this.alarm, 'v.steps');
+    },
 
-        return groupBy(orderedSteps, step => moment.unix(step.t).startOf('day').format());
+    isShowLoader() {
+      return this.pending || !this.hasAlarmSteps;
+    },
+
+    groupedSteps() {
+      if (this.hasAlarmSteps) {
+        return this.groupSteps(this.alarm.v.steps);
       }
 
       return {};
     },
   },
+
+  watch: {
+    hasAlarmSteps(value) {
+      if (!value) {
+        this.fetchItem();
+      }
+    },
+  },
+
   mounted() {
     this.fetchItem();
   },
   methods: {
-    fetchItem() {
+    async fetchItem() {
+      this.pending = true;
+
       const params = {
         sort_key: 't',
         sort_dir: 'DESC',
@@ -63,11 +95,22 @@ export default {
         params.resolved = true;
       }
 
-      this.fetchAlarmItem({
+      await this.fetchAlarmItem({
         id: this.alarm._id,
         params,
       });
+
+      this.setPendingWithDebounce(false);
     },
+    groupSteps(steps) {
+      const orderedSteps = orderBy(steps, ['t'], 'desc');
+
+      return groupBy(orderedSteps, step => moment.unix(step.t).startOf('day').format());
+    },
+
+    setPendingWithDebounce: debounce(function setRequesting(value) {
+      this.pending = value;
+    }, PENDING_SET_DELAY),
   },
 };
 </script>
