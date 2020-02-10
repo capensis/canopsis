759d32da0fea775b365d9e6ed7af06e80cd56c16
Vladimir Erokhov <Vladimir.Erokhov@noveogroup.com>
2020-02-10 17:54:22 +0700

    feature(display-the-moreinfos-near-the-timeline): Moved tab into the first place

--- sources/webcore/src/canopsis-next/src/components/other/alarm/partials/alarms-expand-panel.vue
+++ sources/webcore/src/canopsis-next/src/components/other/alarm/partials/alarms-expand-panel.vue
@@ -1,18 +1,11 @@
 <template lang="pug">
   v-tabs.expand-panel(
-    ref="tabs",
+    :key="tabsKey",
     color="secondary lighten-1",
     slider-color="primary",
     dark,
     centered
   )
-    v-tab {{ $t('alarmList.tabs.timeLine') }}
-    v-tab-item
-      v-layout.pa-3.secondary.lighten-2(row)
-        v-flex(:class="cardFlexClass")
-          v-card.tab-item-card
-            v-card-text
-              time-line(:alarm="alarm", :isHTMLEnabled="isHTMLEnabled")
     template(v-if="widget.parameters.moreInfoTemplate")
       v-tab {{ $t('alarmList.tabs.moreInfos') }}
       v-tab-item
@@ -21,22 +14,28 @@
             v-card.tab-item-card
               v-card-text
                 more-infos(:alarm="alarm", :template="widget.parameters.moreInfoTemplate")
+    v-tab {{ $t('alarmList.tabs.timeLine') }}
+    v-tab-item
+      v-layout.pa-3.secondary.lighten-2(row)
+        v-flex(:class="cardFlexClass")
+          v-card.tab-item-card
+            v-card-text
+              time-line(:alarm="alarm", :isHTMLEnabled="isHTMLEnabled")
 </template>
 
 <script>
 import { GRID_SIZES } from '@/constants';
 
+import uid from '@/helpers/uid';
+
 import TimeLine from '@/components/other/alarm/time-line/time-line.vue';
 import MoreInfos from '@/components/other/alarm/more-infos/more-infos.vue';
-
-import vuetifyTabsMixin from '@/mixins/vuetify/tabs';
 
 export default {
   components: {
     TimeLine,
     MoreInfos,
   },
-  mixins: [vuetifyTabsMixin],
   props: {
     isHTMLEnabled: {
       type: Boolean,
@@ -51,6 +50,11 @@ export default {
       required: true,
     },
   },
+  data() {
+    return {
+      tabsKey: uid(),
+    };
+  },
   computed: {
     cardFlexClass() {
       const { expandGridRangeSize: [start, end] = [GRID_SIZES.min, GRID_SIZES.max] } = this.widget.parameters;
@@ -64,7 +68,7 @@ export default {
   watch: {
     'widget.parameters.moreInfoTemplate': {
       handler() {
-        this.$nextTick(this.callTabsUpdateTabsMethod);
+        this.tabsKey = uid();
       },
     },
   },
