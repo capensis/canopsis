4a07f98c97477f3997b27435f3d7302b853f80d9
Vladimir Erokhov <Vladimir.Erokhov@noveogroup.com>
2020-02-18 21:06:55 +0700

    bugfix(periodic-refresh-on-widgets): Fixed bug

--- sources/webcore/src/canopsis-next/src/components/side-bars/settings/widgets/fields/common/periodic-refresh.vue
+++ sources/webcore/src/canopsis-next/src/components/side-bars/settings/widgets/fields/common/periodic-refresh.vue
@@ -1,23 +1,34 @@
 <template lang="pug">
   v-list-group(data-test="periodicRefresh")
-    v-list-tile(slot="activator") {{$t('settings.periodicRefresh')}}
-      .font-italic.caption.ml-1 ({{ $t('common.optional') }})
+    v-list-tile(slot="activator")
+      div(:class="validationHeaderClass") {{$t('settings.periodicRefresh')}}
+        span.font-italic.caption.ml-1 ({{ $t('common.optional') }})
     v-container
       periodic-refresh-field(v-field="value")
 </template>
 
 <script>
-import { DEFAULT_PERIODIC_REFRESH } from '@/constants';
+import { DEFAULT_PERIODIC_REFRESH, TIME_UNITS } from '@/constants';
+
+import formMixin from '@/mixins/form';
+import formValidationHeaderMixin from '@/mixins/form/validation-header';
 
 import PeriodicRefreshField from '@/components/forms/fields/periodic-refresh-field.vue';
 
 export default {
+  inject: ['$validator'],
   components: { PeriodicRefreshField },
+  mixins: [formMixin, formValidationHeaderMixin],
   props: {
     value: {
       type: Object,
       default: () => ({ ...DEFAULT_PERIODIC_REFRESH }),
     },
+  },
+  created() {
+    if (this.value && !this.value.unit) {
+      this.updateField('unit', TIME_UNITS.second);
+    }
   },
 };
 </script>
