51d57ab0ea336e7f8ba1b82e55eac003094a823f
Dmitry <Dmitriy.Mikhaylishin@noveogroup.com>
2020-03-04 10:31:42 +0700

    feature(correlation-groups-alarms-pagination): fix group query

--- sources/webcore/src/canopsis-next/src/mixins/widget/group-fetch-query.js
+++ sources/webcore/src/canopsis-next/src/mixins/widget/group-fetch-query.js
@@ -1,17 +1,15 @@
 import queryWidgetMixin from '@/mixins/widget/query';
-import entitiesUserPreferenceMixin from '@/mixins/entities/user-preference';
-import { convertWidgetToQuery, convertUserPreferenceToQuery } from '@/helpers/query';
+import { convertWidgetToQuery } from '@/helpers/query';
 
 /**
  * @mixin Add query logic with group fetch
  */
 export default {
-  mixins: [queryWidgetMixin, entitiesUserPreferenceMixin],
-  async mounted() {
+  mixins: [queryWidgetMixin],
+  mounted() {
     this.query = {
       ...this.query,
       ...convertWidgetToQuery(this.widget),
-      ...convertUserPreferenceToQuery(this.userPreference),
     };
   },
   updateRecordsPerPage(limit) {
