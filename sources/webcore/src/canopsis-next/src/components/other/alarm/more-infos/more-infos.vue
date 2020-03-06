d34fccc01bfe0484b1327119f01e0912043a8bea
Vladimir Erokhov <Vladimir.Erokhov@noveogroup.com>
2020-03-06 18:29:40 +0700

    feature(request-handlebars-helper): Added vue-async-computed

--- sources/webcore/src/canopsis-next/src/components/other/alarm/more-infos/more-infos.vue
+++ sources/webcore/src/canopsis-next/src/components/other/alarm/more-infos/more-infos.vue
@@ -1,17 +1,19 @@
 <template lang="pug">
-  .more-infos(data-test="moreInfosTemplateContent", v-if="!template")
+  .more-infos(v-if="!template", data-test="moreInfosTemplateContent")
     v-layout(justify-center)
       v-icon(color="info") infos
       p(class="ma-0") {{ $t('alarmList.moreInfos.defineATemplate') }}
-  .more-infos(data-test="moreInfosContent", v-else)
-    v-runtime-template(:template="output")
+  .more-infos(v-else, data-test="moreInfosContent")
+    v-runtime-template(:template="compiledTemplate")
 </template>
 
 <script>
 import VRuntimeTemplate from 'v-runtime-template';
+
 import { compile } from '@/helpers/handlebars';
 
 export default {
+  name: 'more-infos',
   components: { VRuntimeTemplate },
   props: {
     template: {
@@ -23,27 +25,14 @@ export default {
       required: false,
     },
   },
-  data() {
-    return {
-      output: '',
-    };
-  },
-  watch: {
-    template() {
-      this.compileTemplate();
-    },
-    alarm() {
-      this.compileTemplate();
-    },
-  },
-  created() {
-    this.compileTemplate();
-  },
-  methods: {
-    async compileTemplate() {
-      const compiledTemplate = await compile(this.template, { alarm: this.alarm, entity: this.alarm.entity });
+  asyncComputed: {
+    compiledTemplate: {
+      async get() {
+        const compiledTemplate = await compile(this.template, { alarm: this.alarm, entity: this.alarm.entity });
 
-      this.output = `<div>${compiledTemplate}</div>`;
+        return `<div>${compiledTemplate}</div>`;
+      },
+      default: '',
     },
   },
 };
