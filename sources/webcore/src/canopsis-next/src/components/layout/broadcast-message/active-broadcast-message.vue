19827037aba73ad9bc1993cbada4b75e04cbf39a
Vladimir Erokhov <Vladimir.Erokhov@noveogroup.com>
2020-04-09 14:19:02 +0700

    feature(broadcast-messages): Added registrable mixin

--- sources/webcore/src/canopsis-next/src/components/layout/broadcast-message/active-broadcast-message.vue
+++ sources/webcore/src/canopsis-next/src/components/layout/broadcast-message/active-broadcast-message.vue
@@ -13,12 +13,17 @@ import { createNamespacedHelpers } from 'vuex';
 
 import { ACTIVE_BROADCAST_MESSAGE_FETCHING_INTERVAL } from '@/config';
 
+import { broadcastMessageSchema } from '@/store/schemas';
+
+import registrableMixin from '@/mixins/registrable';
+
 import BroadcastMessage from '@/components/other/broadcast-message/broadcast-message.vue';
 
 const { mapActions, mapGetters } = createNamespacedHelpers('broadcastMessage');
 
 export default {
   components: { BroadcastMessage },
+  mixins: [registrableMixin([broadcastMessageSchema], 'activeMessages')],
   data() {
     return {
       timeout: null,
