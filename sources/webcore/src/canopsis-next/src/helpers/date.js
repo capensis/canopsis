f2d238e9b9b36bcfb5c5cf04f872c9ab9ab47cdb
Dmitry <Dmitriy.Mikhaylishin@noveogroup.com>
2020-05-12 16:14:21 +0700

    bugfix(no-units-in-forms-description): time interval helper on frontend part

--- sources/webcore/src/canopsis-next/src/helpers/date.js
+++ sources/webcore/src/canopsis-next/src/helpers/date.js
@@ -1,4 +1,5 @@
 import moment from 'moment';
+import { TIME_UNITS } from '@/constants';
 
 export default function convertTimestampToMoment(timestamp) {
   let dateObject;
@@ -12,3 +13,28 @@ export default function convertTimestampToMoment(timestamp) {
 
   return dateObject;
 }
+/**
+ * Convert duration to interval object
+ *
+ * @param duration
+ * @return {{unit: string, interval: number}}
+ */
+export const convertDurationToIntervalObject = (duration) => {
+  const durationUnits = [
+    TIME_UNITS.year,
+    TIME_UNITS.month,
+    TIME_UNITS.week,
+    TIME_UNITS.week,
+    TIME_UNITS.day,
+    TIME_UNITS.hour,
+    TIME_UNITS.minute,
+    TIME_UNITS.second,
+  ];
+
+  const durationType = durationUnits.find(unit => moment.duration(duration, 'seconds').as(unit) % 1 === 0);
+
+  return {
+    interval: moment.duration(duration, 'seconds').as(durationType),
+    unit: durationType,
+  };
+};
