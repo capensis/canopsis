b86db6ed5ec45d1a1229228d83e2557a52a3b219
Vladimir Erokhov <Vladimir.Erokhov@noveogroup.com>
2020-05-21 15:28:23 +0700

    feature(pbehavior-ordering): Finished issue

--- sources/webcore/src/canopsis-next/src/mixins/widget/query.js
+++ sources/webcore/src/canopsis-next/src/mixins/widget/query.js
@@ -1,16 +1,26 @@
 import { omit } from 'lodash';
 
 import { PAGINATION_LIMIT } from '@/config';
-import { SORT_ORDERS, DATETIME_FORMATS } from '@/constants';
+import { DATETIME_FORMATS } from '@/constants';
+
+import { dateParse } from '@/helpers/date-intervals';
+
 import queryMixin from '@/mixins/query';
 import entitiesUserPreferenceMixin from '@/mixins/entities/user-preference';
-import { dateParse } from '@/helpers/date-intervals';
+import vuetifyPaginationMixinCreator from '@/mixins/vuetify/pagination-creator';
 
 /**
  * @mixin Add query logic
  */
 export default {
-  mixins: [queryMixin, entitiesUserPreferenceMixin],
+  mixins: [
+    queryMixin,
+    entitiesUserPreferenceMixin,
+    vuetifyPaginationMixinCreator({
+      field: 'vDataTablePagination',
+      mutating: true,
+    }),
+  ],
   props: {
     tabId: {
       type: String,
@@ -33,26 +43,6 @@ export default {
 
     queryId() {
       return this.defaultQueryId || this.widget._id;
-    },
-
-    vDataTablePagination: {
-      get() {
-        const descending = this.query.sortDir !== null ? this.query.sortDir === SORT_ORDERS.desc : null;
-
-        return { sortBy: this.query.sortKey, descending };
-      },
-      set(value) {
-        const isNotEqualSortBy = value.sortBy !== this.vDataTablePagination.sortBy;
-        const isNotEqualDescending = value.descending !== this.vDataTablePagination.descending;
-
-        if (isNotEqualSortBy || isNotEqualDescending) {
-          this.query = {
-            ...this.query,
-            sortKey: value.sortBy,
-            sortDir: value.descending ? SORT_ORDERS.desc : SORT_ORDERS.asc,
-          };
-        }
-      },
     },
 
     tabQueryNonce() {
