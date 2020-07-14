a0efe8f030acef42b4eaa62859e536d4617c5234
Vladimir Erokhov <Vladimir.Erokhov@noveogroup.com>
2020-07-14 18:25:43 +0700

    feature(planning_version-4): Added logic for modal window zIndex calculation

--- sources/webcore/src/canopsis-next/src/plugins/vuetify-replacer/components/v-dialog/v-dialog.vue
+++ sources/webcore/src/canopsis-next/src/plugins/vuetify-replacer/components/v-dialog/v-dialog.vue
@@ -1,10 +1,37 @@
 <script>
 import VDialog from 'vuetify/es5/components/VDialog';
+import { getZIndex } from 'vuetify/es5/util/helpers';
 
 import overlayableMixin from '../../mixins/overlayable';
 
 export default {
   extends: VDialog,
   mixins: [overlayableMixin],
+  props: {
+    zIndex: {
+      type: Number,
+      default: null,
+    },
+  },
+  computed: {
+    activeZIndex() {
+      if (typeof window === 'undefined') return 0;
+
+      const content = this.stackElement || this.$refs.content;
+      // Return current zindex if not active
+      const index = !this.isActive
+        ? getZIndex(content)
+        : this.zIndex || this.getMaxZIndex(this.stackExclude || [content]) + 2;
+
+      if (index == null) {
+        return index;
+      }
+
+      // Return max current z-index (excluding self) + 2
+      // (2 to leave room for an overlay below, if needed)
+      return parseInt(index, 10);
+    },
+  },
+
 };
 </script>
---
+++ sources/webcore/src/canopsis-next/src/plugins/vuetify-replacer/components/v-menu/v-menu.vue
@@ -0,0 +1,27 @@
+<script>
+import VMenu from 'vuetify/es5/components/VMenu';
+import { getZIndex } from 'vuetify/es5/util/helpers';
+
+export default {
+  extends: VMenu,
+  props: {
+    ignoreClickUpperOutside: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  methods: {
+    closeConditional(e) {
+      const targetZIndex = getZIndex(e.target);
+      const contentZIndex = getZIndex(this.$refs.content);
+
+      return (this.ignoreClickUpperOutside && targetZIndex < contentZIndex)
+        || (!this.ignoreClickUpperOutside
+          && this.isActive
+          && this.closeOnClick
+          && !this.$refs.content.contains(e.target)
+        );
+    },
+  },
+};
+</script>
