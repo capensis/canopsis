28dd29990193d6d793d595e5628b49ec31d3128d
Vladimir Erokhov <Vladimir.Erokhov@noveogroup.com>
2020-07-15 20:24:27 +0700

    feature(planning_version-4): Bugfixed

--- sources/webcore/src/canopsis-next/src/plugins/vuetify-replacer/components/v-menu/v-menu.vue
+++ sources/webcore/src/canopsis-next/src/plugins/vuetify-replacer/components/v-menu/v-menu.vue
@@ -22,6 +22,42 @@ export default {
           && !this.$refs.content.contains(e.target)
         );
     },
+
+    genContent() {
+      const options = {
+        attrs: this.getScopeIdAttrs(),
+        staticClass: 'v-menu__content',
+        class: {
+          ...this.rootThemeClasses,
+          'v-menu__content--auto': this.auto,
+          'v-menu__content--fixed': this.activatorFixed,
+          'v-menu__ignore-click-upper-outside': this.ignoreClickUpperOutside,
+          menuable__content__active: this.isActive,
+          [this.contentClass.trim()]: true,
+        },
+        style: this.styles,
+        directives: this.genDirectives(),
+        ref: 'content',
+        on: {
+          click: (e) => {
+            e.stopPropagation();
+            if (e.target.getAttribute('disabled')) return;
+            if (this.closeOnContentClick) this.isActive = false;
+          },
+          keydown: this.onKeyDown,
+        },
+      };
+
+      if (!this.disabled && this.openOnHover) {
+        options.on.mouseenter = this.mouseEnterHandler;
+      }
+
+      if (this.openOnHover) {
+        options.on.mouseleave = this.mouseLeaveHandler;
+      }
+
+      return this.$createElement('div', options, this.showLazyContent(this.$slots.default));
+    },
   },
 };
 </script>
