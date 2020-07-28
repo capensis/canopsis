c856e93ff590690f36b7a383dd3b7c7e44159324
Dmitry <Dmitriy.Mikhaylishin@noveogroup.com>
2020-07-28 13:42:11 +0700

    feature(2502-state-s-badges-applied-on-a-column): fix after review

--- sources/webcore/src/canopsis-next/src/components/other/alarm/columns-formatting/alarm-column-value.vue
+++ sources/webcore/src/canopsis-next/src/components/other/alarm/columns-formatting/alarm-column-value.vue
@@ -1,73 +1,32 @@
 <template lang="pug">
   div(:data-test="`alarmValue-${column.text}`")
-    v-tooltip(right, :disabled="!column.isState || isStateField")
+    v-tooltip(v-if="column.isState", right)
       div(slot="activator", :style="stateStyle", :class="{ 'state-column-wrapper': column.isState }")
-        v-menu(
-          v-if="popupData",
-          v-model="isInfoPopupOpen",
-          :close-on-content-click="false",
-          :open-on-click="false",
-          offset-x,
-          lazy
+        alarm-column-cell(
+          :alarm="alarm",
+          :widget="widget",
+          :column="column",
+          :columnFiltersMap="columnFiltersMap"
         )
-          div.alarm-column-value(slot="activator")
-            v-layout(align-center)
-              div(v-if="column.isHtml", v-html="sanitizedValue")
-              div(v-else, v-bind="component.bind", v-on="component.on")
-              v-btn.ma-0(data-test="alarmInfoPopupOpenButton", icon, small, @click.stop="showInfoPopup")
-                v-icon(small) info
-          v-card(:data-test="`alarmInfoPopup-${alarm._id}-column-${column.text}`", dark)
-            v-card-title.primary.pa-2.white--text
-              v-layout(justify-space-between, align-center)
-                h4 {{ $t('alarmList.infoPopup') }}
-                v-btn.ma-0.ml-3(
-                  data-test="alarmInfoPopupCloseButton",
-                  icon,
-                  small,
-                  @click="hideInfoPopup",
-                  color="white"
-                )
-                  v-icon(small, color="error") close
-            v-fade-transition
-              v-card-text.pa-2(v-if="isInfoPopupOpen", data-test="alarmInfoPopupContent")
-                v-runtime-template(:template="popupTextContent")
-        div.alarm-column-value(v-else-if="column.isHtml", v-html="sanitizedValue")
-        div.alarm-column-value(v-else, v-bind="component.bind", v-on="component.on")
       span {{ stateData.text }}
+    alarm-column-cell(
+      v-else,
+      :alarm="alarm",
+      :widget="widget",
+      :column="column",
+      :columnFiltersMap="columnFiltersMap"
+    )
 </template>
 
 <script>
-import { get } from 'lodash';
-import VRuntimeTemplate from 'v-runtime-template';
-
 import { ALARM_ENTITY_FIELDS } from '@/constants';
-import { compile } from '@/helpers/handlebars';
 import { formatState } from '@/helpers/state-and-status-formatting';
 
-import Ellipsis from '@/components/tables/ellipsis.vue';
+import AlarmColumnCell from '@/components/other/alarm/columns-formatting/alarm-column-cell.vue';
 
-import AlarmColumnValueState from './alarm-column-value-state.vue';
-import AlarmColumnValueLinks from './alarm-column-value-links.vue';
-import AlarmColumnValueLink from './alarm-column-value-link.vue';
-import AlarmColumnValueExtraDetails from './alarm-column-value-extra-details.vue';
-
-/**
- * Component to format alarms list columns
- *
- * @module alarm
- *
- * @prop {Object} alarm - Object representing the alarm
- * @prop {Object} widget - Object representing the widget
- * @prop {Object} column - Property concerned on the column
- */
 export default {
   components: {
-    VRuntimeTemplate,
-    Ellipsis,
-    AlarmColumnValueState,
-    AlarmColumnValueLinks,
-    AlarmColumnValueLink,
-    AlarmColumnValueExtraDetails,
+    AlarmColumnCell,
   },
   props: {
     alarm: {
@@ -87,28 +46,6 @@ export default {
       default: () => ({}),
     },
   },
-  data() {
-    return {
-      isInfoPopupOpen: false,
-    };
-  },
-  asyncComputed: {
-    popupTextContent: {
-      lazy: true,
-
-      async get() {
-        if (this.popupData) {
-          const context = { alarm: this.alarm, entity: this.alarm.entity || {} };
-          const compiledTemplate = await compile(this.popupData.template, context);
-
-          return `<div>${compiledTemplate}</div>`;
-        }
-
-        return '';
-      },
-      default: '',
-    },
-  },
   computed: {
     isStateField() {
       return this.column.value === ALARM_ENTITY_FIELDS.state;
@@ -123,122 +60,11 @@ export default {
         ? { backgroundColor: this.stateData.color }
         : {};
     },
-
-    value() {
-      return this.$options.filters.get(this.alarm, this.column.value, this.columnFilter, '');
-    },
-
-    sanitizedValue() {
-      try {
-        return this.$sanitize(this.value, {
-          allowedTags: ['h3', 'h4', 'h5', 'h6', 'blockquote', 'p', 'a', 'ul', 'ol',
-            'nl', 'li', 'b', 'i', 'strong', 'em', 'strike', 'code', 'hr', 'br', 'div',
-            'table', 'thead', 'caption', 'tbody', 'tr', 'th', 'td', 'pre', 'iframe', 'span', 'font', 'u'],
-          allowedAttributes: {
-            '*': ['style'],
-            a: ['href', 'name', 'target'],
-            img: ['src', 'alt'],
-            font: ['color', 'size', 'face'],
-          },
-        });
-      } catch (err) {
-        console.warn(err);
-
-        return '';
-      }
-    },
-
-    popupData() {
-      const popups = get(this.widget.parameters, 'infoPopups', []);
-
-      return popups.find(popup => popup.column === this.column.value);
-    },
-
-    columnFilter() {
-      const PROPERTIES_FILTERS_MAP = {
-        'v.status.val': value => this.$t(`tables.alarmStatus.${value}`),
-        'v.last_update_date': value => this.$options.filters.date(value, 'long'),
-        'v.creation_date': value => this.$options.filters.date(value, 'long'),
-        'v.last_event_date': value => this.$options.filters.date(value, 'long'),
-        'v.state.t': value => this.$options.filters.date(value, 'long'),
-        'v.status.t': value => this.$options.filters.date(value, 'long'),
-        'v.resolved': value => this.$options.filters.date(value, 'long'),
-        'v.duration': value => this.$options.filters.duration(value),
-        'v.current_state_duration': value => this.$options.filters.duration(value),
-        t: value => this.$options.filters.date(value, 'long'),
-        ...this.columnFiltersMap,
-      };
-
-      return PROPERTIES_FILTERS_MAP[this.column.value];
-    },
-
-    component() {
-      const PROPERTIES_COMPONENTS_MAP = {
-        [ALARM_ENTITY_FIELDS.state]: {
-          bind: {
-            is: 'alarm-column-value-state',
-            alarm: this.alarm,
-          },
-        },
-        links: {
-          bind: {
-            is: 'alarm-column-value-links',
-            links: this.alarm.links,
-          },
-        },
-        extra_details: {
-          bind: {
-            is: 'alarm-column-value-extra-details',
-            alarm: this.alarm,
-          },
-        },
-      };
-
-      if (PROPERTIES_COMPONENTS_MAP[this.column.value]) {
-        return PROPERTIES_COMPONENTS_MAP[this.column.value];
-      }
-
-      if (this.column.value.startsWith('links.')) {
-        const category = this.column.value.slice(6);
-        const links = {
-          [category]: this.$options.filters.get(this.alarm, this.column.value, null, []),
-        };
-
-        return {
-          bind: {
-            links,
-
-            is: 'alarm-column-value-links',
-          },
-        };
-      }
-
-      return {
-        bind: {
-          is: 'ellipsis',
-          text: String(this.$options.filters.get(this.alarm, this.column.value, this.columnFilter, '')),
-        },
-      };
-    },
-  },
-  methods: {
-    showInfoPopup() {
-      if (this.popupData) {
-        this.isInfoPopupOpen = true;
-      }
-    },
-    hideInfoPopup() {
-      this.isInfoPopupOpen = false;
-    },
   },
 };
 </script>
 
 <style lang="scss" scoped>
-  .alarm-column-value {
-    display: inline-block;
-  }
-
   .state-column-wrapper {
     display: inline-block;
     border-radius: 10px;
