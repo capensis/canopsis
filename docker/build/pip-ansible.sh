c458b975576606c6f6df3104f4d49c3d5a421867
remy <rfremont@capensis.fr>
2020-07-29 16:19:39 +0200

    Update pip-ansible.sh

--- docker/build/pip-ansible.sh
+++ docker/build/pip-ansible.sh
@@ -10,8 +10,8 @@ repver="${ID}-${VERSION_ID}"
 source ${CPS_HOME}/venv-ansible/bin/activate
 source /etc/os-release
 
-# duplicate command
-#pip install -U setuptools pip
+# duplicate command for force version pip
+pip install -U setuptools "pip==20.1.1"
 
 # jmespath is required by ansible role repository adder maat
 # pymongo 3.5.0 is required by ansible 2.4.x module mongodb
@@ -19,7 +19,7 @@ source /etc/os-release
 # pyopenssl is required to fix compatibility between systems
 # NEVER, NEVER, NEVER ⚠️⚠️⚠️ UPGRADE ANSIBLE, OR BE PREPARED FOR PAIN.
 # Fix pip version for issue #2512. 
-pip install -U setuptools "pip==20.1.1" wheel jmespath "pymongo==3.5.0" "influxdb==5.1.0" "pyOpenSSL" "ansible==2.8.7"
+pip install -U wheel jmespath "pymongo==3.5.0" "influxdb==5.1.0" "pyOpenSSL" "ansible==2.8.7"
 
 rm -rf /tmp/pipbuild
 rm -rf ~/.cache
