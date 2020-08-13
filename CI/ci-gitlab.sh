41155edfcd92651077b4878703ba035f09c6bfe1
Julien NYCZ <jnycz@capensis.fr>
2020-08-13 13:09:53 +0200

    Fix CI dir into ci-gitlab script

--- CI/ci-gitlab.sh
+++ CI/ci-gitlab.sh
@@ -4,14 +4,15 @@ set -o pipefail
 set -u
 
 git submodule update --init
+cd ..
 ./build-docker.sh
 
 # override .env default variable
 export CANOPSIS_IMAGE_TAG=${CANOPSIS_DISTRIBUTION}-ci-test
 
-docker-compose -f docker-compose.ci.yml -p ${COMPOSE_PROJECT_NAME} up -d webserver mongodb redis rabbitmq influxdb provisionning
+docker-compose --project-directory CI/ -f CI/docker-compose.ci.yml -p ${COMPOSE_PROJECT_NAME} up -d webserver mongodb redis rabbitmq influxdb provisionning
 ./docker/wait-provisionning.sh CI/docker-compose.ci.yml ${COMPOSE_PROJECT_NAME}
-docker-compose -f docker-compose.ci.yml -p ${COMPOSE_PROJECT_NAME} restart webserver mongodb redis rabbitmq influxdb provisionning
+docker-compose --project-directory CI/ -f CI/docker-compose.ci.yml -p ${COMPOSE_PROJECT_NAME} restart webserver mongodb redis rabbitmq influxdb provisionning
 
 docker exec -t ${COMPOSE_PROJECT_NAME}_webserver_1 /bin/bash -c "source ~/.bashrc && /opt/canopsis/bin/ut_runner /opt/canopsis/test"
 docker cp ${COMPOSE_PROJECT_NAME}_webserver_1:/opt/canopsis/tmp/tests_report/ tests_report
