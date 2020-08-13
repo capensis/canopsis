f9d5eca49b98b0ecc6e44cf4f5e2fb3a706ae5d3
Julien NYCZ <jnycz@capensis.fr>
2020-08-13 12:53:18 +0200

    Reorganize CI stufs

--- CI/ci-gitlab.sh
+++ CI/ci-gitlab.sh
@@ -9,9 +9,5 @@ git submodule update --init
 # override .env default variable
 export CANOPSIS_IMAGE_TAG=${CANOPSIS_DISTRIBUTION}-ci-test
 
-docker-compose -f docker-compose.ci.yml -p ${COMPOSE_PROJECT_NAME} up -d
-./docker/wait-provisionning.sh docker-compose.ci.yml ${COMPOSE_PROJECT_NAME}
-docker-compose -f docker-compose.ci.yml -p ${COMPOSE_PROJECT_NAME} restart
-
 docker exec -t ${COMPOSE_PROJECT_NAME}_webserver_1 /bin/bash -c "source ~/.bashrc && /opt/canopsis/bin/ut_runner /opt/canopsis/test"
 docker cp ${COMPOSE_PROJECT_NAME}_webserver_1:/opt/canopsis/tmp/tests_report/ tests_report
