467f9752f974ac23a3b0131753669feb2b278648
Donovan Watteau <dwatteau@capensis.fr>
2020-11-05 14:54:48 +0100

    fix(build): Fix a mixup between the old and the new API…

--- docker/files/entrypoint-nginx.sh
+++ docker/files/entrypoint-nginx.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-sed -i -e "s,{{ CPS_OLD_API_URL }},$CPS_OLD_API_URL,g" /etc/nginx/conf.d/default.conf
+sed -i -e "s,{{ CPS_API_URL }},$CPS_API_URL,g" /etc/nginx/conf.d/default.conf
 exec nginx -g 'daemon off;'
