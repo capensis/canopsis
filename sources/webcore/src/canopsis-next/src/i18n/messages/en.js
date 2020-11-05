13e1cf6058cc96e7522bc797e513bce6b4cd0770
Vladimir Erokhov <Vladimir.Erokhov@noveogroup.com>
2020-11-05 15:36:53 +0700

    feature(#2741-dynamic-info-search-into-pattern): Finished issue

--- sources/webcore/src/canopsis-next/src/i18n/messages/en.js
+++ sources/webcore/src/canopsis-next/src/i18n/messages/en.js
@@ -208,6 +208,19 @@ export default {
       '    <dd>Entities whose infos.custom.value contains 1 or 2</dd><dt>- NOT Name = "name_1"</dt>\n' +
       '    <dd>Entities whose name isn\'t "name_1"</dd>\n' +
       '</dl>',
+    dynamicInfoAdvancedSearch: '<span>Help on the advanced research :</span>\n' +
+      '<p>- [ NOT ] &lt;ColumnName&gt; &lt;Operator&gt; &lt;Value&gt;</p> [ AND|OR [ NOT ] &lt;ColumnName&gt; &lt;Operator&gt; &lt;Value&gt; ]\n' +
+      '<p>The "-" before the research is required</p>\n' +
+      '<p>Operators :\n' +
+      '    <=, <,=, !=,>=, >, LIKE (For MongoDB regular expression)</p>\n' +
+      '<p>Value\'s type : String between quote, Boolean ("TRUE", "FALSE"), Integer, Float, "NULL"</p>\n' +
+      '<dl><dt>Examples :</dt><dt>- Name = "name_1"</dt>\n' +
+      '    <dd>Entities whose names are "name_1"</dd><dt>- Name="name_1" AND Type="watcher"</dt>\n' +
+      '    <dd>Entities whose names is "name_1" and the types is "watcher"</dd><dt>- infos.custom.value="Custom value" OR Type="resource"</dt>\n' +
+      '    <dd>Entities whose infos.custom.value is "Custom value" or the type is "resource"</dd><dt>- infos.custom.value LIKE 1 OR infos.custom.value LIKE 2</dt>\n' +
+      '    <dd>Entities whose infos.custom.value contains 1 or 2</dd><dt>- NOT Name = "name_1"</dt>\n' +
+      '    <dd>Entities whose name isn\'t "name_1"</dd>\n' +
+      '</dl>',
     submit: 'Search',
     clear: 'Clear search input',
   },
