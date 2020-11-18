6dd1f9d1217c67d5b435f88342da92007cfdecab
Alix Biton <abiton@capensis.fr>
2020-11-18 11:00:46 +0100

    Added wait for backend to nginx docker container

--- docker/files/entrypoint-nginx.sh
+++ docker/files/entrypoint-nginx.sh
@@ -1,3 +1,33 @@
 #!/bin/sh
+set -x
+function wait_for_backends {
+        start_time=$(date +%s)
+        timeout=${TIMEOUT:-30}
+        until wget --spider ${@} -O /dev/null;
+        do 
+                echo "Waiting for backends ${@}";
+                sleep 1
+                
+                # Check for timeout
+                if [ $(($(date +%s) - start_time)) -gt ${timeout} ]
+                then
+                        echo "Backends not responding after ${elapsed_time}s, exiting"
+                        exit 1
+                fi
+        done
+}
+
+function fix_permissions {
+        for f in ${@}
+        do
+                echo Fixing permissions for ${f}
+                chmod 0755 -R ${f}
+        done
+}
+
 sed -i -e "s,{{ TARGET }},$TARGET,g" /etc/nginx/conf.d/default.conf
+
+wait_for_backends ${TARGET} ${ADDITIONAL_BACKENDS}
+fix_permissions ${NGINX_ACCESSIBLE_FOLDERS}
+
 exec nginx -g 'daemon off;'
