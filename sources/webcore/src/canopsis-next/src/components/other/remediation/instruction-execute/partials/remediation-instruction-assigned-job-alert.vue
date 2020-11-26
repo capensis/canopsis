7e42eb93dc3f8613ebbdef9e4dda44c8f87eb1f7
Dmitry <Dmitriy.Mikhaylishin@noveogroup.com>
2020-11-26 18:55:32 +0700

    feature(#2617-job-execution-alert): buttons fixed

--- sources/webcore/src/canopsis-next/src/components/other/remediation/instruction-execute/partials/remediation-instruction-assigned-job-alert.vue
+++ sources/webcore/src/canopsis-next/src/components/other/remediation/instruction-execute/partials/remediation-instruction-assigned-job-alert.vue
@@ -1,28 +1,28 @@
 <template lang="pug">
   v-alert.py-1.job-alert(:value="true", type="error", border="left", icon="info")
     v-layout(align-center, justify-space-around)
-      v-flex(xs8)
-        span {{ $t('remediationInstructionExecute.jobs.waitAlert') }}
-      v-flex(xs4)
-        v-layout(row)
-          v-btn(
-            outline,
-            small,
-            block,
-            color="white",
-            @click="$emit('skip')"
-          ) {{ $t('remediationInstructionExecute.jobs.skip') }}
-          v-btn.ml-2(
-            outline,
-            small,
-            block,
-            color="white",
-            @click="$emit('await')"
-          ) {{ $t('remediationInstructionExecute.jobs.await') }}
+      span {{ $t('remediationInstructionExecute.jobs.waitAlert') }}
+      div.job-alert-buttons
+        v-btn.ma-0(
+          outline,
+          small,
+          color="white",
+          @click="$emit('skip')"
+        ) {{ $t('remediationInstructionExecute.jobs.skip') }}
+        v-btn.ma-0.ml-2(
+          outline,
+          small,
+          color="white",
+          @click="$emit('await')"
+        ) {{ $t('remediationInstructionExecute.jobs.await') }}
 </template>
 
 <style lang="scss">
   .job-alert {
     border-top: none;
+
+    &-buttons {
+      display: flex;
+    }
   }
 </style>
