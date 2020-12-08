e14e399320026c312f27589603cc834405e525d0
Dmitry <Dmitriy.Mikhaylishin@noveogroup.com>
2020-12-08 17:05:24 +0700

    feature(#2663-modal-with-no-meaningful-content): filter action buttons changed

--- sources/webcore/src/canopsis-next/src/components/other/filter/form/fields/filter-field.vue
+++ sources/webcore/src/canopsis-next/src/components/other/filter/form/fields/filter-field.vue
@@ -1,26 +1,19 @@
 <template lang="pug">
   v-list-tile.filter-item.pa-0
-    v-layout(:data-test="`filterItem-${filter.title}`")
+    v-layout
       v-flex(xs12)
         v-list-tile-content {{ filter.title }}
       v-list-tile-action(v-if="hasAccessToEdit")
         v-layout
-          v-btn.ma-1(
-            icon,
-            :data-test="`editFilter-${filter.title}`",
-            @click="$emit('edit')"
-          )
-            v-icon edit
-          v-btn.ma-1(
-            icon,
-            :data-test="`deleteFilter-${filter.title}`",
-            @click="$emit('delete')"
-          )
-            v-icon delete
+          action-btn(type="edit", @click="$emit('edit')")
+          action-btn(type="delete", @click="$emit('delete')")
 </template>
 
 <script>
+import ActionBtn from '@/components/tables/action-btn.vue';
+
 export default {
+  components: { ActionBtn },
   model: {
     prop: 'filter',
     event: 'input',
