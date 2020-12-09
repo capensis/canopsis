fadda49ff04598e7799658aba59f3d3ba13105dd
Dmitry <Dmitriy.Mikhaylishin@noveogroup.com>
2020-12-09 19:05:39 +0700

    feature(#2799-add-first-and-last-name-into-users-crud): fixes after review

--- sources/webcore/src/canopsis-next/src/views/admin/users.vue
+++ sources/webcore/src/canopsis-next/src/views/admin/users.vue
@@ -1,7 +1,16 @@
 <template lang="pug">
   div
     the-page-header {{ $t('common.users') }}
-    users
+    v-card-text
+      users-list(
+        :users="users",
+        :total-items="usersMeta.total",
+        :pagination.sync="pagination",
+        :pending="usersPending",
+        @edit="showEditUserModal",
+        @remove="showRemoveUserModal",
+        @remove-selected="showRemoveSelectedUsersModal"
+      )
     fab-buttons(
       :has-access="hasCreateAnyUserAccess",
       @refresh="fetchList",
@@ -14,24 +23,26 @@
 import { MODALS } from '@/constants';
 
 import { prepareUserByData } from '@/helpers/entities';
+import { getUsersSearchByText } from '@/helpers/entities-search';
 
 import entitiesUserMixin from '@/mixins/entities/user';
 import rightsTechnicalUserMixin from '@/mixins/rights/technical/user';
+import localQueryMixin from '@/mixins/query-local/query';
+import authMixin from '@/mixins/auth';
 
-import Users from '@/components/other/users/users.vue';
+import UsersList from '@/components/other/users/users-list.vue';
 import FabButtons from '@/components/other/fab-buttons/fab-buttons.vue';
 
 export default {
   components: {
     FabButtons,
-    Users,
+    UsersList,
   },
-  mixins: [entitiesUserMixin, rightsTechnicalUserMixin],
+  mixins: [entitiesUserMixin, localQueryMixin, rightsTechnicalUserMixin, authMixin],
+  mounted() {
+    this.fetchList();
+  },
   methods: {
-    fetchList() {
-      return this.fetchUsersListWithPreviousParams();
-    },
-
     showCreateUserModal() {
       this.$modals.show({
         name: MODALS.createUser,
@@ -43,6 +54,85 @@ export default {
           },
         },
       });
+    },
+
+    showRemoveUserModal(user) {
+      this.$modals.show({
+        name: MODALS.confirmation,
+        config: {
+          action: async () => {
+            await this.removeUserWithPopup({ id: user._id });
+
+            await this.fetchList();
+          },
+        },
+      });
+    },
+
+    showRemoveSelectedUsersModal(selected) {
+      this.$modals.show({
+        name: MODALS.confirmation,
+        config: {
+          action: async () => {
+            await Promise.all(selected.map(({ _id }) => this.removeUser({ id: _id })));
+
+            this.$popups.success({ text: this.$t('success.default') });
+
+            await this.fetchList();
+          },
+        },
+      });
+    },
+
+    showEditUserModal(user) {
+      this.$modals.show({
+        name: MODALS.createUser,
+        config: {
+          title: this.$t('modals.editUser.title'),
+          user,
+          action: async (data) => {
+            await this.createUserWithPopup({ data: prepareUserByData(data, user) });
+
+            const requests = [this.fetchList()];
+
+            if (user._id === this.currentUser._id) {
+              requests.push(this.fetchCurrentUser());
+            }
+
+            await Promise.all(requests);
+          },
+        },
+      });
+    },
+
+    getQuery({
+      page,
+      search,
+      rowsPerPage,
+      sortKey,
+      sortDir,
+    } = this.query) {
+      const query = {};
+
+      query.limit = rowsPerPage;
+      query.start = (page - 1) * rowsPerPage;
+
+      if (sortKey) {
+        query.sort = [{
+          property: sortKey,
+          direction: sortDir,
+        }];
+      }
+
+      if (search) {
+        query.filter = { $and: [getUsersSearchByText(search)] };
+      }
+
+      return query;
+    },
+
+    fetchList() {
+      return this.fetchUsersList({ params: this.getQuery() });
     },
   },
 };
