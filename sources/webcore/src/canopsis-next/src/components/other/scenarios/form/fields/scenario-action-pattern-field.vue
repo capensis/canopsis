ff02a76d0e1fe43fe798f59fce298e17a1353cee
Dmitry <Dmitriy.Mikhaylishin@noveogroup.com>
2021-01-29 19:06:18 +0700

    feature(#2896-update-existing-actions-types-forms-to-new-format): required for patterns added

--- sources/webcore/src/canopsis-next/src/components/other/scenarios/form/fields/scenario-action-pattern-field.vue
+++ sources/webcore/src/canopsis-next/src/components/other/scenarios/form/fields/scenario-action-pattern-field.vue
@@ -1,11 +1,16 @@
 <template lang="pug">
   div
-    c-patterns-field(v-field="patterns", :disabled="disabled", :name="name", alarm, entity)
+    c-patterns-field(
+      v-field="patterns",
+      :disabled="disabled",
+      :name="name",
+      required,
+      alarm,
+      entity
+    )
 </template>
 
 <script>
-import { isEmpty } from 'lodash';
-
 import formValidationHeaderMixin from '@/mixins/form/validation-header';
 
 export default {
@@ -28,19 +33,6 @@ export default {
       type: String,
       default: 'patterns',
     },
-  },
-  mounted() {
-    this.$validator.attach({
-      name: this.name,
-      rules: 'required:true',
-      getter: () => !isEmpty(this.patterns.alarm_patterns)
-        || !isEmpty(this.patterns.entity_patterns),
-      context: () => this,
-      vm: this,
-    });
-  },
-  beforeDestroy() {
-    this.$validator.detach(this.name);
   },
 };
 </script>
