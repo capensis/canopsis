27ef553d823cd57d4a54ac86e5ceb5d77bc9284d
Dmitry <Dmitriy.Mikhaylishin@noveogroup.com>
2021-06-02 12:23:25 +0700

    feature/#3102(instruction-rating-interval-filter): timezone added

--- community/sources/webcore/src/canopsis-next/src/views/notification/instruction-stats.vue
+++ community/sources/webcore/src/canopsis-next/src/views/notification/instruction-stats.vue
@@ -16,6 +16,7 @@
 import { MODALS, DATETIME_FORMATS, DATETIME_INTERVAL_TYPES, QUICK_RANGES } from '@/constants';
 
 import { dateParse } from '@/helpers/date/date-intervals';
+import { convertDateToTimestampByTimezone } from '@/helpers/date/date';
 
 import { authMixin } from '@/mixins/auth';
 import { localQueryMixin } from '@/mixins/query-local/query';
@@ -25,6 +26,7 @@ import { entitiesRemediationInstructionsMixin } from '@/mixins/entities/remediat
 import RemediationInstructionStatsList from '@/components/other/remediation/instruction-stats/remediation-instruction-stats-list.vue';
 
 export default {
+  inject: ['$system'],
   components: {
     RemediationInstructionStatsList,
   },
@@ -66,16 +68,16 @@ export default {
       const params = this.getQuery();
       params.with_flags = true;
 
-      params.from = dateParse(
+      params.from = convertDateToTimestampByTimezone(dateParse(
         this.pagination.interval.from,
         DATETIME_INTERVAL_TYPES.start,
         DATETIME_FORMATS.datePicker,
-      ).unix();
-      params.to = dateParse(
+      ), this.$system.timezone);
+      params.to = convertDateToTimestampByTimezone(dateParse(
         this.pagination.interval.to,
         DATETIME_INTERVAL_TYPES.stop,
         DATETIME_FORMATS.datePicker,
-      ).unix();
+      ), this.$system.timezone);
 
       this.fetchRemediationInstructionStatsList({ params });
     },
