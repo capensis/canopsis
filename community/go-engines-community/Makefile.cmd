dd615587fafa2a1eda1153cd5942ce991694664c
Donovan Watteau <dwatteau@capensis.fr>
2021-06-17 17:20:17 +0200

    fix(build): Repair the Pro/Community build even more
    Use a WORKDIR that plays nicely with our go.mod trick, and make the
    Pro makefiles a bit less stupid.
    
    As a side effect, the Makefiles in Canopsis Pro are still as
    repetitive as before, but they now only build 6 times instead of 36!
    
    Also make the makefiles more verbose. Don't hide build things (at
    least not without a VERBOSE flag).

--- community/go-engines-community/Makefile.cmd
+++ community/go-engines-community/Makefile.cmd
@@ -5,17 +5,17 @@ BINARY:=${PROJECT}
 
 build:
 	@echo "Building ${PROJECT} project"
-	@env CGO_ENABLED=1 GO111MODULE=on go build -trimpath -o "${OUTPUT_DIR}/${BINARY}" ${LDFLAGS}
+	env CGO_ENABLED=1 GO111MODULE=on go build -trimpath -o "${OUTPUT_DIR}/${BINARY}" ${LDFLAGS}
 
 clean:
 	@echo "Cleaning ${PROJECT} project"
-	@rm -rf ${OUTPUT_DIR}/${BINARY}
+	rm -rf ${OUTPUT_DIR}/${BINARY}
 
 docker_image:
 	@echo "Building ${PROJECT} project docker image"
-	@export GOLANG_IMAGE_TAG="${GOLANG_IMAGE_TAG}" BINARY="${BINARY}" TAG="${TAG}" && \
+	export GOLANG_IMAGE_TAG="${GOLANG_IMAGE_TAG}" BINARY="${BINARY}" TAG="${TAG}" && \
 		cd "${ROOT_DIR}" && ./docker/build-cmd-image.sh
 
 test:
 	@echo "Testing ${PROJECT} project"
-	@go test ${GO_TEST_PARAM}
+	go test ${GO_TEST_PARAM}
--- community/go-engines-community/docker/swagger-init.sh
+++ community/go-engines-community/docker/swagger-init.sh
@@ -3,7 +3,7 @@ set -e
 set -o pipefail
 
 build_image="swagger-init"
-build_dir="/go/src/canopsis/docs"
+build_dir="/go/src/canopsis-community/docs"
 
 docker build -f docker/swagger-init.Dockerfile -t "$build_image" . && \
     docker run --rm -v "$(pwd)/docs:$build_dir" "$build_image" && \
