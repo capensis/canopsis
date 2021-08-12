f9efd59ae8bb6c88b631dfab73193f3e6cbdf05a
ikalugin <ivan.kalugin@noveogroup.com>
2021-08-12 18:58:18 +0700

    add eventfilter migration

---
+++ community/go-engines-community/database/migrations/eventfilterV2/002_migrate_event_filters.js
@@ -0,0 +1,30 @@
+db.eventfilter.find({
+    "type": "enrichment"
+}).forEach(function (doc) {
+    doc.actions.forEach(function (action) {
+        if (action.from !== undefined) {
+            action.value = action.from
+            delete action.from
+        }
+
+        if (action.to !== undefined) {
+            action.name = action.to
+            delete action.to
+        }
+    })
+
+    db.eventfilter.updateOne({_id: doc._id}, {
+        $set: {
+            config: {
+                actions: doc.actions,
+                on_success: doc.on_success,
+                on_failure: doc.on_failure
+            }
+        },
+        $unset: {
+            actions: "",
+            on_success: "",
+            on_failure: ""
+        }
+    });
+})
