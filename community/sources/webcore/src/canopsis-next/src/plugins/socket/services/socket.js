f3401ef200f6f9c5f6acefd1fcb74ccb7a0fbbc5
Dmitry <Dmitriy.Mikhaylishin@noveogroup.com>
2021-09-14 16:28:46 +0700

    feature(#3807): socket reconnect fixed

--- community/sources/webcore/src/canopsis-next/src/plugins/socket/services/socket.js
+++ community/sources/webcore/src/canopsis-next/src/plugins/socket/services/socket.js
@@ -53,7 +53,8 @@ class Socket {
     this.reconnectsCount += 1;
     this.disconnect();
     this.connect(this.url, this.protocols);
-    this.rooms.forEach(({ name }) => this.join(name));
+
+    Object.keys(this.rooms).forEach(name => this.join(name));
 
     return this;
   }
@@ -73,7 +74,6 @@ class Socket {
 
     return this;
   }
-
 
   /**
    * Add event listener for connection
