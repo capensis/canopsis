6c4e0374a7b4f4d264f0459cbce961b5c65a5143
Alix Biton <abiton@capensis.fr>
2021-09-20 15:19:41 +0200

    Add NGINX Docker Https config

--- community/docker/files/entrypoint-nginx.sh
+++ community/docker/files/entrypoint-nginx.sh
@@ -1,6 +1,19 @@
 #!/bin/sh
 set -eu
 
+if [ "$CPS_ENABLE_HTTPS" = "true" ]
+then
+	sed -i -e "s,#include /etc/nginx/https.inc;,include /etc/nginx/https.inc;,g" /etc/nginx/conf.d/default.conf
+	if [ ! -f /etc/nginx/ssl/cert.crt ] && [ ! -f /etc/nginx/ssl/key.key ]
+	then
+		openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+			-keyout /etc/nginx/ssl/key.key \
+			-out /etc/nginx/ssl/cert.crt \
+			-subj "/CN=${CPS_SERVER_NAME}"
+	fi
+fi
+
 sed -i -e "s,{{ CPS_API_URL }},$CPS_API_URL,g" /etc/nginx/conf.d/default.conf
+sed -i -e "s,{{ CPS_SERVER_NAME }},$CPS_SERVER_NAME,g" /etc/nginx/conf.d/default.conf
 echo "resolver $(awk 'BEGIN{ORS=" "} $1=="nameserver" {print $2}' /etc/resolv.conf) valid=20s;" > /etc/nginx/resolvers.inc
 exec nginx -g 'daemon off;'
