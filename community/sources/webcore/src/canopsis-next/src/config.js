cd0c5854bcca1e4f6e324d8ee397e3d818e8fe5a
Vladimir Erokhov <vladimir.erokhov@noveogroup.com>
2021-09-28 14:03:01 +0700

    feature(#3878): Finished issue

--- community/sources/webcore/src/canopsis-next/src/config.js
+++ community/sources/webcore/src/canopsis-next/src/config.js
@@ -101,6 +101,7 @@ export const API_ROUTES = {
   roles: '/api/v4/roles',
   eventFilterRules: '/api/v4/eventfilter/rules',
   file: '/api/v4/file',
+  fileAccess: '/api/v4/file-access',
   snmpRule: {
     list: '/snmprule',
     create: '/snmprule/put',
