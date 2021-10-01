b6d2a8c6bdce41be7824d8dc5c63a121e8faf121
ikalugin <ivan.kalugin@noveogroup.com>
2021-10-01 21:38:31 +0700

    add some variables

--- community/go-engines-community/lib/canopsis/metrics/sender.go
+++ community/go-engines-community/lib/canopsis/metrics/sender.go
@@ -86,12 +86,16 @@ func (s *sender) HandleMetricsByEvent(event types.Event) {
 	if alarm == nil || entity == nil || alarmChange == nil {
 		return
 	}
+	
+	entityID := alarm.EntityID
+	category := entity.Category
+	author := event.Author
 
 	switch alarmChange.Type {
 	case types.AlarmChangeTypeCreate:
 		s.sendMetric(Event{
 			Type:   TotalAlarmNumberEntity,
-			Labels: prometheus.Labels{"entityID": alarm.EntityID, "category": entity.Category},
+			Labels: prometheus.Labels{"entityID": entityID, "category": category},
 		})
 
 		s.sendMetric(Event{
@@ -101,7 +105,7 @@ func (s *sender) HandleMetricsByEvent(event types.Event) {
 	case types.AlarmChangeTypeCreateAndPbhEnter:
 		s.sendMetric(Event{
 			Type:   PbhAlarmNumberEntity,
-			Labels: prometheus.Labels{"entityID": alarm.EntityID, "category": entity.Category},
+			Labels: prometheus.Labels{"entityID": entityID, "category": category},
 		})
 
 		s.sendMetric(Event{
@@ -123,12 +127,12 @@ func (s *sender) HandleMetricsByEvent(event types.Event) {
 
 		s.sendMetric(Event{
 			Type:   TicketAlarmNumberEntity,
-			Labels: prometheus.Labels{"entityID": alarm.EntityID, "category": entity.Category, "username": event.Author},
+			Labels: prometheus.Labels{"entityID": entityID, "category": category, "username": author},
 		})
 
 		s.sendMetric(Event{
 			Type:   TicketAlarmNumberSlice,
-			Labels: prometheus.Labels{"slice": DefaultSlice, "username": event.Author},
+			Labels: prometheus.Labels{"slice": DefaultSlice, "username": author},
 		})
 	case types.AlarmChangeTypeAck:
 		if alarm.Value.ACK == nil {
@@ -137,35 +141,35 @@ func (s *sender) HandleMetricsByEvent(event types.Event) {
 
 		s.sendMetric(Event{
 			Type:   AckAlarmNumberEntity,
-			Labels: prometheus.Labels{"entityID": alarm.EntityID, "category": entity.Category, "username": event.Author},
+			Labels: prometheus.Labels{"entityID": entityID, "category": category, "username": author},
 		})
 
 		s.sendMetric(Event{
 			Type:   AckAlarmNumberSlice,
-			Labels: prometheus.Labels{"slice": DefaultSlice, "username": event.Author},
+			Labels: prometheus.Labels{"slice": DefaultSlice, "username": author},
 		})
 
 		ackDuration := alarm.Value.ACK.Timestamp.Sub(alarm.Value.CreationDate.Time).Seconds()
 		s.sendMetric(Event{
 			Type:       AckDurationEntity,
-			Labels:     prometheus.Labels{"entityID": alarm.EntityID, "category": entity.Category, "username": event.Author},
+			Labels:     prometheus.Labels{"entityID": entityID, "category": category, "username": author},
 			Parameters: Parameters{Value: ackDuration},
 		})
 
 		s.sendMetric(Event{
 			Type:       AckDurationSlice,
-			Labels:     prometheus.Labels{"slice": DefaultSlice, "username": event.Author},
+			Labels:     prometheus.Labels{"slice": DefaultSlice, "username": author},
 			Parameters: Parameters{Value: ackDuration},
 		})
 	case types.AlarmChangeTypeAckremove:
 		s.sendMetric(Event{
 			Type:   CancelAckAlarmNumberEntity,
-			Labels: prometheus.Labels{"entityID": alarm.EntityID, "category": entity.Category, "username": event.Author},
+			Labels: prometheus.Labels{"entityID": entityID, "category": category, "username": author},
 		})
 
 		s.sendMetric(Event{
 			Type:   CancelAckAlarmNumberSlice,
-			Labels: prometheus.Labels{"slice": DefaultSlice, "username": event.Author},
+			Labels: prometheus.Labels{"slice": DefaultSlice, "username": author},
 		})
 	case types.AlarmChangeTypeAutoInstructionStart:
 		foundStep := false
@@ -182,7 +186,7 @@ func (s *sender) HandleMetricsByEvent(event types.Event) {
 
 		s.sendMetric(Event{
 			Type:   InstructionAlarmNumberEntity,
-			Labels: prometheus.Labels{"entityID": alarm.EntityID, "category": entity.Category},
+			Labels: prometheus.Labels{"entityID": entityID, "category": category},
 		})
 
 		s.sendMetric(Event{
@@ -197,7 +201,7 @@ func (s *sender) HandleMetricsByEvent(event types.Event) {
 		resolveDuration := alarm.Value.Resolved.Sub(alarm.Value.CreationDate.Time).Seconds()
 		s.sendMetric(Event{
 			Type:       ResolveDurationEntity,
-			Labels:     prometheus.Labels{"entityID": alarm.EntityID, "category": entity.Category},
+			Labels:     prometheus.Labels{"entityID": entityID, "category": category},
 			Parameters: Parameters{Value: resolveDuration},
 		})
 
