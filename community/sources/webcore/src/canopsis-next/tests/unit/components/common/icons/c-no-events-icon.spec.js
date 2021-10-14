b328563f09c1c85a4a0e30668d6078860a6ddd01
Dmitry <Dmitriy.Mikhaylishin@noveogroup.com>
2021-10-14 20:09:49 +0700

    feature(#3974): fixes after review

--- community/sources/webcore/src/canopsis-next/tests/unit/components/common/icons/c-no-events-icon.spec.js
+++ community/sources/webcore/src/canopsis-next/tests/unit/components/common/icons/c-no-events-icon.spec.js
@@ -6,12 +6,12 @@ import CNoEventsIcon from '@/components/common/icons/c-no-events-icon.vue';
 const localVue = createVueInstance();
 
 const mockData = {
-  timestamp: 1386435600000, // Sun Dec 08 2013 00:00:00
-  now: 1600362123456, // Fri Sep 18 2020 00:02:03
+  firstTimestamp: 1386435600000, // Sun Dec 08 2013 00:00:00
+  secondTimestamp: 1600362123456, // Fri Sep 18 2020 00:02:03
 };
 
 describe('c-no-events-icon', () => {
-  stubDateNow(mockData.now);
+  stubDateNow(mockData.secondTimestamp);
 
   it('Renders `c-no-events-icon` with default props correctly', () => {
     const wrapper = mount(CNoEventsIcon, { localVue });
@@ -23,7 +23,7 @@ describe('c-no-events-icon', () => {
     const wrapper = mount(CNoEventsIcon, {
       localVue,
       propsData: {
-        value: mockData.timestamp,
+        value: mockData.firstTimestamp,
       },
     });
 
@@ -34,7 +34,7 @@ describe('c-no-events-icon', () => {
     const wrapper = mount(CNoEventsIcon, {
       localVue,
       propsData: {
-        value: mockData.timestamp,
+        value: mockData.firstTimestamp,
         color: 'secondary',
         maxWidth: 1120,
         top: true,
