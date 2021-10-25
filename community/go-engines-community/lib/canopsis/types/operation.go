b3914f2e3e3cc9ff2d6fd818758de6f1f0f4d8ce
Zarina Yakubova <Zarina.Yakubova@noveogroup.com>
2021-10-25 18:07:15 +0700

    Fix #3962 : Fix action templates

--- community/go-engines-community/lib/canopsis/types/operation.go
+++ community/go-engines-community/lib/canopsis/types/operation.go
@@ -1,18 +1,9 @@
 package types
 
-import (
-	"strings"
-	"text/template"
-)
-
 // Operation represents alarm modification operation.
 type Operation struct {
 	Type       string      `bson:"type"`
 	Parameters interface{} `bson:"parameters,omitempty"`
-}
-
-type Templater interface {
-	Template(data interface{}) error
 }
 
 // OperationParameters represents default operation parameters.
@@ -21,40 +12,10 @@ type OperationParameters struct {
 	Author string `bson:"author" json:"author"`
 }
 
-func (p *OperationParameters) Template(data interface{}) error {
-	output, err := renderTemplate(p.Output, data, GetTemplateFunc())
-	if err != nil {
-		return err
-	}
-	p.Output = output
-	author, err := renderTemplate(p.Author, data, GetTemplateFunc())
-	if err != nil {
-		return err
-	}
-	p.Author = author
-
-	return nil
-}
-
 type OperationAssocTicketParameters struct {
 	Ticket string `bson:"ticket" json:"ticket"`
 	Output string `bson:"output" json:"output"`
 	Author string `bson:"author" json:"author"`
-}
-
-func (p *OperationAssocTicketParameters) Template(data interface{}) error {
-	output, err := renderTemplate(p.Output, data, GetTemplateFunc())
-	if err != nil {
-		return err
-	}
-	p.Output = output
-	author, err := renderTemplate(p.Author, data, GetTemplateFunc())
-	if err != nil {
-		return err
-	}
-	p.Author = author
-
-	return nil
 }
 
 type OperationSnoozeParameters struct {
@@ -63,40 +24,10 @@ type OperationSnoozeParameters struct {
 	Author   string           `bson:"author" json:"author"`
 }
 
-func (p *OperationSnoozeParameters) Template(data interface{}) error {
-	output, err := renderTemplate(p.Output, data, GetTemplateFunc())
-	if err != nil {
-		return err
-	}
-	p.Output = output
-	author, err := renderTemplate(p.Author, data, GetTemplateFunc())
-	if err != nil {
-		return err
-	}
-	p.Author = author
-
-	return nil
-}
-
 type OperationChangeStateParameters struct {
 	State  CpsNumber `bson:"state" json:"state"`
 	Output string    `bson:"output" json:"output"`
 	Author string    `bson:"author" json:"author"`
-}
-
-func (p *OperationChangeStateParameters) Template(data interface{}) error {
-	output, err := renderTemplate(p.Output, data, GetTemplateFunc())
-	if err != nil {
-		return err
-	}
-	p.Output = output
-	author, err := renderTemplate(p.Author, data, GetTemplateFunc())
-	if err != nil {
-		return err
-	}
-	p.Author = author
-
-	return nil
 }
 
 type OperationDeclareTicketParameters struct {
@@ -106,36 +37,8 @@ type OperationDeclareTicketParameters struct {
 	Author string            `bson:"author" json:"author"`
 }
 
-func (p *OperationDeclareTicketParameters) Template(data interface{}) error {
-	output, err := renderTemplate(p.Output, data, GetTemplateFunc())
-	if err != nil {
-		return err
-	}
-	p.Output = output
-	author, err := renderTemplate(p.Author, data, GetTemplateFunc())
-	if err != nil {
-		return err
-	}
-	p.Author = author
-
-	return nil
-}
-
 type OperationPbhParameters struct {
 	PbehaviorInfo PbehaviorInfo `json:"pbehavior_info"`
 	Output        string        `json:"output"`
 	Author        string        `json:"author"`
-}
-
-func renderTemplate(templateStr string, data interface{}, f template.FuncMap) (string, error) {
-	t, err := template.New("template").Funcs(f).Parse(templateStr)
-	if err != nil {
-		return "", err
-	}
-	b := strings.Builder{}
-	err = t.Execute(&b, data)
-	if err != nil {
-		return "", err
-	}
-	return b.String(), nil
 }
