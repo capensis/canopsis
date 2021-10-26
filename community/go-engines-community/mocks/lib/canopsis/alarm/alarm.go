265e70d3576aeb1d7cdd5f23b3c8d1c8d5f3dd7d
Zarina Yakubova <Zarina.Yakubova@noveogroup.com>
2021-10-26 17:23:30 +0700

    Fix merge conflicts

--- community/go-engines-community/mocks/lib/canopsis/alarm/alarm.go
+++ community/go-engines-community/mocks/lib/canopsis/alarm/alarm.go
@@ -388,9 +388,24 @@ func (m *MockAdapter) GetOpenedAlarmsWithEntity(arg0 context.Context) (mongo.Cur
 }
 
 // GetOpenedAlarmsWithEntity indicates an expected call of GetOpenedAlarmsWithEntity
-func (mr *MockAdapterMockRecorder) GetOpenedAlarmsWithEntity(arg0, arg1 interface{}) *gomock.Call {
+func (mr *MockAdapterMockRecorder) GetOpenedAlarmsWithEntity(arg0 interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
-	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetOpenedAlarmsWithEntity", reflect.TypeOf((*MockAdapter)(nil).GetOpenedAlarmsWithEntity), arg0, arg1)
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetOpenedAlarmsWithEntity", reflect.TypeOf((*MockAdapter)(nil).GetOpenedAlarmsWithEntity), arg0)
+}
+
+// GetOpenedAlarmsWithEntityAfter mocks base method
+func (m *MockAdapter) GetOpenedAlarmsWithEntityAfter(arg0 context.Context, arg1 types.CpsTime) (mongo.Cursor, error) {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "GetOpenedAlarmsWithEntityAfter", arg0, arg1)
+	ret0, _ := ret[0].(mongo.Cursor)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// GetOpenedAlarmsWithEntityAfter indicates an expected call of GetOpenedAlarmsWithEntityAfter
+func (mr *MockAdapterMockRecorder) GetOpenedAlarmsWithEntityAfter(arg0, arg1 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetOpenedAlarmsWithEntityAfter", reflect.TypeOf((*MockAdapter)(nil).GetOpenedAlarmsWithEntityAfter), arg0, arg1)
 }
 
 // GetOpenedAlarmsWithEntityByAlarmIDs mocks base method
