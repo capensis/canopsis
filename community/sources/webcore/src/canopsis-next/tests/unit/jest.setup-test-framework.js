fab1d7e6c5812c070acf2d9da766fb5fc9b814b7
Vladimir Erokhov <vladimir.erokhov@noveogroup.com>
2021-10-27 12:00:29 +0700

    feature(#3974): Started doing the issue

--- community/sources/webcore/src/canopsis-next/tests/unit/jest.setup-test-framework.js
+++ community/sources/webcore/src/canopsis-next/tests/unit/jest.setup-test-framework.js
@@ -1,3 +1,4 @@
+import { kebabCase } from 'lodash';
 import registerRequireContextHook from 'babel-plugin-require-context-hook/register';
 import { toMatchImageSnapshot } from 'jest-image-snapshot';
 import ResizeObserver from 'resize-observer-polyfill';
@@ -6,12 +7,23 @@ registerRequireContextHook();
 
 global.ResizeObserver = ResizeObserver;
 
+/**
+ * @typedef {object} expect
+ */
+
 expect.extend({
   toMatchImageSnapshot,
-  toMatchCanvasSnapshot(canvas, ...args) {
+  toMatchCanvasSnapshot(canvas, options, ...args) {
     const img = canvas.toDataURL();
     const data = img.replace(/^data:image\/(png|jpg);base64,/, '');
+    const newOptions = {
+      customSnapshotIdentifier: ({ currentTestName, counter }) => (
+        kebabCase(`${currentTestName.replace(/(.*\sRenders\s)|(.$)/g, '')}-${counter}`)
+      ),
 
-    return toMatchImageSnapshot.call(this, data, ...args);
+      ...options,
+    };
+
+    return toMatchImageSnapshot.call(this, data, newOptions, ...args);
   },
 });
