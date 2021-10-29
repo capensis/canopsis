f76b22ba57611e9c54c0bd9c7fea53981336680a
Donovan Watteau <dwatteau@capensis.fr>
2021-10-29 11:37:08 +0200

    fix(build): Try "make -e test"

--- community/go-engines-community/go-test.sh
+++ community/go-engines-community/go-test.sh
@@ -2,5 +2,10 @@
 
 [ -f Makefile ] || cd community/go-engines-community
 
-make test
+if [ -z "$CPS_MONGO_URL" ]; then
+	echo "WARNING: no CPS_MONGO_URL variable found in your environment, this will probably fail..." >&2
+	sleep 5
+fi
+
+make -e test
 exit $?
