0ca9403c6296601a2ab973d4af0b3e6e209aee64
Dmitry <Dmitriy.Mikhaylishin@noveogroup.com>
2021-11-03 15:03:52 +0700

    feature(#3918): sampling field added

---
+++ community/sources/webcore/src/canopsis-next/src/components/forms/fields/c-sampling-field.vue
@@ -0,0 +1,46 @@
+<template lang="pug">
+  v-select(
+    v-field="value",
+    :items="samplings",
+    :disabled="disabled",
+    :label="label || $t('common.sampling')",
+    :name="name"
+  )
+</template>
+
+<script>
+import { SAMPLINGS } from '@/constants';
+
+export default {
+  model: {
+    prop: 'value',
+    event: 'input',
+  },
+  props: {
+    value: {
+      type: String,
+      required: true,
+    },
+    label: {
+      type: String,
+      default: '',
+    },
+    name: {
+      type: String,
+      default: 'sampling',
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  computed: {
+    samplings() {
+      return Object.values(SAMPLINGS).map(value => ({
+        value,
+        text: this.$tc(`common.samplings.${value}`),
+      }));
+    },
+  },
+};
+</script>
