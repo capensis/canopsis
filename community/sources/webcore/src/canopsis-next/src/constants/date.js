06a2194eb17adf17cde76a594e30130fe93582cd
Dmitry <Dmitriy.Mikhaylishin@noveogroup.com>
2021-11-08 16:00:40 +0700

    feature(#3918): sampling constant moved

--- community/sources/webcore/src/canopsis-next/src/constants/date.js
+++ community/sources/webcore/src/canopsis-next/src/constants/date.js
@@ -102,3 +102,10 @@ export const DATETIME_INTERVAL_TYPES = {
   start: 'start',
   stop: 'stop',
 };
+
+export const SAMPLINGS = {
+  hour: 'hour',
+  day: 'day',
+  week: 'week',
+  month: 'month',
+};
