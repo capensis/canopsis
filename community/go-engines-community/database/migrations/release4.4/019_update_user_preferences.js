4d3170aa26db009059e69bf16a203666b48d23d0
Zarina Yakubova <Zarina.Yakubova@noveogroup.com>
2021-11-11 10:45:41 +0700

    Feature #52 : Fix migration

--- community/go-engines-community/database/migrations/release4.4/019_update_user_preferences.js
+++ community/go-engines-community/database/migrations/release4.4/019_update_user_preferences.js
@@ -1,10 +1,13 @@
 (function () {
     db.userpreferences.find().forEach(function (doc) {
+        var userId = doc._id.replace(doc.widget_id + "_", "")
+
         db.userpreferences.updateOne({_id: doc._id}, {
             $set: {
-                user: doc.name,
+                user: userId,
                 widget: doc.widget_id,
                 content: doc.widget_preferences,
+                updated: doc.crecord_write_time,
             },
             $unset: {
                 name: "",
