1513bc7a7c7715688b49cb2e1bd16312d09fced6
ikalugin <ivan.kalugin@noveogroup.com>
2021-11-12 13:47:11 +0700

    add docs

--- community/go-engines-community/docs/docs.go
+++ community/go-engines-community/docs/docs.go
@@ -8148,6 +8148,12 @@ var doc = `{
                         "description": "sort query",
                         "name": "sort_by",
                         "in": "query"
+                    },
+                    {
+                        "type": "boolean",
+                        "description": "show assigned instructions and execution flags",
+                        "name": "with_instructions",
+                        "in": "query"
                     }
                 ],
                 "responses": {
@@ -12213,6 +12219,12 @@ var doc = `{
                 "alarm_display_name": {
                     "type": "string"
                 },
+                "assigned_instructions": {
+                    "type": "array",
+                    "items": {
+                        "$ref": "#/definitions/alarm.InstructionWithAlarms"
+                    }
+                },
                 "category": {
                     "type": "object",
                     "$ref": "#/definitions/entitycategory.Category"
@@ -12244,7 +12256,16 @@ var doc = `{
                         "$ref": "#/definitions/serviceweather.Info"
                     }
                 },
+                "is_all_auto_instructions_completed": {
+                    "type": "boolean"
+                },
+                "is_auto_instruction_running": {
+                    "type": "boolean"
+                },
                 "is_grey": {
+                    "type": "boolean"
+                },
+                "is_manual_instruction_waiting_result": {
                     "type": "boolean"
                 },
                 "last_update_date": {
