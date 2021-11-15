2bc6fb90462c6066f52daef1a27c977363a5a3cd
Dmitry <Dmitriy.Mikhaylishin@noveogroup.com>
2021-11-15 18:55:56 +0700

    feature(#3923): alarms filters added

---
+++ community/sources/webcore/src/canopsis-next/src/components/other/kpi/charts/partials/kpi-alarms-filters.vue
@@ -0,0 +1,39 @@
+<template lang="pug">
+  div.kpi-alarms-toolbar
+    v-layout.ml-4.mt-4(align-content-end, wrap)
+      c-quick-date-interval-field.mr-4(v-field="query.interval")
+      c-sampling-field.mr-4.kpi-alarms-toolbar__sampling(v-field="query.sampling")
+      c-filters-field.mr-4.kpi-alarms-toolbar__filters(v-field="query.filter")
+      c-alarm-metric-parameters-field.kpi-alarms-toolbar__parameters(v-field="query.parameters")
+</template>
+
+<script>
+export default {
+  model: {
+    prop: 'query',
+    event: 'input',
+  },
+  props: {
+    query: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.kpi-alarms-toolbar {
+  &__sampling {
+    max-width: 200px;
+  }
+
+  &__filters {
+    max-width: 200px;
+  }
+
+  &__parameters {
+    max-width: 300px;
+  }
+}
+</style>
