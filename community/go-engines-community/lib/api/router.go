6eb5d12dee0aaa41c94573d30e956f640117dfae
Zarina Yakubova <Zarina.Yakubova@noveogroup.com>
2021-11-19 18:58:22 +0700

    Feature #52 : Migrate user preferences to API v4

--- community/go-engines-community/lib/api/router.go
+++ community/go-engines-community/lib/api/router.go
@@ -40,6 +40,7 @@ import (
 	"git.canopsis.net/canopsis/canopsis-community/community/go-engines-community/lib/api/sessionstats"
 	"git.canopsis.net/canopsis/canopsis-community/community/go-engines-community/lib/api/statesettings"
 	"git.canopsis.net/canopsis/canopsis-community/community/go-engines-community/lib/api/user"
+	"git.canopsis.net/canopsis/canopsis-community/community/go-engines-community/lib/api/userpreferences"
 	"git.canopsis.net/canopsis/canopsis-community/community/go-engines-community/lib/api/view"
 	"git.canopsis.net/canopsis/canopsis-community/community/go-engines-community/lib/api/viewgroup"
 	"git.canopsis.net/canopsis/canopsis-community/community/go-engines-community/lib/canopsis/config"
@@ -149,6 +150,14 @@ func RegisterRoutes(
 
 		protected.GET("/account/me", account.NewApi(account.NewStore(dbClient)).Me)
 		protected.GET("/sessions-count", authApi.GetSessionsCount())
+
+		userPreferencesRouter := protected.Group("/user-preferences")
+		{
+			userPreferencesRouter.Use(middleware.OnlyAuth())
+			userPreferencesApi := userpreferences.NewApi(userpreferences.NewStore(dbClient), actionLogger)
+			userPreferencesRouter.GET("/:id", userPreferencesApi.Get)
+			userPreferencesRouter.PUT("", userPreferencesApi.Update)
+		}
 
 		userRouter := protected.Group("/users")
 		{
