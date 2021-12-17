a5cea678aaac93423baec0eb827f15a1b3b8a55c
Zarina Yakubova <Zarina.Yakubova@noveogroup.com>
2021-12-17 19:01:10 +0700

    Feature #4076 : Update mock dependency

--- community/go-engines-community/mocks/lib/canopsis/encoding/encoding.go
+++ community/go-engines-community/mocks/lib/canopsis/encoding/encoding.go
@@ -5,34 +5,35 @@
 package mock_encoding
 
 import (
-	gomock "github.com/golang/mock/gomock"
 	reflect "reflect"
+
+	gomock "github.com/golang/mock/gomock"
 )
 
-// MockEncoder is a mock of Encoder interface
+// MockEncoder is a mock of Encoder interface.
 type MockEncoder struct {
 	ctrl     *gomock.Controller
 	recorder *MockEncoderMockRecorder
 }
 
-// MockEncoderMockRecorder is the mock recorder for MockEncoder
+// MockEncoderMockRecorder is the mock recorder for MockEncoder.
 type MockEncoderMockRecorder struct {
 	mock *MockEncoder
 }
 
-// NewMockEncoder creates a new mock instance
+// NewMockEncoder creates a new mock instance.
 func NewMockEncoder(ctrl *gomock.Controller) *MockEncoder {
 	mock := &MockEncoder{ctrl: ctrl}
 	mock.recorder = &MockEncoderMockRecorder{mock}
 	return mock
 }
 
-// EXPECT returns an object that allows the caller to indicate expected use
+// EXPECT returns an object that allows the caller to indicate expected use.
 func (m *MockEncoder) EXPECT() *MockEncoderMockRecorder {
 	return m.recorder
 }
 
-// Encode mocks base method
+// Encode mocks base method.
 func (m *MockEncoder) Encode(arg0 interface{}) ([]byte, error) {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "Encode", arg0)
@@ -41,36 +42,36 @@ func (m *MockEncoder) Encode(arg0 interface{}) ([]byte, error) {
 	return ret0, ret1
 }
 
-// Encode indicates an expected call of Encode
+// Encode indicates an expected call of Encode.
 func (mr *MockEncoderMockRecorder) Encode(arg0 interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Encode", reflect.TypeOf((*MockEncoder)(nil).Encode), arg0)
 }
 
-// MockDecoder is a mock of Decoder interface
+// MockDecoder is a mock of Decoder interface.
 type MockDecoder struct {
 	ctrl     *gomock.Controller
 	recorder *MockDecoderMockRecorder
 }
 
-// MockDecoderMockRecorder is the mock recorder for MockDecoder
+// MockDecoderMockRecorder is the mock recorder for MockDecoder.
 type MockDecoderMockRecorder struct {
 	mock *MockDecoder
 }
 
-// NewMockDecoder creates a new mock instance
+// NewMockDecoder creates a new mock instance.
 func NewMockDecoder(ctrl *gomock.Controller) *MockDecoder {
 	mock := &MockDecoder{ctrl: ctrl}
 	mock.recorder = &MockDecoderMockRecorder{mock}
 	return mock
 }
 
-// EXPECT returns an object that allows the caller to indicate expected use
+// EXPECT returns an object that allows the caller to indicate expected use.
 func (m *MockDecoder) EXPECT() *MockDecoderMockRecorder {
 	return m.recorder
 }
 
-// Decode mocks base method
+// Decode mocks base method.
 func (m *MockDecoder) Decode(arg0 []byte, arg1 interface{}) error {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "Decode", arg0, arg1)
@@ -78,7 +79,7 @@ func (m *MockDecoder) Decode(arg0 []byte, arg1 interface{}) error {
 	return ret0
 }
 
-// Decode indicates an expected call of Decode
+// Decode indicates an expected call of Decode.
 func (mr *MockDecoderMockRecorder) Decode(arg0, arg1 interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Decode", reflect.TypeOf((*MockDecoder)(nil).Decode), arg0, arg1)
