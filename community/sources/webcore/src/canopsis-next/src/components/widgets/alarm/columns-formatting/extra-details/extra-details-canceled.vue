0055eac3e8fb9f55ce427e0f757ad65035345c20
Dmitry <Dmitriy.Mikhaylishin@noveogroup.com>
2022-01-13 19:35:57 +0700

    feature(#4073): extra details tested

---
+++ community/sources/webcore/src/canopsis-next/src/components/widgets/alarm/columns-formatting/extra-details/extra-details-canceled.vue
@@ -0,0 +1,39 @@
+<template lang="pug">
+  div
+    v-tooltip.c-extra-details(top)
+      v-icon.c-extra-details__badge.blue-grey.white--text(
+        small,
+        slot="activator"
+      ) {{ icon }}
+      div.text-md-center
+        strong {{ $t('alarmList.actions.iconsTitles.canceled') }}
+        div {{ $t('common.by') }} : {{ canceled.a }}
+        div {{ $t('common.date') }} : {{ date }}
+        div.c-extra-details__message(
+          v-if="canceled.m"
+        ) {{ $tc('common.comment') }} : {{ canceled.m }}
+</template>
+
+<script>
+import { EVENT_ENTITY_STYLE, EVENT_ENTITY_TYPES } from '@/constants';
+
+import { convertDateToStringWithFormatForToday } from '@/helpers/date/date';
+
+export default {
+  props: {
+    canceled: {
+      type: Object,
+      required: true,
+    },
+  },
+  computed: {
+    date() {
+      return convertDateToStringWithFormatForToday(this.canceled.t);
+    },
+
+    icon() {
+      return EVENT_ENTITY_STYLE[EVENT_ENTITY_TYPES.delete].icon;
+    },
+  },
+};
+</script>
