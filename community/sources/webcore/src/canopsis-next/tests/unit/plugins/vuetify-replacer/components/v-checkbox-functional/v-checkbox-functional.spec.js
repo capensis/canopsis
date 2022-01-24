8b1166da31029422ca603900cca49d26baa59f65
Dmitry <Dmitriy.Mikhaylishin@noveogroup.com>
2022-01-24 17:49:28 +0700

    feature(#4071): v-checkbox-functional tested

---
+++ community/sources/webcore/src/canopsis-next/tests/unit/plugins/vuetify-replacer/components/v-checkbox-functional/v-checkbox-functional.spec.js
@@ -0,0 +1,73 @@
+import { mount, createVueInstance } from '@unit/utils/vue';
+
+import VCheckboxFunctional from '@/plugins/vuetify-replacer/components/v-checkbox-functional/v-checkbox-functional.vue';
+
+const localVue = createVueInstance();
+
+const snapshotFactory = (options = {}) => mount(VCheckboxFunctional, {
+  localVue,
+
+  ...options,
+});
+
+const selectControlField = wrapper => wrapper.find('.v-input--selection-controls__input');
+const selectLabel = wrapper => wrapper.find('label');
+
+describe('v-checkbox-functional', () => {
+  it('Value changed after click on the control element', () => {
+    const wrapper = snapshotFactory({
+      propsData: {
+        inputValue: true,
+      },
+    });
+
+    const control = selectControlField(wrapper);
+
+    control.trigger('click');
+
+    const changeEvents = wrapper.emitted('change');
+    expect(changeEvents).toHaveLength(1);
+
+    const [eventData] = changeEvents[0];
+
+    expect(eventData).toBe(false);
+  });
+
+  it('Value changed after click on the label', () => {
+    const wrapper = snapshotFactory({
+      propsData: {
+        inputValue: false,
+      },
+    });
+
+    const label = selectLabel(wrapper);
+
+    label.trigger('click');
+
+    const changeEvents = wrapper.emitted('change');
+    expect(changeEvents).toHaveLength(1);
+
+    const [eventData] = changeEvents[0];
+
+    expect(eventData).toBe(true);
+  });
+
+  it('Renders `v-checkbox-functional` with default props', () => {
+    const wrapper = snapshotFactory();
+
+    expect(wrapper.element).toMatchSnapshot();
+  });
+
+  it('Renders `v-checkbox-functional` with custom props', () => {
+    const wrapper = snapshotFactory({
+      propsData: {
+        inputValue: true,
+        hideDetails: true,
+        label: 'Custom label',
+        disabled: true,
+      },
+    });
+
+    expect(wrapper.element).toMatchSnapshot();
+  });
+});
