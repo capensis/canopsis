3a5733c9303a36c4699d77b14e9606c2679d3cc4
Vladimir Erokhov <vladimir.erokhov@noveogroup.com>
2022-01-25 12:52:49 +0700

    feature(#3912): Finished issue

---
+++ community/sources/webcore/src/canopsis-next/public/scripts/external.js
@@ -0,0 +1,19 @@
+/**
+ * We may put only script tags into this array (NOT SRC only the WHOLE script tags)
+ *
+ * @example window.EXTERNAL_SCRIPTS = [
+ *   '<script type="text/javascript" src="https://host.com/some-script.js"></script>'
+ *   '<script type="text/javascript">console.log('We can also put inline script')</script>'
+ *   `<script>
+ *      console.log('And multiple line inline script too')
+ *    </script>`
+ * ];
+ */
+
+window.EXTERNAL_SCRIPTS = [
+  // '<script type="text/javascript" src="https://host.com/some-script.js"></script>'
+  // '<script type="text/javascript">console.log('We can also put inline script')</script>'
+  // `<script>
+  //   console.log('And multiple line inline script too')
+  // </script>`
+];
