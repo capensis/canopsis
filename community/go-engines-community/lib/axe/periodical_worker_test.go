2785caec8b09b07069a6c7f3686e7709fde2b06b
Zarina Yakubova <Zarina.Yakubova@noveogroup.com>
2022-01-25 18:10:08 +0700

    Fix #4107 : Fix flapping rules load

--- community/go-engines-community/lib/axe/periodical_worker_test.go
+++ community/go-engines-community/lib/axe/periodical_worker_test.go
@@ -7,7 +7,6 @@ import (
 
 	"git.canopsis.net/canopsis/canopsis-community/community/go-engines-community/lib/canopsis/config"
 	mock_alarm "git.canopsis.net/canopsis/canopsis-community/community/go-engines-community/mocks/lib/canopsis/alarm"
-	mock_alarmstatus "git.canopsis.net/canopsis/canopsis-community/community/go-engines-community/mocks/lib/canopsis/alarmstatus"
 	mock_config "git.canopsis.net/canopsis/canopsis-community/community/go-engines-community/mocks/lib/canopsis/config"
 	mock_idlealarm "git.canopsis.net/canopsis/canopsis-community/community/go-engines-community/mocks/lib/canopsis/idlealarm"
 	"github.com/golang/mock/gomock"
@@ -23,14 +22,12 @@ func TestPeriodicalWorker_Work(t *testing.T) {
 	mockAlarmAdapter := mock_alarm.NewMockAdapter(ctrl)
 	mockIdleAlarmService := mock_idlealarm.NewMockService(ctrl)
 	mockAlarmConfigProvider := mock_config.NewMockAlarmConfigProvider(ctrl)
-	mockAlarmStatusService := mock_alarmstatus.NewMockService(ctrl)
 
 	interval := time.Minute
 	worker := periodicalWorker{
 		PeriodicalInterval:  interval,
 		AlarmService:        mockAlarmService,
 		AlarmAdapter:        mockAlarmAdapter,
-		AlarmStatusService:  mockAlarmStatusService,
 		IdleAlarmService:    mockIdleAlarmService,
 		AlarmConfigProvider: mockAlarmConfigProvider,
 	}
@@ -50,7 +47,6 @@ func TestPeriodicalWorker_Work(t *testing.T) {
 	mockAlarmService.EXPECT().ResolveDone(gomock.Any())
 	mockAlarmService.EXPECT().UpdateFlappingAlarms(gomock.Any())
 	mockIdleAlarmService.EXPECT().Process(gomock.Any())
-	mockAlarmStatusService.EXPECT().Load(gomock.Any())
 
 	worker.Work(ctx)
 }
