76b78e334803f387a94142ee36ee50d7b4f22587
Dmitry <Dmitriy.Mikhaylishin@noveogroup.com>
2022-01-26 18:25:43 +0700

    feature(#4070): alarms header cell tested

---
+++ community/sources/webcore/src/canopsis-next/tests/unit/components/widgets/alarm/headers-formatting/alarm-header-cell.spec.js
@@ -0,0 +1,45 @@
+import { mount, createVueInstance } from '@unit/utils/vue';
+
+import { ALARM_ENTITY_FIELDS } from '@/constants';
+import AlarmHeaderCell from '@/components/widgets/alarm/headers-formatting/alarm-header-cell.vue';
+
+const localVue = createVueInstance();
+
+const stubs = {
+  'alarm-header-priority': true,
+};
+
+const snapshotFactory = (options = {}) => mount(AlarmHeaderCell, {
+  localVue,
+  stubs,
+
+  ...options,
+});
+
+describe('alarm-header-cell', () => {
+  it('Renders `alarm-header-cell` with priority header', () => {
+    const wrapper = snapshotFactory({
+      propsData: {
+        header: {
+          value: ALARM_ENTITY_FIELDS.priority,
+          text: 'Priority',
+        },
+      },
+    });
+
+    expect(wrapper.element).toMatchSnapshot();
+  });
+
+  it('Renders `alarm-header-cell` with custom header', () => {
+    const wrapper = snapshotFactory({
+      propsData: {
+        header: {
+          value: 'custom-header',
+          text: 'Custom header',
+        },
+      },
+    });
+
+    expect(wrapper.element).toMatchSnapshot();
+  });
+});
