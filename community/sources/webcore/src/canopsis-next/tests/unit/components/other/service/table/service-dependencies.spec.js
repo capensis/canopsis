74bdb182f4d360189b744e210a2c81575905a0f3
Dmitry <Dmitriy.Mikhaylishin@noveogroup.com>
2022-02-01 14:39:49 +0700

    feature(#4112): fixes after self review

--- community/sources/webcore/src/canopsis-next/tests/unit/components/other/service/table/service-dependencies.spec.js
+++ community/sources/webcore/src/canopsis-next/tests/unit/components/other/service/table/service-dependencies.spec.js
@@ -4,9 +4,9 @@ import { mount, shallowMount, createVueInstance } from '@unit/utils/vue';
 import { createMockedStoreModules } from '@unit/utils/store';
 
 import { mockModals } from '@unit/utils/mock-hooks';
-import ServiceDependencies from '@/components/other/service/table/service-dependencies.vue';
-import CTreeviewDataTable from '@/components/common/table/c-treeview-data-table.vue';
 import { ENTITY_TYPES, MODALS } from '@/constants';
+import CTreeviewDataTable from '@/components/common/table/c-treeview-data-table.vue';
+import ServiceDependencies from '@/components/other/service/table/service-dependencies.vue';
 
 const localVue = createVueInstance();
 
