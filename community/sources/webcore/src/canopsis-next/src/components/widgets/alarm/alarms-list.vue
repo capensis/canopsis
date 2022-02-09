fc18a502c9cbb52ffb656df0d0318ca7a06db8c1
Dmitry <Dmitriy.Mikhaylishin@noveogroup.com>
2022-02-09 16:06:25 +0700

    fix(#4139): useless optional chaining removed

--- community/sources/webcore/src/canopsis-next/src/components/widgets/alarm/alarms-list.vue
+++ community/sources/webcore/src/canopsis-next/src/components/widgets/alarm/alarms-list.vue
@@ -205,7 +205,7 @@ export default {
   methods: {
     refreshExpanded() {
       if (this.$refs.alarmsTable?.expanded) {
-        Object.entries(this.$refs.alarmsTable?.expanded).forEach(([id, expanded]) => {
+        Object.entries(this.$refs.alarmsTable.expanded).forEach(([id, expanded]) => {
           if (expanded && !this.alarms.some(alarm => alarm._id === id)) {
             this.$set(this.$refs.alarmsTable.expanded, id, false);
           }
