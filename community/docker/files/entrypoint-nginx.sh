b26b11da0c73d4772b32c7caa64bf9308b921439
Alix Biton <abiton@capensis.fr>
2022-02-15 17:35:45 +0100

    Remove useless code in nginx entrypoint

--- community/docker/files/entrypoint-nginx.sh
+++ community/docker/files/entrypoint-nginx.sh
@@ -1,15 +1,6 @@
 #!/bin/sh
 set -eu
 
-fix_permissions() {
-        for f in ${@}
-        do
-                echo Fixing permissions for ${f}
-                chmod -R 0755 ${f}
-        done
-}
-
-
 if [ "$CPS_ENABLE_HTTPS" = "true" ]
 then
 	sed -i -e "s,#include /etc/nginx/https.inc;,include /etc/nginx/https.inc;,g" /etc/nginx/conf.d/default.conf
@@ -32,5 +23,4 @@ sed -i -e "s,{{ CPS_SERVER_NAME }},$CPS_SERVER_NAME,g" /etc/nginx/conf.d/default
 sed -i -e "s,{{ RUNDECK_GRAILS_URL }},$RUNDECK_GRAILS_URL,g" /etc/nginx/rundeck.inc
 sed -i -e "s,{{ NGINX_URL }},$NGINX_URL,g" /etc/nginx/rundeck.inc
 echo "resolver $(awk 'BEGIN{ORS=" "} $1=="nameserver" {print $2}' /etc/resolv.conf) valid=20s;" > /etc/nginx/resolvers.inc
-fix_permissions ${NGINX_ACCESSIBLE_FOLDERS}
 exec nginx -g 'daemon off;'
