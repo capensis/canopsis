d2eb35a288a1d1220818569e77b64ddfac80a238
Zarina Yakubova <Zarina.Yakubova@noveogroup.com>
2022-02-21 11:14:09 +0700

    feature(#4151): Create new pattern structs for alarm, entity and pbehavior

--- community/go-engines-community/lib/canopsis/pattern/conditions.go
+++ community/go-engines-community/lib/canopsis/pattern/conditions.go
@@ -1,12 +1,16 @@
+// Package pattern provides functionality for filtering and matching models.
 package pattern
 
 import (
 	"errors"
+	"time"
+
 	"git.canopsis.net/canopsis/canopsis-community/community/go-engines-community/lib/canopsis/types"
 	"git.canopsis.net/canopsis/canopsis-community/community/go-engines-community/lib/utils"
-	"time"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
+var ErrUnsupportedField = errors.New("unsupported field")
 var ErrUnsupportedConditionType = errors.New("unsupported condition type")
 var ErrWrongConditionValue = errors.New("wrong condition value")
 
@@ -20,14 +24,35 @@ const (
 	ConditionHasOneOf     = "has_one_of"
 	ConditionHasNot       = "has_not"
 	ConditionIsEmpty      = "is_empty"
+	ConditionExist        = "exist"
 	ConditionTimeRelative = "relative_time"
 	ConditionTimeAbsolute = "absolute_time"
 )
 
+const (
+	FieldTypeString      = "string"
+	FieldTypeInt         = "int"
+	FieldTypeBool        = "bool"
+	FieldTypeStringArray = "string_array"
+)
+
+// FieldCondition represents a condition for a specific field.
+type FieldCondition struct {
+	Field string `json:"field" bson:"field"`
+	// FieldType is only defined for custom fields, ex: infos.
+	FieldType string    `json:"field_type" bson:"field_type,omitempty"`
+	Condition Condition `json:"cond" bson:"cond"`
+}
+
+// Condition represents an expression to decide if a value fits.
 type Condition struct {
 	Type  string      `json:"cond" bson:"cond"`
 	Value interface{} `json:"value" bson:"value"`
 }
+
+// RegexMatches is a type that contains the values of the sub-expressions of a
+// regular expression.
+type RegexMatches map[string]string
 
 func (c Condition) MatchString(value string) (bool, RegexMatches, error) {
 	conditionValue, ok := c.Value.(string)
@@ -95,8 +120,8 @@ func (c Condition) MatchRef(value interface{}) (bool, error) {
 	}
 
 	switch c.Type {
-	case ConditionIsEmpty:
-		return conditionValue == (value == nil), nil
+	case ConditionExist:
+		return conditionValue == (value != nil), nil
 	}
 
 	return false, ErrUnsupportedConditionType
@@ -244,4 +269,78 @@ func (c Condition) MatchDuration(value int64) (bool, error) {
 	}
 
 	return false, ErrUnsupportedConditionType
+}
+
+func (c Condition) ToMongoQuery(f string) (bson.M, error) {
+	switch c.Type {
+	case ConditionEqual:
+		return bson.M{f: bson.M{"$eq": c.Value}}, nil
+	case ConditionNotEqual:
+		return bson.M{f: bson.M{"$ne": c.Value}}, nil
+	case ConditionGT:
+		return bson.M{f: bson.M{"$gt": c.Value}}, nil
+	case ConditionLT:
+		return bson.M{f: bson.M{"$lt": c.Value}}, nil
+	case ConditionRegexp:
+		return bson.M{f: bson.M{"$regex": c.Value}}, nil
+	case ConditionHasEvery:
+		return bson.M{f: bson.M{"$all": c.Value}}, nil
+	case ConditionHasOneOf:
+		return bson.M{f: bson.M{"$elemMatch": bson.M{"$in": c.Value}}}, nil
+	case ConditionHasNot:
+		return bson.M{f: bson.M{"$elemMatch": bson.M{"$nin": c.Value}}}, nil
+	case ConditionIsEmpty:
+		conditionValue, ok := c.Value.(bool)
+		if !ok {
+			return nil, ErrWrongConditionValue
+		}
+
+		if conditionValue {
+			return bson.M{f: bson.M{"$in": bson.A{nil, bson.A{}}}}, nil
+		}
+
+		return bson.M{f: bson.M{"$exists": true, "$type": "array", "$ne": bson.A{}}}, nil
+	case ConditionExist:
+		conditionValue, ok := c.Value.(bool)
+		if !ok {
+			return nil, ErrWrongConditionValue
+		}
+
+		if conditionValue {
+			return bson.M{f: bson.M{"$exists": true, "$ne": nil}}, nil
+		}
+
+		return bson.M{"$or": []bson.M{{f: bson.M{"$exists": false}}, {f: bson.M{"$eq": nil}}}}, nil
+	case ConditionTimeRelative:
+		conditionValue, ok := c.Value.(int)
+		if !ok {
+			return nil, ErrWrongConditionValue
+		}
+
+		t := types.CpsTime{Time: time.Now().Add(time.Duration(-conditionValue) * time.Second)}
+
+		return bson.M{f: bson.M{"$gt": t}}, nil
+	case ConditionTimeAbsolute:
+		conditionValue, ok := c.Value.(map[string]int64)
+		if !ok {
+			return nil, ErrWrongConditionValue
+		}
+
+		from, ok := conditionValue["from"]
+		if !ok {
+			return nil, errors.New("condition value expected 'from' key")
+		}
+
+		to, ok := conditionValue["to"]
+		if !ok {
+			return nil, errors.New("condition value expected 'to' key")
+		}
+
+		ft := types.NewCpsTime(from)
+		tt := types.NewCpsTime(to)
+
+		return bson.M{f: bson.M{"$gt": ft, "$lt": tt}}, nil
+	default:
+		return nil, ErrUnsupportedConditionType
+	}
 }
---
+++ community/go-engines-community/lib/canopsis/pattern/entity.go
@@ -0,0 +1,199 @@
+package pattern
+
+import (
+	"fmt"
+	"strings"
+	"time"
+
+	"git.canopsis.net/canopsis/canopsis-community/community/go-engines-community/lib/canopsis/types"
+	"go.mongodb.org/mongo-driver/bson"
+)
+
+type Entity [][]FieldCondition
+
+func (p Entity) Match(entity types.Entity) (bool, error) {
+	for _, group := range p {
+		matched := false
+
+		for _, v := range group {
+			f := v.Field
+			cond := v.Condition
+			var err error
+			matched = false
+
+			if infoName := getEntityInfoName(f); infoName != "" {
+				infoVal := getEntityInfoVal(entity, infoName)
+
+				switch v.FieldType {
+				case FieldTypeString:
+					if s, ok := infoVal.(string); ok {
+						matched, _, err = cond.MatchString(s)
+					}
+				case FieldTypeInt:
+					if i, ok := infoVal.(int); ok {
+						matched, err = cond.MatchInt(i)
+					}
+				case FieldTypeBool:
+					if b, ok := infoVal.(bool); ok {
+						matched, err = cond.MatchBool(b)
+					}
+				case FieldTypeStringArray:
+					if a, ok := infoVal.([]string); ok {
+						matched, err = cond.MatchStringArray(a)
+					}
+				default:
+					matched, err = cond.MatchRef(infoVal)
+				}
+
+				if err != nil {
+					return false, fmt.Errorf("invalid condition for %q field: %w", f, err)
+				}
+
+				if !matched {
+					break
+				}
+
+				continue
+			}
+
+			if str, ok := getEntityStringField(entity, f); ok {
+				matched, _, err = cond.MatchString(str)
+			} else if i, ok := getEntityIntField(entity, f); ok {
+				matched, err = cond.MatchInt(i)
+			} else if t, ok := getEntityTimeField(entity, f); ok {
+				matched, err = cond.MatchTime(t)
+			} else if a, ok := getEntityStringArrayField(entity, f); ok {
+				matched, err = cond.MatchStringArray(a)
+			} else {
+				err = ErrUnsupportedField
+			}
+
+			if err != nil {
+				return false, fmt.Errorf("invalid condition for %q field: %w", f, err)
+			}
+
+			if !matched {
+				break
+			}
+		}
+
+		if matched {
+			return true, nil
+		}
+	}
+
+	return false, nil
+}
+
+func (p Entity) ToMongoQuery(prefix string) ([]bson.M, error) {
+	pipeline := make([]bson.M, 0)
+	if len(p) == 0 {
+		return nil, nil
+	}
+
+	if prefix != "" {
+		prefix += "."
+	}
+
+	groupQueries := make([]bson.M, len(p))
+	var err error
+
+	for i, group := range p {
+		condQueries := make([]bson.M, len(group))
+		for j, cond := range group {
+			f := cond.Field
+
+			if infoName := getEntityInfoName(f); infoName != "" {
+				f = prefix + "infos." + infoName + ".val"
+
+				condQueries[j], err = cond.Condition.ToMongoQuery(f)
+				if err != nil {
+					return nil, err
+				}
+
+				conds := getTypeMongoQuery(f, cond.FieldType)
+
+				if len(conds) > 0 {
+					conds = append(conds, condQueries[j])
+					condQueries[j] = bson.M{"$and": conds}
+				}
+
+				continue
+			}
+
+			f = prefix + f
+			condQueries[j], err = cond.Condition.ToMongoQuery(f)
+			if err != nil {
+				return nil, err
+			}
+		}
+
+		groupQueries[i] = bson.M{"$and": condQueries}
+	}
+
+	pipeline = append(pipeline, bson.M{"$match": bson.M{"$or": groupQueries}})
+
+	return pipeline, nil
+}
+
+func getEntityStringField(entity types.Entity, f string) (string, bool) {
+	switch f {
+	case "_id":
+		return entity.ID, true
+	case "name":
+		return entity.Name, true
+	case "category":
+		return entity.Category, true
+	default:
+		return "", false
+	}
+}
+
+func getEntityIntField(entity types.Entity, f string) (int, bool) {
+	switch f {
+	case "impact_level":
+		return int(entity.ImpactLevel), true
+	default:
+		return 0, false
+	}
+}
+
+func getEntityTimeField(entity types.Entity, field string) (time.Time, bool) {
+	switch field {
+	case "last_event_date":
+		if entity.LastEventDate != nil {
+			return entity.LastEventDate.Time, true
+		}
+
+		return time.Time{}, true
+	default:
+		return time.Time{}, false
+	}
+}
+
+func getEntityStringArrayField(entity types.Entity, f string) ([]string, bool) {
+	switch f {
+	case "impact":
+		return entity.Impacts, true
+	case "depends":
+		return entity.Depends, true
+	default:
+		return nil, false
+	}
+}
+
+func getEntityInfoVal(entity types.Entity, f string) interface{} {
+	if v, ok := entity.Infos[f]; ok {
+		return v.Value
+	}
+
+	return nil
+}
+
+func getEntityInfoName(f string) string {
+	if n := strings.TrimPrefix(f, "infos."); n != f {
+		return n
+	}
+
+	return ""
+}
