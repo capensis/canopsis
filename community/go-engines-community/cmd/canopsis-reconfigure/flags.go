0e6de98f0a79828da1f45b7659fdd7c832d7b5cc
Zarina Yakubova <Zarina.Yakubova@noveogroup.com>
2022-02-22 16:01:08 +0700

    feature(#3744): Update fixtures

--- community/go-engines-community/cmd/canopsis-reconfigure/flags.go
+++ community/go-engines-community/cmd/canopsis-reconfigure/flags.go
@@ -22,7 +22,7 @@ func (f *flags) Parse() {
 	flag.StringVar(&f.confFile, "conf", DefaultCfgFile, FlagUsageConf)
 	flag.BoolVar(&f.modeDebug, "d", false, "debug mode")
 
-	flag.BoolVar(&f.modeMigrateMongo, "migrate-mongo", false, "If true, it will execute Mongo migration scripts")
+	flag.BoolVar(&f.modeMigrateMongo, "migrate-mongo", true, "If true, it will execute Mongo migration scripts")
 	flag.StringVar(&f.mongoMigrationDirectory, "mongo-migration-directory", DefaultMongoMigrationsPath, "The directory with Mongo migration scripts")
 	flag.StringVar(&f.mongoFixtureDirectory, "mongo-fixture-directory", DefaultMongoFixturesPath, "The directory with Mongo fixtures")
 
