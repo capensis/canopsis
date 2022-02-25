b4656682bcf5f1dfd5071042667534f3143691c8
Dmitry <Dmitriy.Mikhaylishin@noveogroup.com>
2022-02-25 13:25:51 +0700

    feature(#4195): fixes after self review

--- community/sources/webcore/src/canopsis-next/src/app.vue
+++ community/sources/webcore/src/canopsis-next/src/app.vue
@@ -3,7 +3,6 @@
     v-layout(v-if="!pending")
       the-navigation#main-navigation(v-if="shownNavigation")
       v-content#main-content
-        c-filter-operator-field
         active-broadcast-message
         router-view(:key="routeViewKey")
     the-sidebar
