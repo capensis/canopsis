36c2961809892bdad3f4242764fb3ba2eb6aa0a9
Dmitry <Dmitriy.Mikhaylishin@noveogroup.com>
2022-03-01 11:21:11 +0700

    feature(#4195): select replaced on the combobox

--- community/sources/webcore/src/canopsis-next/src/app.vue
+++ community/sources/webcore/src/canopsis-next/src/app.vue
@@ -3,6 +3,8 @@
     v-layout(v-if="!pending")
       the-navigation#main-navigation(v-if="shownNavigation")
       v-content#main-content
+        | {{ test }}
+        c-filter-infos-attribute-field(v-model="test", :items="[{ value: 'test', text: 'Test' }]")
         active-broadcast-message
         router-view(:key="routeViewKey")
     the-sidebar
@@ -47,6 +49,7 @@ export default {
   ],
   data() {
     return {
+      test: 'infos.',
       pending: true,
     };
   },
