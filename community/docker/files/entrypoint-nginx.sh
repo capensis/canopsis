96c28b3fb284619cac3b0a59829e60165f10424e
Alix Biton <abiton@capensis.fr>
2022-03-02 14:40:35 +0100

    Fix NGINX version an broken HTTPS include

--- community/docker/files/entrypoint-nginx.sh
+++ community/docker/files/entrypoint-nginx.sh
@@ -12,7 +12,7 @@ sed -i -e "s,/etc/nginx,${NGINX_CONFIGURATION_DIRECTORY},g" "${NGINX_CONFIGURATI
 
 if [ "$CPS_ENABLE_HTTPS" = "true" ]
 then
-	sed -i -e "s,#include /etc/nginx/https.inc;,include ${NGINX_CONFIGURATION_DIRECTORY}/https.inc;,g" "${NGINX_CONFIGURATION_DIRECTORY}"/conf.d/default.conf
+	sed -i -e "s,#include ${NGINX_CONFIGURATION_DIRECTORY}/https.inc;,include ${NGINX_CONFIGURATION_DIRECTORY}/https.inc;,g" "${NGINX_CONFIGURATION_DIRECTORY}"/conf.d/default.conf
 	if [ ! -f /etc/nginx/ssl/cert.crt ] && [ ! -f /etc/nginx/ssl/key.key ]
 	then
 		openssl req -x509 -nodes -days 730 -newkey rsa:2048 -sha256 \
@@ -24,7 +24,7 @@ fi
 
 if [ "$ENABLE_RUNDECK" = "true" ]
 then
-	sed -i -e "s,#include /etc/nginx/rundeck.inc;,include ${NGINX_CONFIGURATION_DIRECTORY}/rundeck.inc;,g" "${NGINX_CONFIGURATION_DIRECTORY}"/conf.d/default.conf
+	sed -i -e "s,#include ${NGINX_CONFIGURATION_DIRECTORY}/rundeck.inc;,include ${NGINX_CONFIGURATION_DIRECTORY}/rundeck.inc;,g" "${NGINX_CONFIGURATION_DIRECTORY}"/conf.d/default.conf
 fi
 
 sed -i -e "s,{{ CPS_API_URL }},$CPS_API_URL,g" "${NGINX_CONFIGURATION_DIRECTORY}"/conf.d/default.conf
