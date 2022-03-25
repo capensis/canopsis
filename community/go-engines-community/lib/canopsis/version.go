c290827093b33783ac41aeb1e728844f9c4f18cc
Zarina Yakubova <Zarina.Yakubova@noveogroup.com>
2022-03-25 09:44:28 +0600

    feature(#4259): Add connector-centreon-downtime-adapter

--- community/go-engines-community/lib/canopsis/version.go
+++ community/go-engines-community/lib/canopsis/version.go
@@ -3,15 +3,20 @@ package canopsis
 import (
 	"fmt"
 	"os"
+	"strconv"
+	"time"
 )
 
-// BuildDate ...
+// Version is a version of service.
+var Version string
+
+// BuildDate is a Unix timestamp (as a string).
 var BuildDate string
 
 // BuildGitBranch ...
 var BuildGitBranch string
 
-// BuildGitCommit is the short version of git commit
+// BuildGitCommit is the short version of Git commit.
 var BuildGitCommit string
 
 // PrintVersion outputs versions informations
@@ -25,4 +30,20 @@ func PrintVersion() {
 func PrintVersionExit() {
 	PrintVersion()
 	os.Exit(0)
+}
+
+func PrintVersionInfo(name string) {
+	unitTs, err := strconv.ParseInt(BuildDate, 10, 64)
+	if err == nil {
+		BuildDate = time.Unix(unitTs, 0).UTC().Format(time.RFC3339)
+	}
+
+	if Version == "" {
+		Version = "development"
+	}
+	if BuildGitCommit == "" {
+		BuildGitCommit = "unknown"
+	}
+
+	fmt.Printf("%s version %s, build %s %s\n", name, Version, BuildGitCommit, BuildDate)
 }
