a8f728eeebabc4be99bd692e0608ab399aa8d03c
Dmitry <Dmitriy.Mikhaylishin@noveogroup.com>
2022-03-28 14:28:03 +0600

    feature(#4213): all idle rule patterns moved in field

--- community/sources/webcore/src/canopsis-next/src/components/other/idle-rule/form/idle-rule-form.vue
+++ community/sources/webcore/src/canopsis-next/src/components/other/idle-rule/form/idle-rule-form.vue
@@ -7,7 +7,7 @@
         idle-rule-general-form(ref="general", v-field="form", :is-entity-type="isEntityType")
       v-tab(:class="{ 'error--text': hasPatternsError }") {{ $tc('common.pattern') }}
       v-tab-item
-        idle-rule-patterns-form(ref="patterns", v-field="form", :is-entity-type="isEntityType")
+        idle-rule-patterns-form(ref="patterns", v-field="form.patterns", :is-entity-type="isEntityType")
 </template>
 
 <script>
