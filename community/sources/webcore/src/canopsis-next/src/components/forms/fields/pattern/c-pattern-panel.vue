ff695f6e16f450dec6f7464c4175dac8bea037b1
Dmitry <Dmitriy.Mikhaylishin@noveogroup.com>
2022-03-28 16:22:37 +0600

    feature(#4213): validation for pattern panel added

---
+++ community/sources/webcore/src/canopsis-next/src/components/forms/fields/pattern/c-pattern-panel.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+  c-collapse-panel(:error="hasChildrenError", color="grey")
+    template(#header="")
+      span.white--text {{ title }}
+    v-card
+      v-card-text
+        slot
+</template>
+
+<script>
+import { validationChildrenMixin } from '@/mixins/form';
+
+export default {
+  inject: ['$validator'],
+  mixins: [validationChildrenMixin],
+  model: {
+    prop: 'value',
+    event: 'input',
+  },
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
