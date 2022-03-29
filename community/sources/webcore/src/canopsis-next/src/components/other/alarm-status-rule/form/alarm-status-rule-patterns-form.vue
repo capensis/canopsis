73a57f53e863bd5c938750d4edfefbd817c70578
Dmitry <Dmitriy.Mikhaylishin@noveogroup.com>
2022-03-29 14:39:10 +0600

    feature(#4218): expand panel changed

--- community/sources/webcore/src/canopsis-next/src/components/other/alarm-status-rule/form/alarm-status-rule-patterns-form.vue
+++ community/sources/webcore/src/canopsis-next/src/components/other/alarm-status-rule/form/alarm-status-rule-patterns-form.vue
@@ -1,33 +1,20 @@
 <template lang="pug">
-  v-layout(column)
-    v-text-field(
-      v-field="form.name",
-      v-validate="'required'",
-      :label="$t('common.name')",
-      :error-messages="errors.collect('name')",
-      name="name"
-    )
-    c-duration-field(v-field="form.duration", required)
-    c-priority-field(v-field="form.priority", required)
-    c-number-field(
-      v-if="flapping",
-      v-field="form.freq_limit",
-      :label="$t('alarmStatusRules.frequencyLimit')",
-      :min="1",
-      name="freq_limit"
-    )
-    c-description-field(v-field="form.description", required)
-    alarm-status-rule-patterns-form(v-field="form.patterns", :flapping="flapping")
+  c-patterns-field(
+    v-field="form",
+    :disabled="disabled",
+    :some-required="flapping",
+    :alarm-attributes="alarmAttributes",
+    :alarm-excluded-attributes="alarmExcludedAttributes",
+    :entity-excluded-items="entityExcludedItems",
+    with-alarm,
+    with-entity
+  )
 </template>
 
 <script>
 import { ALARM_PATTERN_FIELDS, ENTITY_PATTERN_FIELDS, QUICK_RANGES } from '@/constants';
 
-import AlarmStatusRulePatternsForm from './alarm-status-rule-patterns-form.vue';
-
 export default {
-  inject: ['$validator'],
-  components: { AlarmStatusRulePatternsForm },
   model: {
     prop: 'form',
     event: 'input',
@@ -38,6 +25,10 @@ export default {
       default: () => ({}),
     },
     flapping: {
+      type: Boolean,
+      default: false,
+    },
+    disabled: {
       type: Boolean,
       default: false,
     },
