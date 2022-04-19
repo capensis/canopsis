7335cd766778300b255a6b3ae3d7726a7f53481d
Zarina Yakubova <Zarina.Yakubova@noveogroup.com>
2022-04-19 09:36:47 +0600

    feature(#132): Fix code style

--- community/go-engines-community/cmd/engine-pbehavior/rpc_server_message_processor.go
+++ community/go-engines-community/cmd/engine-pbehavior/rpc_server_message_processor.go
@@ -131,9 +131,9 @@ func (p *createPbehaviorMessageProcessor) createPbehavior(
 	if err != nil {
 		if err == mongodriver.ErrNoDocuments {
 			return nil, fmt.Errorf("pbehavior type not exist: %q", params.Type)
-		} else {
-			return nil, fmt.Errorf("cannot get pbehavior type: %w", err)
 		}
+
+		return nil, fmt.Errorf("cannot get pbehavior type: %w", err)
 	}
 
 	reasonCollection := p.DbClient.Collection(mongo.PbehaviorReasonMongoCollection)
@@ -141,9 +141,9 @@ func (p *createPbehaviorMessageProcessor) createPbehavior(
 	if err != nil {
 		if err == mongodriver.ErrNoDocuments {
 			return nil, fmt.Errorf("pbehavior reason not exist: %q", params.Reason)
-		} else {
-			return nil, fmt.Errorf("cannot get pbehavior reason: %w", err)
 		}
+
+		return nil, fmt.Errorf("cannot get pbehavior reason: %w", err)
 	}
 
 	now := types.NewCpsTime()
