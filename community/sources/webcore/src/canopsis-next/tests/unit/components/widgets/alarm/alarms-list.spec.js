3a4e805ed1d694369d84394660ab62a2b372946e
Vladimir Erokhov <vladimir.erokhov@noveogroup.com>
2022-04-21 12:22:24 +0600

    feature(#4251): Updated some tests

--- community/sources/webcore/src/canopsis-next/tests/unit/components/widgets/alarm/alarms-list.spec.js
+++ community/sources/webcore/src/canopsis-next/tests/unit/components/widgets/alarm/alarms-list.spec.js
@@ -9,13 +9,15 @@ import { createMockedStoreModules } from '@unit/utils/store';
 import { fakeStaticAlarms } from '@unit/data/alarm';
 import { alarmListWidgetToForm } from '@/helpers/forms/widgets/alarm';
 import {
-  CANOPSIS_EDITION, EXPORT_CSV_DATETIME_FORMATS,
+  CANOPSIS_EDITION,
+  EXPORT_CSV_DATETIME_FORMATS,
   EXPORT_STATUSES,
   FILTER_DEFAULT_VALUES,
   FILTER_MONGO_OPERATORS,
   MODALS,
   QUICK_RANGES,
-  REMEDIATION_INSTRUCTION_TYPES, TIME_UNITS,
+  REMEDIATION_INSTRUCTION_TYPES,
+  TIME_UNITS,
   USERS_PERMISSIONS,
 } from '@/constants';
 
@@ -57,6 +59,7 @@ const snapshotStubs = {
   'c-pagination': true,
   'alarms-list-table': true,
   'c-table-pagination': true,
+  'c-density-btn-toggle': true,
   'alarms-expand-panel-tour': true,
 };
 
