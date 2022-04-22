249ecf2c8cc849958f1323e27ecb35d56345a666
Zarina Yakubova <Zarina.Yakubova@noveogroup.com>
2022-04-22 10:39:29 +0600

    feature(#4300): Update author behavior in scenario and idle rule API

--- community/go-engines-community/lib/canopsis/operation/executor/comment_executor.go
+++ community/go-engines-community/lib/canopsis/operation/executor/comment_executor.go
@@ -2,7 +2,6 @@ package executor
 
 import (
 	"context"
-	"fmt"
 	"git.canopsis.net/canopsis/canopsis-community/community/go-engines-community/lib/canopsis/config"
 	operationlib "git.canopsis.net/canopsis/canopsis-community/community/go-engines-community/lib/canopsis/operation"
 	"git.canopsis.net/canopsis/canopsis-community/community/go-engines-community/lib/canopsis/types"
@@ -25,11 +24,7 @@ func (e *commentExecutor) Exec(
 	time types.CpsTime,
 	userID, role, initiator string,
 ) (types.AlarmChangeType, error) {
-	var params types.OperationParameters
-	var ok bool
-	if params, ok = operation.Parameters.(types.OperationParameters); !ok {
-		return "", fmt.Errorf("invalid parameters")
-	}
+	params := operation.Parameters
 
 	if userID == "" {
 		userID = params.User
