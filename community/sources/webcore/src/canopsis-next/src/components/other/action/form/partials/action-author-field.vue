d59ba48b4c02bb9d449a5757b2ec28f958e3b551
Dmitry <Dmitriy.Mikhaylishin@noveogroup.com>
2022-04-27 18:15:14 +0600

    feature(#4300): forward author added

---
+++ community/sources/webcore/src/canopsis-next/src/components/other/action/form/partials/action-author-field.vue
@@ -0,0 +1,31 @@
+<template lang="pug">
+  v-layout(column)
+    c-enabled-field(
+      v-field="value.forward_author",
+      :label="$t('scenario.forwardAuthor')"
+    )
+    v-text-field(
+      v-if="!value.forward_author",
+      v-field="value.author",
+      v-validate="'required'",
+      :error-messages="errors.collect('author')",
+      :label="$t('common.author')",
+      name="author"
+    )
+</template>
+
+<script>
+export default {
+  inject: ['$validator'],
+  model: {
+    prop: 'value',
+    event: 'input',
+  },
+  props: {
+    value: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
