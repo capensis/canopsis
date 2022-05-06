5e99247968146b8acd6b8cc793c6324e97a0e287
Dmitry <Dmitriy.Mikhaylishin@noveogroup.com>
2022-05-06 15:15:12 +0600

    feature(#4300): author field moved

--- community/sources/webcore/src/canopsis-next/src/components/other/action/form/action-parameters-form.vue
+++ community/sources/webcore/src/canopsis-next/src/components/other/action/form/action-parameters-form.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
   div
-    component(v-model="value", :is="component", :name="name")
+    component(v-field="value", :is="component", :name="name")
 </template>
 
 <script>
@@ -8,7 +8,6 @@ import { ACTION_TYPES } from '@/constants';
 
 import { formMixin, formValidationHeaderMixin } from '@/mixins/form';
 
-import ActionChangeStateForm from './action-change-state-form.vue';
 import ActionAssocticketForm from './action-assocticket-form.vue';
 import ActionNoteForm from './action-note-form.vue';
 import ActionPbehaviorForm from './action-pbehavior-form.vue';
@@ -18,7 +17,6 @@ import ActionWebhookForm from './action-webhook-form.vue';
 export default {
   inject: ['$validator'],
   components: {
-    ActionChangeStateForm,
     ActionAssocticketForm,
     ActionNoteForm,
     ActionPbehaviorForm,
@@ -27,13 +25,17 @@ export default {
   },
   mixins: [formMixin, formValidationHeaderMixin],
   model: {
-    prop: 'action',
+    prop: 'value',
     event: 'input',
   },
   props: {
-    action: {
+    value: {
       type: Object,
       default: () => ({}),
+    },
+    type: {
+      type: String,
+      default: ACTION_TYPES.ack,
     },
     name: {
       type: String,
@@ -41,19 +43,9 @@ export default {
     },
   },
   computed: {
-    value: {
-      get() {
-        const { type, parameters } = this.action;
-
-        return parameters[type];
-      },
-      set(value) {
-        this.updateField(`parameters.${this.action.type}`, value);
-      },
-    },
     component() {
       return {
-        [ACTION_TYPES.changeState]: 'action-change-state-form',
+        [ACTION_TYPES.changeState]: 'c-change-state-field',
         [ACTION_TYPES.snooze]: 'action-snooze-form',
         [ACTION_TYPES.pbehavior]: 'action-pbehavior-form',
         [ACTION_TYPES.assocticket]: 'action-assocticket-form',
@@ -61,7 +53,7 @@ export default {
         [ACTION_TYPES.ackremove]: 'action-note-form',
         [ACTION_TYPES.cancel]: 'action-note-form',
         [ACTION_TYPES.webhook]: 'action-webhook-form',
-      }[this.action.type];
+      }[this.type];
     },
   },
 };
