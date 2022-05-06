5569378949f790b41c2c21abb115d5214ffa3b31
Alexander Pavlov <alexander.pavlov@noveogroup.com>
2022-05-06 18:24:27 +0300

    fix(#4292): pbehaviorInfo with undefined timestamp

--- community/go-engines-community/database/migrations/release4.5/000_add_pbehavior_info_to_entity.js
+++ community/go-engines-community/database/migrations/release4.5/000_add_pbehavior_info_to_entity.js
@@ -7,9 +7,11 @@ db.periodical_alarm.find({"v.pbehavior_info.id": {$nin: ["", null]}}).forEach(fu
         }
     }
 
-    db.periodical_alarm.updateOne({_id: doc._id}, {$set: {"v.pbehavior_info.timestamp": timestamp}});
+    if (timestamp) {
+        db.periodical_alarm.updateOne({_id: doc._id}, {$set: {"v.pbehavior_info.timestamp": timestamp}});
 
-    var pbehaviorInfo = doc.v.pbehavior_info;
-    pbehaviorInfo.timestamp = timestamp;
-    db.default_entities.updateOne({_id: doc.d}, {$set: {"pbehavior_info": pbehaviorInfo}});
+        var pbehaviorInfo = doc.v.pbehavior_info;
+        pbehaviorInfo.timestamp = timestamp;
+        db.default_entities.updateOne({_id: doc.d}, {$set: {"pbehavior_info": pbehaviorInfo}});
+    }
 });
