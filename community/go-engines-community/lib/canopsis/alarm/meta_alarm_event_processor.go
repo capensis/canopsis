fb17301708f50dc52a12632d5e1dcaf6ddc1e323
Zarina Yakubova <Zarina.Yakubova@noveogroup.com>
2022-05-11 11:47:42 +0600

    Fix merge conflicts

--- community/go-engines-community/lib/canopsis/alarm/meta_alarm_event_processor.go
+++ community/go-engines-community/lib/canopsis/alarm/meta_alarm_event_processor.go
@@ -334,40 +334,19 @@ func (p *metaAlarmEventProcessor) processParentRpc(ctx context.Context, event ty
 		childEvent := types.Event{
 			EventType: event.EventType,
 			Timestamp: types.NewCpsTime(),
-			Author:    canopsis.DefaultEventAuthor,
+			Output:    event.Parameters.Output,
+			Author:    event.Parameters.Author,
+			UserID:    event.Parameters.User,
 			Initiator: types.InitiatorSystem,
+			Ticket:    event.Parameters.Ticket,
 		}
 
-		if params, ok := event.Parameters.(types.OperationParameters); ok {
-			childEvent.Output = params.Output
-			childEvent.Author = params.Author
-			childEvent.UserID = params.User
+		if event.Parameters.State != nil {
+			childEvent.State = *event.Parameters.State
 		}
 
-		if params, ok := event.Parameters.(types.OperationAssocTicketParameters); ok {
-			childEvent.Ticket = params.Ticket
-			childEvent.Output = params.Output
-			childEvent.Author = params.Author
-			childEvent.UserID = params.User
-		}
-
-		if params, ok := event.Parameters.(types.OperationChangeStateParameters); ok {
-			childEvent.State = params.State
-			childEvent.Output = params.Output
-			childEvent.Author = params.Author
-			childEvent.UserID = params.User
-		}
-
-		if params, ok := event.Parameters.(types.OperationDeclareTicketParameters); ok {
-			childEvent.Ticket = params.Ticket
-			childEvent.TicketData = params.Data
-			childEvent.Output = params.Output
-			childEvent.Author = params.Author
-			childEvent.UserID = params.User
-		}
-
-		if params, ok := event.Parameters.(types.OperationSnoozeParameters); ok {
-			seconds, err := params.Duration.To("s")
+		if event.Parameters.Duration != nil {
+			seconds, err := event.Parameters.Duration.To("s")
 			if err == nil {
 				childEvent.Duration = types.CpsNumber(seconds.Value)
 			}
