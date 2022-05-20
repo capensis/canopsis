02f8c0925fe8d251177353f9b549e96cede576ed
Zarina Yakubova <Zarina.Yakubova@noveogroup.com>
2022-05-20 10:54:00 +0600

    fix(#4343): Fix migration with too long execution

--- community/go-engines-community/database/migrations/release4.3/020_clean_entities_impact.js
+++ community/go-engines-community/database/migrations/release4.3/020_clean_entities_impact.js
@@ -1,36 +1,59 @@
 (function () {
-    var cursor = db.default_entities.aggregate([
-        {$match: {type: "service"}},
-        {$lookup: {
-            from: "default_entities",
-            let: {"id": "$_id"},
-            pipeline: [
-                {$match: 
-                    {$expr: 
-                        {$and: [{$in: ["$$id", "$impact"]}]}
-                    }
-                },
-                {$project: {_id: 1}}
-            ],
-            as: "depends_by_impact"
-        }},
-        {$addFields: {
-                depends_by_impact: {$map: {
-                        input: "$depends_by_impact",
-                        in: "$$this._id"
-                    }}
-            }},
-        {$project: {
-                diff: { $setDifference: [ "$depends_by_impact", "$depends" ] }
-            }},
-        {$match: {$expr: {$gt: [{$size: "$diff"}, 0]}}}
-    ]);
-    while (cursor.hasNext()) {
-        var doc = cursor.next();
-        db.default_entities.updateMany({_id: {$in: doc.diff}}, {
-            $pull: {
-                "impact": doc._id,
+    var offset = 0;
+    var limit = 100;
+
+    while (true) {
+        var cursor = db.default_entities.aggregate([
+            {$match: {type: "service"}},
+            {$sort: {_id: 1}},
+            {$skip: offset},
+            {$limit: limit},
+            {
+                $project: {
+                    depends: 1,
+                }
             },
-        });
+            {
+                $lookup: {
+                    from: "default_entities",
+                    localField: "_id",
+                    foreignField: "impact",
+                    as: "depends_by_impact"
+                }
+            },
+            {
+                $addFields: {
+                    depends_by_impact: {
+                        $map: {
+                            input: "$depends_by_impact",
+                            in: "$$this._id"
+                        }
+                    }
+                }
+            },
+            {
+                $project: {
+                    diff: {$setDifference: ["$depends_by_impact", "$depends"]}
+                }
+            },
+            {$match: {$expr: {$gt: [{$size: "$diff"}, 0]}}}
+        ]);
+        if (!cursor.hasNext()) {
+            return;
+        }
+
+        while (cursor.hasNext()) {
+            var doc = cursor.next();
+
+            if (doc.diff && doc.diff.length > 0) {
+                db.default_entities.updateMany({_id: {$in: doc.diff}}, {
+                    $pull: {
+                        "impact": doc._id,
+                    },
+                });
+            }
+        }
+
+        offset += limit;
     }
 })();
