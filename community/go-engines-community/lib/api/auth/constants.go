3abe0f321f82cf1968f4b1123765dd23fc93815b
Zarina Yakubova <Zarina.Yakubova@noveogroup.com>
2022-05-25 17:24:33 +0600

    Fix go fmt

--- community/go-engines-community/lib/api/auth/constants.go
+++ community/go-engines-community/lib/api/auth/constants.go
@@ -2,7 +2,7 @@ package auth
 
 const (
 	// UserKey is the context name for user credential.
-	UserKey = "user_id"
+	UserKey  = "user_id"
 	Username = "username"
 	// ApiKey is the context name for user api key.
 	ApiKey = "api_key"
