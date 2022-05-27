9ea1b0ce5c586ce7adef2425fd91fc645f320764
Vladimir Erokhov <vladimir.erokhov@noveogroup.com>
2022-05-27 17:03:19 +0600

    feature(#3320): Updated some wrong tests

--- community/sources/webcore/src/canopsis-next/tests/unit/jest.setup-test-framework.js
+++ community/sources/webcore/src/canopsis-next/tests/unit/jest.setup-test-framework.js
@@ -14,7 +14,7 @@ expect.extend({
     const img = canvas.toDataURL();
     const data = img.replace(/^data:image\/(png|jpg);base64,/, '');
     const newOptions = {
-      failureThreshold: 0.02,
+      failureThreshold: 0.05,
       failureThresholdType: 'percent',
       customSnapshotIdentifier: ({ currentTestName, counter }) => (
         kebabCase(`${currentTestName.replace(/(.*\sRenders\s)|(.$)/g, '')}-${counter}`)
