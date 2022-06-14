65d3913452aac55e3834bc5461cd8d7c2a7383fe
ikalugin <ivan.kalugin@noveogroup.com>
2022-06-14 17:27:12 +0700

    feature(#4166): fix typo

--- community/go-engines-community/lib/canopsis/pattern/errors.go
+++ community/go-engines-community/lib/canopsis/pattern/errors.go
@@ -5,4 +5,4 @@ import "errors"
 var InvalidOldAlarmPattern = errors.New("old alarm pattern is invalid")
 var InvalidOldEntityPattern = errors.New("old entity pattern is invalid")
 var AlarmPatternError = errors.New("alarm pattern returned error")
-var EntityPatternError = errors.New("alarm pattern returned error")
+var EntityPatternError = errors.New("entity pattern returned error")
