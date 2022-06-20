665adb2ef061b1198a69debe567da539748b590b
Dmitry <Dmitriy.Mikhaylishin@noveogroup.com>
2022-06-20 20:39:49 +0600

    feature(#4375): storage added

---
+++ community/sources/webcore/src/canopsis-next/src/plugins/flowchart/components/common/storage-figure.vue
@@ -0,0 +1,49 @@
+<template lang="pug">
+  path(v-on="$listeners", :d="path")
+</template>
+
+<script>
+import PointsPath from './points-path.vue';
+
+export default {
+  components: { PointsPath },
+  props: {
+    width: {
+      type: Number,
+      required: true,
+    },
+    height: {
+      type: Number,
+      required: true,
+    },
+    radius: {
+      type: Number,
+      required: true,
+    },
+    x: {
+      type: Number,
+      required: true,
+    },
+    y: {
+      type: Number,
+      required: true,
+    },
+  },
+  computed: {
+    halfWidth() {
+      return this.width / 2;
+    },
+
+    path() {
+      return [
+        `M ${this.x}, ${this.y + this.radius}`,
+        `a ${this.halfWidth},${this.radius} 0 1, 0 ${this.width}, 0`,
+        `a ${this.halfWidth},${this.radius} 0 1, 0 ${-this.width}, 0`,
+        ` L ${this.x} ${this.y + this.height - this.radius}`,
+        `a ${this.halfWidth},${this.radius} 0 1, 0 ${this.width}, 0`,
+        `L ${this.x + this.width} ${this.y + this.radius}`,
+      ].join('');
+    },
+  },
+};
+</script>
