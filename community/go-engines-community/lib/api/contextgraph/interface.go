fc90df6366a9677044f33cd01faca99e5ae78eb7
Zarina Yakubova <Zarina.Yakubova@noveogroup.com>
2022-06-22 15:48:46 +0600

    feature(#4386): Add new import API endpoint

--- community/go-engines-community/lib/api/contextgraph/interface.go
+++ community/go-engines-community/lib/api/contextgraph/interface.go
@@ -3,14 +3,8 @@ package contextgraph
 import (
 	"context"
 	"git.canopsis.net/canopsis/canopsis-community/community/go-engines-community/lib/canopsis/importcontextgraph"
-	"git.canopsis.net/canopsis/canopsis-community/community/go-engines-community/lib/canopsis/types"
 	"time"
 )
-
-type EventPublisher interface {
-	SendImportResultEvent(uuid string, execTime time.Duration, state types.CpsNumber) error
-	SendPerfDataEvent(uuid string, stats importcontextgraph.Stats, state types.CpsNumber) error
-}
 
 type StatusReporter interface {
 	GetStatus(ctx context.Context, id string) (ImportJob, error)
@@ -18,6 +12,7 @@ type StatusReporter interface {
 	ReportOngoing(ctx context.Context, job ImportJob) error
 	ReportDone(ctx context.Context, job ImportJob, stats importcontextgraph.Stats) error
 	ReportError(ctx context.Context, job ImportJob, execDuration time.Duration, err error) error
+	Clean(ctx context.Context, interval time.Duration) error
 }
 
 type ImportWorker interface {
