ecd25d04ba5a60b848b51315dd8372bf6e1d9820
Zarina Yakubova <Zarina.Yakubova@noveogroup.com>
2022-06-22 15:48:46 +0600

    feature(#4386): Add new import API endpoint

--- community/go-engines-community/lib/api/contextgraph/worker.go
+++ community/go-engines-community/lib/api/contextgraph/worker.go
@@ -2,73 +2,58 @@ package contextgraph
 
 import (
 	"context"
-	"errors"
 	"fmt"
 	"git.canopsis.net/canopsis/canopsis-community/community/go-engines-community/lib/canopsis/config"
 	"git.canopsis.net/canopsis/canopsis-community/community/go-engines-community/lib/canopsis/importcontextgraph"
-	"git.canopsis.net/canopsis/canopsis-community/community/go-engines-community/lib/canopsis/types"
 	"github.com/rs/zerolog"
 	"time"
 )
 
 const (
-	DefaultThdWarnMinPerImport = 30 * time.Minute
-	DefaultThdCritMinPerImport = 60 * time.Minute
+	queueCheckTickInterval  = time.Second
+	reportCleanTickInterval = time.Hour
+	reportCleanInterval     = 24 * time.Hour
 )
 
 type worker struct {
-	importQueue         JobQueue
-	reporter            StatusReporter
-	publisher           EventPublisher
-	logger              zerolog.Logger
-	filePattern         string
-	thdWarnMinPerImport time.Duration
-	thdCritMinPerImport time.Duration
-	worker              importcontextgraph.Worker
+	importQueue JobQueue
+	reporter    StatusReporter
+	logger      zerolog.Logger
+	filePattern string
+	worker      importcontextgraph.Worker
 }
 
 func NewImportWorker(
 	conf config.CanopsisConf,
-	publisher EventPublisher,
 	reporter StatusReporter,
 	queue JobQueue,
 	importWorker importcontextgraph.Worker,
 	logger zerolog.Logger,
 ) ImportWorker {
-	w := &worker{
+	return &worker{
 		importQueue: queue,
-		publisher:   publisher,
 		reporter:    reporter,
 		filePattern: conf.ImportCtx.FilePattern,
 		worker:      importWorker,
+		logger:      logger,
 	}
-
-	thdWarnMinPerImport, err := time.ParseDuration(conf.ImportCtx.ThdWarnMinPerImport)
-	if err != nil {
-		logger.Warn().Err(err).Msg("Can't parse thdWarnMinPerImport value, use default")
-		thdWarnMinPerImport = DefaultThdWarnMinPerImport
-	}
-
-	thdCritMinPerImport, err := time.ParseDuration(conf.ImportCtx.ThdCritMinPerImport)
-	if err != nil {
-		logger.Warn().Err(err).Msg("Can't parse thdCritMinPerImport value, use default")
-		thdCritMinPerImport = DefaultThdCritMinPerImport
-	}
-
-	w.thdWarnMinPerImport = thdWarnMinPerImport
-	w.thdCritMinPerImport = thdCritMinPerImport
-
-	return w
 }
 
 func (w *worker) Run(ctx context.Context) {
-	ticker := time.NewTicker(1 * time.Second)
+	ticker := time.NewTicker(queueCheckTickInterval)
 	defer ticker.Stop()
+	cleanTicker := time.NewTicker(reportCleanTickInterval)
+	defer cleanTicker.Stop()
 
 	for {
 		select {
 		case <-ctx.Done():
 			return
+		case <-cleanTicker.C:
+			err := w.reporter.Clean(ctx, reportCleanInterval)
+			if err != nil {
+				w.logger.Err(err).Msg("Import-ctx: Failed to clean import reports")
+			}
 		case <-ticker.C:
 			job := w.importQueue.Pop()
 			if job.ID == "" {
@@ -79,11 +64,6 @@ func (w *worker) Run(ctx context.Context) {
 			if err != nil {
 				w.logger.Err(err).Str("job_id", job.ID).Msg("Import-ctx: Failed to update import info")
 
-				err = w.publisher.SendImportResultEvent(job.ID, 0, types.AlarmStateCritical)
-				if err != nil {
-					w.logger.Err(err).Str("job_id", job.ID).Msg("Import-ctx: Failed to send import result event")
-				}
-
 				continue
 			}
 
@@ -91,14 +71,8 @@ func (w *worker) Run(ctx context.Context) {
 			stats, err := w.doJob(ctx, job)
 			stats.ExecTime = time.Since(startTime)
 
-			resultState := types.AlarmStateOK
 			if err != nil {
 				w.logger.Err(err).Str("job_id", job.ID).Msg("Import-ctx: Error during the import.")
-
-				resultState = types.AlarmStateCritical
-				if errors.Is(err, importcontextgraph.ErrNotImplemented) {
-					resultState = types.AlarmStateMinor
-				}
 
 				err = w.reporter.ReportError(ctx, job, stats.ExecTime, err)
 				if err != nil {
@@ -112,23 +86,6 @@ func (w *worker) Run(ctx context.Context) {
 					w.logger.Err(err).Str("job_id", job.ID).Msg("Import-ctx: Failed to update import info")
 				}
 			}
-
-			perfDataState := types.AlarmStateOK
-			if stats.ExecTime > w.thdCritMinPerImport {
-				perfDataState = types.AlarmStateMajor
-			} else if stats.ExecTime > w.thdWarnMinPerImport {
-				perfDataState = types.AlarmStateMinor
-			}
-
-			err = w.publisher.SendPerfDataEvent(job.ID, stats, types.CpsNumber(perfDataState))
-			if err != nil {
-				w.logger.Err(err).Str("job_id", job.ID).Msg("Import-ctx: Failed to send perf data")
-			}
-
-			err = w.publisher.SendImportResultEvent(job.ID, stats.ExecTime, types.CpsNumber(resultState))
-			if err != nil {
-				w.logger.Err(err).Str("job_id", job.ID).Msg("Import-ctx: Failed to send import result event")
-			}
 		}
 	}
 }
@@ -136,5 +93,8 @@ func (w *worker) Run(ctx context.Context) {
 func (w *worker) doJob(ctx context.Context, job ImportJob) (importcontextgraph.Stats, error) {
 	w.logger.Info().Str("job_id", job.ID).Msg("Import-ctx: Processing import")
 	filename := fmt.Sprintf(w.filePattern, job.ID)
+	if job.IsPartial {
+		return w.worker.WorkPartial(ctx, filename, job.Source)
+	}
 	return w.worker.Work(ctx, filename, job.Source)
 }
