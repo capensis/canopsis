655dc80b0d1e0167b36ec48f1d4875f0f85d40db
Dmitry <Dmitriy.Mikhaylishin@noveogroup.com>
2022-07-05 12:38:52 +0600

    feature(#4375): curves arrows added

--- community/sources/webcore/src/canopsis-next/src/components/common/flowchart/icons/curve-arrow-line-shape.vue
+++ community/sources/webcore/src/canopsis-next/src/components/common/flowchart/icons/curve-arrow-line-shape.vue
@@ -6,14 +6,17 @@
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
   >
-    <line
-      x1="1"
-      y1="44"
-      x2="44"
-      y2="1"
+    <path
+      fill="currentColor"
+      d="M33 0 l12 6 l-12 6 l3 -6 Z"
+    />
+    <path
+      d="M 0 44
+      Q 45 44 22.5 22.5
+      Q 0 6 36 6"
+      fill="none"
       stroke="currentColor"
       stroke-width="2"
-      stroke-dasharray="4 4"
     />
   </svg>
 </template>
