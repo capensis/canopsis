9801e8ffb74db4538a867c277392dd8188bb8778
Dmitry <Dmitriy.Mikhaylishin@noveogroup.com>
2022-07-28 15:35:59 +0600

    feature(#4441): choose map type modal added

--- community/sources/webcore/src/canopsis-next/src/components/modals/map/create-map.vue
+++ community/sources/webcore/src/canopsis-next/src/components/modals/map/create-map.vue
@@ -4,11 +4,22 @@
       template(#title="")
         span {{ $t('modals.createMap.title') }}
       template(#text="")
-        | Create map
+        v-layout(column)
+          v-flex.my-1.cursor-pointer(
+            v-for="type in availableTypes",
+            :key="type",
+            @click="selectType(type)"
+          )
+            v-card
+              v-card-title.py-3(primary-title)
+                v-layout
+                  div.subheading {{ $t(`map.types.${type}`) }}
+                  v-spacer
+                  v-icon {{ getIconByType(type) }}
 </template>
 
 <script>
-import { MODALS } from '@/constants';
+import { MODALS, MAP_TYPES, MAP_ICON_BY_TYPES } from '@/constants';
 
 import { modalInnerMixin } from '@/mixins/modal/inner';
 
@@ -22,5 +33,35 @@ export default {
   mixins: [
     modalInnerMixin,
   ],
+  computed: {
+    availableTypes() {
+      return Object.values(MAP_TYPES);
+    },
+  },
+  methods: {
+    getIconByType(type) {
+      return MAP_ICON_BY_TYPES[type];
+    },
+
+    selectType(type) {
+      const modalName = {
+        [MAP_TYPES.geo]: MODALS.createGeoMap,
+        [MAP_TYPES.flowchart]: MODALS.createFlowchartMap,
+        [MAP_TYPES.mermaid]: MODALS.createMermaidMap,
+        [MAP_TYPES.treeOfDependencies]: MODALS.createTreeOfDependenciesMap,
+      }[type];
+
+      this.$modals.show({
+        name: modalName,
+        config: {
+          action: async (map) => {
+            await this.config.action(map);
+
+            this.$modals.hide();
+          },
+        },
+      });
+    },
+  },
 };
 </script>
