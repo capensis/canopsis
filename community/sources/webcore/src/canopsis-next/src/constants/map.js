b2d9b12f19a025192b139cd2f0da1accc5420489
Dmitry <Dmitriy.Mikhaylishin@noveogroup.com>
2022-07-28 15:35:59 +0600

    feature(#4441): choose map type modal added

--- community/sources/webcore/src/canopsis-next/src/constants/map.js
+++ community/sources/webcore/src/canopsis-next/src/constants/map.js
@@ -4,3 +4,10 @@ export const MAP_TYPES = {
   mermaid: 'mermaid',
   treeOfDependencies: 'treeOfDependencies',
 };
+
+export const MAP_ICON_BY_TYPES = {
+  [MAP_TYPES.geo]: 'place',
+  [MAP_TYPES.flowchart]: 'category',
+  [MAP_TYPES.mermaid]: 'code',
+  [MAP_TYPES.treeOfDependencies]: 'scatter_plot',
+};
