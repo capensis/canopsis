69bcd436da69eb8c15df1c524e56ed38d156085d
Dmitry <Dmitriy.Mikhaylishin@noveogroup.com>
2022-08-01 21:08:47 +0600

    feature(#4449): mermaid diagram preview added

---
+++ community/sources/webcore/src/canopsis-next/src/components/other/map/form/mermaid-map-form.vue
@@ -0,0 +1,30 @@
+<template lang="pug">
+  v-layout(column)
+    v-text-field(
+      v-field="form.name",
+      v-validate="'required'",
+      :label="$t('common.name')",
+      :error-messages="errors.collect('name')",
+      name="name"
+    )
+    mermaid-editor(v-model="form.properties")
+</template>
+
+<script>
+import MermaidEditor from '@/components/common/mermaid/mermaid-editor.vue';
+
+export default {
+  inject: ['$validator'],
+  components: { MermaidEditor },
+  model: {
+    prop: 'form',
+    event: 'input',
+  },
+  props: {
+    form: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
