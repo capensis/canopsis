3a4ff64bf7785cc2c724aef611b2f250b7d900fd
Dmitry <Dmitriy.Mikhaylishin@noveogroup.com>
2022-08-01 12:04:28 +0600

    feature(#4435): fixes after review

--- community/sources/webcore/src/canopsis-next/src/constants/map.js
+++ community/sources/webcore/src/canopsis-next/src/constants/map.js
@@ -1,6 +1,15 @@
+import { MODALS } from '@/constants/modal';
+
 export const MAP_TYPES = {
   geo: 'geo',
   flowchart: 'flowchart',
   mermaid: 'mermaid',
   treeOfDependencies: 'treeOfDependencies',
+};
+
+export const MAP_CREATE_MODAL_NAMES_BY_TYPE = {
+  [MAP_TYPES.geo]: MODALS.createGeoMap,
+  [MAP_TYPES.flowchart]: MODALS.createFlowchartMap,
+  [MAP_TYPES.mermaid]: MODALS.createMermaidMap,
+  [MAP_TYPES.treeOfDependencies]: MODALS.createTreeOfDependenciesMap,
 };
