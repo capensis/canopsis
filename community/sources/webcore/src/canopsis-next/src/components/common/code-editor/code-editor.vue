0c1c11fa66c1a5031fbfa906f9cf443e9c3b3a28
Vladimir Erokhov <Vladimir.Erokhov@noveogroup.com>
2022-08-03 16:55:50 +0600

    feature(#4443): Added monaco-editor and mermaid highlight

---
+++ community/sources/webcore/src/canopsis-next/src/components/common/code-editor/code-editor.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+  div(ref="codeEditor")
+</template>
+
+<script>
+import initEditor from 'monaco-mermaid';
+import * as monaco from 'monaco-editor';
+
+initEditor(monaco);
+
+export default {
+  mounted() {
+    this.$editor = monaco.editor.create(this.$refs.codeEditor, {
+      value: '',
+      language: 'mermaid',
+    });
+  },
+};
+</script>
