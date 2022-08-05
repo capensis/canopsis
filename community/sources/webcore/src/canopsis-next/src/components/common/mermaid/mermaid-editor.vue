a865c94974fe3c1b6ec02a032cb0592d457230fd
Dmitry <Dmitriy.Mikhaylishin@noveogroup.com>
2022-08-05 12:44:50 +0600

    feature(#4450): issue finished

--- community/sources/webcore/src/canopsis-next/src/components/common/mermaid/mermaid-editor.vue
+++ community/sources/webcore/src/canopsis-next/src/components/common/mermaid/mermaid-editor.vue
@@ -1,26 +1,20 @@
 <template lang="pug">
-  v-layout.mermaid-editor
+  v-layout.mermaid-editor(:style="editorStyles")
     v-flex.mermaid-editor__sidebar
       mermaid-code-editor.fill-height(v-field="form.code")
     v-flex.mermaid-editor__content
-      v-layout.fill-height(column)
-        v-flex.mermaid-editor__toolbar
-          v-layout(row, align-center, justify-end)
-            v-btn-toggle.mr-2
-              v-btn.ma-0(v-model="addOnClick", flat, large)
-                v-icon(left) add_location
-                span {{ $t('mermaid.addPoint') }}
-            v-flex(xs4)
-              mermaid-theme-field.my-2(v-field="form.theme")
-        v-flex
-          div.position-relative
-            mermaid-preview(:value="form.code", :config="config")
-            mermaid-points.mermaid-editor__points(v-field="form.points", :add-on-click="addOnClick")
+      v-layout.mermaid-editor__toolbar.px-2(row, align-center, justify-end)
+        v-btn-toggle.mr-2
+          v-btn.ma-0(v-model="addOnClick", flat, large)
+            v-icon(left) add_location
+            span {{ $t('mermaid.addPoint') }}
+        mermaid-theme-field.my-2.mermaid-editor__theme-picker(v-field="form.theme")
+      div.mermaid-editor__preview
+        mermaid-preview(:value="form.code", :theme="form.theme")
+        mermaid-points.mermaid-editor__points(v-field="form.points", :add-on-click="addOnClick")
 </template>
 
 <script>
-import { MERMAID_THEME_PROPERTIES_BY_NAME } from '@/constants';
-
 import { formMixin } from '@/mixins/form';
 
 import MermaidCodeEditor from './partials/mermaid-code-editor.vue';
@@ -40,6 +34,10 @@ export default {
       type: Object,
       required: true,
     },
+    minHeight: {
+      type: Number,
+      default: 500,
+    },
   },
   data() {
     return {
@@ -47,27 +45,11 @@ export default {
     };
   },
   computed: {
-    config() {
-      const themeProperties = MERMAID_THEME_PROPERTIES_BY_NAME[this.form.theme] ?? {};
+    editorStyles() {
+      const minHeight = Math.max(...this.form.points.map(({ y }) => y), this.minHeight);
 
       return {
-        theme: this.form.theme,
-        ...themeProperties,
-
-        er: {
-          useMaxWidth: false,
-        },
-        pie: {
-          useMaxWidth: false,
-        },
-        sequence: {
-          useMaxWidth: false,
-        },
-        flowchart: {
-        },
-        requirement: {
-          useMaxWidth: false,
-        },
+        minHeight: `${minHeight}px`,
       };
     },
   },
@@ -76,29 +58,50 @@ export default {
 
 <style lang="scss">
 $borderColor: #e5e5e5;
-$sideBarWidth: 500px;
+$contentBackgroundColor: #f9f9f9;
 $contentWidth: 800px;
+$toolbarHeight: 60px;
 
 .mermaid-editor {
-  min-height: 500px;
+  border: 1px solid $borderColor;
 
   &__sidebar {
-    width: $sideBarWidth;
-    max-width: $sideBarWidth;
-    border: 1px solid $borderColor;
-    border-right: none;
+    flex: 1;
+    min-width: 200px;
   }
 
   &__content {
+    background: $contentBackgroundColor;
+
+    border-left: 1px solid $borderColor;
+
+    position: relative;
+
     width: $contentWidth;
     max-width: $contentWidth;
-    border: 1px solid $borderColor;
   }
 
   &__toolbar {
-    height: 60px;
-    max-height: 60px;
+    background: white;
+
+    position: absolute;
+    right: 0;
+
+    height: $toolbarHeight;
+    max-height: $toolbarHeight;
+
     border-bottom: 1px solid $borderColor;
+    border-left: 1px solid $borderColor;
+  }
+
+  &__preview {
+    position: relative;
+
+    margin-top: $toolbarHeight;
+  }
+
+  &__theme-picker {
+    width: 200px;
   }
 
   &__points {
