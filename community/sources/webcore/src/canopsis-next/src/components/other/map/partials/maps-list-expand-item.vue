b9d59d1e40228882950580345f11703f7c39b739
Dmitry <Dmitriy.Mikhaylishin@noveogroup.com>
2022-08-08 13:15:15 +0600

    feature(#4439): issue finished

--- community/sources/webcore/src/canopsis-next/src/components/other/map/partials/maps-list-expand-item.vue
+++ community/sources/webcore/src/canopsis-next/src/components/other/map/partials/maps-list-expand-item.vue
@@ -1,13 +1,29 @@
 <template lang="pug">
-  div {{ map | json }}
+  v-tabs(color="secondary lighten-1", slider-color="primary", dark, centered)
+    v-tab {{ $t(`map.types.${map.type}`) }}
+    v-tab-item
+      v-layout.pa-3
+        component(:is="component", :map="map")
 </template>
 
 <script>
+import { MAP_TYPES } from '@/constants';
+
+import MapsListExpandMermaidItem from './maps-list-expand-mermaid-item.vue';
+
 export default {
+  components: { MapsListExpandMermaidItem },
   props: {
     map: {
       type: Object,
       required: true,
+    },
+  },
+  computed: {
+    component() {
+      return {
+        [MAP_TYPES.mermaid]: 'maps-list-expand-mermaid-item',
+      }[this.map.type];
     },
   },
 };
