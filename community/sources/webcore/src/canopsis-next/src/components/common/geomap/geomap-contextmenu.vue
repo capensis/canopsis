e9f0f3aa7eb0bbb085eed16f0dfa5f9f7f8a6e76
Dmitry <Dmitriy.Mikhaylishin@noveogroup.com>
2022-08-10 15:22:19 +0600

    feature(#4458): context menu finished

---
+++ community/sources/webcore/src/canopsis-next/src/components/common/geomap/geomap-contextmenu.vue
@@ -0,0 +1,91 @@
+<template lang="pug">
+  v-menu(
+    v-model="shown",
+    :position-x="pageX",
+    :position-y="pageY",
+    :close-on-content-click="false",
+    ignore-click-upper-outside,
+    offset-overflow,
+    offset-x,
+    absolute
+  )
+    v-list(dense)
+      v-list-tile(v-for="action in availableActions", :key="action.text", @click="applyAction(action)")
+        v-list-tile-content
+          v-list-tile-title {{ action.text }}
+</template>
+
+<script>
+import { findRealParent } from 'vue2-leaflet';
+
+export default {
+  props: {
+    items: {
+      type: Array,
+      default: () => [],
+    },
+    markerItems: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  data() {
+    return {
+      mapObject: undefined,
+      latlng: undefined,
+      marker: undefined,
+      shown: false,
+      pageX: 0,
+      pageY: 0,
+    };
+  },
+  computed: {
+    availableActions() {
+      if (this.marker) {
+        return this.markerItems;
+      }
+
+      return this.items;
+    },
+  },
+  mounted() {
+    this.parentContainer = findRealParent(this.$parent);
+
+    this.mapObject = this.parentContainer.mapObject;
+
+    this.mapObject.on('contextmenu', this.open);
+  },
+
+  beforeDestroy() {
+    this.mapObject.off('contextmenu', this.open);
+  },
+
+  methods: {
+    open({ latlng, originalEvent, marker }) {
+      if (this.shown) {
+        this.shown = false;
+        return;
+      }
+
+      this.latlng = latlng;
+      this.marker = marker;
+      this.pageX = originalEvent.pageX;
+      this.pageY = originalEvent.pageY;
+      this.shown = true;
+    },
+
+    close() {
+      this.shown = false;
+    },
+
+    applyAction({ action }) {
+      action({
+        latlng: this.latlng,
+        marker: this.marker,
+        pageX: this.pageX,
+        pageY: this.pageY,
+      });
+    },
+  },
+};
+</script>
--- community/sources/webcore/src/canopsis-next/src/components/examples/example-map.vue
+++ community/sources/webcore/src/canopsis-next/src/components/examples/example-map.vue
@@ -2,6 +2,11 @@
   geomap(style="height: 300px")
     geomap-control-zoom(position="topleft")
     geomap-control-layers(position="topright")
+    geomap-contextmenu(
+      ref="contextmenu",
+      :items="mapItems",
+      :marker-items="markerItems"
+    )
 
     geomap-tile-layer(
       name="Open street map",
@@ -23,6 +28,8 @@
         :key="marker.id",
         :lat-lng="marker.coordinate",
         :options="{ data: marker.data }",
+        draggable,
+        @dragend="finishMovingMarker",
         @click="showInformationModal"
       )
 
@@ -39,6 +46,8 @@ import defaultLogo from '@/assets/canopsis.png';
 
 import { MODALS } from '@/constants';
 
+import uuid from '@/helpers/uuid';
+
 import Geomap from '@/components/common/geomap/geomap.vue';
 import GeomapMarker from '@/components/common/geomap/geomap-marker.vue';
 import GeomapTileLayer from '@/components/common/geomap/geomap-tile-layer.vue';
@@ -48,12 +57,14 @@ import GeomapControl from '@/components/common/geomap/geomap-control.vue';
 import GeomapPopup from '@/components/common/geomap/geomap-popup.vue';
 import GeomapControlZoom from '@/components/common/geomap/geomap-control-zoom.vue';
 import GeomapControlLayers from '@/components/common/geomap/geomap-control-layers.vue';
+import GeomapContextmenu from '@/components/common/geomap/geomap-contextmenu.vue';
 
 /**
  * TODO: Component should be removed in the end feature development
  */
 export default {
   components: {
+    GeomapContextmenu,
     Geomap,
     GeomapPopup,
     GeomapControl,
@@ -64,20 +75,75 @@ export default {
     GeomapControlZoom,
     GeomapControlLayers,
   },
+  data() {
+    return {
+      markers: range(5).map(value => ({
+        id: value,
+        coordinate: [value * 10, 50 + value * 10],
+        data: { id: uuid() },
+      })),
+    };
+  },
   computed: {
     logo() {
       return defaultLogo;
     },
 
-    markers() {
-      return range(5).map(value => ({
-        id: value,
-        coordinate: [value * 10, 50 + value * 10],
-        data: { id: value },
-      }));
+    mapItems() {
+      return [{ text: 'Add point', action: this.addPoint }];
+    },
+
+    markerItems() {
+      return [
+        { text: 'Edit point', action: this.editPoint },
+        { text: 'Remove point', action: this.removePoint },
+      ];
     },
   },
   methods: {
+    finishMovingMarker({ target }) {
+      const { data } = target.options;
+      const index = this.markers.findIndex(({ data: { id } }) => data.id === id);
+
+      const { lat, lng } = target.getLatLng();
+
+      this.markers[index].coordinate = [lat, lng];
+    },
+
+    addPoint({ latlng }) {
+      const id = uuid();
+
+      this.markers.push({
+        id,
+        coordinate: [latlng.lat, latlng.lng],
+        data: { id },
+      });
+
+      this.$refs.contextmenu.close();
+    },
+
+    removePoint({ marker }) {
+      const { data } = marker.options;
+
+      this.markers = this.markers.filter(({ data: { id } }) => data.id !== id);
+
+      this.$refs.contextmenu.close();
+    },
+
+    editPoint({ latlng, marker }) {
+      const { data } = marker.options;
+
+      this.$modals.show({
+        name: MODALS.info,
+        config: {
+          title: `Edit marker: ${data.id}`,
+          text: `latitude: ${latlng.lat}, longitude: ${latlng.lng}`,
+        },
+      });
+
+      this.$refs.contextmenu.close();
+    },
+
     showInformationModal(event) {
       const {
         latlng,
