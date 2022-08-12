380af85da016443c68f72306185c9aabb18b96ef
Dmitry <Dmitriy.Mikhaylishin@noveogroup.com>
2022-08-12 17:56:10 +0600

    feature(#4437): styles replaced on className

--- community/sources/webcore/src/canopsis-next/src/components/common/geomap/geomap-tooltip.vue
+++ community/sources/webcore/src/canopsis-next/src/components/common/geomap/geomap-tooltip.vue
@@ -1,25 +1,20 @@
 <script>
 import { LTooltip } from 'vue2-leaflet';
+import { Tooltip } from 'leaflet';
+
+Tooltip.mergeOptions({
+  className: 'elevation-3 grey darken-2 white--text',
+});
 
 export default LTooltip;
 </script>
 
 <style lang="scss">
-
 .leaflet-tooltip {
-  background: #616161;
-  color: #fff;
   border-radius: 2px;
   border: none;
   font-size: 12px;
-  display: inline-block;
   padding: 5px 8px;
-  position: absolute;
-  text-transform: initial;
-  width: auto;
-  box-shadow: 0 3px 1px -2px rgba(0,0,0,0.2),
-    0px 2px 2px 0px rgba(0,0,0,0.14),
-    0px 1px 5px 0px rgba(0,0,0,0.12);
 
   &:before {
     content: none;
