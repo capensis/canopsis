78520a7e390600f63e62296e1bc2927b19c77de6
Dmitry <Dmitriy.Mikhaylishin@noveogroup.com>
2022-08-12 15:42:34 +0600

    feature(#4437): geo map expand panel added

--- community/sources/webcore/src/canopsis-next/src/components/other/map/partials/maps-list-expand-item.vue
+++ community/sources/webcore/src/canopsis-next/src/components/other/map/partials/maps-list-expand-item.vue
@@ -1,13 +1,29 @@
 <template lang="pug">
-  div {{ map | json }}
+  v-tabs(color="secondary lighten-1", slider-color="primary", dark, centered)
+    v-tab {{ $t(`map.types.${map.type}`) }}
+    v-tab-item
+      v-layout.pa-3
+        component(:is="component", :map="map")
 </template>
 
 <script>
+import { MAP_TYPES } from '@/constants';
+
+import MapsListExpandGeomapItem from './maps-list-expand-geomap-item.vue';
+
 export default {
+  components: { MapsListExpandGeomapItem },
   props: {
     map: {
       type: Object,
       required: true,
+    },
+  },
+  computed: {
+    component() {
+      return {
+        [MAP_TYPES.geo]: 'maps-list-expand-geomap-item',
+      }[this.map.type];
     },
   },
 };
