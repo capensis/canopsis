88bedcfa65efe73a06f42f5ca4d38c441d4196e7
ikalugin <ivan.kalugin@noveogroup.com>
2022-08-23 23:36:57 +0700

    feature(#4457): add pbehavior periodical metric

--- community/go-engines-community/database/postgres_migrations/7_add_fifo_queue_metric.down.sql
+++ community/go-engines-community/database/postgres_migrations/7_add_fifo_queue_metric.down.sql
@@ -4,5 +4,7 @@ DROP TABLE IF EXISTS fifo_queue;
 DROP TABLE IF EXISTS fifo_event;
 DROP TABLE IF EXISTS che_event;
 DROP TABLE IF EXISTS axe_event;
+DROP TABLE IF EXISTS axe_periodical;
+DROP TABLE IF EXISTS pbehavior_periodical;
 
 COMMIT;
