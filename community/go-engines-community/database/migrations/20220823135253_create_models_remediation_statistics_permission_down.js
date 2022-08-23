348cace85952c9eb240a96df045c3c7f55a08d56
ikalugin <ivan.kalugin@noveogroup.com>
2022-08-23 14:15:41 +0700

    feature(#4277): add mongo migration with models remediation statistics permission

---
+++ community/go-engines-community/database/migrations/20220823135253_create_models_remediation_statistics_permission_down.js
@@ -0,0 +1,10 @@
+b.default_rights.deleteMany({
+    _id: {
+        $in: ["models_remediationStatistic"],
+    }
+});
+db.default_rights.updateOne({_id: "admin"}, {
+    $unset: {
+        "rights.models_remediationStatistic": "",
+    }
+});
