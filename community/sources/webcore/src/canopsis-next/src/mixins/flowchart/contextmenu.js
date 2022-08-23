3ef5c57e9d551ebc63137ca1bf2402539d0d067d
Dmitry <Dmitriy.Mikhaylishin@noveogroup.com>
2022-08-23 17:27:50 +0600

    feature(#4375): points refactored

---
+++ community/sources/webcore/src/canopsis-next/src/mixins/flowchart/contextmenu.js
@@ -0,0 +1,25 @@
+import Observer from '@/services/observer';
+
+export const contextmenuMixin = {
+  provide() {
+    return {
+      $contextmenu: this.$contextmenu,
+    };
+  },
+  props: {
+    points: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  beforeCreate() {
+    this.$contextmenu = new Observer();
+  },
+  methods: {
+    handleContextmenu(event) {
+      const { x, y } = this.normalizeCursor({ x: event.clientX, y: event.clientY });
+
+      this.$contextmenu.notify({ event, x, y });
+    },
+  },
+};
