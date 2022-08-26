caaf342bebc5ad1eb40b5f2b45ade27e5a042f09
Zarina Yakubova <Zarina.Yakubova@noveogroup.com>
2022-08-26 14:23:59 +0600

    feature(#4277): Remove redundant migration

--- community/go-engines-community/database/migrations/20220823135253_create_models_remediation_statistics_permission_down.js
+++
@@ -1,6 +0,0 @@
-db.default_rights.deleteOne({_id: "models_remediationStatistic"});
-db.default_rights.updateOne({_id: "admin"}, {
-    $unset: {
-        "rights.models_remediationStatistic": "",
-    }
-});
