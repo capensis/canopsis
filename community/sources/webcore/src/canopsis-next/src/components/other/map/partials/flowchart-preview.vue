9be459d0cefd54bdf98e33cfa20a762c5ec265ce
Dmitry <Dmitriy.Mikhaylishin@noveogroup.com>
2022-09-02 13:18:53 +0600

    feature(#4438): help text added

--- community/sources/webcore/src/canopsis-next/src/components/other/map/partials/flowchart-preview.vue
+++ community/sources/webcore/src/canopsis-next/src/components/other/map/partials/flowchart-preview.vue
@@ -1,18 +1,21 @@
 <template lang="pug">
-  flowchart.flowchart-preview(
-    :shapes="shapes",
-    :background-color="map.parameters.background_color",
-    readonly
-  )
-    template(#layers="{ data }")
-      flowchart-points-preview(
-        :points="map.parameters.points",
-        :popup-template="popupTemplate",
-        :popup-actions="popupActions",
-        :color-indicator="colorIndicator",
-        :pbehavior-enabled="pbehaviorEnabled",
-        :shapes="data"
-      )
+  div.flowchart-preview
+    flowchart(
+      :shapes="shapes",
+      :background-color="map.parameters.background_color",
+      readonly
+    )
+      template(#layers="{ data }")
+        flowchart-points-preview(
+          :points="map.parameters.points",
+          :popup-template="popupTemplate",
+          :popup-actions="popupActions",
+          :color-indicator="colorIndicator",
+          :pbehavior-enabled="pbehaviorEnabled",
+          :shapes="data"
+        )
+    c-help-icon.flowchart-preview__help-icon(size="32", color="secondary", icon="help", top)
+      div.pre-wrap(v-html="$t('flowchart.panzoom.helpText')")
 </template>
 
 <script>
@@ -56,7 +59,15 @@ export default {
 
 <style lang="scss">
 .flowchart-preview {
+  position: relative;
   height: 800px;
   width: 100%;
+
+  &__help-icon {
+    cursor: pointer;
+    position: absolute;
+    bottom: 10px;
+    right: 10px;
+  }
 }
 </style>
