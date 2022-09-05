e57f057d8dc21ee03fb1bf1f57389933ee407e20
Dmitry <Dmitriy.Mikhaylishin@noveogroup.com>
2022-09-05 19:48:20 +0600

    fix(#4115): broadcast message width fixed

--- community/sources/webcore/src/canopsis-next/src/components/other/broadcast-message/broadcast-message.vue
+++ community/sources/webcore/src/canopsis-next/src/components/other/broadcast-message/broadcast-message.vue
@@ -3,7 +3,6 @@
     :style="{ backgroundColor: color }",
     :title="message"
   )
-    v-icon.broadcast-message__icon.mr-1(small) $vuetify.icons.bullhorn
     span.broadcast-message__text(v-html="message")
 </template>
 
@@ -32,6 +31,7 @@ export default {
     justify-content: center;
 
     &__text {
+      width: 100%;
       text-align: center;
       white-space: nowrap;
       overflow: hidden;
@@ -40,11 +40,6 @@ export default {
       & /deep/ p {
         margin: 0 !important;
       }
-    }
-
-    &__icon {
-      min-width: 16px;
-      color: inherit !important;
     }
   }
 </style>
