51f72ed093008aa50a4217a06ed4b0eac719b21c
Dmitry <Dmitriy.Mikhaylishin@noveogroup.com>
2022-09-07 17:10:41 +0600

    feature(#4503): mermaid points preview tested

--- community/sources/webcore/src/canopsis-next/src/components/other/map/partials/mermaid-points-preview.vue
+++ community/sources/webcore/src/canopsis-next/src/components/other/map/partials/mermaid-points-preview.vue
@@ -7,107 +7,38 @@
       :x="point.x",
       :y="point.y",
       :entity="point.entity",
-      :size="markerSize",
+      :size="iconSize",
       :color-indicator="colorIndicator",
       :pbehavior-enabled="pbehaviorEnabled",
       @click="openPopup(point, $event)"
     )
-    v-menu(
+    point-popup-dialog(
       v-if="activePoint",
-      :value="true",
+      :point="activePoint",
       :position-x="positionX",
       :position-y="positionY",
-      :close-on-content-click="false",
-      ignore-click-outside,
-      offset-overflow,
-      offset-x,
-      absolute,
-      top
+      :popup-template="popupTemplate",
+      :color-indicator="colorIndicator",
+      :popup-actions="popupActions",
+      @show:alarms="showAlarms",
+      @show:map="showLinkedMap",
+      @close="closePopup"
     )
-      point-popup(
-        v-click-outside="clickOutsideDirective",
-        :point="activePoint",
-        :template="popupTemplate",
-        :color-indicator="colorIndicator",
-        :actions="popupActions",
-        @show:alarms="showAlarms",
-        @show:map="showLinkedMap",
-        @close="closePopup"
-      )
 </template>
 
 <script>
-import { entitiesServiceEntityMixin } from '@/mixins/entities/service-entity';
+import { mapInformationPopup } from '@/mixins/map/map-information-popup';
 
 import MermaidPointMarker from './mermaid-point-marker.vue';
-import PointPopup from './point-popup.vue';
+import PointPopupDialog from './point-popup-dialog.vue';
 
 export default {
-  components: { MermaidPointMarker, PointPopup },
-  mixins: [entitiesServiceEntityMixin],
+  components: { MermaidPointMarker, PointPopupDialog },
+  mixins: [mapInformationPopup],
   props: {
     points: {
       type: Array,
       required: true,
-    },
-    markerSize: {
-      type: Number,
-      default: 24,
-    },
-    popupTemplate: {
-      type: String,
-      required: false,
-    },
-    popupActions: {
-      type: Boolean,
-      default: false,
-    },
-    colorIndicator: {
-      type: String,
-      required: false,
-    },
-    pbehaviorEnabled: {
-      type: Boolean,
-      default: false,
-    },
-  },
-  data() {
-    return {
-      positionX: 0,
-      positionY: 0,
-      activePoint: undefined,
-    };
-  },
-  computed: {
-    clickOutsideDirective() {
-      return {
-        handler: this.closePopup,
-        include: () => this.$refs.points.map(({ $el }) => $el),
-        closeConditional: () => true,
-      };
-    },
-  },
-  methods: {
-    openPopup(point, event) {
-      const { top, left, width } = event.target.getBoundingClientRect();
-
-      this.positionY = top;
-      this.positionX = left + width / 2;
-      this.activePoint = point;
-    },
-
-    closePopup() {
-      this.activePoint = undefined;
-    },
-
-    showLinkedMap() {
-      this.$emit('show:map', this.activePoint.map);
-      this.closePopup();
-    },
-
-    showAlarms() {
-      this.$emit('show:alarms', this.activePoint);
-      this.closePopup();
     },
   },
 };
