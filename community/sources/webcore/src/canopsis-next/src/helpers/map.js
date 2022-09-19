e4e202059d6cdb29dcdf90f66eb30040d294c1af
Vladimir Erokhov <vladimir.erokhov@noveogroup.com>
2022-09-19 21:42:02 +0600

    feature(#4440): Added entity-dependencies-list modal window

--- community/sources/webcore/src/canopsis-next/src/helpers/map.js
+++ community/sources/webcore/src/canopsis-next/src/helpers/map.js
@@ -1,3 +1,4 @@
+import { cloneDeep } from 'lodash';
 import { ENTITY_TYPES } from '@/constants';
 
 /**
@@ -35,4 +36,40 @@ export const getTreeOfDependenciesEntityText = entity => (
   entity.type === ENTITY_TYPES.resource
     ? entity._id
     : entity.name
+);
+
+/**
+ * @typedef {Object} TreeOfDependenciesMapEntity
+ * @property {Entity} entity
+ * @property {string[]} [dependencies]
+ */
+
+/**
+ *
+ * @param {Entity[]} [entities = []]
+ * @returns {Object<string, TreeOfDependenciesMapEntity>}
+ */
+export const normalizeTreeOfDependenciesMapEntities = (entities = []) => (
+  entities.reduce((acc, { entity, pinned_entities: pinnedEntities }) => {
+    const newEntity = {
+      entity: cloneDeep(entity),
+      dependencies: [],
+    };
+
+    pinnedEntities.forEach((pinnedEntity) => {
+      const { _id: id } = pinnedEntity;
+
+      newEntity.dependencies.push(id);
+
+      if (!acc[id]) {
+        acc[id] = {
+          entity: cloneDeep(pinnedEntity),
+        };
+      }
+    });
+
+    acc[entity._id] = newEntity;
+
+    return acc;
+  }, {})
 );
