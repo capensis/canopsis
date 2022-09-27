2770a6fcaec2c90397e70dff0432fb6f7331fbd4
Zarina Yakubova <Zarina.Yakubova@noveogroup.com>
2022-09-27 13:40:40 +0600

    feature(#4394): Add UI permission

--- community/go-engines-community/database/migrations/20220926134510_create_share_token_collection_down.js
+++ community/go-engines-community/database/migrations/20220926134510_create_share_token_collection_down.js
@@ -1,8 +1,16 @@
 db.share_token.dropIndex("value_1");
 
-db.default_rights.deleteOne({_id: "api_share_token"});
+db.default_rights.deleteMany({
+    _id: {
+        $in: [
+            "api_share_token",
+            "models_shareToken",
+        ]
+    }
+});
 db.default_rights.updateMany({crecord_type: "role"}, {
     $unset: {
         "rights.api_share_token": "",
+        "rights.models_shareToken": "",
     }
 });
