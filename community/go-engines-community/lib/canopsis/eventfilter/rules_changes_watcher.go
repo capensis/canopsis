8956177f0a2707f689c4185db030ac0ab2cc7819
ikalugin <ivan.kalugin@noveogroup.com>
2022-09-27 15:50:41 +0700

    feature(#4324): use errgroup in rules changes watcher

--- community/go-engines-community/lib/canopsis/eventfilter/rules_changes_watcher.go
+++ community/go-engines-community/lib/canopsis/eventfilter/rules_changes_watcher.go
@@ -6,6 +6,7 @@ import (
 
 	"git.canopsis.net/canopsis/canopsis-community/community/go-engines-community/lib/mongo"
 	"go.mongodb.org/mongo-driver/bson"
+	"golang.org/x/sync/errgroup"
 )
 
 const loadTimerDuration = 200 * time.Millisecond
@@ -29,57 +30,62 @@ type rulesChangesWatcher struct {
 }
 
 func (w *rulesChangesWatcher) Watch(ctx context.Context, types []string) error {
-	ctx, cancel := context.WithCancel(ctx)
-	defer cancel()
-
-	stream, err := w.collection.Watch(ctx, []bson.M{})
-	if err != nil {
-		return err
-	}
-
-	defer stream.Close(ctx)
+	eg, ctx := errgroup.WithContext(ctx)
 
 	// buf = 1, in order not to lose stream event when loader was slept
 	loadChan := make(chan struct{}, 1)
 
 	// load in case of something was changed in the collection when it wasn't watched
-	err = w.service.LoadRules(ctx, types)
+	err := w.service.LoadRules(ctx, types)
 	if err != nil {
 		return err
 	}
 
-	go func() {
+	eg.Go(func() error {
 		defer close(loadChan)
+
+		stream, err := w.collection.Watch(ctx, []bson.M{})
+		if err != nil {
+			return err
+		}
+
+		defer stream.Close(ctx)
 
 		for stream.Next(ctx) {
 			select {
 			case <-ctx.Done():
-				return
+				return nil
 			case loadChan <- struct{}{}:
 			default:
 			}
 		}
-	}()
 
-	for {
-		select {
-		case <-ctx.Done():
-			return nil
-		case _, ok := <-loadChan:
-			if !ok {
-				return nil
-			}
+		return nil
+	})
 
+	eg.Go(func() error {
+		for {
 			select {
 			case <-ctx.Done():
 				return nil
-			case <-time.After(w.loadSleepDuration):
-			}
+			case _, ok := <-loadChan:
+				if !ok {
+					return nil
+				}
 
-			err := w.service.LoadRules(ctx, types)
-			if err != nil {
-				return err
+				select {
+				case <-ctx.Done():
+					return nil
+				case <-time.After(w.loadSleepDuration):
+				}
+
+				err := w.service.LoadRules(ctx, types)
+				if err != nil {
+					return err
+				}
 			}
 		}
-	}
+	})
+
+	return eg.Wait()
 }
