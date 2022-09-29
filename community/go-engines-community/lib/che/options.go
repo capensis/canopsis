e845dcbac83e0e3329c64df8b04fa4d59be29ac8
Zarina Yakubova <Zarina.Yakubova@noveogroup.com>
2022-09-29 17:05:45 +0600

    feature(#4536): Remove deprecated flags

--- community/go-engines-community/lib/che/options.go
+++ community/go-engines-community/lib/che/options.go
@@ -16,7 +16,6 @@ type Options struct {
 	ModeDebug               bool
 	ConsumeFromQueue        string
 	PublishToQueue          string
-	DataSourceDirectory     string
 	PeriodicalWaitTime      time.Duration
 	InfosDictionaryWaitTime time.Duration
 	ExternalDataApiTimeout  time.Duration
@@ -29,7 +28,6 @@ func ParseOptions() Options {
 	flag.BoolVar(&opts.FeatureContextCreation, "createContext", true, "enable context graph creation. enabled by default. WARNING: disable the old context-graph engine when using this.")
 	flag.StringVar(&opts.PublishToQueue, "publishQueue", canopsis.AxeQueueName, "Publish event to this queue.")
 	flag.StringVar(&opts.ConsumeFromQueue, "consumeQueue", canopsis.CheQueueName, "Consume events from this queue.")
-	flag.StringVar(&opts.DataSourceDirectory, "dataSourceDirectory", ".", "The path of the directory containing the event filter's data source plugins.")
 	flag.BoolVar(&opts.ModeDebug, "d", false, "debug")
 	flag.BoolVar(&opts.PrintEventOnError, "printEventOnError", false, "Print event on processing error")
 	flag.BoolVar(&opts.Purge, "purge", false, "purge consumer queue(s) before work")
@@ -37,11 +35,6 @@ func ParseOptions() Options {
 	flag.DurationVar(&opts.InfosDictionaryWaitTime, "infosDictionaryWaitTime", time.Hour, "Duration to wait between two runs of update entity infos dictionary process")
 	flag.DurationVar(&opts.ExternalDataApiTimeout, "externalDataApiTimeout", 30*time.Second, "External API HTTP Request Timeout.")
 	flag.StringVar(&opts.FifoAckExchange, "fifoAckExchange", canopsis.FIFOAckExchangeName, "Publish FIFO Ack event to this exchange.")
-
-	flag.Bool("enrichContext", false, "enable context graph enrichment from event. disabled by default. WARNING: disable the old context-graph engine when using this. - deprecated")
-	flag.String("enrichExclude", "", "Coma separated list of fields that shall not be part of context enrichment. - deprecated")
-	flag.String("enrichInclude", "", "Coma separated list of the only fields that will be part of context enrichment. If present, -enrichExclude is ignored. - deprecated")
-
 	flag.BoolVar(&opts.Version, "version", false, "Show the version information")
 
 	flag.Parse()
