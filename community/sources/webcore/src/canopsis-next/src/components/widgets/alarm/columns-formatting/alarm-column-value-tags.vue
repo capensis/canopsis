f170d0aa76dc258e6fa9c792ab443dad4b747357
Vladimir Erokhov <vladimir.erokhov@noveogroup.com>
2022-09-30 17:14:18 +0600

    feature(#4534): Added basic functionality

---
+++ community/sources/webcore/src/canopsis-next/src/components/widgets/alarm/columns-formatting/alarm-column-value-tags.vue
@@ -0,0 +1,79 @@
+<template lang="pug">
+  v-layout(row, align-center)
+    c-alarm-tag-chip(
+      v-for="tag in inlineTags",
+      :key="tag._id",
+      :color="getTagColor(tag)",
+      @click="selectTag(tag)"
+    ) {{ tag }}
+    v-menu(
+      v-if="dropDownTags.length",
+      bottom,
+      left
+    )
+      template(#activator="{ on }")
+        v-btn.ma-1(v-on="on", color="grey", icon, small)
+          v-icon(color="white", small) more_horiz
+      v-card
+        v-card-text
+          c-alarm-tag-chip(
+            v-for="tag in dropDownTags",
+            :key="tag",
+            :color="getTagColor(tag)",
+            @click="selectTag(tag)"
+          ) {{ tag }}
+</template>
+
+<script>
+import { entitiesAlarmTagMixin } from '@/mixins/entities/alarm-tag';
+
+export default {
+  mixins: [entitiesAlarmTagMixin],
+  props: {
+    alarm: {
+      type: Object,
+      required: true,
+    },
+    selectedTag: {
+      type: String,
+      default: '',
+    },
+  },
+  computed: {
+    tags() {
+      return [...(this.alarm.tags ?? [])].sort((firstTag, secondTag) => {
+        if (firstTag === this.selectedTag) {
+          return -1;
+        }
+
+        if (secondTag === this.selectedTag) {
+          return 0;
+        }
+
+        if (firstTag < secondTag) {
+          return -1;
+        }
+
+        if (firstTag > secondTag) {
+          return 1;
+        }
+
+        return 0;
+      });
+    },
+
+    inlineTags() {
+      return this.tags.slice(0, 2);
+    },
+
+    dropDownTags() {
+      return this.tags.slice(2);
+    },
+  },
+  methods: {
+    selectTag(tag) {
+      this.$emit('select', tag);
+    },
+  },
+};
+</script>
