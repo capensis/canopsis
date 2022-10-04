47dc62b990917f4ce21ef817229016da18f8cc34
Dmitry <Dmitriy.Mikhaylishin@noveogroup.com>
2022-10-04 11:30:01 +0600

    feature(#4474): execute instruction simple type started implementing

--- community/sources/webcore/src/canopsis-next/src/components/other/remediation/instruction-execute/remediation-instruction-execute.vue
+++ community/sources/webcore/src/canopsis-next/src/components/other/remediation/instruction-execute/remediation-instruction-execute.vue
@@ -1,27 +1,42 @@
 <template lang="pug">
   div
-    v-text-field(
-      :value="instructionExecution.description",
-      :label="$t('common.description')",
-      readonly,
-      box
+    remediation-instruction-execute-simple-steps(
+      v-if="isManualSimpleType",
+      :jobs="instructionExecution.steps[0].operations[0].jobs"
     )
-    remediation-instruction-execute-steps(
-      :steps="instructionExecution.steps",
-      :execution-id="instructionExecution._id",
-      v-on="$listeners"
-    )
+    template(v-else)
+      v-text-field(
+        :value="instructionExecution.description",
+        :label="$t('common.description')",
+        readonly,
+        box
+      )
+      remediation-instruction-execute-steps(
+        :steps="instructionExecution.steps",
+        :execution-id="instructionExecution._id",
+        v-on="$listeners"
+      )
 </template>
 
 <script>
 import RemediationInstructionExecuteSteps from './remediation-instruction-execute-steps.vue';
+import RemediationInstructionExecuteSimpleSteps from './remediation-instruction-execute-simple-steps.vue';
 
 export default {
-  components: { RemediationInstructionExecuteSteps },
+  components: { RemediationInstructionExecuteSimpleSteps, RemediationInstructionExecuteSteps },
   props: {
     instructionExecution: {
       type: Object,
       required: true,
+    },
+    simple: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  computed: {
+    isManualSimpleType() {
+      return this.instructionExecution;
     },
   },
 };
