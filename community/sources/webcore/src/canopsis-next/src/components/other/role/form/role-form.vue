3e074ffb1ce6df493bdde47145945df75e7fd39d
Dmitry <Dmitriy.Mikhaylishin@noveogroup.com>
2022-10-07 11:30:44 +0600

    feature(#3996): fixes after review

--- community/sources/webcore/src/canopsis-next/src/components/other/role/form/role-form.vue
+++ community/sources/webcore/src/canopsis-next/src/components/other/role/form/role-form.vue
@@ -1,11 +1,7 @@
 <template lang="pug">
   v-layout(column)
     c-name-field(v-field="form.name")
-    v-text-field(
-      v-field="form.description",
-      :label="$t('common.description')",
-      data-test="description"
-    )
+    v-text-field(v-field="form.description", :label="$t('common.description')")
 
     c-information-block(:title="$t('role.expirationSettings')")
       c-enabled-field(
@@ -13,17 +9,18 @@
         :label="form.auth_config.intervals_enabled ? $t('common.enabled') : $t('common.disabled')"
       )
 
-      v-layout(v-if="form.auth_config.intervals_enabled", row)
-        c-information-block(
-          :title="$t('role.inactivityInterval')",
-          :help-text="$t('role.inactivityIntervalHelpText')"
-        )
-          c-duration-field(v-field="form.auth_config.inactivity_interval", long)
-        c-information-block.ml-3(
-          :title="$t('role.expirationInterval')",
-          :help-text="$t('role.expirationIntervalHelpText')"
-        )
-          c-duration-field(v-field="form.auth_config.expiration_interval", long)
+      v-expand-transition
+        v-layout(v-if="form.auth_config.intervals_enabled", row)
+          c-information-block(
+            :title="$t('role.inactivityInterval')",
+            :help-text="$t('role.inactivityIntervalHelpText')"
+          )
+            c-duration-field(v-field="form.auth_config.inactivity_interval", long)
+          c-information-block.ml-3(
+            :title="$t('role.expirationInterval')",
+            :help-text="$t('role.expirationIntervalHelpText')"
+          )
+            c-duration-field(v-field="form.auth_config.expiration_interval", long)
     view-selector(v-field="form.defaultview")
 </template>
 
