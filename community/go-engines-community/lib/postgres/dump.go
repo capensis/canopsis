32b9d5c77e0a38f3e65c86862d3d4a9438497a03
Zarina Yakubova <Zarina.Yakubova@noveogroup.com>
2022-10-11 11:25:57 +0600

    feature(#4457): Add tech metrics dump endpoints

--- community/go-engines-community/lib/postgres/dump.go
+++ community/go-engines-community/lib/postgres/dump.go
@@ -7,11 +7,10 @@ import (
 
 // Dump calls pg_dump binary.
 func Dump(connStr, filepath string) error {
-	cmd := fmt.Sprintf("pg_dump %s -Fc -f %s", connStr, filepath)
-	result := exec.Command("bash", "-c", cmd)
+	result := exec.Command("pg_dump", connStr, "-Fc", "-f", filepath)
 	output, err := result.CombinedOutput()
 	if err != nil {
-		return fmt.Errorf("cannot execute command %q: %w: %s", cmd, err, string(output))
+		return fmt.Errorf("cannot execute command \"pg_dump\": %w: %s", err, string(output))
 	}
 
 	return nil
