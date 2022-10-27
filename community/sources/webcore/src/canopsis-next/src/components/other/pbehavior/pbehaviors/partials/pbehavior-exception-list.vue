7a591d8c9c370562154b5628ae8f24a878943a86
Dmitry <Dmitriy.Mikhaylishin@noveogroup.com>
2022-10-27 12:34:03 +0600

    feature(#4360): pbehavior-comment-field, pbehavior-comments-field, pbehavior-exception-field, pbehavior-exceptions-field, pbehavior-patterns-form tested

--- community/sources/webcore/src/canopsis-next/src/components/other/pbehavior/pbehaviors/partials/pbehavior-exception-list.vue
+++ community/sources/webcore/src/canopsis-next/src/components/other/pbehavior/pbehaviors/partials/pbehavior-exception-list.vue
@@ -1,12 +1,14 @@
 <template lang="pug">
   v-data-iterator(:items="exceptions", hide-actions)
-    v-flex(slot="item", slot-scope="props")
-      v-card
-        v-card-title {{ props.item.name }}
-      v-divider
-    v-flex(slot="no-data")
-      v-card
-        v-card-title {{ $t('tables.noData') }}
+    template(#item="{ item }")
+      v-flex
+        v-card
+          v-card-title {{ item.name }}
+        v-divider
+    template(#no-data="")
+      v-flex
+        v-card
+          v-card-title {{ $t('tables.noData') }}
 </template>
 
 <script>
---
+++ community/sources/webcore/src/canopsis-next/tests/unit/components/other/pbehavior/pbehaviors/field/pbehavior-comments-field.spec.js
@@ -0,0 +1,106 @@
+import Faker from 'faker';
+
+import { createVueInstance, generateRenderer, generateShallowRenderer } from '@unit/utils/vue';
+
+import PbehaviorCommentsField from '@/components/other/pbehavior/pbehaviors/fields/pbehavior-comments-field.vue';
+
+const localVue = createVueInstance();
+
+const stubs = {
+  'pbehavior-comment-field': true,
+};
+
+const selectAddCommentButton = wrapper => wrapper.find('v-btn-stub');
+const selectCommentFieldByIndex = (wrapper, index) => wrapper
+  .findAll('pbehavior-comment-field-stub')
+  .at(index);
+
+describe('pbehavior-comments-field', () => {
+  const factory = generateShallowRenderer(PbehaviorCommentsField, {
+    localVue,
+    stubs,
+  });
+  const snapshotFactory = generateRenderer(PbehaviorCommentsField, { localVue, stubs });
+
+  test('Comment added after trigger create button', () => {
+    const comments = [{ key: Faker.datatype.string(), message: Faker.datatype.string() }];
+    const wrapper = factory({
+      propsData: {
+        comments,
+      },
+    });
+
+    selectAddCommentButton(wrapper).vm.$emit('click');
+
+    expect(wrapper).toEmit('input', [
+      ...comments,
+      {
+        key: expect.any(String),
+        message: '',
+      },
+    ]);
+  });
+
+  test('Comment changed after trigger comment field', () => {
+    const comments = [
+      { key: 'comment-1', message: 'message-1' },
+      { key: 'comment-2', message: 'message-2' },
+    ];
+    const wrapper = factory({
+      propsData: {
+        comments,
+      },
+    });
+
+    const newComment = {
+      key: Faker.datatype.string(),
+      message: Faker.datatype.number(),
+    };
+
+    selectCommentFieldByIndex(wrapper, 1).vm.$emit('input', newComment);
+
+    expect(wrapper).toEmit('input', [
+      comments[0],
+      newComment,
+    ]);
+  });
+
+  test('Comment removed after trigger comment field', () => {
+    const comments = [
+      { key: 'comment-1', message: 'message-1' },
+      { key: 'comment-2', message: 'message-2' },
+    ];
+    const wrapper = factory({
+      propsData: {
+        comments,
+      },
+    });
+
+    selectCommentFieldByIndex(wrapper, 1).vm.$emit('remove');
+
+    expect(wrapper).toEmit('input', [
+      comments[0],
+    ]);
+  });
+
+  test('Renders `pbehavior-comments-field` with default props', () => {
+    const wrapper = snapshotFactory({
+      propsData: { comments: [] },
+    });
+
+    expect(wrapper.element).toMatchSnapshot();
+  });
+
+  test('Renders `pbehavior-comments-field` with custom props', () => {
+    const wrapper = snapshotFactory({
+      propsData: {
+        comments: [
+          { key: 'comment-1', message: 'message-1' },
+          { key: 'comment-2', message: 'message-2' },
+        ],
+      },
+    });
+
+    expect(wrapper.element).toMatchSnapshot();
+  });
+});
