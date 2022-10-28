c25153afe33ff9a02fe75a5a7af18e4784758a97
Vladimir Erokhov <vladimir.erokhov@noveogroup.com>
2022-10-28 16:24:12 +0600

    feature(#4572): Finished issue

--- community/sources/webcore/src/canopsis-next/src/mixins/entities/view/widget/index.js
+++ community/sources/webcore/src/canopsis-next/src/mixins/entities/view/widget/index.js
@@ -22,6 +22,7 @@ export const entitiesWidgetMixin = {
       createWidgetFilter: 'createWidgetFilter',
       updateWidgetFilter: 'updateWidgetFilter',
       removeWidgetFilter: 'removeWidgetFilter',
+      updateWidgetFiltersPositions: 'updateWidgetFiltersPositions',
     }),
   },
 };
