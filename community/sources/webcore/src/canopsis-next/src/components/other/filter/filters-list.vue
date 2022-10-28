92c1d2a0ac19cac1660618d3ec3f210d385d1a04
Vladimir Erokhov <vladimir.erokhov@noveogroup.com>
2022-10-28 16:24:12 +0600

    feature(#4572): Finished issue

--- community/sources/webcore/src/canopsis-next/src/components/other/filter/filters-list.vue
+++ community/sources/webcore/src/canopsis-next/src/components/other/filter/filters-list.vue
@@ -4,14 +4,15 @@
       :value="!pending && !filters.length",
       type="info"
     ) {{ $t('modals.createFilter.emptyFilters') }}
-    filter-tile(
-      v-for="filter in filters",
-      :filter="filter",
-      :key="filter._id",
-      :editable="editable",
-      @edit="$emit('edit', filter)",
-      @delete="$emit('delete', filter)"
-    )
+    c-draggable-list-field(v-field="filters", handle=".action-drag-handler")
+      filter-tile(
+        v-for="filter in filters",
+        :filter="filter",
+        :key="filter._id",
+        :editable="editable",
+        @edit="$emit('edit', filter)",
+        @delete="$emit('delete', filter)"
+      )
     v-btn.ml-0(
       v-if="addable",
       color="primary",
@@ -28,6 +29,10 @@ import FilterTile from './partials/filter-tile.vue';
 export default {
   components: { FilterTile },
   mixins: [entitiesWidgetMixin],
+  model: {
+    prop: 'filters',
+    event: 'input',
+  },
   props: {
     filters: {
       type: Array,
