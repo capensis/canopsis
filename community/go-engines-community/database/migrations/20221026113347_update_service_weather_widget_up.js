a00fe3c757e5f119ba2b539756a932bb0c54811b
Zarina Yakubova <Zarina.Yakubova@noveogroup.com>
2022-11-01 12:35:13 +0600

    feature(#4557): Add migration scripts

--- community/go-engines-community/database/migrations/20221026113347_update_service_weather_widget_up.js
+++ community/go-engines-community/database/migrations/20221026113347_update_service_weather_widget_up.js
@@ -20,3 +20,45 @@ db.widgets.find({type: "ServiceWeather"}).forEach(function (doc) {
 
     db.widgets.updateOne({_id: doc._id}, {$set: set, $unset: unset});
 });
+
+db.widgets.find({type: {$in: ["ServiceWeather", "Counter"]}}).forEach(function (doc) {
+    var set = {};
+    var unset = {};
+
+    set["parameters.columnMobile"] = 2;
+    set["parameters.columnTablet"] = 3;
+    set["parameters.columnDesktop"] = 4;
+    unset["parameters.columnSM"] = "";
+    unset["parameters.columnMD"] = "";
+    unset["parameters.columnLG"] = "";
+
+    if (doc.parameters) {
+        if (doc.parameters.columnSM <= 6) {
+            set["parameters.columnMobile"] = 2;
+        } else {
+            set["parameters.columnMobile"] = 1;
+        }
+        if (doc.parameters.columnMD <= 3) {
+            set["parameters.columnTablet"] = 4;
+        } else if (doc.parameters.columnMD === 4) {
+            set["parameters.columnTablet"] = 3;
+        } else if (doc.parameters.columnMD <= 6) {
+            set["parameters.columnTablet"] = 2;
+        } else {
+            set["parameters.columnTablet"] = 1;
+        }
+        if (doc.parameters.columnLG <= 2) {
+            set["parameters.columnDesktop"] = 6;
+        } else if (doc.parameters.columnLG === 3) {
+            set["parameters.columnDesktop"] = 4;
+        } else if (doc.parameters.columnLG <= 4) {
+            set["parameters.columnDesktop"] = 3;
+        } else if (doc.parameters.columnLG <= 6) {
+            set["parameters.columnDesktop"] = 2;
+        } else {
+            set["parameters.columnDesktop"] = 1;
+        }
+    }
+
+    db.widgets.updateOne({_id: doc._id}, {$set: set, $unset: unset});
+});
