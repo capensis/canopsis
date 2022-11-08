bdcc130195914662676e8732352d9cbc4f0c44c7
Zarina Yakubova <Zarina.Yakubova@noveogroup.com>
2022-11-08 18:33:24 +0600

    feature(#4483): Add order to simplified instruction jobs

--- community/go-engines-community/database/migrations/20221103133437_add_fields_to_job_history_collection_up.js
+++ community/go-engines-community/database/migrations/20221103133437_add_fields_to_job_history_collection_up.js
@@ -19,6 +19,8 @@ db.job_history.aggregate([
     {$unwind: "$config"},
     {
         $project: {
+            execution: 1,
+            next_exec: 1,
             name: "$job.name",
             retry_amount: "$job.retry_amount",
             retry_interval: "$job.retry_interval",
@@ -28,18 +30,92 @@ db.job_history.aggregate([
             auth_username: "$config.auth_username",
             auth_token: "$config.auth_token",
         }
-    }
+    },
+    {
+        $group: {
+            _id: "$execution",
+            jobs: {$push: "$$ROOT"}
+        }
+    },
+    {
+        $lookup: {
+            from: "execution",
+            localField: "execution",
+            foreignField: "_id",
+            as: "execution"
+        }
+    },
+    {$unwind: "$execution"},
+    {
+        $lookup: {
+            from: "instruction",
+            localField: "execution.instruction",
+            foreignField: "_id",
+            as: "instruction"
+        }
+    },
+    {$unwind: "$instruction"},
+    {
+        $project: {
+            execution: "$execution._id",
+            instruction_type: "$instruction.type",
+            jobs: 1
+        }
+    },
 ]).forEach(function (doc) {
-    db.job_history.updateOne({_id: doc._id}, {
-        $set: {
-            name: doc.name,
-            retry_amount: doc.retry_amount,
-            retry_interval: doc.retry_interval,
-            multiple_executions: doc.multiple_executions,
-            type: doc.type,
-            host: doc.host,
-            auth_username: doc.auth_username,
-            auth_token: doc.auth_token,
+    if (doc.instruction_type === 0) {
+        doc.jobs.forEach(function (job) {
+            var set = {
+                name: job.name,
+                retry_amount: job.retry_amount,
+                retry_interval: job.retry_interval,
+                multiple_executions: job.multiple_executions,
+                type: job.type,
+                host: job.host,
+                auth_username: job.auth_username,
+                auth_token: job.auth_token,
+            };
+
+            db.job_history.updateOne({_id: job._id}, {$set: set});
+        });
+        return;
+    }
+
+    var jobs = {};
+    var hasRef = {};
+    doc.jobs.forEach(function (job) {
+        jobs[job._id] = job;
+
+        if (job.next_exec) {
+            hasRef[job.next_exec] = true;
         }
     });
+    var jobId = null;
+    doc.jobs.forEach(function (job) {
+        if (!hasRef[job._id]) {
+            jobId = job._id;
+        }
+    });
+    var index = 0;
+    while (true) {
+        if (!jobId) {
+            break;
+        }
+        var job = jobs[jobId];
+        var set = {
+            name: job.name,
+            retry_amount: job.retry_amount,
+            retry_interval: job.retry_interval,
+            multiple_executions: job.multiple_executions,
+            type: job.type,
+            host: job.host,
+            auth_username: job.auth_username,
+            auth_token: job.auth_token,
+            index: index,
+        };
+
+        db.job_history.updateOne({_id: job._id}, {$set: set});
+        index++;
+        jobId = job.next_exec;
+    }
 });
