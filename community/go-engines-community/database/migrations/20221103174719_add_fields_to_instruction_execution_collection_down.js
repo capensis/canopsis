c2727d1cf8ba9dff1a66835f229638448ca8e705
Zarina Yakubova <Zarina.Yakubova@noveogroup.com>
2022-11-10 12:45:02 +0600

    feature(#4483): Copy instruction to instruction execution

--- community/go-engines-community/database/migrations/20221103174719_add_fields_to_instruction_execution_collection_down.js
+++ community/go-engines-community/database/migrations/20221103174719_add_fields_to_instruction_execution_collection_down.js
@@ -1,6 +1,7 @@
 db.instruction_execution.updateMany({}, {
     $unset: {
         name: "",
+        description: "",
         type: "",
         timeout_after_execution: "",
         created_at: "",
