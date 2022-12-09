5f49bfc500e1c96321ff01f98836c342d08531e2
Dmitry <Dmitriy.Mikhaylishin@noveogroup.com>
2022-12-09 18:06:47 +0600

    feature(#4622): fixes after review

--- community/sources/webcore/src/canopsis-next/tests/unit/i18n/messages.spec.js
+++ community/sources/webcore/src/canopsis-next/tests/unit/i18n/messages.spec.js
@@ -1,20 +1,5 @@
-import { isString, get } from 'lodash';
-
 import fr from '@/i18n/messages/fr';
 import en from '@/i18n/messages/en';
-
-const checkMessageKeysIsEqual = (firstMessages, secondMessages, path) => {
-  const messages = path.length ? get(firstMessages, path) : firstMessages;
-
-  Object.entries(messages)
-    .forEach(([key, message]) => {
-      if (isString(message)) {
-        expect(secondMessages).toHaveProperty([...path, key]);
-      } else {
-        checkMessageKeysIsEqual(firstMessages, secondMessages, [...path, key]);
-      }
-    });
-};
 
 describe('I18n messages', () => {
   const enKeys = Object.keys(en);
@@ -25,6 +10,6 @@ describe('I18n messages', () => {
   });
 
   test.each(enKeys)('Translation module: "%s" count is equal', (module) => {
-    checkMessageKeysIsEqual(en[module], fr[module], []);
+    expect(en[module]).toStructureEqual(fr[module]);
   });
 });
