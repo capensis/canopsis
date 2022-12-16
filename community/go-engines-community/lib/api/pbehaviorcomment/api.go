e40116e9dec527839a546a519b81d3b20456f3d6
Zarina Yakubova <Zarina.Yakubova@noveogroup.com>
2022-12-16 17:34:44 +0600

    feature(#4686): Add name of author to all API endpoints

--- community/go-engines-community/lib/api/pbehaviorcomment/api.go
+++ community/go-engines-community/lib/api/pbehaviorcomment/api.go
@@ -14,23 +14,20 @@ type API interface {
 }
 
 type api struct {
-	transformer ModelTransformer
-	store       Store
+	store Store
 }
 
 func NewApi(
-	transformer ModelTransformer,
 	store Store,
 ) API {
 	return &api{
-		transformer: transformer,
-		store:       store,
+		store: store,
 	}
 }
 
 // Create
 // @Param body body Request true "body"
-// @Success 201 {object} pbehavior.Comment
+// @Success 201 {object} Response
 func (a *api) Create(c *gin.Context) {
 	var request Request
 
@@ -40,8 +37,7 @@ func (a *api) Create(c *gin.Context) {
 		return
 	}
 
-	model := a.transformer.TransformRequestToModel(&request)
-	ok, err := a.store.Insert(c.Request.Context(), request.Pbehavior, model)
+	response, err := a.store.Insert(c.Request.Context(), request)
 	if err != nil {
 		valErr := common.ValidationError{}
 		if errors.As(err, &valErr) {
@@ -51,12 +47,12 @@ func (a *api) Create(c *gin.Context) {
 		panic(err)
 	}
 
-	if !ok {
+	if response == nil {
 		c.AbortWithStatusJSON(http.StatusNotFound, common.NotFoundResponse)
 		return
 	}
 
-	c.JSON(http.StatusCreated, model)
+	c.JSON(http.StatusCreated, response)
 }
 
 func (a *api) Delete(c *gin.Context) {
