352fe29340a45436ff5ffbd7b2cc09922c740fcc
Zarina Yakubova <Zarina.Yakubova@noveogroup.com>
2022-12-30 18:30:05 +0600

    feature(#4642): Add functional tests

--- community/go-engines-community/lib/canopsis/types/event_easyjson.go
+++ community/go-engines-community/lib/canopsis/types/event_easyjson.go
@@ -147,6 +147,8 @@ func easyjsonF642ad3eDecodeGitCanopsisNetCanopsisCanopsisCommunityCommunityGoEng
 			out.RK = string(in.String())
 		case "ack_resources":
 			out.AckResources = bool(in.Bool())
+		case "ticket_resources":
+			out.TicketResources = bool(in.Bool())
 		case "duration":
 			if data := in.Raw(); in.Ok() {
 				in.AddError((out.Duration).UnmarshalJSON(data))
@@ -512,12 +514,17 @@ func easyjsonF642ad3eEncodeGitCanopsisNetCanopsisCanopsisCommunityCommunityGoEng
 		out.RawString(prefix)
 		out.String(string(in.RK))
 	}
-	{
+	if in.AckResources {
 		const prefix string = ",\"ack_resources\":"
 		out.RawString(prefix)
 		out.Bool(bool(in.AckResources))
 	}
-	{
+	if in.TicketResources {
+		const prefix string = ",\"ticket_resources\":"
+		out.RawString(prefix)
+		out.Bool(bool(in.TicketResources))
+	}
+	if in.Duration != 0 {
 		const prefix string = ",\"duration\":"
 		out.RawString(prefix)
 		out.Raw((in.Duration).MarshalJSON())
