81971bd56cd87ce4e337c65822224ca0901482f9
Zarina Yakubova <Zarina.Yakubova@noveogroup.com>
2022-12-30 11:43:35 +0600

    feature(#4647): Fix log messages

--- community/go-engines-community/lib/canopsis/webhook/models.go
+++ community/go-engines-community/lib/canopsis/webhook/models.go
@@ -15,24 +15,24 @@ const (
 type History struct {
 	ID            string                        `bson:"_id" json:"_id"`
 	Alarms        []string                      `bson:"alarms" json:"alarms"`
-	Scenario      string                        `bson:"scenario,omitempty" json:"scenario"`
-	Action        int64                         `bson:"action,omitempty" json:"action"`
-	Execution     string                        `bson:"execution,omitempty" json:"execution"`
-	Name          string                        `bson:"name,omitempty" json:"name"`
-	SystemName    string                        `bson:"system_name,omitempty" json:"system_name"`
+	Scenario      string                        `bson:"scenario,omitempty" json:"scenario,omitempty"`
+	Action        int64                         `bson:"action,omitempty" json:"action,omitempty"`
+	Execution     string                        `bson:"execution" json:"execution"`
+	Name          string                        `bson:"name" json:"name"`
+	SystemName    string                        `bson:"system_name" json:"system_name"`
 	Status        int64                         `bson:"status" json:"status"`
-	Comment       string                        `bson:"comment,omitempty" json:"comment"`
+	Comment       string                        `bson:"comment,omitempty" json:"comment,omitempty"`
 	Request       request.Parameters            `bson:"request" json:"request"`
-	DeclareTicket *request.WebhookDeclareTicket `bson:"declare_ticket,omitempty" json:"declare_ticket"`
+	DeclareTicket *request.WebhookDeclareTicket `bson:"declare_ticket,omitempty" json:"declare_ticket,omitempty"`
 	NextExec      string                        `bson:"next_exec,omitempty" json:"next_exec,omitempty"`
 	Index         *int64                        `bson:"index,omitempty" json:"index,omitempty"`
-	StopOnFail    bool                          `bson:"stop_on_fail,omitempty" json:"stop_on_fail"`
+	StopOnFail    bool                          `bson:"stop_on_fail,omitempty" json:"stop_on_fail,omitempty"`
 	FailReason    string                        `bson:"fail_reason,omitempty" json:"fail_reason,omitempty"`
 	RawRequest    string                        `bson:"raw_request,omitempty" json:"raw_request,omitempty"`
 	RawResponse   string                        `bson:"raw_response,omitempty" json:"raw_response,omitempty"`
-	TicketID      string                        `bson:"ticket_id,omitempty" json:"ticket_id"`
-	TicketURL     string                        `bson:"ticket_url,omitempty" json:"ticket_url"`
-	TicketData    map[string]string             `bson:"ticket_data,omitempty" json:"ticket_data"`
+	TicketID      string                        `bson:"ticket_id,omitempty" json:"ticket_id,omitempty"`
+	TicketURL     string                        `bson:"ticket_url,omitempty" json:"ticket_url,omitempty"`
+	TicketData    map[string]string             `bson:"ticket_data,omitempty" json:"ticket_data,omitempty"`
 	UserID        string                        `bson:"user,omitempty" json:"user,omitempty"`
 	Username      string                        `bson:"username,omitempty" json:"username,omitempty"`
 	CreatedAt     types.CpsTime                 `bson:"created_at,omitempty" json:"created_at,omitempty"`
