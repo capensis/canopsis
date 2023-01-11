c3c11e18f182c967afabc0c9a9ff537ef1f4728c
ikalugin <ivan.kalugin@noveogroup.com>
2023-01-11 19:02:38 +0100

    feature(#4706): clean context graph on archive entities + do not cascade-archive for connectors

--- community/go-engines-community/lib/api/entity/store.go
+++ community/go-engines-community/lib/api/entity/store.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"time"
 
+	"git.canopsis.net/canopsis/canopsis-community/community/go-engines-community/lib/canopsis"
 	"git.canopsis.net/canopsis/canopsis-community/community/go-engines-community/lib/canopsis/config"
 	"git.canopsis.net/canopsis/canopsis-community/community/go-engines-community/lib/canopsis/types"
 	"git.canopsis.net/canopsis/canopsis-community/community/go-engines-community/lib/mongo"
@@ -12,8 +13,6 @@ import (
 	mongodriver "go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
-
-const bulkMaxSize = 10000
 
 type Store interface {
 	Find(ctx context.Context, r ListRequestWithPagination) (*AggregationResult, error)
@@ -73,7 +72,8 @@ func (s *store) Find(ctx context.Context, r ListRequestWithPagination) (*Aggrega
 func (s *store) ArchiveDisabledEntities(ctx context.Context, archiveDeps bool) (int64, error) {
 	var totalArchived int64
 
-	archived, err := s.archiveEntitiesByType(ctx, types.EntityTypeConnector, archiveDeps)
+	// do not cascade-archive connector dependencies
+	archived, err := s.archiveEntitiesByType(ctx, types.EntityTypeConnector, false)
 	if err != nil {
 		return 0, err
 	}
@@ -87,7 +87,7 @@ func (s *store) ArchiveDisabledEntities(ctx context.Context, archiveDeps bool) (
 
 	totalArchived += archived
 
-	archived, err = s.archiveEntitiesByType(ctx, types.EntityTypeResource, archiveDeps)
+	archived, err = s.archiveEntitiesByType(ctx, types.EntityTypeResource, false)
 	if err != nil {
 		return 0, err
 	}
@@ -98,9 +98,6 @@ func (s *store) ArchiveDisabledEntities(ctx context.Context, archiveDeps bool) (
 }
 
 func (s *store) archiveEntitiesByType(ctx context.Context, eType string, archiveDeps bool) (int64, error) {
-	models := make([]mongodriver.WriteModel, 0, bulkMaxSize)
-	ids := make([]string, 0, bulkMaxSize)
-
 	cursor, err := s.mainCollection.Find(
 		ctx,
 		bson.M{
@@ -112,24 +109,22 @@ func (s *store) archiveEntitiesByType(ctx context.Context, eType string, archive
 		return 0, err
 	}
 
-	totalArchived, err := s.processCursor(ctx, cursor, &models, &ids, archiveDeps)
+	totalArchived, err := s.processCursor(ctx, cursor, archiveDeps)
 	if err != nil {
 		return 0, err
-	}
-
-	if len(models) != 0 {
-		archived, err := s.bulkArchive(ctx, &models, &ids)
-		if err != nil {
-			return 0, err
-		}
-
-		totalArchived += archived
 	}
 
 	return totalArchived, cursor.Close(ctx)
 }
 
-func (s *store) processCursor(ctx context.Context, cursor mongo.Cursor, models *[]mongodriver.WriteModel, ids *[]string, archiveDeps bool) (int64, error) {
+func (s *store) processCursor(ctx context.Context, cursor mongo.Cursor, archiveDeps bool) (int64, error) {
+	archiveModels := make([]mongodriver.WriteModel, 0, canopsis.DefaultBulkSize/2) // per 1 archive model there are 2 context graph update models
+	contextGraphModels := make([]mongodriver.WriteModel, 0, canopsis.DefaultBulkSize)
+	ids := make([]string, 0, canopsis.DefaultBulkSize)
+
+	archiveBulkBytesSize := 0
+	contextGraphBulkBytesSize := 0
+
 	var totalArchived int64
 
 	for cursor.Next(ctx) {
@@ -140,51 +135,99 @@ func (s *store) processCursor(ctx context.Context, cursor mongo.Cursor, models *
 			return 0, err
 		}
 
-		if archiveDeps {
-			switch entity.Type {
-			case types.EntityTypeConnector:
-				archived, err := s.archiveDependencies(ctx, append(entity.Impacts, entity.Depends...), models, ids)
-				if err != nil {
-					return 0, err
-				}
-
-				totalArchived += archived
-			case types.EntityTypeComponent:
-				archived, err := s.archiveDependencies(ctx, entity.Depends, models, ids)
-				if err != nil {
-					return 0, err
-				}
-
-				totalArchived += archived
-			case types.EntityTypeResource:
-			default:
-				continue
-			}
-		}
-
-		*ids = append(*ids, entity.ID)
-		*models = append(
-			*models,
-			mongodriver.NewUpdateOneModel().
-				SetFilter(bson.M{"_id": entity.ID}).
-				SetUpdate(bson.M{"$set": entity}).
-				SetUpsert(true),
-		)
-
-		if len(*models) == bulkMaxSize {
-			archived, err := s.bulkArchive(ctx, models, ids)
+		if entity.Type == types.EntityTypeComponent && archiveDeps {
+			archived, err := s.archiveComponentDependencies(ctx, entity.Depends)
 			if err != nil {
 				return 0, err
 			}
 
 			totalArchived += archived
 		}
+
+		newContextGraphModels := []mongodriver.WriteModel{
+			mongodriver.NewUpdateManyModel().
+				SetFilter(bson.M{"_id": bson.M{"$in": entity.Depends}}).
+				SetUpdate(bson.M{"$pull": bson.M{"impact": entity.ID}}),
+			mongodriver.NewUpdateManyModel().
+				SetFilter(bson.M{"_id": bson.M{"$in": entity.Impacts}}).
+				SetUpdate(bson.M{"$pull": bson.M{"depends": entity.ID}}),
+		}
+
+		b, err := bson.Marshal(struct {
+			Arr []mongodriver.WriteModel
+		}{Arr: newContextGraphModels})
+		if err != nil {
+			return 0, err
+		}
+
+		newContextGraphModelsLen := len(b)
+
+		newArchiveModel := mongodriver.NewUpdateOneModel().
+			SetFilter(bson.M{"_id": entity.ID}).
+			SetUpdate(bson.M{"$set": entity}).
+			SetUpsert(true)
+
+		b, err = bson.Marshal(newArchiveModel)
+		if err != nil {
+			return 0, err
+		}
+
+		newArchiveModelLen := len(b)
+
+		if contextGraphBulkBytesSize+newContextGraphModelsLen > canopsis.DefaultBulkBytesSize ||
+			archiveBulkBytesSize+newArchiveModelLen > canopsis.DefaultBulkBytesSize {
+			archived, err := s.bulkArchive(ctx, archiveModels, contextGraphModels, ids)
+			if err != nil {
+				return 0, err
+			}
+
+			totalArchived += archived
+
+			contextGraphModels = contextGraphModels[:0]
+			archiveModels = archiveModels[:0]
+			ids = ids[:0]
+
+			contextGraphBulkBytesSize = 0
+			archiveBulkBytesSize = 0
+		}
+
+		archiveModels = append(archiveModels, newArchiveModel)
+		contextGraphModels = append(contextGraphModels, newContextGraphModels...)
+		ids = append(ids, entity.ID)
+
+		contextGraphBulkBytesSize += newContextGraphModelsLen
+		archiveBulkBytesSize += newArchiveModelLen
+
+		if len(contextGraphModels) == canopsis.DefaultBulkSize {
+			archived, err := s.bulkArchive(ctx, archiveModels, contextGraphModels, ids)
+			if err != nil {
+				return 0, err
+			}
+
+			totalArchived += archived
+
+			contextGraphModels = contextGraphModels[:0]
+			archiveModels = archiveModels[:0]
+			ids = ids[:0]
+
+			contextGraphBulkBytesSize = 0
+			archiveBulkBytesSize = 0
+		}
+	}
+
+	if len(archiveModels) != 0 {
+		archived, err := s.bulkArchive(ctx, archiveModels, contextGraphModels, ids)
+		if err != nil {
+			return 0, err
+		}
+
+		totalArchived += archived
 	}
 
 	return totalArchived, nil
 }
 
-func (s *store) archiveDependencies(ctx context.Context, depIds []string, models *[]mongodriver.WriteModel, ids *[]string) (int64, error) {
+func (s *store) archiveComponentDependencies(ctx context.Context, depIds []string) (int64, error) {
 	cursor, err := s.mainCollection.Find(
 		ctx,
 		bson.M{"_id": bson.M{"$in": depIds}},
@@ -193,8 +236,7 @@ func (s *store) archiveDependencies(ctx context.Context, depIds []string, models
 		return 0, err
 	}
 
-	// do not archive dependencies' dependencies
-	archived, err := s.processCursor(ctx, cursor, models, ids, false)
+	archived, err := s.processCursor(ctx, cursor, false)
 	if err != nil {
 		return 0, err
 	}
@@ -202,8 +244,13 @@ func (s *store) archiveDependencies(ctx context.Context, depIds []string, models
 	return archived, cursor.Close(ctx)
 }
 
-func (s *store) bulkArchive(ctx context.Context, models *[]mongodriver.WriteModel, ids *[]string) (int64, error) {
-	res, err := s.archivedCollection.BulkWrite(ctx, *models)
+func (s *store) bulkArchive(ctx context.Context, models, contextGraphModels []mongodriver.WriteModel, ids []string) (int64, error) {
+	res, err := s.archivedCollection.BulkWrite(ctx, models)
+	if err != nil {
+		return 0, err
+	}
+
+	_, err = s.mainCollection.BulkWrite(ctx, contextGraphModels)
 	if err != nil {
 		return 0, err
 	}
@@ -216,10 +263,7 @@ func (s *store) bulkArchive(ctx context.Context, models *[]mongodriver.WriteMode
 		return 0, err
 	}
 
-	*models = (*models)[:0]
-	*ids = (*ids)[:0]
-
-	return res.UpsertedCount, err
+	return res.UpsertedCount + res.ModifiedCount, err
 }
 
 func (s *store) DeleteArchivedEntities(ctx context.Context) (int64, error) {
