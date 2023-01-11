bde44ee6c3da4b48ee21bc3f6f8cf29b17e128a5
Dmitry <Dmitriy.Mikhaylishin@noveogroup.com>
2023-01-11 11:24:27 +0600

    feature(#4653): fixes after self review

--- community/sources/webcore/src/canopsis-next/src/components/common/card-iterator/c-card-iterator-item.vue
+++ community/sources/webcore/src/canopsis-next/src/components/common/card-iterator/c-card-iterator-item.vue
@@ -1,22 +1,21 @@
 <template lang="pug">
-  v-card.c-card-iterator-item
-    v-card-text
-      v-layout(row, align-center, justify-space-between)
-        v-layout.c-card-iterator-item__actions
-          c-draggable-step-number(
-            :color="!expanded && hasChildrenError ? 'error' : 'primary'",
-            drag-class="item-drag-handler"
-          ) {{ itemNumber }}
-          c-expand-btn(v-model="expanded")
-        slot(name="header")
-        c-action-btn(type="delete", @click="$emit('remove')")
-      v-expand-transition(mode="out-in")
-        v-layout.c-card-iterator-item__content(
-          v-show="expanded",
-          :class="{ 'c-card-iterator-item__content--offset': offsetLeft }",
-          column
-        )
-          slot
+  div.c-card-iterator-item
+    v-layout(row, align-center, justify-space-between)
+      v-layout.c-card-iterator-item__actions
+        c-draggable-step-number(
+          :color="!expanded && hasChildrenError ? 'error' : 'primary'",
+          drag-class="item-drag-handler"
+        ) {{ itemNumber }}
+        c-expand-btn(v-model="expanded")
+      slot(name="header")
+      c-action-btn(type="delete", @click="$emit('remove')")
+    v-expand-transition(mode="out-in")
+      v-layout.c-card-iterator-item__content(
+        v-show="expanded",
+        :class="{ 'c-card-iterator-item__content--offset': offsetLeft }",
+        column
+      )
+        slot
 </template>
 
 <script>
