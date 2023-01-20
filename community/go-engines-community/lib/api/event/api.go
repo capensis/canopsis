789c2b4dccec727f3f9afe876529a20c238fb640
Zarina Yakubova <Zarina.Yakubova@noveogroup.com>
2023-01-20 11:09:31 +0600

    feature(#4637): Remove declareticket trigger

--- community/go-engines-community/lib/api/event/api.go
+++ community/go-engines-community/lib/api/event/api.go
@@ -167,7 +167,6 @@ func (api *api) processValue(c *gin.Context, value *fastjson.Value) bool {
 		eventType == types.EventTypeCancel ||
 		eventType == types.EventTypeComment ||
 		eventType == types.EventTypeUncancel ||
-		eventType == types.EventTypeDeclareTicket ||
 		eventType == types.EventTypeDone ||
 		eventType == types.EventTypeAssocTicket ||
 		eventType == types.EventTypeChangestate ||
