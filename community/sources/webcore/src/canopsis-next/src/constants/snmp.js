7c2c963a97a6d91fdfebc7247b270c9445eb250d
Vladimir Erokhov <vladimir.erokhov@noveogroup.com>
2023-01-26 00:07:36 +0600

    feature(#4083): Finished issue

--- community/sources/webcore/src/canopsis-next/src/constants/snmp.js
+++ community/sources/webcore/src/canopsis-next/src/constants/snmp.js
@@ -1,4 +1,20 @@
+import { COLORS } from '@/config';
+
 export const SNMP_STATE_TYPES = {
   simple: 'simple',
   template: 'template',
+};
+
+export const SNMP_TEMPLATE_STATE_STATES = {
+  info: 'info',
+  minor: 'minor',
+  major: 'major',
+  critical: 'critical',
+};
+
+export const SNMP_TEMPLATE_STATE_STATES_COLORS = {
+  [SNMP_TEMPLATE_STATE_STATES.info]: COLORS.state.ok,
+  [SNMP_TEMPLATE_STATE_STATES.minor]: COLORS.state.minor,
+  [SNMP_TEMPLATE_STATE_STATES.major]: COLORS.state.major,
+  [SNMP_TEMPLATE_STATE_STATES.critical]: COLORS.state.critical,
 };
