1f099fde4b8199c2faa6be7a5d64241bb61e31e4
Dmitry <Dmitriy.Mikhaylishin@noveogroup.com>
2023-01-31 19:40:11 +0600

    feature(#4657): fix executions

--- community/sources/webcore/src/canopsis-next/src/components/modals/declare-ticket/execute-declare-tickets-rule.vue
+++ community/sources/webcore/src/canopsis-next/src/components/modals/declare-ticket/execute-declare-tickets-rule.vue
@@ -11,19 +11,24 @@
           color="white"
         )
     template(#text="")
-      v-layout.mb-4(v-if="isOneExecution", row, align-center)
-        span.subheading.mr-5 {{ $t('declareTicket.webhookStatus') }}:
-        declare-ticket-rule-execution-status(
-          :running="isExecutionsRunning",
-          :success="isExecutionsSucceeded",
-          :fail-reason="failReason"
-        )
-      declare-ticket-rule-execution-alarms(:alarm-executions="alarmExecutions", :is-one-execution="isOneExecution")
+      v-layout(v-if="pending", justify-center)
+        v-progress-circular(color="primary", indeterminate)
+      template(v-else)
+        v-layout.mb-4(v-if="isOneTicket", row, align-center)
+          span.subheading.mr-5 {{ $t('declareTicket.webhookStatus') }}:
+          declare-ticket-rule-execution-status(
+            :running="isExecutionsRunning",
+            :success="isExecutionsSucceeded",
+            :fail-reason="failReason"
+          )
+        declare-ticket-rule-execution-alarms(:alarm-executions="alarmExecutions", :is-one-execution="isOneTicket")
     template(#actions="")
       v-btn(depressed, flat, @click="$modals.hide") {{ $t('common.close') }}
 </template>
 
 <script>
+import { keyBy } from 'lodash';
+
 import { SOCKET_ROOMS } from '@/config';
 
 import { DECLARE_TICKET_EXECUTION_STATUSES, MODALS } from '@/constants';
@@ -36,6 +41,7 @@ import {
 } from '@/helpers/forms/declare-ticket-rule';
 
 import { modalInnerMixin } from '@/mixins/modal/inner';
+import { entitiesDeclareTicketRuleMixin } from '@/mixins/entities/declare-ticket-rule';
 
 import DeclareTicketRuleExecutionStatus from '@/components/other/declare-ticket/partials/declare-ticket-rule-execution-status.vue';
 import DeclareTicketRuleExecutionAlarms from '@/components/other/declare-ticket/partials/declare-ticket-rule-execution-alarms.vue';
@@ -46,26 +52,29 @@ import ModalWrapper from '../modal-wrapper.vue';
  * Modal to declare a ticket
  */
 export default {
-  name: MODALS.executeDeclareTicket,
+  name: MODALS.executeDeclareTickets,
   $_veeValidate: {
     validator: 'new',
   },
   components: { DeclareTicketRuleExecutionAlarms, DeclareTicketRuleExecutionStatus, ModalWrapper },
   mixins: [
     modalInnerMixin,
+    entitiesDeclareTicketRuleMixin,
   ],
   data() {
     return {
+      pending: false,
+      successExecutions: [],
       executionsStatusesById: {},
     };
   },
   computed: {
-    isOneExecution() {
-      return this.config.executions.length === 1;
+    isOneTicket() {
+      return this.config.tickets.length === 1;
     },
 
     alarmExecutions() {
-      return this.config.executions.reduce((acc, { executionId, ruleName, alarms }) => {
+      return this.successExecutions.reduce((acc, { executionId, ruleName, alarms }) => {
         alarms.forEach((alarm) => {
           const status = this.executionsStatusesById[executionId] ?? {
             status: DECLARE_TICKET_EXECUTION_STATUSES.running,
@@ -100,12 +109,14 @@ export default {
         return this.config.title;
       }
 
-      return this.isOneExecution
-        ? this.config.executions[0].ruleName
-        : this.$t('modals.executeDeclareTicket.title');
+      return this.isOneTicket
+        ? this.config.tickets[0].name
+        : this.$t('modals.executeDeclareTickets.title');
     },
   },
-  mounted() {
+  async mounted() {
+    await this.createExecutions();
+    await this.fetchExecutionsStatuses();
     this.joinToSocketRooms();
   },
   beforeDestroy() {
@@ -144,7 +155,7 @@ export default {
      * Join from execution room
      */
     joinToSocketRooms() {
-      this.config.executions.forEach(({ executionId }) => {
+      this.successExecutions.forEach(({ executionId }) => {
         this.$socket
           .on(Socket.EVENTS_TYPES.customClose, this.socketCloseHandler)
           .on(Socket.EVENTS_TYPES.closeRoom, this.socketCloseRoomHandler)
@@ -157,12 +168,40 @@ export default {
      * Leave from execution room
      */
     leaveFromSocketRooms() {
-      this.config.executions.forEach(({ executionId }) => {
+      this.successExecutions.forEach(({ executionId }) => {
         this.$socket
           .off(Socket.EVENTS_TYPES.customClose, this.socketCloseHandler)
           .off(Socket.EVENTS_TYPES.closeRoom, this.socketCloseRoomHandler)
           .leave(this.getSocketRoomName(executionId))
           .removeListener(this.setExecutionStatus);
+      });
+    },
+
+    async createExecutions() {
+      this.pending = true;
+
+      try {
+        const items = await this.bulkCreateDeclareTicketExecution({ data: this.config.executions });
+        const successExecutions = items.filter(({ status }) => status >= 200 && status < 300);
+        const alarmsById = keyBy(this.config.alarms, '_id');
+        const ticketsById = keyBy(this.config.tickets, '_id');
+
+        this.successExecutions = successExecutions.map(({ id, item }) => ({
+          executionId: id,
+          ruleName: ticketsById[item._id].name,
+          alarms: item.alarms.map(alarmId => alarmsById[alarmId]),
+        }));
+      } catch (err) {
+        console.error(err);
+      } finally {
+        this.pending = false;
+      }
+    },
+
+    fetchExecutionsStatuses() {
+      this.successExecutions.forEach(({ executionId }) => {
+        this.fetchDeclareTicketExecutionWithoutStore({ id: executionId })
+          .then(this.setExecutionStatus);
       });
     },
   },
