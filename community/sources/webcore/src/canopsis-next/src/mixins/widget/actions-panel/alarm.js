3c1a6c342c72dae4219e7497b87d616fc7c4a9d6
Dmitry <Dmitriy.Mikhaylishin@noveogroup.com>
2023-02-01 17:18:12 +0600

    feature(#4666): change alarms timeline

--- community/sources/webcore/src/canopsis-next/src/mixins/widget/actions-panel/alarm.js
+++ community/sources/webcore/src/canopsis-next/src/mixins/widget/actions-panel/alarm.js
@@ -99,6 +99,7 @@ export const widgetActionsPanelAlarmMixin = {
                     name: alarmsByTickets[id].name,
                   })),
                   alarms,
+                  onExecute: this.afterSubmit,
                 },
               });
             },
