0e1e840522c8fe23b4e4c6c346c0955edcc6e0a1
Zarina Yakubova <Zarina.Yakubova@noveogroup.com>
2023-02-06 17:38:26 +0600

    feature(#4733): Add manual meta alarm API endpoints

--- community/go-engines-community/lib/api/alarm/models.go
+++ community/go-engines-community/lib/api/alarm/models.go
@@ -117,15 +117,6 @@ type SortRequest struct {
 	SortBy    string   `form:"sort_by" json:"sort_by"`
 }
 
-type ManualRequest struct {
-	Search string `form:"search" json:"search"`
-}
-
-type ManualResponse struct {
-	ID   string `bson:"_id" json:"_id"`
-	Name string `bson:"name" json:"name"`
-}
-
 type DetailsRequest struct {
 	ID               string               `json:"_id" binding:"required"`
 	Opened           *bool                `json:"opened"`
@@ -235,6 +226,7 @@ type Alarm struct {
 type MetaAlarmRule struct {
 	ID   string `bson:"_id" json:"_id"`
 	Name string `bson:"name" json:"name"`
+	Type string `bson:"type" json:"type"`
 }
 
 type AlarmValue struct {
--- community/go-engines-community/lib/api/router.go
+++ community/go-engines-community/lib/api/router.go
@@ -303,11 +303,6 @@ func RegisterRoutes(
 			alarmAPI.GetDetails,
 		)
 		protected.GET(
-			"/manual-meta-alarms",
-			middleware.Authorize(authPermAlarmRead, permCan, enforcer),
-			alarmAPI.ListManual,
-		)
-		protected.GET(
 			"/entityservice-alarms/:id",
 			middleware.Authorize(authPermAlarmRead, permCan, enforcer),
 			alarmAPI.ListByService,
