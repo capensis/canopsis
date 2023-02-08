9b951fae81b229bbe87913602769e6169d991873
ikalugin <ivan.kalugin@noveogroup.com>
2023-02-08 17:39:26 +0100

    feature/#4721/add_metrics_settings_config

---
+++ community/go-engines-community/database/migrations/20230208160848_add_metrics_settings_permission_up.js
@@ -0,0 +1,17 @@
+if (!db.default_rights.findOne({_id: "api_metrics_settings"})) {
+    db.default_rights.insertOne({
+        _id: "api_metrics_settings",
+        crecord_name: "api_metrics_settings",
+        crecord_type: "action",
+        desc: "Metrics settings",
+        type: "CRUD"
+    });
+    db.default_rights.updateOne({_id: "admin"}, {
+        $set: {
+            "rights.api_metrics_settings": {
+                checksum: 15,
+                crecord_type: "right"
+            }
+        }
+    });
+}
