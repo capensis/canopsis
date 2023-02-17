3f198fe658c4b465121cd60de34ff5da1c3e9978
Dmitry <Dmitriy.Mikhaylishin@noveogroup.com>
2023-02-17 18:05:41 +0600

    feature(#4650): actualize branche after resolve conflicts

--- community/sources/webcore/src/canopsis-next/src/components/widgets/alarm/columns-formatting/extra-details/extra-details-ticket.vue
+++ community/sources/webcore/src/canopsis-next/src/components/widgets/alarm/columns-formatting/extra-details/extra-details-ticket.vue
@@ -1,20 +1,22 @@
 <template lang="pug">
-  v-tooltip.extra-details-ticket(top)
-    template(#activator="{ on }")
-      v-badge.time-line-flag(:value="isSomeOneFailed", color="transparent", overlap)
-        template(#badge="")
-          v-icon.extra-details-ticket__badge-icon(color="error", size="14") error
-        v-icon.blue.white--text.c-extra-details__badge(v-on="on", small) {{ icon }}
-    v-layout.extra-details-ticket__list(column)
-      div.text-md-center(v-for="(ticket, index) in shownTickets", :key="index")
-        strong {{ ticket.ticket_rule_name }} {{ getTicketStatusText(ticket) }}
-        div {{ $t('common.by') }} : {{ ticket.a }}
-        div {{ $t('common.date') }} : {{ convertDateWithToday(ticket.t) }}
-        div(
-          v-if="ticket.ticket"
-        ) {{ $t('alarm.actions.iconsFields.ticketNumber') }} : {{ ticket.ticket }}
-    div.mt-2
-      i(v-if="tickets.length > limit") {{ $t('alarm.otherTickets') }}
+  div
+    v-tooltip.c-extra-details.extra-details-ticket(top)
+      template(#activator="{ on }")
+        v-badge.time-line-flag(:value="isSomeOneFailed", color="transparent", overlap)
+          template(#badge="")
+            v-icon.extra-details-ticket__badge-icon(color="error", size="14") error
+          span.c-extra-details__badge.blue(v-on="on")
+            v-icon(color="white", small) {{ icon }}
+      v-layout.extra-details-ticket__list(column)
+        div.text-md-center(v-for="(ticket, index) in shownTickets", :key="index")
+          strong {{ ticket.ticket_rule_name }} {{ getTicketStatusText(ticket) }}
+          div {{ $t('common.by') }} : {{ ticket.a }}
+          div {{ $t('common.date') }} : {{ convertDateWithToday(ticket.t) }}
+          div(
+            v-if="ticket.ticket"
+          ) {{ $t('alarm.actions.iconsFields.ticketNumber') }} : {{ ticket.ticket }}
+      div.mt-2
+        i(v-if="tickets.length > limit") {{ $t('alarm.otherTickets') }}
 </template>
 
 <script>
