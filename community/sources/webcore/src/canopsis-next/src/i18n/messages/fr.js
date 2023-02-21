cd1cdcaebc16a57ee4760cd614bee2cfd06d68d8
Vladimir Erokhov <vladimir.erokhov@noveogroup.com>
2023-02-21 10:57:05 +0600

    feature(#4688): Fixed translations

--- community/sources/webcore/src/canopsis-next/src/i18n/messages/fr.js
+++ community/sources/webcore/src/canopsis-next/src/i18n/messages/fr.js
@@ -3381,7 +3381,7 @@ export default merge({
   widgetTemplate: {
     types: {
       [WIDGET_TEMPLATES_TYPES.alarmColumns]: 'Général : Colonnes des alarmes',
-      [WIDGET_TEMPLATES_TYPES.entityColumns]: 'Général : Colonnes des alarmes',
+      [WIDGET_TEMPLATES_TYPES.entityColumns]: 'Général : Colonnes des entités',
       [WIDGET_TEMPLATES_TYPES.alarmMoreInfos]: 'Bac à alarmes : plus d\'infos',
       [WIDGET_TEMPLATES_TYPES.weatherItem]: 'Météo des services : Modèle de tuile',
       [WIDGET_TEMPLATES_TYPES.weatherModal]: 'Météo des services : Modèle de modale',
