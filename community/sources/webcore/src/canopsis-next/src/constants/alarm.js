075f35ff0f5ba3fa3b454ec5f246a48e9b2dd974
Dmitry <Dmitriy.Mikhaylishin@noveogroup.com>
2023-03-03 20:05:26 +0600

    feature(#4650): fix entity infos variable

--- community/sources/webcore/src/canopsis-next/src/constants/alarm.js
+++ community/sources/webcore/src/canopsis-next/src/constants/alarm.js
@@ -389,7 +389,7 @@ export const ALARM_PAYLOADS_VARIABLES = {
   ackMessage: '.Value.ACK.Message',
   lastCommentAuthor: '.Value.LastComment.Author',
   lastCommentMessage: '.Value.LastComment.Message',
-  entityInfosValue: '.Entity.Infos.%infos_name%.Value ',
+  entityInfosValue: '(index .Entity.Infos.%infos_name%).Value',
 };
 
 export const ACK_MODAL_ACTIONS_TYPES = {
