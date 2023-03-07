b73edf522810b5c1b5653e0de415324c7f52bef5
Vladimir Erokhov <vladimir.erokhov@noveogroup.com>
2023-03-07 17:06:22 +0600

    feature(#4695): Some fixes after testing

--- community/sources/webcore/src/canopsis-next/src/components/sidebars/settings/alarm.vue
+++ community/sources/webcore/src/canopsis-next/src/components/sidebars/settings/alarm.vue
@@ -128,7 +128,10 @@
         :title="$t('settings.isMultiDeclareTicketEnabled')"
       )
       v-divider
-      field-number(v-model="form.parameters.inlineLinksCount", :title="$t('settings.limit')")
+      field-number(
+        v-model="form.parameters.inlineLinksCount",
+        :title="$t('settings.inlineLinksCount')"
+      )
       v-divider
       export-csv-form(
         v-model="form.parameters",
