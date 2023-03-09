2b25e46c38a54d987949f6e429fc072fea086882
Dmitry <Dmitriy.Mikhaylishin@noveogroup.com>
2023-03-09 14:15:59 +0600

    feature(#4650): fix styles in modals wrapper

--- community/sources/webcore/src/canopsis-next/src/plugins/modals/components/the-modals.vue
+++ community/sources/webcore/src/canopsis-next/src/plugins/modals/components/the-modals.vue
@@ -37,7 +37,7 @@ $minimizedDialogMaxWidth: 360px;
   display: none;
 
   &--active {
-    position: fixed;
+    position: absolute;
     display: flex;
     bottom: 0;
     width: 100%;
@@ -47,7 +47,6 @@ $minimizedDialogMaxWidth: 360px;
     justify-content: center;
     align-items: flex-end;
     align-content: flex-start;
-    z-index: 300;
 
     & ::v-deep .v-dialog__content {
       &--minimized {
