d2289a1efa2b28f07d739122b768b40ce34fde18
Dmitry <Dmitriy.Mikhaylishin@noveogroup.com>
2023-03-09 16:58:54 +0600

    feature(#4815): add settings field for quick date interval

--- community/sources/webcore/src/canopsis-next/src/i18n/messages/fr/settings.js
+++ community/sources/webcore/src/canopsis-next/src/i18n/messages/fr/settings.js
@@ -66,6 +66,7 @@ export default {
   entitiesColumns: 'Colonnes de l\'explorateur de contexte',
   entityInfoPopup: 'Fenêtre contextuelle d\'informations sur l\'entité',
   modal: '(Modal)',
+  defaultTimeRange: 'Plage horaire par défaut',
   exportCsv: {
     title: 'Exporter CSV',
     fields: {
