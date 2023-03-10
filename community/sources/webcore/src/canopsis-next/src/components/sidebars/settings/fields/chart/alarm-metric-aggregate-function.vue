355bd1625c616418b5abbc675e9ca92e992778a0
Dmitry <Dmitriy.Mikhaylishin@noveogroup.com>
2023-03-10 20:04:41 +0600

    feature(#4820): decompose calcualtion method

---
+++ community/sources/webcore/src/canopsis-next/src/components/sidebars/settings/fields/chart/alarm-metric-aggregate-function.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+  widget-settings-item(:title="$t('kpi.calculationMethod')")
+    c-alarm-metric-aggregate-function-field(v-field="value")
+</template>
+
+<script>
+import WidgetSettingsItem from '@/components/sidebars/settings/partials/widget-settings-item.vue';
+
+export default {
+  components: { WidgetSettingsItem },
+  props: {
+    value: {
+      type: String,
+      required: true,
+    },
+    withColor: {
+      type: Boolean,
+      default: false,
+    },
+    withAggregateFunction: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
