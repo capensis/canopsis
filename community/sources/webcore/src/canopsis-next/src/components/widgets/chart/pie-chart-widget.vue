4d9abc28f401d79ff4452ff6785f5d637971f889
Dmitry <Dmitriy.Mikhaylishin@noveogroup.com>
2023-03-15 14:38:52 +0600

    feature(#4835): add charts toolbar

---
+++ community/sources/webcore/src/canopsis-next/src/components/widgets/chart/pie-chart-widget.vue
@@ -0,0 +1,72 @@
+<template lang="pug">
+  v-layout.px-3.pb-3(column)
+    chart-widget-filters(
+      :widget-id="widget._id",
+      :user-filters="userPreference.filters",
+      :widget-filters="widget.filters",
+      :locked-value="lockedFilter",
+      :filters="mainFilter",
+      :interval="query.interval",
+      :sampling="query.sampling",
+      :show-filter="hasAccessToUserFilter",
+      :show-interval="hasAccessToInterval",
+      :show-sampling="hasAccessToSampling",
+      :filter-disabled="!hasAccessToListFilters",
+      :filter-addable="hasAccessToAddFilter",
+      :filter-editable="hasAccessToEditFilter",
+      @update:filters="updateSelectedFilter",
+      @update:sampling="updateSampling",
+      @update:interval="updateInterval"
+    )
+    v-layout
+</template>
+
+<script>
+import { widgetFilterSelectMixin } from '@/mixins/widget/filter-select';
+import { widgetFetchQueryMixin } from '@/mixins/widget/fetch-query';
+import { permissionsWidgetsPieChartInterval } from '@/mixins/permissions/widgets/chart/pie/interval';
+import { permissionsWidgetsPieChartSampling } from '@/mixins/permissions/widgets/chart/pie/sampling';
+import { permissionsWidgetsPieChartFilters } from '@/mixins/permissions/widgets/chart/pie/filters';
+import { widgetIntervalFilterMixin } from '@/mixins/widget/interval';
+import { widgetSamplingFilterMixin } from '@/mixins/widget/sampling';
+
+import ChartWidgetFilters from '@/components/widgets/chart/partials/chart-widget-filters.vue';
+
+export default {
+  inject: ['$system'],
+  components: {
+    ChartWidgetFilters,
+  },
+  mixins: [
+    widgetFilterSelectMixin,
+    widgetFetchQueryMixin,
+    widgetIntervalFilterMixin,
+    widgetSamplingFilterMixin,
+    permissionsWidgetsPieChartInterval,
+    permissionsWidgetsPieChartSampling,
+    permissionsWidgetsPieChartFilters,
+  ],
+  props: {
+    widget: {
+      type: Object,
+      required: true,
+    },
+    tabId: {
+      type: String,
+      default: '',
+    },
+  },
+  methods: {
+    getQuery() {
+      return {
+        ...this.getIntervalQuery(),
+
+        sampling: this.query.sampling,
+        filter: this.query.filter,
+      };
+    },
+
+    fetchList() {},
+  },
+};
+</script>
