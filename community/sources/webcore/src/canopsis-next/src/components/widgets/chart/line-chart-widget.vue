4bb6101f17d05436f9ac9fdbb0412338b19a362a
Dmitry <Dmitriy.Mikhaylishin@noveogroup.com>
2023-03-15 23:38:34 +0600

    feature(#4835): fix after review

--- community/sources/webcore/src/canopsis-next/src/components/widgets/chart/line-chart-widget.vue
+++ community/sources/webcore/src/canopsis-next/src/components/widgets/chart/line-chart-widget.vue
@@ -24,6 +24,8 @@
 </template>
 
 <script>
+import { convertDateToStartOfDayTimestampByTimezone } from '@/helpers/date/date';
+
 import { widgetFetchQueryMixin } from '@/mixins/widget/fetch-query';
 import { widgetFilterSelectMixin } from '@/mixins/widget/filter-select';
 import { permissionsWidgetsLineChartInterval } from '@/mixins/permissions/widgets/chart/line/interval';
@@ -34,7 +36,6 @@ import { widgetSamplingFilterMixin } from '@/mixins/widget/chart/sampling';
 import { entitiesVectorMetricsMixin } from '@/mixins/entities/vector-metrics';
 
 import ChartWidgetFilters from '@/components/widgets/chart/partials/chart-widget-filters.vue';
-import { convertDateToStartOfDayTimestampByTimezone } from '@/helpers/date/date';
 
 export default {
   inject: ['$system'],
