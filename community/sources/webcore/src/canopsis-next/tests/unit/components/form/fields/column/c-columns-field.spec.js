d45001de7affbcda8dbba33e99698ace76802577
Dmitry <Dmitriy.Mikhaylishin@noveogroup.com>
2023-03-23 15:05:04 +0600

    feature(#4841): add some tests for columns

---
+++ community/sources/webcore/src/canopsis-next/tests/unit/components/form/fields/column/c-columns-field.spec.js
@@ -0,0 +1,80 @@
+import { generateRenderer, generateShallowRenderer } from '@unit/utils/vue';
+import { createButtonStub } from '@unit/stubs/button';
+import { ENTITIES_TYPES } from '@/constants';
+
+import CColumnsField from '@/components/forms/fields/column/c-columns-field.vue';
+
+const snapshotStubs = {
+  'c-movable-card-iterator-field': {
+    props: ['value'],
+    template: `
+      <div class="c-movable-card-iterator-field">
+        <div v-for="(item, index) in value" :key="index">
+          <slot name="item" :item="item" :index="index" />
+        </div>
+        <button class="add-item-btn" type="button" @click="$emit('add')" />
+      </div>
+    `,
+  },
+  'column-field': true,
+};
+const stubs = {
+  ...snapshotStubs,
+  'v-btn': createButtonStub('v-btn'),
+};
+
+const selectAddCardButton = wrapper => wrapper.find('.add-item-btn');
+
+describe('c-columns-field', () => {
+  const columns = [
+    { key: 'key-1', label: 'column-1' },
+    { key: 'key-2', label: 'column-2' },
+    { key: 'key-3', label: 'column-3' },
+    { key: 'key-4', label: 'column-4' },
+  ];
+
+  const factory = generateShallowRenderer(CColumnsField, { stubs });
+  const snapshotFactory = generateRenderer(CColumnsField, { stubs: snapshotStubs });
+
+  test('Column added after trigger add event', () => {
+    const wrapper = factory({
+      propsData: {
+        columns,
+      },
+    });
+
+    selectAddCardButton(wrapper).trigger('click');
+
+    expect(wrapper).toEmit('input', [
+      ...columns,
+      {
+        column: '',
+        key: expect.any(String),
+        label: '',
+      },
+    ]);
+  });
+
+  test('Renders `c-columns-field` with default props', async () => {
+    const wrapper = snapshotFactory();
+
+    expect(wrapper.element).toMatchSnapshot();
+  });
+
+  test('Renders `c-columns-field` with custom props', async () => {
+    const wrapper = snapshotFactory({
+      propsData: {
+        columns: [
+          { id: 'id-1', key: 'key-1', label: 'label-1' },
+          { id: 'id-2', key: 'key-1', label: 'label-2' },
+        ],
+        type: ENTITIES_TYPES.entity,
+        withTemplate: true,
+        withHtml: true,
+        withColorIndicator: true,
+      },
+    });
+
+    expect(wrapper.element).toMatchSnapshot();
+  });
+});
