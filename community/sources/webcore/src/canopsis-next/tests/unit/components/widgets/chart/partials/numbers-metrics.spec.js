4b5ebd0a290bf45286e8048b8021de99d9f0bcfb
Dmitry <Dmitriy.Mikhaylishin@noveogroup.com>
2023-03-24 11:17:12 +0600

    feature(#4841): test numbers metrics

---
+++ community/sources/webcore/src/canopsis-next/tests/unit/components/widgets/chart/partials/numbers-metrics.spec.js
@@ -0,0 +1,33 @@
+import { generateRenderer } from '@unit/utils/vue';
+import { ALARM_METRIC_PARAMETERS } from '@/constants';
+
+import NumbersMetrics from '@/components/widgets/chart/partials/numbers-metrics.vue';
+
+const stubs = {
+  'numbers-metrics-item': true,
+};
+
+describe('numbers-metrics', () => {
+  const snapshotFactory = generateRenderer(NumbersMetrics, { stubs });
+
+  test('Renders `numbers-metrics` with default props', async () => {
+    const wrapper = snapshotFactory();
+
+    expect(wrapper.element).toMatchSnapshot();
+  });
+
+  test('Renders `numbers-metrics` with custom props', async () => {
+    const wrapper = snapshotFactory({
+      propsData: {
+        metrics: [
+          { title: ALARM_METRIC_PARAMETERS.createdAlarms },
+          { title: ALARM_METRIC_PARAMETERS.ackAlarms },
+        ],
+        title: 'Custom title',
+        showTrend: true,
+      },
+    });
+
+    expect(wrapper.element).toMatchSnapshot();
+  });
+});
