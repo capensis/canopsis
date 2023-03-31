526e68ebdb2776cfd348564a7b1ef5a0c9a7d3bd
Zarina Yakubova <Zarina.Yakubova@noveogroup.com>
2023-03-31 16:39:06 +0200

    feature(#4855): Add perf data

---
+++ community/go-engines-community/database/postgres_migrations/10_create_perf_data_table.up.sql
@@ -0,0 +1,15 @@
+BEGIN;
+
+CREATE TABLE IF NOT EXISTS perf_data
+(
+    time      TIMESTAMP    NOT NULL,
+    entity_id INT          NOT NULL,
+    name      VARCHAR(255) NOT NULL,
+    value     NUMERIC      NOT NULL,
+    unit      VARCHAR(5)   NOT NULL
+);
+SELECT create_hypertable('perf_data', 'time', if_not_exists => TRUE);
+
+CREATE INDEX IF NOT EXISTS perf_data_entity_id_time_idx ON perf_data (entity_id, time DESC);
+
+END;
---
+++ community/go-engines-community/lib/canopsis/types/event_perf_data_test.go
@@ -0,0 +1,158 @@
+package types_test
+
+import (
+	"testing"
+
+	"git.canopsis.net/canopsis/canopsis-community/community/go-engines-community/lib/canopsis/types"
+	"github.com/kylelemons/godebug/pretty"
+)
+
+func TestEvent_GetPerfData(t *testing.T) {
+	dataSet := []struct {
+		Input    string
+		Expected []types.PerfData
+	}{
+		{
+			Input: "cpu=20%;80;90;0;100",
+			Expected: []types.PerfData{
+				{
+					Name:  "cpu_%",
+					Value: 20,
+					Unit:  "%",
+				},
+			},
+		},
+		{
+			Input: "'memory'=2GB;1;2.5;0;3",
+			Expected: []types.PerfData{
+				{
+					Name:  "memory_GB",
+					Value: 2,
+					Unit:  "GB",
+				},
+			},
+		},
+		{
+			Input: "'disk i/o'=1.5TB",
+			Expected: []types.PerfData{
+				{
+					Name:  "disk i/o_TB",
+					Value: 1.5,
+					Unit:  "TB",
+				},
+			},
+		},
+		{
+			Input: "temp=57.5",
+			Expected: []types.PerfData{
+				{
+					Name:  "temp",
+					Value: 57.5,
+					Unit:  "",
+				},
+			},
+		},
+		{
+			Input:    "temp=57.",
+			Expected: nil,
+		},
+		{
+			Input: "'temp ''C'''=57.5",
+			Expected: []types.PerfData{
+				{
+					Name:  "temp 'C'",
+					Value: 57.5,
+					Unit:  "",
+				},
+			},
+		},
+		{
+			Input: "cpu=20%;80;90;0;100 'memory'=2GB;1;2.5;0;3 'disk i/o'=1.5TB temp=57.5",
+			Expected: []types.PerfData{
+				{
+					Name:  "cpu_%",
+					Value: 20,
+					Unit:  "%",
+				},
+				{
+					Name:  "memory_GB",
+					Value: 2,
+					Unit:  "GB",
+				},
+				{
+					Name:  "disk i/o_TB",
+					Value: 1.5,
+					Unit:  "TB",
+				},
+				{
+					Name:  "temp",
+					Value: 57.5,
+					Unit:  "",
+				},
+			},
+		},
+		{
+			Input: "",
+		},
+		{
+			Input: "=",
+		},
+		{
+			Input: "temp=57.5 cpu=",
+		},
+		{
+			Input: "temp=57.5 cpu=%",
+		},
+		{
+			Input: "temp=57.5 'cpu=20%",
+		},
+		{
+			Input: "temp=57.5 'cpu''=20%",
+		},
+		{
+			Input: "cp''u=20%",
+			Expected: []types.PerfData{
+				{
+					Name:  "cp'u_%",
+					Value: 20,
+					Unit:  "%",
+				},
+			},
+		},
+		{
+			Input: "temp=57.5 cpu =20%",
+		},
+		{
+			Input: "temp=U",
+		},
+		{
+			Input: "temp=U cpu=20%",
+			Expected: []types.PerfData{
+				{
+					Name:  "cpu_%",
+					Value: 20,
+					Unit:  "%",
+				},
+			},
+		},
+		{
+			Input: "memory=UGB cpu=20%",
+			Expected: []types.PerfData{
+				{
+					Name:  "cpu_%",
+					Value: 20,
+					Unit:  "%",
+				},
+			},
+		},
+	}
+
+	event := types.Event{}
+	for _, data := range dataSet {
+		event.PerfData = data.Input
+		result := event.GetPerfData()
+		if diff := pretty.Compare(data.Expected, result); diff != "" {
+			t.Errorf("%q: %s", data.Input, diff)
+		}
+	}
+}
