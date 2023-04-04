1a167048ad0273d793f72ebc14805f84d73ee926
Vladimir Erokhov <vladimir.erokhov@noveogroup.com>
2023-04-04 17:25:50 +0600

    fix(#4695): Added some fixes for variables in declare ticket. And bugfixing

--- community/sources/webcore/src/canopsis-next/src/i18n/messages/en/link-rule.js
+++ community/sources/webcore/src/canopsis-next/src/i18n/messages/en/link-rule.js
@@ -9,11 +9,11 @@ export default {
   sourceCodeAlert: 'Please, change this script only if you are completely aware of what you are doing',
   type: 'Link type',
   single: 'Apply this link only to single alarm ?',
+  actionType: 'Action type',
   types: {
     [LINK_RULE_TYPES.alarm]: 'Alarm',
     [LINK_RULE_TYPES.entity]: 'Entity',
   },
-  actionType: 'Action type',
   actionTypes: {
     open: 'Open the link URL',
     copy: 'Copy link URL',
