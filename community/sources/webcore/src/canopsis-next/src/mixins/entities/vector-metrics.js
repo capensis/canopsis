eb92128de334786a36c7ce0c2ea5e11b15be901c
Vladimir Erokhov <vladimir.erokhov@noveogroup.com>
2023-04-14 11:38:24 +0600

    feature(#4863): Started doing the issue

--- community/sources/webcore/src/canopsis-next/src/mixins/entities/vector-metrics.js
+++ community/sources/webcore/src/canopsis-next/src/mixins/entities/vector-metrics.js
@@ -25,7 +25,6 @@ export const entitiesVectorMetricsMixin = {
   methods: {
     ...mapActions({
       fetchVectorMetricsList: 'fetchList',
-      fetchVectorMetricsListWithoutStore: 'fetchListWithoutStore',
     }),
   },
 };
