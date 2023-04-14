d8b05644e7999816568ff4b3f79cf26cf7bc90fa
Alexander Pavlov <alexander.pavlov@noveogroup.com>
2023-04-14 23:15:49 +0200

    fix(#4887):drop idx_instruction_time

--- community/go-engines-community/database/postgres_migrations/8_create_instruction_stats_tables.down.sql
+++ community/go-engines-community/database/postgres_migrations/8_create_instruction_stats_tables.down.sql
@@ -2,4 +2,5 @@ BEGIN;
 DROP MATERIALIZED VIEW IF EXISTS instruction_execution_hourly;
 DROP TABLE IF EXISTS instruction_execution;
 DROP TABLE IF EXISTS instruction_mod_stats;
+DROP INDEX IF EXISTS idx_instruction_time;
 COMMIT;
