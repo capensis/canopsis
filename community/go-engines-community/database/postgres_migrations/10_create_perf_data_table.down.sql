44547adeff4ad2b1a3cb8c8fb28064f832626854
Zarina Yakubova <Zarina.Yakubova@noveogroup.com>
2023-04-18 09:47:18 +0200

    feature(#4858): Fix migration

--- community/go-engines-community/database/postgres_migrations/10_create_perf_data_table.down.sql
+++ community/go-engines-community/database/postgres_migrations/10_create_perf_data_table.down.sql
@@ -1,2 +1,8 @@
+BEGIN;
+
+DROP INDEX IF EXISTS perf_data_name_name_idx;
 DROP MATERIALIZED VIEW IF EXISTS perf_data_name;
+DROP INDEX IF EXISTS perf_data_name_entity_id_time_idx;
 DROP TABLE IF EXISTS perf_data;
+
+END;
