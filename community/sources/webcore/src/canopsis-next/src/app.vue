408904e3eaf601168b8888e0a82f27b1ebada9ad
Dmitry <Dmitriy.Mikhaylishin@noveogroup.com>
2023-05-02 10:56:39 +0600

    feature(#4797): add promise all and watch

--- community/sources/webcore/src/canopsis-next/src/app.vue
+++ community/sources/webcore/src/canopsis-next/src/app.vue
@@ -73,6 +73,10 @@ export default {
       return this.$route.fullPath;
     },
   },
+  watch: {
+    templateVars: 'setTitle',
+    currentUser: 'setTitle',
+  },
   beforeCreate() {
     reloadPageWithTrailingSlashes();
   },
@@ -81,10 +85,13 @@ export default {
   },
   async mounted() {
     this.socketConnectWithErrorHandling();
-    await this.fetchCurrentUserWithErrorHandling();
     this.showLocalStorageWarningPopupMessage();
 
-    await this.fetchTemplateVars();
+    await Promise.all([
+      this.fetchCurrentUserWithErrorHandling(),
+      this.fetchTemplateVars(),
+    ]);
+
     this.fetchAppInfoWithErrorHandling();
   },
   methods: {
