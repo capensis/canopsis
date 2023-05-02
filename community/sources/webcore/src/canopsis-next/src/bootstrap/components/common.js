c6ec66a01d55a91d5d3a183cb4a15032d9b1caa4
Dmitry <Dmitriy.Mikhaylishin@noveogroup.com>
2023-05-02 13:20:12 +0600

    feature(#4623): restructure common registration

--- community/sources/webcore/src/canopsis-next/src/bootstrap/components/common.js
+++ community/sources/webcore/src/canopsis-next/src/bootstrap/components/common.js
@@ -1,3 +1,5 @@
+export { default as CAlert } from '@/components/common/alert/c-alert.vue';
+export { default as CEnabled } from '@/components/icons/c-enabled.vue';
 export { default as CPageHeader } from '@/components/common/page/c-page-header.vue';
 export { default as CEllipsis } from '@/components/common/table/c-ellipsis.vue';
 export { default as CPagination } from '@/components/common/pagination/c-pagination.vue';
@@ -7,49 +9,7 @@ export { default as CInformationBlockRow } from '@/components/common/block/c-inf
 export { default as CResponsiveList } from '@/components/common/responsive-list/c-responsive-list.vue';
 export { default as CRuntimeTemplate } from '@/components/common/runtime-template/c-runtime-template.vue';
 export { default as CCopyWrapper } from '@/components/common/runtime-template/c-copy-wrapper.vue';
-export { default as CAlert } from '@/components/common/alert/c-alert.vue';
 export { default as CLinksList } from '@/components/common/links/c-links-list.vue';
-
-/**
- * Overlays
- */
-export { default as CAlertOverlay } from '@/components/common/overlay/c-alert-overlay.vue';
-export { default as CProgressOverlay } from '@/components/common/overlay/c-progress-overlay.vue';
-export { default as CZoomOverlay } from '@/components/common/overlay/c-zoom-overlay.vue';
-
-/**
- * Chips
- */
-export { default as CAlarmChip } from '@/components/common/chips/c-alarm-chip.vue';
-export { default as CAlarmActionChip } from '@/components/common/chips/c-alarm-action-chip.vue';
-export { default as CAlarmActionsChips } from '@/components/common/chips/c-alarm-actions-chips.vue';
-export { default as CAlarmTagsChips } from '@/components/common/chips/c-alarm-tags-chips.vue';
-export { default as CAlarmLinksChips } from '@/components/common/chips/c-alarm-links-chips.vue';
-export { default as CStateCountChangesChips } from '@/components/common/chips/c-state-count-changes-chips.vue';
-export { default as CTestSuiteChip } from '@/components/common/chips/c-test-suite-chip.vue';
-export { default as CInstructionJobChip } from '@/components/common/chips/c-instruction-job-chip.vue';
-export { default as CEngineChip } from '@/components/common/chips/c-engine-chip.vue';
-export { default as CPatternOperatorChip } from '@/components/common/chips/c-pattern-operator-chip.vue';
-
-/**
- * Table
- */
-export { default as CAdvancedDataTable } from '@/components/common/table/c-advanced-data-table.vue';
-export { default as CTreeviewDataTable } from '@/components/common/table/c-treeview-data-table.vue';
-export { default as CEmptyDataTableColumns } from '@/components/common/table/c-empty-data-table-columns.vue';
-export { default as CTablePagination } from '@/components/common/pagination/c-table-pagination.vue';
-
-/**
- * Buttons
- */
-export { default as CExpandBtn } from '@/components/common/buttons/c-expand-btn.vue';
-export { default as CActionBtn } from '@/components/common/buttons/c-action-btn.vue';
-export { default as CDownloadBtn } from '@/components/common/buttons/c-download-btn.vue';
-export { default as CCopyBtn } from '@/components/common/buttons/c-copy-btn.vue';
-export { default as CActionFabBtn } from '@/components/common/buttons/c-action-fab-btn.vue';
-export { default as CFabExpandBtn } from '@/components/common/buttons/c-fab-expand-btn.vue';
-export { default as CFabBtn } from '@/components/common/buttons/c-fab-btn.vue';
-export { default as CRefreshBtn } from '@/components/common/buttons/c-refresh-btn.vue';
 export { default as CRequestTextInformation } from '@/components/common/request/c-request-text-information.vue';
 export { default as CJsonTreeview } from '@/components/common/request/c-json-treeview.vue';
 export { default as CMiniBarChart } from '@/components/common/chart/c-mini-bar-chart.vue';
@@ -57,14 +17,3 @@ export { default as CImagesViewer } from '@/components/common/images-viewer/c-im
 export { default as CClickableTooltip } from '@/components/common/clickable-tooltip/c-clickable-tooltip.vue';
 export { default as CCollapsePanel } from '@/components/common/block/c-collapse-panel.vue';
 export { default as CContextmenu } from '@/components/common/contextmenu/c-contextmenu.vue';
-
-/**
- * Icons
- */
-export { default as CHelpIcon } from '@/components/common/icons/c-help-icon.vue';
-export { default as CNoEventsIcon } from '@/components/common/icons/c-no-events-icon.vue';
-
-/**
- * Groups
- */
-export { default as CDensityBtnToggle } from '@/components/common/groups/c-density-btn-toggle.vue';
