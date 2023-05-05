adebed74d77aa8fc76d3d2fa1584816d82f7bfb3
Dmitry <Dmitriy.Mikhaylishin@noveogroup.com>
2023-05-05 13:08:00 +0600

    feature(#4627): fixes after self review

--- community/sources/webcore/src/canopsis-next/tests/unit/components/sidebars/settings/fields/common/filters.spec.js
+++ community/sources/webcore/src/canopsis-next/tests/unit/components/sidebars/settings/fields/common/filters.spec.js
@@ -3,7 +3,7 @@ import Faker from 'faker';
 import { createVueInstance, generateRenderer, generateShallowRenderer } from '@unit/utils/vue';
 import { mockModals } from '@unit/utils/mock-hooks';
 import { MODALS } from '@/constants';
-import uuid from '@/helpers/uuid';
+import { uuid } from '@/helpers/uuid';
 
 import Filters from '@/components/sidebars/settings/fields/common/filters.vue';
 
