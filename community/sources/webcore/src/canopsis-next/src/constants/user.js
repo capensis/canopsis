8ecfff2b9c0079a28ba25561a816f364093616c5
Dmitry <Dmitriy.Mikhaylishin@noveogroup.com>
2023-05-10 14:43:06 +0600

    feature(#4797): add dot before each user variables

--- community/sources/webcore/src/canopsis-next/src/constants/user.js
+++ community/sources/webcore/src/canopsis-next/src/constants/user.js
@@ -8,11 +8,11 @@ export const USERS_TABS = {
 };
 
 export const USER_PAYLOADS_VARIABLES = {
-  email: 'User.Email',
-  username: 'User.Username',
-  firstname: 'User.Firstname',
-  lastname: 'User.Lastname',
-  externalId: 'User.ExternalID',
-  source: 'User.Source',
-  role: 'User.Role',
+  email: '.User.Email',
+  username: '.User.Username',
+  firstname: '.User.Firstname',
+  lastname: '.User.Lastname',
+  externalId: '.User.ExternalID',
+  source: '.User.Source',
+  role: '.User.Role',
 };
