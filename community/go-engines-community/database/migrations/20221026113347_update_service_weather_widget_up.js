4411995ad6032c060244952c3af5e166721aa164
Zarina Yakubova <Zarina.Yakubova@noveogroup.com>
2023-05-15 11:54:12 +0200

    fix(#4942): Fix migration

--- community/go-engines-community/database/migrations/20221026113347_update_service_weather_widget_up.js
+++ community/go-engines-community/database/migrations/20221026113347_update_service_weather_widget_up.js
@@ -2,6 +2,10 @@ db.widgets.find({type: "ServiceWeather"}).forEach(function (doc) {
     var set = {};
     var unset = {};
 
+    if (!doc.parameters || !doc.parameters.counters || doc.parameters.counters.pbehavior_types !== undefined) {
+        return;
+    }
+
     set["parameters.counters.pbehavior_enabled"] = false;
     set["parameters.counters.pbehavior_types"] = [];
     set["parameters.counters.state_enabled"] = false;
@@ -9,13 +13,11 @@ db.widgets.find({type: "ServiceWeather"}).forEach(function (doc) {
     unset["parameters.counters.enabled"] = "";
     unset["parameters.counters.types"] = "";
 
-    if (doc.parameters && doc.parameters.counters) {
-        if (doc.parameters.counters.enabled) {
-            set["parameters.counters.pbehavior_enabled"] = doc.parameters.counters.enabled;
-        }
-        if (doc.parameters.counters.types) {
-            set["parameters.counters.pbehavior_types"] = doc.parameters.counters.types;
-        }
+    if (doc.parameters.counters.enabled) {
+        set["parameters.counters.pbehavior_enabled"] = doc.parameters.counters.enabled;
+    }
+    if (doc.parameters.counters.types) {
+        set["parameters.counters.pbehavior_types"] = doc.parameters.counters.types;
     }
 
     db.widgets.updateOne({_id: doc._id}, {$set: set, $unset: unset});
@@ -25,6 +27,10 @@ db.widgets.find({type: {$in: ["ServiceWeather", "Counter"]}}).forEach(function (
     var set = {};
     var unset = {};
 
+    if (!doc.parameters || doc.parameters.columnDesktop !== undefined) {
+        return;
+    }
+
     set["parameters.columnMobile"] = 2;
     set["parameters.columnTablet"] = 3;
     set["parameters.columnDesktop"] = 4;
@@ -32,32 +38,30 @@ db.widgets.find({type: {$in: ["ServiceWeather", "Counter"]}}).forEach(function (
     unset["parameters.columnMD"] = "";
     unset["parameters.columnLG"] = "";
 
-    if (doc.parameters) {
-        if (doc.parameters.columnSM <= 6) {
-            set["parameters.columnMobile"] = 2;
-        } else {
-            set["parameters.columnMobile"] = 1;
-        }
-        if (doc.parameters.columnMD <= 3) {
-            set["parameters.columnTablet"] = 4;
-        } else if (doc.parameters.columnMD === 4) {
-            set["parameters.columnTablet"] = 3;
-        } else if (doc.parameters.columnMD <= 6) {
-            set["parameters.columnTablet"] = 2;
-        } else {
-            set["parameters.columnTablet"] = 1;
-        }
-        if (doc.parameters.columnLG <= 2) {
-            set["parameters.columnDesktop"] = 6;
-        } else if (doc.parameters.columnLG === 3) {
-            set["parameters.columnDesktop"] = 4;
-        } else if (doc.parameters.columnLG <= 4) {
-            set["parameters.columnDesktop"] = 3;
-        } else if (doc.parameters.columnLG <= 6) {
-            set["parameters.columnDesktop"] = 2;
-        } else {
-            set["parameters.columnDesktop"] = 1;
-        }
+    if (doc.parameters.columnSM <= 6) {
+        set["parameters.columnMobile"] = 2;
+    } else {
+        set["parameters.columnMobile"] = 1;
+    }
+    if (doc.parameters.columnMD <= 3) {
+        set["parameters.columnTablet"] = 4;
+    } else if (doc.parameters.columnMD === 4) {
+        set["parameters.columnTablet"] = 3;
+    } else if (doc.parameters.columnMD <= 6) {
+        set["parameters.columnTablet"] = 2;
+    } else {
+        set["parameters.columnTablet"] = 1;
+    }
+    if (doc.parameters.columnLG <= 2) {
+        set["parameters.columnDesktop"] = 6;
+    } else if (doc.parameters.columnLG === 3) {
+        set["parameters.columnDesktop"] = 4;
+    } else if (doc.parameters.columnLG <= 4) {
+        set["parameters.columnDesktop"] = 3;
+    } else if (doc.parameters.columnLG <= 6) {
+        set["parameters.columnDesktop"] = 2;
+    } else {
+        set["parameters.columnDesktop"] = 1;
     }
 
     db.widgets.updateOne({_id: doc._id}, {$set: set, $unset: unset});
