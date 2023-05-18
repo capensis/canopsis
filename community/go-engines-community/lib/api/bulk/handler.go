1cb6a591309fef44ae10cd491a1d37b98039fa01
Zarina Yakubova <Zarina.Yakubova@noveogroup.com>
2023-05-18 11:54:16 +0200

    feature(#4800): Refactor bulk endpoints

--- community/go-engines-community/lib/api/bulk/handler.go
+++ community/go-engines-community/lib/api/bulk/handler.go
@@ -12,6 +12,8 @@ import (
 	"github.com/valyala/fastjson"
 )
 
+var ErrUnauthorized = errors.New("unauthorized")
+
 func Handler[T any](
 	c *gin.Context,
 	f func(T) (string, error),
@@ -39,43 +41,69 @@ func Handler[T any](
 	for idx, rawObject := range rawObjects {
 		object, err := rawObject.Object()
 		if err != nil {
-			response.SetArrayItem(idx, common.GetBulkResponseItem(&arena, "", http.StatusBadRequest, rawObject, arena.NewString(err.Error())))
+			response.SetArrayItem(idx, GetResponseItem(&arena, "", http.StatusBadRequest, rawObject, arena.NewString(err.Error())))
 			continue
 		}
 
 		var request T
 		err = json.Unmarshal(object.MarshalTo(nil), &request)
 		if err != nil {
-			response.SetArrayItem(idx, common.GetBulkResponseItem(&arena, "", http.StatusBadRequest, rawObject, arena.NewString(err.Error())))
+			response.SetArrayItem(idx, GetResponseItem(&arena, "", http.StatusBadRequest, rawObject, arena.NewString(err.Error())))
 			continue
 		}
 
 		err = binding.Validator.ValidateStruct(request)
 		if err != nil {
-			response.SetArrayItem(idx, common.GetBulkResponseItem(&arena, "", http.StatusBadRequest, rawObject, common.NewValidationErrorFastJsonValue(&arena, err, request)))
+			response.SetArrayItem(idx, GetResponseItem(&arena, "", http.StatusBadRequest, rawObject, common.NewValidationErrorFastJsonValue(&arena, err, request)))
 			continue
 		}
 
 		id, err := f(request)
 		if err != nil {
+			if errors.Is(err, ErrUnauthorized) {
+				response.SetArrayItem(idx, GetResponseItem(&arena, "", http.StatusForbidden, rawObject, arena.NewString(common.ForbiddenResponse.Error)))
+				continue
+			}
+
 			valErr := common.ValidationError{}
 			if errors.As(err, &valErr) {
-				response.SetArrayItem(idx, common.GetBulkResponseItem(&arena, "", http.StatusBadRequest, rawObject, common.NewValidationErrorFastJsonValue(&arena, valErr, request)))
+				response.SetArrayItem(idx, GetResponseItem(&arena, "", http.StatusBadRequest, rawObject, common.NewValidationErrorFastJsonValue(&arena, valErr, request)))
 				continue
 			}
 
 			logger.Err(err).Msg("cannot process bulk item")
-			response.SetArrayItem(idx, common.GetBulkResponseItem(&arena, "", http.StatusInternalServerError, rawObject, arena.NewString(common.InternalServerErrorResponse.Error)))
+			response.SetArrayItem(idx, GetResponseItem(&arena, "", http.StatusInternalServerError, rawObject, arena.NewString(common.InternalServerErrorResponse.Error)))
 			continue
 		}
 
 		if id == "" {
-			response.SetArrayItem(idx, common.GetBulkResponseItem(&arena, "", http.StatusNotFound, rawObject, arena.NewString(common.NotFoundResponse.Error)))
+			response.SetArrayItem(idx, GetResponseItem(&arena, "", http.StatusNotFound, rawObject, arena.NewString(common.NotFoundResponse.Error)))
 			continue
 		}
 
-		response.SetArrayItem(idx, common.GetBulkResponseItem(&arena, id, http.StatusOK, rawObject, nil))
+		response.SetArrayItem(idx, GetResponseItem(&arena, id, http.StatusOK, rawObject, nil))
 	}
 
 	c.Data(http.StatusMultiStatus, gin.MIMEJSON, response.MarshalTo(nil))
+}
+
+func GetResponseItem(ar *fastjson.Arena, id string, status int, rawUser, err *fastjson.Value) *fastjson.Value {
+	item := ar.NewObject()
+	item.Set("status", ar.NewNumberInt(status))
+	item.Set("item", rawUser)
+
+	if err == nil {
+		item.Set("id", ar.NewString(id))
+		return item
+	}
+
+	if err.Type() == fastjson.TypeString {
+		item.Set("error", err)
+	}
+
+	if err.Type() == fastjson.TypeObject {
+		item.Set("errors", err)
+	}
+
+	return item
 }
