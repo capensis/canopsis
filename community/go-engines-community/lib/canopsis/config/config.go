8b26c6a7f5a0b44c7305b49157f68ff998b751b7
Zarina Yakubova <Zarina.Yakubova@noveogroup.com>
2023-05-25 13:38:56 +0200

    feature(#4873): Add user sessions metrics

--- community/go-engines-community/lib/canopsis/config/config.go
+++ community/go-engines-community/lib/canopsis/config/config.go
@@ -90,6 +90,7 @@ type ConsoleWriter struct {
 type SectionMetrics struct {
 	FlushInterval          string `toml:"FlushInterval"`
 	SliInterval            string `toml:"SliInterval"`
+	UserSessionGapInterval string `toml:"UserSessionGapInterval"`
 	EnabledInstructions    bool   `toml:"EnabledInstructions"`
 	EnabledNotAckedMetrics bool   `toml:"EnabledNotAckedMetrics"`
 }
