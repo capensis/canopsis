9b4b7c2979f0f44faf61081789730847b850289b
Zarina Yakubova <Zarina.Yakubova@noveogroup.com>
2023-05-30 12:29:22 +0200

    fix(#4956): Fix skip for child

--- community/go-engines-community/lib/canopsis/action/worker_pool.go
+++ community/go-engines-community/lib/canopsis/action/worker_pool.go
@@ -29,20 +29,21 @@ const (
 )
 
 type Task struct {
-	Source            string
-	Action            Action
-	Alarm             types.Alarm
-	Entity            types.Entity
-	Step              int
-	ExecutionCacheKey string
-	ExecutionID       string
-	ScenarioID        string
-	ScenarioName      string
-	SkipForChild      bool
-	Header            map[string]string
-	Response          map[string]interface{}
-	ResponseMap       map[string]interface{}
-	AdditionalData    AdditionalData
+	Source             string
+	Action             Action
+	Alarm              types.Alarm
+	Entity             types.Entity
+	Step               int
+	ExecutionCacheKey  string
+	ExecutionID        string
+	ScenarioID         string
+	ScenarioName       string
+	SkipForChild       bool
+	IsMetaAlarmUpdated bool
+	Header             map[string]string
+	Response           map[string]interface{}
+	ResponseMap        map[string]interface{}
+	AdditionalData     AdditionalData
 }
 
 type TaskResult struct {
@@ -297,16 +298,20 @@ func (s *pool) getRPCWebhookEvent(ctx context.Context, task Task) (*rpc.WebhookE
 		}
 	}
 	// Skip webhooks for children
-	if task.SkipForChild && len(task.Alarm.Value.Parents) > 0 {
-		err := s.alarmCollection.FindOne(ctx, bson.M{
-			"d":          bson.M{"$in": task.Alarm.Value.Parents},
-			"v.resolved": nil,
-		}).Err()
-		if err != nil && !errors.Is(err, mongodriver.ErrNoDocuments) {
-			return nil, false, fmt.Errorf("cannot find parents: %w", err)
-		}
-		if err == nil {
+	if task.SkipForChild {
+		if task.IsMetaAlarmUpdated {
 			return nil, true, nil
+		} else if len(task.Alarm.Value.Parents) > 0 {
+			err := s.alarmCollection.FindOne(ctx, bson.M{
+				"d":          bson.M{"$in": task.Alarm.Value.Parents},
+				"v.resolved": nil,
+			}).Err()
+			if err != nil && !errors.Is(err, mongodriver.ErrNoDocuments) {
+				return nil, false, fmt.Errorf("cannot find parents: %w", err)
+			}
+			if err == nil {
+				return nil, true, nil
+			}
 		}
 	}
 
