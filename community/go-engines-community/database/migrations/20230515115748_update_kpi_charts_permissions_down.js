f4d30d4e842b4116fa8693ebccecfa0eb168bb0b
ikalugin <ivan.kalugin@noveogroup.com>
2023-05-31 12:59:39 +0200

    fix(#4228): update kpi charts permissons, update rw to can

---
+++ community/go-engines-community/database/migrations/20230515115748_update_kpi_charts_permissions_down.js
@@ -0,0 +1,71 @@
+db.default_rights.updateOne({_id: "barchart_interval"}, {$set: {"type": "RW"}});
+db.default_rights.updateOne({_id: "admin"}, {$set: {"rights.barchart_interval.checksum": 15}});
+
+db.default_rights.updateOne({_id: "barchart_sampling"}, {$set: {"type": "RW"}});
+db.default_rights.updateOne({_id: "admin"}, {$set: {"rights.barchart_sampling.checksum": 15}});
+
+db.default_rights.updateOne({_id: "barchart_listFilters"}, {$set: {"type": "RW"}});
+db.default_rights.updateOne({_id: "admin"}, {$set: {"rights.barchart_listFilters.checksum": 15}});
+
+db.default_rights.updateOne({_id: "barchart_editFilter"}, {$set: {"type": "RW"}});
+db.default_rights.updateOne({_id: "admin"}, {$set: {"rights.barchart_editFilter.checksum": 15}});
+
+db.default_rights.updateOne({_id: "barchart_addFilter"}, {$set: {"type": "RW"}});
+db.default_rights.updateOne({_id: "admin"}, {$set: {"rights.barchart_addFilter.checksum": 15}});
+
+db.default_rights.updateOne({_id: "barchart_userFilter"}, {$set: {"type": "RW"}});
+db.default_rights.updateOne({_id: "admin"}, {$set: {"rights.barchart_userFilter.checksum": 15}});
+
+db.default_rights.updateOne({_id: "linechart_interval"}, {$set: {"type": "RW"}});
+db.default_rights.updateOne({_id: "admin"}, {$set: {"rights.linechart_interval.checksum": 15}});
+
+db.default_rights.updateOne({_id: "linechart_sampling"}, {$set: {"type": "RW"}});
+db.default_rights.updateOne({_id: "admin"}, {$set: {"rights.linechart_sampling.checksum": 15}});
+
+db.default_rights.updateOne({_id: "linechart_listFilters"}, {$set: {"type": "RW"}});
+db.default_rights.updateOne({_id: "admin"}, {$set: {"rights.linechart_listFilters.checksum": 15}});
+
+db.default_rights.updateOne({_id: "linechart_editFilter"}, {$set: {"type": "RW"}});
+db.default_rights.updateOne({_id: "admin"}, {$set: {"rights.linechart_editFilter.checksum": 15}});
+
+db.default_rights.updateOne({_id: "linechart_addFilter"}, {$set: {"type": "RW"}});
+db.default_rights.updateOne({_id: "admin"}, {$set: {"rights.linechart_addFilter.checksum": 15}});
+
+db.default_rights.updateOne({_id: "linechart_userFilter"}, {$set: {"type": "RW"}});
+db.default_rights.updateOne({_id: "admin"}, {$set: {"rights.linechart_userFilter.checksum": 15}});
+
+db.default_rights.updateOne({_id: "piechart_interval"}, {$set: {"type": "RW"}});
+db.default_rights.updateOne({_id: "admin"}, {$set: {"rights.piechart_interval.checksum": 15}});
+
+db.default_rights.updateOne({_id: "piechart_sampling"}, {$set: {"type": "RW"}});
+db.default_rights.updateOne({_id: "admin"}, {$set: {"rights.piechart_sampling.checksum": 15}});
+
+db.default_rights.updateOne({_id: "piechart_listFilters"}, {$set: {"type": "RW"}});
+db.default_rights.updateOne({_id: "admin"}, {$set: {"rights.piechart_listFilters.checksum": 15}});
+
+db.default_rights.updateOne({_id: "piechart_editFilter"}, {$set: {"type": "RW"}});
+db.default_rights.updateOne({_id: "admin"}, {$set: {"rights.piechart_editFilter.checksum": 15}});
+
+db.default_rights.updateOne({_id: "piechart_addFilter"}, {$set: {"type": "RW"}});
+db.default_rights.updateOne({_id: "admin"}, {$set: {"rights.piechart_addFilter.checksum": 15}});
+
+db.default_rights.updateOne({_id: "piechart_userFilter"}, {$set: {"type": "RW"}});
+db.default_rights.updateOne({_id: "admin"}, {$set: {"rights.piechart_userFilter.checksum": 15}});
+
+db.default_rights.updateOne({_id: "numbers_interval"}, {$set: {"type": "RW"}});
+db.default_rights.updateOne({_id: "admin"}, {$set: {"rights.numbers_interval.checksum": 15}});
+
+db.default_rights.updateOne({_id: "numbers_sampling"}, {$set: {"type": "RW"}});
+db.default_rights.updateOne({_id: "admin"}, {$set: {"rights.numbers_sampling.checksum": 15}});
+
+db.default_rights.updateOne({_id: "numbers_listFilters"}, {$set: {"type": "RW"}});
+db.default_rights.updateOne({_id: "admin"}, {$set: {"rights.numbers_listFilters.checksum": 15}});
+
+db.default_rights.updateOne({_id: "numbers_editFilter"}, {$set: {"type": "RW"}});
+db.default_rights.updateOne({_id: "admin"}, {$set: {"rights.numbers_editFilter.checksum": 15}});
+
+db.default_rights.updateOne({_id: "numbers_addFilter"}, {$set: {"type": "RW"}});
+db.default_rights.updateOne({_id: "admin"}, {$set: {"rights.numbers_addFilter.checksum": 15}});
+
+db.default_rights.updateOne({_id: "numbers_userFilter"}, {$set: {"type": "RW"}});
+db.default_rights.updateOne({_id: "admin"}, {$set: {"rights.numbers_userFilter.checksum": 15}});
