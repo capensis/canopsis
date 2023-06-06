4ba4377d279443e5d70689febcb013bfff080dd2
Zarina Yakubova <Zarina.Yakubova@noveogroup.com>
2023-06-06 14:55:03 +0200

    feature(#4957): Run scenarios on triggers for resolved alarms

--- community/go-engines-community/lib/canopsis/types/event_easyjson.go
+++ community/go-engines-community/lib/canopsis/types/event_easyjson.go
@@ -100,6 +100,8 @@ func easyjsonF642ad3eDecodeGitCanopsisNetCanopsisCanopsisCommunityCommunityGoEng
 				}
 				easyjsonF642ad3eDecodeGitCanopsisNetCanopsisCanopsisCommunityCommunityGoEnginesCommunityLibCanopsisTypes2(in, out.Entity)
 			}
+		case "aid":
+			out.AlarmID = string(in.String())
 		case "author":
 			out.Author = string(in.String())
 		case "user_id":
@@ -435,6 +437,11 @@ func easyjsonF642ad3eEncodeGitCanopsisNetCanopsisCanopsisCommunityCommunityGoEng
 		} else {
 			easyjsonF642ad3eEncodeGitCanopsisNetCanopsisCanopsisCommunityCommunityGoEnginesCommunityLibCanopsisTypes2(out, *in.Entity)
 		}
+	}
+	if in.AlarmID != "" {
+		const prefix string = ",\"aid\":"
+		out.RawString(prefix)
+		out.String(string(in.AlarmID))
 	}
 	{
 		const prefix string = ",\"author\":"
