bc185dab994b86fca90e072b79bf95853d06cbca
Zarina Yakubova <Zarina.Yakubova@noveogroup.com>
2023-06-16 15:22:41 +0200

    feature(#4520): Update alarm export endpoint

--- community/go-engines-community/lib/canopsis/link/generator.go
+++ community/go-engines-community/lib/canopsis/link/generator.go
@@ -3,12 +3,15 @@ package link
 import (
 	"context"
 	"errors"
+
+	"git.canopsis.net/canopsis/canopsis-community/community/go-engines-community/lib/canopsis/types"
 )
 
 var ErrNoRule = errors.New("rule not found")
 var ErrNotMatchedAlarm = errors.New("alarms aren't matched to rule")
 
 type Generator interface {
+	GenerateForAlarm(ctx context.Context, alarm types.Alarm, entity types.Entity, user User) (LinksByCategory, error)
 	GenerateForAlarms(ctx context.Context, alarmIds []string, user User) (map[string]LinksByCategory, error)
 	GenerateForEntities(ctx context.Context, entityIds []string, user User) (map[string]LinksByCategory, error)
 	GenerateCombinedForAlarmsByRule(ctx context.Context, ruleId string, alarmIds []string, user User) ([]Link, error)
