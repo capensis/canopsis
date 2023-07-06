57edd8a23bb0a7e043872e6eb7bd3a70af189a65
Zarina Yakubova <Zarina.Yakubova@noveogroup.com>
2023-07-06 17:15:26 +0200

    feature(#4554): Add internal tags

--- community/go-engines-community/lib/canopsis/types/alarm_value_test.go
+++ community/go-engines-community/lib/canopsis/types/alarm_value_test.go
@@ -1,144 +1,187 @@
 package types_test
 
 import (
-	gocontext "context"
+	"context"
 	"testing"
 	"time"
 
 	"git.canopsis.net/canopsis/canopsis-community/community/go-engines-community/lib/canopsis/types"
 	"git.canopsis.net/canopsis/canopsis-community/community/go-engines-community/lib/mongo"
+	"git.canopsis.net/canopsis/canopsis-community/community/go-engines-community/lib/utils"
+	"github.com/kylelemons/godebug/pretty"
 	"github.com/rs/zerolog"
-	. "github.com/smartystreets/goconvey/convey"
 	"go.mongodb.org/mongo-driver/bson"
 )
 
-func TestAlarmStepsCrop(t *testing.T) {
-	Convey("Setup", t, func() {
-		steps := make(types.AlarmSteps, 0)
-		st := types.AlarmStep{
-			Type:    types.AlarmStepStatusIncrease,
-			Value:   1,
-			Author:  "test",
-			Message: "test",
+func TestAlarmSteps_Crop_GivenLessThenLimitSteps_ShouldDoNothing(t *testing.T) {
+	statusStep := types.AlarmStep{
+		Type:    types.AlarmStepStatusIncrease,
+		Value:   1,
+		Author:  "test",
+		Message: "test",
+	}
+	stateIncStep := types.AlarmStep{
+		Type:  types.AlarmStepStateIncrease,
+		Value: 2,
+	}
+	stateDecStep := types.AlarmStep{
+		Type:  types.AlarmStepStateDecrease,
+		Value: 1,
+	}
+	steps := make(types.AlarmSteps, 0)
+	err := steps.Add(statusStep)
+	if err != nil {
+		t.Fatalf("expected no error but got %v", err)
+	}
+
+	for i := 0; i < 10; i++ {
+		err = steps.Add(stateIncStep)
+		if err != nil {
+			t.Fatalf("expected no error but got %v", err)
 		}
-		s1 := types.AlarmStep{
-			Type:  types.AlarmStepStateIncrease,
-			Value: 2,
+		err = steps.Add(stateDecStep)
+		if err != nil {
+			t.Fatalf("expected no error but got %v", err)
 		}
-		s2 := types.AlarmStep{
-			Type:  types.AlarmStepStateDecrease,
-			Value: 1,
-		}
-		err := steps.Add(st)
-		So(err, ShouldBeNil)
-		for i := 0; i < 10; i++ {
-			err = steps.Add(s1)
-			So(err, ShouldBeNil)
-			err = steps.Add(s2)
-			So(err, ShouldBeNil)
-		}
-		cropNum := 30
-		Convey("Crop does not work for the first 21 steps", func() {
-			_, update := steps.Crop(&st, cropNum)
-			So(update, ShouldBeFalse)
-			Convey("Crop does work for the 41 steps", func() {
-				for i := 0; i < 10; i++ {
-					err = steps.Add(s1)
-					So(err, ShouldBeNil)
-					err = steps.Add(s2)
-					So(err, ShouldBeNil)
-				}
+	}
 
-				steps, update := steps.Crop(&st, cropNum)
-
-				So(steps[0].Type, ShouldEqual, st.Type)
-				So(steps[1].Type, ShouldEqual, types.AlarmStepStateCounter)
-
-				nstep := steps[1]
-
-				So(update, ShouldBeTrue)
-				So(len(steps), ShouldEqual, cropNum+2)
-				// Ten steps were cropped from the 40 croppable steps
-				testCounter := types.CropCounter{
-					StateChanges:  10,
-					Stateinc:      5,
-					Statedec:      5,
-					StateInfo:     0,
-					StateMinor:    5,
-					StateMajor:    5,
-					StateCritical: 0,
-				}
-				So(nstep.StateCounter, ShouldResemble, testCounter)
-				So(nstep.Author, ShouldEqual, "test")
-				So(nstep.Message, ShouldEqual, "test")
-				So(nstep.Value, ShouldEqual, st.Value)
-
-			})
-		})
-	})
+	_, update := steps.Crop(&statusStep, 30)
+	if update {
+		t.Fatalf("expected no update")
+	}
 }
 
-func TestAlarmStepCropOldData(t *testing.T) {
-	ctx, cancel := gocontext.WithCancel(gocontext.Background())
+func TestAlarmSteps_Crop_GivenGreaterThenLimitSteps_ShouldCropSteps(t *testing.T) {
+	statusStep := types.AlarmStep{
+		Type:    types.AlarmStepStatusIncrease,
+		Value:   1,
+		Author:  "test",
+		Message: "test",
+	}
+	stateIncStep := types.AlarmStep{
+		Type:  types.AlarmStepStateIncrease,
+		Value: 2,
+	}
+	stateDecStep := types.AlarmStep{
+		Type:  types.AlarmStepStateDecrease,
+		Value: 1,
+	}
+	steps := make(types.AlarmSteps, 0)
+	err := steps.Add(statusStep)
+	if err != nil {
+		t.Fatalf("expected no error but got %v", err)
+	}
+
+	for i := 0; i < 20; i++ {
+		err = steps.Add(stateIncStep)
+		if err != nil {
+			t.Fatalf("expected no error but got %v", err)
+		}
+		err = steps.Add(stateDecStep)
+		if err != nil {
+			t.Fatalf("expected no error but got %v", err)
+		}
+	}
+
+	cropNum := 30
+	steps, update := steps.Crop(&statusStep, cropNum)
+	if !update {
+		t.Fatalf("expected update")
+	}
+	if len(steps) != cropNum+2 {
+		t.Fatalf("expected %d but got %d", cropNum+2, len(steps))
+	}
+	if steps[0].Type != statusStep.Type {
+		t.Fatalf("expected %q but got %s", statusStep.Type, steps[0].Type)
+	}
+	if steps[1].Type != types.AlarmStepStateCounter {
+		t.Fatalf("expected %q but got %s", types.AlarmStepStateCounter, steps[1].Type)
+	}
+	if steps[1].Author != "test" {
+		t.Fatalf("expected %q but got %s", "test", steps[1].Author)
+	}
+	if steps[1].Message != "test" {
+		t.Fatalf("expected %q but got %s", "test", steps[1].Message)
+	}
+	if steps[1].Value != statusStep.Value {
+		t.Fatalf("expected %d but got %d", statusStep.Value, steps[1].Value)
+	}
+	expectedCounter := types.CropCounter{
+		StateChanges:  10,
+		Stateinc:      5,
+		Statedec:      5,
+		StateInfo:     0,
+		StateMinor:    5,
+		StateMajor:    5,
+		StateCritical: 0,
+	}
+	if diff := pretty.Compare(steps[1].StateCounter, expectedCounter); diff != "" {
+		t.Fatalf("unexpected counter %s", diff)
+	}
+
+}
+
+func TestAlarmSteps_Crop_GivenCounterStep_ShouldSaveInDB(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	Convey("Setup", t, func() {
-		s, err := mongo.NewClient(ctx, 0, 0, zerolog.Nop())
-		So(err, ShouldBeNil)
+	dbClient, err := mongo.NewClient(ctx, 0, 0, zerolog.Nop())
+	if err != nil {
+		t.Fatalf("expected no error but got %v", err)
+	}
 
-		c := s.Collection(mongo.AlarmMongoCollection)
-		_, err = c.DeleteMany(ctx, bson.M{})
-		So(err, ShouldBeNil)
-		steps := make(types.AlarmSteps, 0)
-		s1 := types.AlarmStep{
-			Value:     1,
-			Message:   "coucou",
-			Author:    "coucou",
-			Timestamp: types.CpsTime{Time: time.Now()},
+	dbCollection := dbClient.Collection(mongo.AlarmMongoCollection)
+	statusStep := types.AlarmStep{
+		Type:      types.AlarmStepStatusIncrease,
+		Value:     1,
+		Message:   "coucou",
+		Author:    "coucou",
+		Timestamp: types.NewCpsTime(time.Now().Unix()),
+	}
+	stateCounter := types.CropCounter{}
+	stateCounter.Stateinc = 1
+	stateCounter.Statedec = 2
+	counterStep := types.AlarmStep{
+		Type:         types.AlarmStepStateCounter,
+		StateCounter: stateCounter,
+		Timestamp:    types.NewCpsTime(time.Now().Unix()),
+	}
+
+	steps := make(types.AlarmSteps, 0)
+	err = steps.Add(statusStep)
+	if err != nil {
+		t.Fatalf("expected no error but got %v", err)
+	}
+	err = steps.Add(counterStep)
+	if err != nil {
+		t.Fatalf("expected no error but got %v", err)
+	}
+
+	alarm := types.Alarm{
+		ID: utils.NewID(),
+		Value: types.AlarmValue{
+			Steps: steps,
+		},
+	}
+	_, err = dbCollection.InsertOne(ctx, alarm)
+	if err != nil {
+		t.Fatalf("expected no error but got %v", err)
+	}
+
+	defer func() {
+		_, err = dbCollection.DeleteOne(ctx, bson.M{"_id": alarm.ID})
+		if err != nil {
+			t.Fatalf("expected no error but got %v", err)
 		}
-		stateCounter := types.CropCounter{}
-		stateCounter.Stateinc = 1
-		stateCounter.Statedec = 2
+	}()
 
-		s2 := types.AlarmStep{
-			Type:         types.AlarmStepStateCounter,
-			StateCounter: stateCounter,
-		}
+	fetchedAlarm := types.Alarm{}
+	err = dbCollection.FindOne(ctx, bson.M{"_id": alarm.ID}).Decode(&fetchedAlarm)
+	if err != nil {
+		t.Fatalf("expected no error but got %v", err)
+	}
 
-		So(steps.Add(s1), ShouldBeNil)
-		So(steps.Add(s2), ShouldBeNil)
-
-		al := types.Alarm{
-			ID: "cropalarm",
-			Value: types.AlarmValue{
-				Steps: steps,
-			},
-		}
-
-		Convey("Inserting the alarm is not a problem", func() {
-			_, err := c.InsertOne(ctx, al)
-			So(err, ShouldBeNil)
-
-			Convey("Getting the alarm back isn't a problem either", func() {
-				var al types.Alarm
-				So(c.FindOne(ctx, bson.M{"_id": "cropalarm"}).Decode(&al), ShouldBeNil)
-
-				So(len(al.Value.Steps), ShouldEqual, 2)
-				So(al.Value.Steps[0].Value, ShouldEqual, 1)
-				So(al.Value.Steps[0].StateCounter, ShouldResemble, types.CropCounter{})
-				So(al.Value.Steps[1].Value, ShouldEqual, 0)
-				So(al.Value.Steps[1].Type, ShouldEqual, types.AlarmStepStateCounter)
-				So(al.Value.Steps[1].StateCounter, ShouldNotBeNil)
-
-				sc := al.Value.Steps[1].StateCounter
-
-				So(sc.Statedec, ShouldEqual, 2)
-				So(sc.Stateinc, ShouldEqual, 1)
-				So(sc.StateInfo, ShouldEqual, 0)
-				So(sc.StateMinor, ShouldEqual, 0)
-				So(sc.StateMajor, ShouldEqual, 0)
-				So(sc.StateCritical, ShouldEqual, 0)
-			})
-		})
-	})
+	if diff := pretty.Compare(alarm.Value.Steps, fetchedAlarm.Value.Steps); diff != "" {
+		t.Errorf("unexpected steps %s", diff)
+	}
 }
