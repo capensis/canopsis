3c72431ebc7392359354bddde204569807fae88a
Vladimir Erokhov <vladimir.erokhov@noveogroup.com>
2023-07-06 17:14:46 +0600

    feature(#4554): Added small fixes for integration backend side

--- community/sources/webcore/src/canopsis-next/src/store/modules/entities/alarm-tag/index.js
+++ community/sources/webcore/src/canopsis-next/src/store/modules/entities/alarm-tag/index.js
@@ -1,10 +1,19 @@
 import { API_ROUTES } from '@/config';
 import { ENTITIES_TYPES } from '@/constants';
 
+import request from '@/services/request';
+
 import { createEntityModule } from '@/store/plugins/entities';
 
 export default createEntityModule({
-  route: API_ROUTES.alarmTags,
+  route: API_ROUTES.alarmTag.list,
   entityType: ENTITIES_TYPES.alarmTag,
   dataPreparer: d => d.data,
+  withMeta: true,
+}, {
+  actions: {
+    bulkRemove(context, { data }) {
+      return request.delete(API_ROUTES.alarmTag.bulkList, { data });
+    },
+  },
 });
