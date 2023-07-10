499d0b802239ad2b1b768b673f317f5a91a504df
Vladimir Erokhov <vladimir.erokhov@noveogroup.com>
2023-07-10 16:41:06 +0600

    fix(#4873): Fixed bug with splitting

--- community/sources/webcore/src/canopsis-next/src/components/sidebars/statistics/form/fields/statistics-columns.vue
+++ community/sources/webcore/src/canopsis-next/src/components/sidebars/statistics/form/fields/statistics-columns.vue
@@ -7,39 +7,18 @@
     c-progress-overlay(:pending="pending", transition)
     c-movable-card-iterator-field(v-field="columns", addable, @add="add")
       template(#item="{ item, index }")
-        v-layout(column)
-          kpi-rating-metric-field(
-            v-field="columns[index].metric",
-            :metrics="availableMetrics",
-            :type="type",
-            :label="$tc('common.column')",
-            :name="`column-${item.key}.column`",
-            required
-          )
-          c-enabled-field(
-            v-field="columns[index].split",
-            :label="$t('settings.statisticsWidgetColumn.split')"
-          )
-          c-select-field(
-            v-if="columns[index].split",
-            v-field="columns[index].criteria",
-            :items="ratingSettings",
-            :label="$t('common.infos')",
-            :name="`column-${item.key}.infos`",
-            item-text="label",
-            item-value="id",
-            required
-          )
+        statistics-column(
+          v-field="columns[index]",
+          :type="type",
+          :name="item.key",
+          :rating-settings="ratingSettings"
+        )
 </template>
 
 <script>
 import { createNamespacedHelpers } from 'vuex';
 
-import {
-  KPI_RATING_SETTINGS_TYPES,
-  STATISTICS_WIDGETS_ENTITY_METRICS,
-  STATISTICS_WIDGETS_USER_METRICS,
-} from '@/constants';
+import { KPI_RATING_SETTINGS_TYPES } from '@/constants';
 
 import { statisticsWidgetColumnToForm } from '@/helpers/entities/widget/forms/statistics';
 
@@ -50,6 +29,8 @@ import WidgetSettingsItem from '@/components/sidebars/partials/widget-settings-i
 import CEnabledField from '@/components/forms/fields/c-enabled-field.vue';
 import KpiRatingMetricField from '@/components/other/kpi/charts/form/fields/kpi-rating-metric-field.vue';
 
+import StatisticsColumn from './statistics-column.vue';
+
 const { mapActions } = createNamespacedHelpers('ratingSettings');
 
 export default {
@@ -58,6 +39,7 @@ export default {
     WidgetSettingsItem,
     CEnabledField,
     KpiRatingMetricField,
+    StatisticsColumn,
   },
   mixins: [formArrayMixin, validationAttachRequiredMixin],
   model: {
@@ -84,31 +66,6 @@ export default {
       pending: false,
     };
   },
-  computed: {
-    availableMetrics() {
-      const metrics = this.type === KPI_RATING_SETTINGS_TYPES.entity
-        ? STATISTICS_WIDGETS_ENTITY_METRICS
-        : STATISTICS_WIDGETS_USER_METRICS;
-
-      return metrics.map((value) => {
-        const kpiKey = `kpi.statisticsWidgets.metrics.${value}`;
-        const alarmKey = `alarm.metrics.${value}`;
-
-        let text;
-
-        if (this.$te(kpiKey)) {
-          text = this.$t(kpiKey);
-        } else if (this.$te(alarmKey)) {
-          text = this.$t(alarmKey);
-        }
-
-        return {
-          value,
-          text: text ?? this.$t(`user.metrics.${value}`),
-        };
-      });
-    },
-  },
   watch: {
     columns() {
       this.validateRequiredRule();
@@ -133,7 +90,10 @@ export default {
 
       const { data: ratingSettings = [] } = await this.fetchRatingSettingsWithoutStore({
         params: {
-          type: this.type,
+          /**
+           * We don't have rating settings for KPI_RATING_SETTINGS_TYPES.user type with `main` === false
+           */
+          type: KPI_RATING_SETTINGS_TYPES.entity,
           main: false,
           paginate: false,
         },
