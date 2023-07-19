059490b1a481bd45d98bedf9b09a66b3b3df9ce5
Zarina Yakubova <Zarina.Yakubova@noveogroup.com>
2023-07-19 15:40:45 +0200

    fix(#4995): Fix migration

--- community/go-engines-community/database/migrations/release4.5/000_add_pbehavior_info_to_entity.js
+++ community/go-engines-community/database/migrations/release4.5/000_add_pbehavior_info_to_entity.js
@@ -1,19 +1,49 @@
-db.periodical_alarm.find({"v.pbehavior_info.id": {$nin: ["", null]}}).forEach(function (doc) {
-    var timestamp;
-    for (var i = doc.v.steps.length-1; i >= 0; i--) {
+db.periodical_alarm.aggregate([
+    {
+        $match: {
+            "v.pbehavior_info.id": {$nin: ["", null]},
+            "v.pbehavior_info.timestamp": null,
+        }
+    },
+    {
+        $lookup: {
+            from: "pbehavior",
+            localField: "v.pbehavior_info.id",
+            foreignField: "_id",
+            as: "pbehavior"
+        }
+    },
+    {
+        $unwind: {path: "$pbehavior", preserveNullAndEmptyArrays: true}
+    }
+]).forEach(function (doc) {
+    var timestamp = -1;
+    for (var i = doc.v.steps.length - 1; i >= 0; i--) {
         if (doc.v.steps[i]._t === "pbhenter") {
             timestamp = doc.v.steps[i].t;
             break;
         }
     }
 
-    var pbehaviorInfo = doc.v.pbehavior_info;
-    if (timestamp) {
-        db.periodical_alarm.updateOne({_id: doc._id}, {$set: {"v.pbehavior_info.timestamp": timestamp}});
+    if (timestamp <= 0) {
+        if (doc.pbehavior && doc.pbehavior.created > doc.t) {
+            timestamp = doc.pbehavior.created;
+        } else {
+            timestamp = doc.t;
+        }
+    }
 
-        pbehaviorInfo.timestamp = timestamp;
-    }
-    if (pbehaviorInfo.timestamp) {
-        db.default_entities.updateOne({_id: doc.d}, {$set: {"pbehavior_info": pbehaviorInfo}});
-    }
+    db.periodical_alarm.updateOne({_id: doc._id}, {
+        $set: {
+            "v.pbehavior_info.timestamp": timestamp,
+        }
+    });
+
+    var pbehaviorInfo = doc.v.pbehavior_info;
+    pbehaviorInfo.timestamp = timestamp;
+    db.default_entities.updateOne({_id: doc.d}, {
+        $set: {
+            "pbehavior_info": pbehaviorInfo,
+        }
+    });
 });
