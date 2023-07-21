27e0372b2e84a77e15843baf89dbd713dc569c28
ikalugin <ivan.kalugin@noveogroup.com>
2023-07-21 12:25:47 +0200

    fix(#5000): use composite ids for infos dictionaries

--- community/go-engines-community/database/migrations/20230721105023_create_infos_dictionary_indexes_up.js
+++ community/go-engines-community/database/migrations/20230721105023_create_infos_dictionary_indexes_up.js
@@ -1,2 +1,2 @@
-db.entity_infos_dictionary.createIndex({k: 1}, {name: "k_1"});
-db.dynamic_infos_dictionary.createIndex({k: 1}, {name: "k_1"});
+db.entity_infos_dictionary.createIndex({"_id.k": 1}, {name: "id_k_1"});
+db.dynamic_infos_dictionary.createIndex({"_id.k": 1}, {name: "id_k_1"});
