02dbb86ddd66d5d5c665b3c8d3b08debeae4f6c5
Zarina Yakubova <Zarina.Yakubova@noveogroup.com>
2023-07-27 09:21:30 +0200

    feature(#4473): Refactor filtered children in alarms API

--- community/go-engines-community/lib/api/alarm/models.go
+++ community/go-engines-community/lib/api/alarm/models.go
@@ -130,6 +130,8 @@ type SortRequest struct {
 
 type DetailsRequest struct {
 	ID                 string               `json:"_id" binding:"required"`
+	Search             string               `json:"search"`
+	SearchBy           []string             `json:"search_by"`
 	Opened             *bool                `json:"opened"`
 	WithInstructions   bool                 `json:"with_instructions"`
 	WithDeclareTickets bool                 `json:"with_declare_tickets"`
@@ -228,7 +230,6 @@ type Alarm struct {
 	OpenedChildren       *int64         `bson:"opened_children,omitempty" json:"opened_children,omitempty"`
 	ClosedChildren       *int64         `bson:"closed_children,omitempty" json:"closed_children,omitempty"`
 	ChildrenInstructions *bool          `bson:"children_instructions" json:"children_instructions,omitempty"`
-	FilteredChildrenIDs  []string       `bson:"filtered_children,omitempty" json:"filtered_children,omitempty"`
 	// Meta alarm child fields
 	Parents        *int64          `bson:"parents" json:"parents,omitempty"`
 	MetaAlarmRules []MetaAlarmRule `bson:"meta_alarm_rules" json:"meta_alarm_rules,omitempty"`
@@ -246,6 +247,9 @@ type Alarm struct {
 	ImpactState int64                `bson:"impact_state" json:"impact_state"`
 
 	AssignedDeclareTicketRules []AssignedDeclareTicketRule `bson:"-" json:"assigned_declare_ticket_rules,omitempty"`
+
+	// Only for details request
+	Filtered *bool `bson:"filtered" json:"filtered,omitempty"`
 }
 
 type MetaAlarmRule struct {
--- community/go-engines-community/lib/expression/parser/parser_test.go
+++ community/go-engines-community/lib/expression/parser/parser_test.go
@@ -1,175 +1,231 @@
 package parser
 
 import (
-	"encoding/json"
 	"fmt"
-	"reflect"
+	"strconv"
 	"testing"
 
+	"github.com/kylelemons/godebug/pretty"
 	"go.mongodb.org/mongo-driver/bson"
 )
 
 func TestParser_Parse(t *testing.T) {
 	p := NewParser()
-	for _, data := range getParseDataSets() {
-		output, err := p.Parse(data.Expression)
+	for i, data := range getParseDataSets() {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			output, err := p.Parse(data.Expression)
 
-		if err != nil {
-			if data.Err == nil || data.Err.Error() != err.Error() {
-				t.Errorf("%s: expected no error but got %s %+v", data.Expression, err, data.Err)
+			if err != nil {
+				if data.Err == nil || data.Err.Error() != err.Error() {
+					t.Errorf("%s: expected no error but got %s %+v", data.Expression, err, data.Err)
+				}
+				return
 			}
-			continue
-		}
 
-		if !reflect.DeepEqual(output.Query(), data.Expected) {
-			expectedMsg, _ := json.MarshalIndent(data.Expected, "", "  ")
-			outputMsg, _ := json.MarshalIndent(output.Query(), "", "  ")
-			t.Errorf("%s: expected\n%s\nbut got\n%s", data.Expression, expectedMsg, outputMsg)
-		}
+			if diff := pretty.Compare(output.Query(), data.ExpectedQuery); diff != "" {
+				t.Errorf("unexpected query: %s", diff)
+			}
+			if diff := pretty.Compare(output.ExprQuery(), data.ExpectedExprQuery); diff != "" {
+				t.Errorf("unexpected query: %s", diff)
+			}
+		})
 	}
 }
 
 type dateSet struct {
-	Expression string
-	Expected   interface{}
-	Err        error
+	Expression        string
+	ExpectedQuery     bson.M
+	ExpectedExprQuery bson.M
+	Err               error
 }
 
 func getParseDataSets() []dateSet {
 	return []dateSet{
 		{
-			Expression: "connector=\"test_connector\"",
-			Expected:   bson.M{"connector": bson.M{"$eq": "test_connector"}},
+			Expression:        "connector=\"test_connector\"",
+			ExpectedQuery:     bson.M{"connector": bson.M{"$eq": "test_connector"}},
+			ExpectedExprQuery: bson.M{"$eq": bson.A{"$connector", "test_connector"}},
 		},
 		{
-			Expression: "connector!=\"test_connector\"",
-			Expected:   bson.M{"connector": bson.M{"$ne": "test_connector"}},
+			Expression:        "connector!=\"test_connector\"",
+			ExpectedQuery:     bson.M{"connector": bson.M{"$ne": "test_connector"}},
+			ExpectedExprQuery: bson.M{"$ne": bson.A{"$connector", "test_connector"}},
 		},
 		{
-			Expression: "connector<=\"test_connector\"",
-			Expected:   bson.M{"connector": bson.M{"$lte": "test_connector"}},
+			Expression:        "connector<=\"test_connector\"",
+			ExpectedQuery:     bson.M{"connector": bson.M{"$lte": "test_connector"}},
+			ExpectedExprQuery: bson.M{"$lte": bson.A{"$connector", "test_connector"}},
 		},
 		{
-			Expression: "connector>=\"test_connector\"",
-			Expected:   bson.M{"connector": bson.M{"$gte": "test_connector"}},
+			Expression:        "connector>=\"test_connector\"",
+			ExpectedQuery:     bson.M{"connector": bson.M{"$gte": "test_connector"}},
+			ExpectedExprQuery: bson.M{"$gte": bson.A{"$connector", "test_connector"}},
 		},
 		{
-			Expression: "connector<\"test_connector\"",
-			Expected:   bson.M{"connector": bson.M{"$lt": "test_connector"}},
+			Expression:        "connector<\"test_connector\"",
+			ExpectedQuery:     bson.M{"connector": bson.M{"$lt": "test_connector"}},
+			ExpectedExprQuery: bson.M{"$lt": bson.A{"$connector", "test_connector"}},
 		},
 		{
-			Expression: "connector>\"test_connector\"",
-			Expected:   bson.M{"connector": bson.M{"$gt": "test_connector"}},
+			Expression:        "connector>\"test_connector\"",
+			ExpectedQuery:     bson.M{"connector": bson.M{"$gt": "test_connector"}},
+			ExpectedExprQuery: bson.M{"$gt": bson.A{"$connector", "test_connector"}},
 		},
 		{
-			Expression: "d=\"test_component/test_resource\"",
-			Expected:   bson.M{"d": bson.M{"$eq": "test_component/test_resource"}},
+			Expression:        "d=\"test_component/test_resource\"",
+			ExpectedQuery:     bson.M{"d": bson.M{"$eq": "test_component/test_resource"}},
+			ExpectedExprQuery: bson.M{"$eq": bson.A{"$d", "test_component/test_resource"}},
 		},
 		{
-			Expression: "connector LIKE \"test_connector\"",
-			Expected:   bson.M{"connector": bson.M{"$regex": "test_connector"}},
+			Expression:        "connector LIKE \"test_connector\"",
+			ExpectedQuery:     bson.M{"connector": bson.M{"$regex": "test_connector"}},
+			ExpectedExprQuery: bson.M{"$regexMatch": bson.M{"input": "$connector", "regex": "test_connector"}},
 		},
 		{
-			Expression: "name LIKE \"criticité\"",
-			Expected:   bson.M{"name": bson.M{"$regex": "criticité"}},
+			Expression:        "name LIKE \"criticité\"",
+			ExpectedQuery:     bson.M{"name": bson.M{"$regex": "criticité"}},
+			ExpectedExprQuery: bson.M{"$regexMatch": bson.M{"input": "$name", "regex": "criticité"}},
 		},
 		{
-			Expression: "name LIKE 10",
-			Expected:   bson.M{"name": bson.M{"$regex": "10"}},
+			Expression:        "name LIKE 10",
+			ExpectedQuery:     bson.M{"name": bson.M{"$regex": "10"}},
+			ExpectedExprQuery: bson.M{"$regexMatch": bson.M{"input": "$name", "regex": "10"}},
 		},
 		{
-			Expression: "connector NOT LIKE \"test_connector\"",
-			Expected:   bson.M{"connector": bson.M{"$not": bson.M{"$regex": "test_connector"}}},
+			Expression:        "connector NOT LIKE \"test_connector\"",
+			ExpectedQuery:     bson.M{"connector": bson.M{"$not": bson.M{"$regex": "test_connector"}}},
+			ExpectedExprQuery: bson.M{"$not": bson.M{"$regexMatch": bson.M{"input": "$connector", "regex": "test_connector"}}},
 		},
 		{
-			Expression: "connector NOT LIKE 10",
-			Expected:   bson.M{"connector": bson.M{"$not": bson.M{"$regex": "10"}}},
+			Expression:        "connector NOT LIKE 10",
+			ExpectedQuery:     bson.M{"connector": bson.M{"$not": bson.M{"$regex": "10"}}},
+			ExpectedExprQuery: bson.M{"$not": bson.M{"$regexMatch": bson.M{"input": "$connector", "regex": "10"}}},
 		},
 		{
-			Expression: "children CONTAINS \"test_connector\"",
-			Expected:   bson.M{"children": bson.M{"$in": []interface{}{"test_connector"}}},
+			Expression:        "children CONTAINS \"test_connector\"",
+			ExpectedQuery:     bson.M{"children": bson.M{"$in": []interface{}{"test_connector"}}},
+			ExpectedExprQuery: bson.M{"$in": bson.A{"$children", []interface{}{"test_connector"}}},
 		},
 		{
-			Expression: "children NOT CONTAINS \"test_connector\"",
-			Expected:   bson.M{"children": bson.M{"$nin": []interface{}{"test_connector"}}},
+			Expression:        "children NOT CONTAINS \"test_connector\"",
+			ExpectedQuery:     bson.M{"children": bson.M{"$nin": []interface{}{"test_connector"}}},
+			ExpectedExprQuery: bson.M{"$not": bson.M{"$in": bson.A{"$children", []interface{}{"test_connector"}}}},
 		},
 		{
-			Expression: "NOT connector=\"test_connector1\"",
-			Expected:   bson.M{"connector": bson.M{"$not": bson.M{"$eq": "test_connector1"}}},
+			Expression:        "NOT connector=\"test_connector1\"",
+			ExpectedQuery:     bson.M{"connector": bson.M{"$not": bson.M{"$eq": "test_connector1"}}},
+			ExpectedExprQuery: bson.M{"$not": bson.M{"$eq": bson.A{"$connector", "test_connector1"}}},
 		},
 		{
 			Expression: "connector=\"test_connector1\" AND connector=\"test_connector2\"",
-			Expected: bson.M{"$and": []bson.M{
+			ExpectedQuery: bson.M{"$and": []bson.M{
 				{"connector": bson.M{"$eq": "test_connector1"}},
 				{"connector": bson.M{"$eq": "test_connector2"}},
+			}},
+			ExpectedExprQuery: bson.M{"$and": []bson.M{
+				{"$eq": bson.A{"$connector", "test_connector1"}},
+				{"$eq": bson.A{"$connector", "test_connector2"}},
 			}},
 		},
 		{
 			Expression: "connector=\"test_connector1\" OR connector=\"test_connector2\"",
-			Expected: bson.M{"$or": []bson.M{
+			ExpectedQuery: bson.M{"$or": []bson.M{
 				{"connector": bson.M{"$eq": "test_connector1"}},
 				{"connector": bson.M{"$eq": "test_connector2"}},
+			}},
+			ExpectedExprQuery: bson.M{"$or": []bson.M{
+				{"$eq": bson.A{"$connector", "test_connector1"}},
+				{"$eq": bson.A{"$connector", "test_connector2"}},
 			}},
 		},
 		{
 			Expression: "connector=\"test_connector\" AND resource=\"test_resource1\" OR resource=\"test_resource2\"",
-			Expected: bson.M{"$or": []bson.M{
+			ExpectedQuery: bson.M{"$or": []bson.M{
 				{"$and": []bson.M{
 					{"connector": bson.M{"$eq": "test_connector"}},
 					{"resource": bson.M{"$eq": "test_resource1"}},
 				}},
 				{"resource": bson.M{"$eq": "test_resource2"}},
 			}},
+			ExpectedExprQuery: bson.M{"$or": []bson.M{
+				{"$and": []bson.M{
+					{"$eq": bson.A{"$connector", "test_connector"}},
+					{"$eq": bson.A{"$resource", "test_resource1"}},
+				}},
+				{"$eq": bson.A{"$resource", "test_resource2"}},
+			}},
 		},
 		{
 			Expression: "connector=\"test_connector\" OR resource=\"test_resource1\" AND resource=\"test_resource2\"",
-			Expected: bson.M{"$or": []bson.M{
+			ExpectedQuery: bson.M{"$or": []bson.M{
 				{"connector": bson.M{"$eq": "test_connector"}},
 				{"$and": []bson.M{
 					{"resource": bson.M{"$eq": "test_resource1"}},
 					{"resource": bson.M{"$eq": "test_resource2"}},
 				}},
 			}},
+			ExpectedExprQuery: bson.M{"$or": []bson.M{
+				{"$eq": bson.A{"$connector", "test_connector"}},
+				{"$and": []bson.M{
+					{"$eq": bson.A{"$resource", "test_resource1"}},
+					{"$eq": bson.A{"$resource", "test_resource2"}},
+				}},
+			}},
 		},
 		{
-			Expression: "v.connector=\"test_connector\"",
-			Expected:   bson.M{"v.connector": bson.M{"$eq": "test_connector"}},
+			Expression:        "v.connector=\"test_connector\"",
+			ExpectedQuery:     bson.M{"v.connector": bson.M{"$eq": "test_connector"}},
+			ExpectedExprQuery: bson.M{"$eq": bson.A{"$v.connector", "test_connector"}},
 		},
 		{
-			Expression: "v.state.val=0",
-			Expected:   bson.M{"v.state.val": bson.M{"$eq": 0}},
+			Expression:        "v.state.val=0",
+			ExpectedQuery:     bson.M{"v.state.val": bson.M{"$eq": 0}},
+			ExpectedExprQuery: bson.M{"$eq": bson.A{"$v.state.val", 0}},
 		},
 		{
-			Expression: "v.state.val=1",
-			Expected:   bson.M{"v.state.val": bson.M{"$eq": 1}},
+			Expression:        "v.state.val=1",
+			ExpectedQuery:     bson.M{"v.state.val": bson.M{"$eq": 1}},
+			ExpectedExprQuery: bson.M{"$eq": bson.A{"$v.state.val", 1}},
 		},
 		{
-			Expression: "v.state.val>1.5",
-			Expected:   bson.M{"v.state.val": bson.M{"$gt": 1.5}},
+			Expression:        "v.state.val>1.5",
+			ExpectedQuery:     bson.M{"v.state.val": bson.M{"$gt": 1.5}},
+			ExpectedExprQuery: bson.M{"$gt": bson.A{"$v.state.val", 1.5}},
 		},
 		{
-			Expression: "v.state.val=v.status.val",
-			Expected:   bson.M{"v.state.val": bson.M{"$eq": "v.status.val"}},
+			Expression:        "v.state.val=v.status.val",
+			ExpectedQuery:     bson.M{"v.state.val": bson.M{"$eq": "v.status.val"}},
+			ExpectedExprQuery: bson.M{"$eq": bson.A{"$v.state.val", "v.status.val"}},
 		},
 		{
-			Expression: "v.state.val=NULL",
-			Expected:   bson.M{"v.state.val": bson.M{"$eq": nil}},
+			Expression:        "v.state.val=NULL",
+			ExpectedQuery:     bson.M{"v.state.val": bson.M{"$eq": nil}},
+			ExpectedExprQuery: bson.M{"$eq": bson.A{"$v.state.val", nil}},
 		},
 		{
-			Expression: "v.meta=TRUE",
-			Expected:   bson.M{"v.meta": bson.M{"$eq": true}},
+			Expression:        "v.meta=TRUE",
+			ExpectedQuery:     bson.M{"v.meta": bson.M{"$eq": true}},
+			ExpectedExprQuery: bson.M{"$eq": bson.A{"$v.meta", true}},
 		},
 		{
-			Expression: "v.meta=FALSE",
-			Expected:   bson.M{"v.meta": bson.M{"$eq": false}},
+			Expression:        "v.meta=FALSE",
+			ExpectedQuery:     bson.M{"v.meta": bson.M{"$eq": false}},
+			ExpectedExprQuery: bson.M{"$eq": bson.A{"$v.meta", false}},
 		},
 		{
 			Expression: "NOT connector=\"test_connector1\" OR connector=\"test_connector2\" AND connector LIKE \"test_connector3\"",
-			Expected: bson.M{"$or": []bson.M{
+			ExpectedQuery: bson.M{"$or": []bson.M{
 				{"connector": bson.M{"$not": bson.M{"$eq": "test_connector1"}}},
 				{"$and": []bson.M{
 					{"connector": bson.M{"$eq": "test_connector2"}},
 					{"connector": bson.M{"$regex": "test_connector3"}},
+				}},
+			}},
+			ExpectedExprQuery: bson.M{"$or": []bson.M{
+				{"$not": bson.M{"$eq": bson.A{"$connector", "test_connector1"}}},
+				{"$and": []bson.M{
+					{"$eq": bson.A{"$connector", "test_connector2"}},
+					{"$regexMatch": bson.M{"input": "$connector", "regex": "test_connector3"}},
 				}},
 			}},
 		},
