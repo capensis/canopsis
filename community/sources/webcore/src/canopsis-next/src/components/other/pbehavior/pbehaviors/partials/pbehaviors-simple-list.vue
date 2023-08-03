94c51fac6c421fee9e10b9f81e5f0230d14042f5
Vladimir Erokhov <vladimir.erokhov@noveogroup.com>
2023-08-03 14:59:01 +0600

    fix(#5006): Fixed bug

--- community/sources/webcore/src/canopsis-next/src/components/other/pbehavior/pbehaviors/partials/pbehaviors-simple-list.vue
+++ community/sources/webcore/src/canopsis-next/src/components/other/pbehavior/pbehaviors/partials/pbehaviors-simple-list.vue
@@ -18,32 +18,32 @@
         left,
         @click="showPbehaviorsCalendarModal"
       )
-    v-data-table.ma-0(:items="pbehaviors", :headers="headers", :loading="pending", :dense="dense")
-      template(#items="{ item }")
-        td {{ item.name }}
-        td {{ item.author.display_name }}
-        td
-          c-enabled(:value="item.enabled")
-        td {{ item.tstart | timezone($system.timezone) }}
-        td {{ item.tstop | timezone($system.timezone) }}
-        td {{ item.type.name }}
-        td {{ item.reason.name }}
-        td
-          v-icon {{ item.rrule ? 'check' : 'clear' }}
-        td
-          v-icon(color="primary") {{ item.type.icon_name }}
-        td(v-if="withActiveStatus")
-          v-icon(:color="item.is_active_status ? 'primary' : 'error'") $vuetify.icons.settings_sync
-        td
-          v-layout(row)
-            c-action-btn(
-              v-if="updatable",
-              :disabled="!item.editable",
-              :tooltip="item.editable ? $t('common.edit') : $t('pbehavior.notEditable')",
-              type="edit",
-              @click="showEditPbehaviorModal(item)"
-            )
-            c-action-btn(v-if="removable", type="delete", @click="showDeletePbehaviorModal(item._id)")
+    c-advanced-data-table.ma-0(
+      :items="pbehaviors",
+      :headers="headers",
+      :loading="pending",
+      :dense="dense"
+    )
+      template(#enabled="{ item }")
+        c-enabled(:value="item.enabled")
+      template(#tstart="{ item }") {{ item.tstart | timezone($system.timezone) }}
+      template(#tstop="{ item }") {{ item.tstop | timezone($system.timezone) }}
+      template(#rrule="{ item }")
+        v-icon {{ item.rrule ? 'check' : 'clear' }}
+      template(#icon="{ item }")
+        v-icon(color="primary") {{ item.type.icon_name }}
+      template(#status="{ item }")
+        v-icon(:color="item.is_active_status ? 'primary' : 'error'") $vuetify.icons.settings_sync
+      template(#actions="{ item }")
+        v-layout(row)
+          c-action-btn(
+            v-if="updatable",
+            :disabled="!item.editable",
+            :tooltip="item.editable ? $t('common.edit') : $t('pbehavior.notEditable')",
+            type="edit",
+            @click="showEditPbehaviorModal(item)"
+          )
+          c-action-btn(v-if="removable", type="delete", @click="showDeletePbehaviorModal(item._id)")
 </template>
 
 <script>
@@ -106,18 +106,18 @@ export default {
         { text: this.$t('pbehavior.isEnabled'), value: 'enabled' },
         { text: this.$t('pbehavior.begins'), value: 'tstart' },
         { text: this.$t('pbehavior.ends'), value: 'tstop' },
-        { text: this.$t('common.type'), value: 'type.type' },
+        { text: this.$t('common.type'), value: 'type.name' },
         { text: this.$t('common.reason'), value: 'reason.name' },
         { text: this.$t('common.recurrence'), value: 'rrule' },
-        { text: this.$t('common.icon'), value: 'type.icon_name' },
+        { text: this.$t('common.icon'), value: 'icon' },
       ];
 
       if (this.withActiveStatus) {
-        headers.push({ text: this.$t('common.status'), value: 'is_active_status', sortable: false });
+        headers.push({ text: this.$t('common.status'), value: 'status', sortable: false });
       }
 
       if (this.updatable || this.removable) {
-        headers.push({ text: this.$t('common.actionsLabel'), value: 'actionsLabel', sortable: false });
+        headers.push({ text: this.$t('common.actionsLabel'), value: 'actions', sortable: false });
       }
 
       return headers;
