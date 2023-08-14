6a2c6a1c62af0a9c0f91211d2ebc78c9fab308ed
Dmitry <Dmitriy.Mikhaylishin@noveogroup.com>
2023-08-14 14:11:52 +0600

    feature(#5009): migrate some modules to crud creator

--- community/sources/webcore/src/canopsis-next/src/store/schemas/index.js
+++ community/sources/webcore/src/canopsis-next/src/store/schemas/index.js
@@ -40,89 +40,11 @@ export const userPreferenceSchema = new schema.Entity(ENTITIES_TYPES.userPrefere
   idAttribute: 'widget',
 });
 
-export const userSchema = new schema.Entity(ENTITIES_TYPES.user, {}, { idAttribute: '_id' });
-
-export const roleSchema = new schema.Entity(ENTITIES_TYPES.role, {}, { idAttribute: '_id' });
-
-export const eventFilterSchema = new schema.Entity(ENTITIES_TYPES.eventFilter, {}, { idAttribute: '_id' });
-
-export const metaAlarmRuleSchema = new schema.Entity(ENTITIES_TYPES.metaAlarmRule, {}, { idAttribute: '_id' });
-
 export const snmpRuleSchema = new schema.Entity(ENTITIES_TYPES.snmpRule, {}, { idAttribute: '_id' });
-
-export const dynamicInfoSchema = new schema.Entity(ENTITIES_TYPES.dynamicInfo, {}, { idAttribute: '_id' });
-
-export const broadcastMessageSchema = new schema.Entity(ENTITIES_TYPES.broadcastMessage, {}, { idAttribute: '_id' });
-
-export const playlistSchema = new schema.Entity(ENTITIES_TYPES.playlist, {}, { idAttribute: '_id' });
-
-export const pbehaviorTypesSchema = new schema.Entity(ENTITIES_TYPES.pbehaviorTypes, {}, { idAttribute: '_id' });
-
-export const pbehaviorReasonsSchema = new schema.Entity(ENTITIES_TYPES.pbehaviorReasons, {}, { idAttribute: '_id' });
-
-export const pbehaviorExceptionsSchema = new schema.Entity(
-  ENTITIES_TYPES.pbehaviorExceptions,
-  {},
-  { idAttribute: '_id' },
-);
-
-export const remediationInstructionSchema = new schema.Entity(
-  ENTITIES_TYPES.remediationInstruction,
-  {},
-  { idAttribute: '_id' },
-);
-
-export const remediationJobSchema = new schema.Entity(
-  ENTITIES_TYPES.remediationJob,
-  {},
-  { idAttribute: '_id' },
-);
-
-export const remediationConfigurationSchema = new schema.Entity(
-  ENTITIES_TYPES.remediationConfiguration,
-  {},
-  { idAttribute: '_id' },
-);
-
-export const remediationInstructionStatsSchema = new schema.Entity(
-  ENTITIES_TYPES.remediationInstructionStats,
-  {},
-  { idAttribute: '_id' },
-);
-
-export const scenarioSchema = new schema.Entity(ENTITIES_TYPES.scenario, {}, { idAttribute: '_id' });
-
-export const entityCategorySchema = new schema.Entity(ENTITIES_TYPES.entityCategory, {}, { idAttribute: '_id' });
 
 export const testSuiteSchema = new schema.Entity(ENTITIES_TYPES.testSuite, {}, { idAttribute: '_id' });
 
 export const testSuiteHistorySchema = new schema.Entity(ENTITIES_TYPES.testSuiteHistory, {}, { idAttribute: '_id' });
-
-export const stateSettingSchema = new schema.Entity(ENTITIES_TYPES.stateSetting, {}, { idAttribute: '_id' });
-
-export const idleRulesSchema = new schema.Entity(ENTITIES_TYPES.idleRules, {}, { idAttribute: '_id' });
-
-export const flappingRulesSchema = new schema.Entity(ENTITIES_TYPES.flappingRules, {}, { idAttribute: '_id' });
-
-export const resolveRulesSchema = new schema.Entity(ENTITIES_TYPES.resolveRules, {}, { idAttribute: '_id' });
-
-export const filterSchema = new schema.Entity(ENTITIES_TYPES.filter, {}, { idAttribute: '_id' });
-
-export const ratingSettingsSchema = new schema.Entity(ENTITIES_TYPES.ratingSettings, {}, { idAttribute: 'label' });
-
-export const patternSchema = new schema.Entity(ENTITIES_TYPES.pattern, {}, { idAttribute: '_id' });
-
-export const mapSchema = new schema.Entity(ENTITIES_TYPES.map, {}, { idAttribute: '_id' });
-
-export const alarmTagSchema = new schema.Entity(ENTITIES_TYPES.alarmTag, {}, { idAttribute: '_id' });
-
-export const shareTokenSchema = new schema.Entity(ENTITIES_TYPES.shareToken, {}, { idAttribute: '_id' });
-
-export const widgetTemplateSchema = new schema.Entity(ENTITIES_TYPES.widgetTemplate, {}, { idAttribute: '_id' });
-
-export const declareTicketRuleSchema = new schema.Entity(ENTITIES_TYPES.declareTicketRule, {}, { idAttribute: '_id' });
-
-export const linkRuleSchema = new schema.Entity(ENTITIES_TYPES.linkRule, {}, { idAttribute: '_id' });
 
 export default {
   [ENTITIES_TYPES.alarm]: alarmSchema,
@@ -132,36 +54,7 @@ export default {
   [ENTITIES_TYPES.weatherService]: weatherServiceSchema,
   [ENTITIES_TYPES.pbehavior]: pbehaviorSchema,
   [ENTITIES_TYPES.userPreference]: userPreferenceSchema,
-  [ENTITIES_TYPES.user]: userSchema,
-  [ENTITIES_TYPES.role]: roleSchema,
-  [ENTITIES_TYPES.eventFilter]: eventFilterSchema,
   [ENTITIES_TYPES.snmpRule]: snmpRuleSchema,
-  [ENTITIES_TYPES.dynamicInfo]: dynamicInfoSchema,
-  [ENTITIES_TYPES.broadcastMessage]: broadcastMessageSchema,
-  [ENTITIES_TYPES.playlist]: playlistSchema,
-  [ENTITIES_TYPES.metaAlarmRule]: metaAlarmRuleSchema,
-  [ENTITIES_TYPES.pbehaviorTypes]: pbehaviorTypesSchema,
-  [ENTITIES_TYPES.pbehaviorReasons]: pbehaviorReasonsSchema,
-  [ENTITIES_TYPES.pbehaviorExceptions]: pbehaviorExceptionsSchema,
-  [ENTITIES_TYPES.remediationInstruction]: remediationInstructionSchema,
-  [ENTITIES_TYPES.remediationJob]: remediationJobSchema,
-  [ENTITIES_TYPES.remediationConfiguration]: remediationConfigurationSchema,
-  [ENTITIES_TYPES.scenario]: scenarioSchema,
-  [ENTITIES_TYPES.entityCategory]: entityCategorySchema,
   [ENTITIES_TYPES.testSuite]: testSuiteSchema,
   [ENTITIES_TYPES.testSuiteHistory]: testSuiteHistorySchema,
-  [ENTITIES_TYPES.stateSetting]: stateSettingSchema,
-  [ENTITIES_TYPES.remediationInstructionStats]: remediationInstructionStatsSchema,
-  [ENTITIES_TYPES.idleRules]: idleRulesSchema,
-  [ENTITIES_TYPES.flappingRules]: flappingRulesSchema,
-  [ENTITIES_TYPES.resolveRules]: resolveRulesSchema,
-  [ENTITIES_TYPES.filter]: filterSchema,
-  [ENTITIES_TYPES.ratingSettings]: ratingSettingsSchema,
-  [ENTITIES_TYPES.pattern]: patternSchema,
-  [ENTITIES_TYPES.map]: mapSchema,
-  [ENTITIES_TYPES.alarmTag]: alarmTagSchema,
-  [ENTITIES_TYPES.shareToken]: shareTokenSchema,
-  [ENTITIES_TYPES.widgetTemplate]: widgetTemplateSchema,
-  [ENTITIES_TYPES.declareTicketRule]: declareTicketRuleSchema,
-  [ENTITIES_TYPES.linkRule]: linkRuleSchema,
 };
--- community/sources/webcore/src/canopsis-next/tests/unit/store/modules/entities/pattern.spec.js
+++ community/sources/webcore/src/canopsis-next/tests/unit/store/modules/entities/pattern.spec.js
@@ -3,9 +3,6 @@ import Faker from 'faker';
 import { testsEntityModule } from '@unit/utils/store';
 import { API_ROUTES } from '@/config';
 
-import { ENTITIES_TYPES } from '@/constants';
-
-import { patternSchema } from '@/store/schemas';
 import patternModule from '@/store/modules/entities/pattern';
 
 describe('Entities pattern module', () => {
@@ -18,8 +15,6 @@ describe('Entities pattern module', () => {
   const { axiosMockAdapter } = testsEntityModule({
     route: API_ROUTES.pattern.list,
     module: patternModule,
-    schema: patternSchema,
-    entityType: ENTITIES_TYPES.pattern,
     entities: patterns,
     entityIds: patternIds,
   });
@@ -42,7 +37,7 @@ describe('Entities pattern module', () => {
       status: 200,
     }));
     axiosMockAdapter
-      .onDelete(API_ROUTES.bulkPatterns, patterns)
+      .onDelete(API_ROUTES.pattern.bulkList, patterns)
       .reply(200, response);
 
     const result = await patternModule.actions.bulkRemove({}, { data: patterns });
