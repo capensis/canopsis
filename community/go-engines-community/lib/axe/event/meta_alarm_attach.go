2efe1c1cfc81f21f22b47601f30656ef28f6970b
ikalugin <ivan.kalugin@noveogroup.com>
2023-08-24 19:30:27 +0200

    feature(#4997): create meta_alarm_attach and meta_alarm_detach event processors

--- community/go-engines-community/lib/axe/event/meta_alarm_attach.go
+++ community/go-engines-community/lib/axe/event/meta_alarm_attach.go
@@ -10,7 +10,6 @@ import (
 
 	libalarm "git.canopsis.net/canopsis/canopsis-community/community/go-engines-community/lib/canopsis/alarm"
 	"git.canopsis.net/canopsis/canopsis-community/community/go-engines-community/lib/canopsis/encoding"
-	"git.canopsis.net/canopsis/canopsis-community/community/go-engines-community/lib/canopsis/engine"
 	"git.canopsis.net/canopsis/canopsis-community/community/go-engines-community/lib/canopsis/metrics"
 	"git.canopsis.net/canopsis/canopsis-community/community/go-engines-community/lib/canopsis/rpc"
 	"git.canopsis.net/canopsis/canopsis-community/community/go-engines-community/lib/canopsis/types"
@@ -19,18 +18,14 @@ import (
 
 func NewMetaAlarmAttachProcessor(
 	metaAlarmEventProcessor libalarm.MetaAlarmEventProcessor,
-	autoInstructionMatcher AutoInstructionMatcher,
 	metricsSender metrics.Sender,
-	remediationRpcClient engine.RPCClient,
 	encoder encoding.Encoder,
 	amqpPublisher libamqp.Publisher,
 	logger zerolog.Logger,
 ) Processor {
 	return &metaAlarmAttachProcessor{
 		metaAlarmEventProcessor: metaAlarmEventProcessor,
-		autoInstructionMatcher:  autoInstructionMatcher,
 		metricsSender:           metricsSender,
-		remediationRpcClient:    remediationRpcClient,
 		encoder:                 encoder,
 		amqpPublisher:           amqpPublisher,
 		logger:                  logger,
@@ -39,9 +34,7 @@ func NewMetaAlarmAttachProcessor(
 
 type metaAlarmAttachProcessor struct {
 	metaAlarmEventProcessor libalarm.MetaAlarmEventProcessor
-	autoInstructionMatcher  AutoInstructionMatcher
 	metricsSender           metrics.Sender
-	remediationRpcClient    engine.RPCClient
 	encoder                 encoding.Encoder
 	amqpPublisher           libamqp.Publisher
 	logger                  zerolog.Logger
@@ -71,31 +64,7 @@ func (p *metaAlarmAttachProcessor) Process(ctx context.Context, event rpc.AxeEve
 		}
 	}
 
-	go p.postProcess(context.Background(), event, result)
-
 	return result, nil
-}
-
-func (p *metaAlarmAttachProcessor) postProcess(
-	ctx context.Context,
-	event rpc.AxeEvent,
-	result Result,
-) {
-	p.metricsSender.SendEventMetrics(
-		result.Alarm,
-		*event.Entity,
-		result.AlarmChange,
-		event.Parameters.Timestamp.Time,
-		event.Parameters.Initiator,
-		event.Parameters.User,
-		event.Parameters.Instruction,
-		"",
-	)
-
-	err := sendRemediationEvent(ctx, event, result, p.remediationRpcClient, p.encoder)
-	if err != nil {
-		p.logger.Err(err).Msg("cannot send event to engine-remediation")
-	}
 }
 
 func (p *metaAlarmAttachProcessor) sendToFifo(ctx context.Context, event types.Event) error {
--- community/go-engines-community/lib/axe/event/meta_alarm_detach.go
+++ community/go-engines-community/lib/axe/event/meta_alarm_detach.go
@@ -3,43 +3,20 @@ package event
 import (
 	"context"
 
-	libamqp "git.canopsis.net/canopsis/canopsis-community/community/go-engines-community/lib/amqp"
 	libalarm "git.canopsis.net/canopsis/canopsis-community/community/go-engines-community/lib/canopsis/alarm"
-	"git.canopsis.net/canopsis/canopsis-community/community/go-engines-community/lib/canopsis/encoding"
-	"git.canopsis.net/canopsis/canopsis-community/community/go-engines-community/lib/canopsis/engine"
-	"git.canopsis.net/canopsis/canopsis-community/community/go-engines-community/lib/canopsis/metrics"
 	"git.canopsis.net/canopsis/canopsis-community/community/go-engines-community/lib/canopsis/rpc"
-	"github.com/rs/zerolog"
 )
 
 func NewMetaAlarmDetachProcessor(
 	metaAlarmEventProcessor libalarm.MetaAlarmEventProcessor,
-	autoInstructionMatcher AutoInstructionMatcher,
-	metricsSender metrics.Sender,
-	remediationRpcClient engine.RPCClient,
-	encoder encoding.Encoder,
-	amqpPublisher libamqp.Publisher,
-	logger zerolog.Logger,
 ) Processor {
 	return &metaAlarmDetachProcessor{
 		metaAlarmEventProcessor: metaAlarmEventProcessor,
-		autoInstructionMatcher:  autoInstructionMatcher,
-		metricsSender:           metricsSender,
-		remediationRpcClient:    remediationRpcClient,
-		encoder:                 encoder,
-		amqpPublisher:           amqpPublisher,
-		logger:                  logger,
 	}
 }
 
 type metaAlarmDetachProcessor struct {
 	metaAlarmEventProcessor libalarm.MetaAlarmEventProcessor
-	autoInstructionMatcher  AutoInstructionMatcher
-	metricsSender           metrics.Sender
-	remediationRpcClient    engine.RPCClient
-	encoder                 encoding.Encoder
-	amqpPublisher           libamqp.Publisher
-	logger                  zerolog.Logger
 }
 
 func (p *metaAlarmDetachProcessor) Process(ctx context.Context, event rpc.AxeEvent) (Result, error) {
