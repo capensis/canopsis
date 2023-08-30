602995dd67cebc27a1bcc0b3fb755a034cc28a78
ikalugin <ivan.kalugin@noveogroup.com>
2023-08-30 12:32:34 +0200

    feature(#4114): add models_maintenance permission

--- community/go-engines-community/database/migrations/20230816181505_api_maintenance_permission_up.js
+++ community/go-engines-community/database/migrations/20230816181505_api_maintenance_permission_up.js
@@ -6,3 +6,12 @@ if (!db.permission.findOne({_id: "api_maintenance"})) {
     });
     db.role.updateMany({_id: "admin"}, {$set: {"permissions.api_maintenance": 1}});
 }
+
+if (!db.permission.findOne({_id: "models_maintenance"})) {
+    db.permission.insertOne({
+        _id: "models_maintenance",
+        crecord_name: "models_maintenance",
+        description: "Maintenance"
+    });
+    db.role.updateMany({_id: "admin"}, {$set: {"permissions.models_maintenance": 1}});
+}
