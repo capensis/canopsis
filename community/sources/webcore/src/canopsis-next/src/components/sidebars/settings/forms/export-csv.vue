ccde94a5b4e9218fdb35a0499295671232489d97
Dmitry <Dmitriy.Mikhaylishin@noveogroup.com>
2023-09-05 16:57:03 +0600

    fix(#5039): fix categories filter

--- community/sources/webcore/src/canopsis-next/src/components/sidebars/settings/forms/export-csv.vue
+++ community/sources/webcore/src/canopsis-next/src/components/sidebars/settings/forms/export-csv.vue
@@ -1,30 +1,27 @@
 <template lang="pug">
-  v-list-group
-    template(#activator="")
-      v-list-tile {{ $t('settings.exportCsv.title') }}
-    v-container
-      v-select(
-        v-field="form.exportCsvSeparator",
-        :items="separators",
-        :label="$t('settings.exportCsv.fields.separator')"
+  widget-settings-item(:title="$t('settings.exportCsv.title')")
+    v-select(
+      v-field="form.exportCsvSeparator",
+      :items="separators",
+      :label="$t('settings.exportCsv.fields.separator')"
+    )
+    v-select(
+      v-if="datetimeFormat",
+      v-field="form.exportCsvDatetimeFormat",
+      :items="formats",
+      :label="$t('settings.exportCsv.fields.datetimeFormat')"
+    )
+    v-layout(column)
+      h4.subheading.my-4 {{ $t('settings.exportColumnNames') }}
+      c-columns-with-template-field(
+        v-field="form.widgetExportColumns",
+        :template="form.widgetExportColumnsTemplate",
+        :templates="templates",
+        :templates-pending="templatesPending",
+        :label="$t('settings.exportColumnNames')",
+        :type="type",
+        @update:template="updateTemplate"
       )
-      v-select(
-        v-if="datetimeFormat",
-        v-field="form.exportCsvDatetimeFormat",
-        :items="formats",
-        :label="$t('settings.exportCsv.fields.datetimeFormat')"
-      )
-      v-layout(column)
-        h4.subheading.my-4 {{ $t('settings.exportColumnNames') }}
-        c-columns-with-template-field(
-          v-field="form.widgetExportColumns",
-          :template="form.widgetExportColumnsTemplate",
-          :templates="templates",
-          :templates-pending="templatesPending",
-          :label="$t('settings.exportColumnNames')",
-          :type="type",
-          @update:template="updateTemplate"
-        )
 </template>
 
 <script>
@@ -36,9 +33,10 @@ import {
 import { formBaseMixin } from '@/mixins/form';
 
 import FieldColumns from '../fields/common/columns.vue';
+import WidgetSettingsItem from '../partials/widget-settings-item.vue';
 
 export default {
-  components: { FieldColumns },
+  components: { WidgetSettingsItem, FieldColumns },
   mixins: [formBaseMixin],
   model: {
     prop: 'form',
