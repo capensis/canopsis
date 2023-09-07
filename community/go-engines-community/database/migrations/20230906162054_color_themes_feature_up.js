ea7fea92e26c29040b42ef07320fd46ee75fd4e2
ikalugin <ivan.kalugin@noveogroup.com>
2023-09-07 17:52:37 +0200

    feature(#4511): add default color themes

---
+++ community/go-engines-community/database/migrations/20230906162054_color_themes_feature_up.js
@@ -0,0 +1,151 @@
+db.color_theme.createIndex({name: 1}, {name: "name_1", unique: true});
+
+if (!db.permission.findOne({_id: "api_color_theme"})) {
+    db.permission.insertOne({
+        _id: "api_color_theme",
+        name: "api_color_theme",
+        type: "CRUD",
+        description: "Api color themes"
+    });
+    db.role.updateMany({_id: "admin"}, {$set: {"permissions.api_color_theme": 15}});
+}
+
+if (!db.permission.findOne({_id: "models_color_theme"})) {
+    db.permission.insertOne({
+        _id: "models_color_theme",
+        name: "models_color_theme",
+        type: "CRUD",
+        description: "Models color themes"
+    });
+    db.role.updateMany({_id: "admin"}, {$set: {"permissions.models_color_theme": 15}});
+}
+
+var now = Math.ceil((new Date()).getTime() / 1000);
+
+if (!db.color_theme.findOne({_id: "canopsis"})) {
+    db.color_theme.insertOne({
+        _id: "canopsis",
+        name: "Canopsis",
+        colors: {
+            main: {
+                primary: '#2fab63',
+                secondary: '#2b3e4f',
+                accent: '#82b1ff',
+                error: '#ff5252',
+                info: '#2196f3',
+                success: '#4caf50',
+                warning: '#fb8c00',
+                background: '#ffffff',
+            },
+            state: {
+                ok: '#00a65a',
+                minor: '#fcdc00',
+                major: '#ff9900',
+                critical: '#f56954',
+            },
+            table: {
+                background: '#fff',
+                active_color: '#000',
+                row_color: '#fff',
+                hover_row_color: '#eee',
+            }
+        },
+        last_modified: now,
+    });
+}
+
+if (!db.color_theme.findOne({_id: "canopsis_dark"})) {
+    db.color_theme.insertOne({
+        _id: "canopsis_dark",
+        name: "Canopsis dark",
+        colors: {
+            main: {
+                primary: '#2fab63',
+                secondary: '#2b3e4f',
+                accent: '#82b1ff',
+                error: '#ff8b8b',
+                info: '#2196f3',
+                success: '#4caf50',
+                warning: '#fb8c00',
+                background: '#303030',
+            },
+            state: {
+                ok: '#00a65a',
+                minor: '#fcdc00',
+                major: '#ff9900',
+                critical: '#f56954',
+            },
+            table: {
+                background: '#424242',
+                active_color: '#fff',
+                row_color: '#424242',
+                hover_row_color: '#616161',
+            }
+        },
+        last_modified: now,
+    });
+}
+
+if (!db.color_theme.findOne({_id: "color_blind"})) {
+    db.color_theme.insertOne({
+        _id: "color_blind",
+        name: "Color blind",
+        colors: {
+            main: {
+                primary: '#2196f3',
+                secondary: '#2b3e4f',
+                accent: '#82b1ff',
+                error: '#ff5252',
+                info: '#2196f3',
+                success: '#4caf50',
+                warning: '#fb8c00',
+                background: '#ffffff',
+            },
+            state: {
+                ok: '#00a65a',
+                minor: '#fcdc00',
+                major: '#ff9900',
+                critical: '#f56954',
+            },
+            table: {
+                background: '#fff',
+                active_color: '#000',
+                row_color: '#fff',
+                hover_row_color: '#eee',
+            }
+        },
+        last_modified: now,
+    });
+}
+
+if (!db.color_theme.findOne({_id: "color_blind_dark"})) {
+    db.color_theme.insertOne({
+        _id: "color_blind_dark",
+        name: "Color blind dark",
+        colors: {
+            main: {
+                primary: '#2196f3',
+                secondary: '#2b3e4f',
+                accent: '#82b1ff',
+                error: '#ff8b8b',
+                info: '#2196f3',
+                success: '#4caf50',
+                warning: '#fb8c00',
+                background: '#303030',
+            },
+            state: {
+                ok: '#00a65a',
+                minor: '#fcdc00',
+                major: '#ff9900',
+                critical: '#f56954',
+            },
+            table: {
+                background: '#424242',
+                active_color: '#fff',
+                row_color: '#424242',
+                hover_row_color: '#616161',
+            }
+        },
+        last_modified: now,
+    });
+}
