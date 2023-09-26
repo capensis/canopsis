0394dfc5d30964c171bb7cd163618e254358c85c
Vladimir Erokhov <vladimir.erokhov@noveogroup.com>
2023-09-26 14:52:57 +0600

    feature(#5036): Add small fixes after review

--- community/sources/webcore/src/canopsis-next/src/helpers/html.js
+++ community/sources/webcore/src/canopsis-next/src/helpers/html.js
@@ -52,3 +52,17 @@ export const sanitizeHtml = (html = '', options = DEFAULT_SANITIZE_OPTIONS) => s
  * @return {string}
  */
 export const linkifyHtml = (html = '', options = DEFAULT_LINKIFY_OPTIONS) => linkifyHtmlLib(html, options);
+
+/**
+ * Normilize HTML (close not closed tags and etc.)
+ *
+ * @param {string} [html = '']
+ * @return {string}
+ */
+export const normalizeHtml = (html = '') => {
+  const element = document.createElement('div');
+
+  element.innerHTML = html;
+
+  return element.innerHTML;
+};
