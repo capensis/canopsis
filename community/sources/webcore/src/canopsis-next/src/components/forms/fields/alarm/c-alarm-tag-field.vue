5289612061e3f4d7d8796cdbd20896c63b825175
Dmitry <Dmitriy.Mikhaylishin@noveogroup.com>
2023-10-04 20:00:59 +0600

    fix(#5081): fix tag width

--- community/sources/webcore/src/canopsis-next/src/components/forms/fields/alarm/c-alarm-tag-field.vue
+++ community/sources/webcore/src/canopsis-next/src/components/forms/fields/alarm/c-alarm-tag-field.vue
@@ -1,11 +1,12 @@
 <template lang="pug">
-  v-select(
+  v-select.c-alarm-tag-field(
     v-field="value",
     :items="alarmTags",
     :label="label || $tc('common.tag')",
     :loading="alarmTagsPending",
     :disabled="disabled",
     :name="name",
+    :menu-props="{ contentClass: 'c-alarm-tag-field__list' }",
     item-text="value",
     item-value="value",
     hide-details,
@@ -16,7 +17,18 @@
     clearable
   )
     template(#selection="{ item, index }")
-      c-alarm-action-chip(:color="item.color", closable, @close="removeItemFromArray(index)") {{ item.value }}
+      c-alarm-action-chip.c-alarm-tag-field__tag(
+        :color="item.color",
+        :title="item.value",
+        closable,
+        ellipsis,
+        @close="removeItemFromArray(index)"
+      ) {{ item.value }}
+    template(#item="{ item, tile, parent }")
+      v-list-tile.c-alarm-tag-field__list-item(v-bind="tile.props", v-on="tile.on")
+        v-list-tile-action
+          v-checkbox(:input-value="tile.props.value", :color="parent.color")
+        v-list-tile-content.c-word-break-all {{ item.value }}
 </template>
 
 <script>
@@ -50,3 +62,30 @@ export default {
   },
 };
 </script>
+
+<style lang="scss">
+$selectIconsWidth: 56px;
+
+.c-alarm-tag-field {
+  .v-select__selections {
+    width: calc(100% - #{$selectIconsWidth});
+  }
+
+  &__tag {
+    max-width: 100%;
+  }
+
+  &__list {
+    max-width: 400px;
+  }
+
+  &__list-item .v-list__tile__action {
+    height: 40px !important;
+    width: 40px !important;
+  }
+
+  &__list-item .v-list__tile {
+    height: unset !important;
+  }
+}
+</style>
