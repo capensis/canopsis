eeab382083695907f2cf044af768ae2da75a10b9
Vladimir Erokhov <vladimir.erokhov@noveogroup.com>
2023-10-06 16:59:02 +0600

    feature(#4400): Finished frontend part

--- community/sources/webcore/src/canopsis-next/src/views/home.vue
+++ community/sources/webcore/src/canopsis-next/src/views/home.vue
@@ -6,6 +6,8 @@
 <script>
 import { ROUTES_NAMES } from '@/constants';
 
+import { getFirstRoleWithDefaultView } from '@/helpers/entities/user/entity';
+
 import { authMixin } from '@/mixins/auth';
 import { entitiesRoleMixin } from '@/mixins/entities/role';
 
@@ -37,7 +39,7 @@ export default {
     },
 
     async redirectToRoleDefaultView() {
-      const { defaultview: roleDefaultView } = this.currentUser.roles.find(role => !!role.defaultview);
+      const { defaultview: roleDefaultView } = getFirstRoleWithDefaultView(this.currentUser);
 
       if (!roleDefaultView) {
         this.addRedirectInfoPopup(this.$t('home.popups.info.notSelectedRoleDefaultView'));
