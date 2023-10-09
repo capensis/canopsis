f4b49d51cddd309a85e2eaa962f3c3a17e5c0679
Vladimir Erokhov <vladimir.erokhov@noveogroup.com>
2023-10-09 14:25:25 +0600

    feature(#5096): Finished issue and fixed some bugs on snmp rules

--- community/sources/webcore/src/canopsis-next/src/constants/snmp.js
+++ community/sources/webcore/src/canopsis-next/src/constants/snmp.js
@@ -1,5 +1,3 @@
-import { COLORS } from '@/config';
-
 export const SNMP_STATE_TYPES = {
   simple: 'simple',
   template: 'template',
@@ -10,11 +8,4 @@ export const SNMP_TEMPLATE_STATE_STATES = {
   minor: 'minor',
   major: 'major',
   critical: 'critical',
-};
-
-export const SNMP_TEMPLATE_STATE_STATES_COLORS = {
-  [SNMP_TEMPLATE_STATE_STATES.info]: COLORS.state.ok,
-  [SNMP_TEMPLATE_STATE_STATES.minor]: COLORS.state.minor,
-  [SNMP_TEMPLATE_STATE_STATES.major]: COLORS.state.major,
-  [SNMP_TEMPLATE_STATE_STATES.critical]: COLORS.state.critical,
 };
