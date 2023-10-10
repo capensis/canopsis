09122fe372cc45d32ed2e026dc6a98fbbf4b33c0
ikalugin <ivan.kalugin@noveogroup.com>
2023-10-10 12:53:49 +0200

    feature(#4734): add models_privateView permission

--- community/go-engines-community/database/migrations/20231006101716_private_view_flags_and_permissions_down.js
+++ community/go-engines-community/database/migrations/20231006101716_private_view_flags_and_permissions_down.js
@@ -4,12 +4,13 @@ db.viewtabs.updateMany({}, {$unset: {"is_private": ""}})
 db.widgets.updateMany({}, {$unset: {"is_private": ""}})
 db.widget_filters.updateMany({}, {$rename: {"is_user_preference": "is_private"}})
 
-db.permission.deleteOne({_id: "api_private_view_groups"});
+db.permission.deleteMany({_id: {$in: ["api_private_view_groups", "models_privateView"]}});
 db.role.updateMany(
     {},
     {
         $unset: {
-            "permissions.api_private_view_groups": ""
+            "permissions.api_private_view_groups": "",
+            "permissions.models_privateView": ""
         }
     },
 );
