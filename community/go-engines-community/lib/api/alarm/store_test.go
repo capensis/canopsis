4da561af6fc022fb433dc95d911d35c7215e95ab
Zarina Yakubova <Zarina.Yakubova@noveogroup.com>
2023-10-12 16:13:15 +0200

    fix(#5105): Add mongo client timeout for API export

--- community/go-engines-community/lib/api/alarm/store_test.go
+++ community/go-engines-community/lib/api/alarm/store_test.go
@@ -85,7 +85,7 @@ func benchmarkStoreFind(b *testing.B, fixturesPath string, request alarm.ListReq
 	})
 	userId := "test"
 	authorProvider := author.NewProvider(dbClient, config.NewApiConfigProvider(config.CanopsisConf{}, zerolog.Nop()))
-	s := alarm.NewStore(dbClient, nil, config.NewTimezoneConfigProvider(config.CanopsisConf{}, zerolog.Nop()),
+	s := alarm.NewStore(dbClient, dbClient, nil, config.NewTimezoneConfigProvider(config.CanopsisConf{}, zerolog.Nop()),
 		authorProvider, json.NewDecoder(), zerolog.Nop())
 
 	b.ResetTimer()
