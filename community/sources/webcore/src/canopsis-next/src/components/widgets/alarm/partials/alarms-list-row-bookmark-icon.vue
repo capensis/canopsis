66ab9f6a96ddd27da399df58b5e74ff1e61ef5eb
Vladimir Erokhov <vladimir.erokhov@noveogroup.com>
2023-10-12 17:04:33 +0600

    feature(#4736): Finished issue

---
+++ community/sources/webcore/src/canopsis-next/src/components/widgets/alarm/partials/alarms-list-row-bookmark-icon.vue
@@ -0,0 +1,10 @@
+<template lang="pug">
+  v-tooltip(top)
+    template(#activator="{ on }")
+      v-icon.cursor-default(v-on="on", size="22") bookmark
+    span {{ $t('alarm.hasBookmark') }}
+</template>
+
+<script>
+export default {};
+</script>
