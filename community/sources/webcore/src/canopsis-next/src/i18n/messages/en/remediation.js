dfc8bd1573216e27a5e25e7e5f43b98829bb4aa9
Dmitry <Dmitriy.Mikhaylishin@noveogroup.com>
2023-10-25 10:28:06 +0600

    fix(#4744): fix charts label

--- community/sources/webcore/src/canopsis-next/src/i18n/messages/en/remediation.js
+++ community/sources/webcore/src/canopsis-next/src/i18n/messages/en/remediation.js
@@ -170,7 +170,7 @@ export default {
     autoInstructions: 'Automatic instructions',
     labels: {
       remediated: 'Remediated',
-      notRemediated: 'Not remediated',
+      withAssignedRemediations: 'With assigned instructions',
     },
     tooltips: {
       remediated: '{value} alarms remediated',
