316662c2f093b17f1f95879f9cdb2f4264b43850
Vladimir Erokhov <vladimir.erokhov@noveogroup.com>
2023-10-27 11:45:50 +0600

    feature(#5130): Started doing the form

---
+++ community/sources/webcore/src/canopsis-next/src/components/other/state-setting/form/fields/state-setting-compute-method-field.vue
@@ -0,0 +1,44 @@
+<template lang="pug">
+  v-layout(column)
+    span.subheading.font-weight-bold {{ $t('stateSetting.computeMethod') }}
+    v-radio-group(
+      v-field="value",
+      hide-details,
+      mandatory
+    )
+      v-radio(
+        v-for="method in methods",
+        :key="method",
+        :value="method",
+        color="primary"
+      )
+        template(#label)
+          span.mr-2 {{ $t(`stateSetting.computeMethods.${method}.label`) }}
+          c-help-icon(
+            :text="$t(`stateSetting.computeMethods.${method}.tooltip`)",
+            top
+          )
+</template>
+
+<script>
+import { STATE_SETTING_COMPUTE_METHODS } from '@/constants';
+
+export default {
+  inject: ['$validator'],
+  model: {
+    prop: 'value',
+    event: 'input',
+  },
+  props: {
+    value: {
+      type: String,
+      default: STATE_SETTING_COMPUTE_METHODS.inherit,
+    },
+  },
+  computed: {
+    methods() {
+      return STATE_SETTING_COMPUTE_METHODS;
+    },
+  },
+};
+</script>
--- community/sources/webcore/src/canopsis-next/src/i18n/messages/en/state-setting.js
+++ community/sources/webcore/src/canopsis-next/src/i18n/messages/en/state-setting.js
@@ -1,15 +1,38 @@
-import { STATE_SETTING_METHODS } from '@/constants';
+import {
+  STATE_SETTING_COMPUTE_METHODS,
+  STATE_SETTING_CALCULATION_METHODS,
+  STATE_SETTING_CONDITIONS,
+} from '@/constants';
 
 export default {
   worstLabel: 'The worst of:',
   worstHelpText: 'Canopsis counts the state for each criterion defined. The final state of JUnit test suite is taken as a worst of resulting states.',
   criterion: 'Criterion',
   serviceState: 'Service state',
-  methods: {
-    [STATE_SETTING_METHODS.worst]: 'Worst',
-    [STATE_SETTING_METHODS.worstOfShare]: 'Worst of share',
+  targetEntityState: 'Target entity state',
+  computeMethod: 'State compute method',
+  computeMethods: {
+    [STATE_SETTING_COMPUTE_METHODS.inherit]: {
+      label: 'State is inherited from impacting entities',
+      tooltip: 'State is defined by the one or several impacting resources.\n'
+          + 'When several resources are defined, the worst state of them is taken.',
+    },
+    [STATE_SETTING_COMPUTE_METHODS.shareOfDependencies]: {
+      label: 'State is defined by a share or number of impacting entities of a specific state',
+      tooltip: 'Entity states can be overridden with custom rule defined by number or share of impacting entities of specific states.',
+    },
   },
+  calculationMethods: {
+    [STATE_SETTING_CALCULATION_METHODS.share]: 'Share',
+    [STATE_SETTING_CALCULATION_METHODS.number]: 'Number',
+  },
+  conditions: {
+    [STATE_SETTING_CONDITIONS.greater]: 'Greater than',
+    [STATE_SETTING_CONDITIONS.less]: 'Less than',
+  },
+  entitiesStates: 'Impacting entities states',
   states: {
+    ok: 'ok',
     minor: 'Minor',
     major: 'Major',
     critical: 'Critical',
