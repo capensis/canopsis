b699af477e83955e01bd44d9cbaf140d3a77b456
Vladimir Erokhov <vladimir.erokhov@noveogroup.com>
2023-11-08 14:29:33 +0600

    feature(#5128): Some fixes after review

--- community/sources/webcore/src/canopsis-next/src/components/other/state-setting/form/steps/state-setting-basics-step.vue
+++ community/sources/webcore/src/canopsis-next/src/components/other/state-setting/form/steps/state-setting-basics-step.vue
@@ -1,22 +1,21 @@
 <template lang="pug">
-  div
-    v-layout(column)
-      v-layout.mb-2(row)
-        v-flex(xs7)
-          c-name-field.mr-2(
-            v-field="form.title",
-            :label="$t('common.title')",
-            name="title",
-            required
-          )
-        v-flex(xs3)
-          c-priority-field.mx-2(
-            v-field="form.priority",
-            required
-          )
-        v-flex(xs2)
-          c-enabled-field.ml-2(v-field="form.enabled")
-      state-setting-method-field(v-field="form.method")
+  v-layout(column)
+    v-layout.mb-2(row)
+      v-flex(xs7)
+        c-name-field.mr-2(
+          v-field="form.title",
+          :label="$t('common.title')",
+          name="title",
+          required
+        )
+      v-flex(xs3)
+        c-priority-field.mx-2(
+          v-field="form.priority",
+          required
+        )
+      v-flex(xs2)
+        c-enabled-field.ml-2(v-field="form.enabled")
+    state-setting-method-field(v-field="form.method")
 </template>
 
 <script>
