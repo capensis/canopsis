a4b83c7cc882ba4253ea88a356df0af122289666
Mikael Mourcia <mmourcia@capensis.fr>
2023-11-09 08:08:57 +0100

    une belle trad

--- community/sources/webcore/src/canopsis-next/src/i18n/messages/fr/user-interface.js
+++ community/sources/webcore/src/canopsis-next/src/i18n/messages/fr/user-interface.js
@@ -11,7 +11,7 @@ export default {
   showHeaderOnKioskMode: 'Afficher l\'en-tête en mode kiosque',
   maxMatchedItems: 'Seuil d\'éléments avant avertissement',
   checkCountRequestTimeout: 'Délai d\'expiration de la requête',
-  requiredInstructionApprove: 'Amélioration pédagogique requise',
+  requiredInstructionApprove: 'Approbation des consignes requise',
   tooltips: {
     maxMatchedItems: 'Avertit l\'utilisateur lorsque le nombre d\'éléments correspondant aux modèles est supérieur à cette valeur',
     checkCountRequestTimeout: 'Définit le délai d\'expiration (en secondes) de la demande d\'éléments correspondants',
