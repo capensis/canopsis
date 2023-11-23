554a8e83e8d8b23f3e80051b3bfbd9569e404cc9
Dmitry <Dmitriy.Mikhaylishin@noveogroup.com>
2023-11-23 13:58:01 +0600

    feature(#4628): fix some stubs and test

--- community/sources/webcore/src/canopsis-next/tests/unit/components/other/pbehavior/pbehaviors/calendar/pbehavior-planning-calendar.spec.js
+++ community/sources/webcore/src/canopsis-next/tests/unit/components/other/pbehavior/pbehaviors/calendar/pbehavior-planning-calendar.spec.js
@@ -11,9 +11,9 @@ const localVue = createVueInstance();
 const stubs = {
   'c-action-btn': true,
   'c-progress-overlay': true,
+  'c-calendar': true,
   'pbehavior-create-event': true,
   'pbehavior-planning-calendar-legend': true,
-  'calendar-app-period-picker': true,
 };
 
 describe('pbehavior-planning-calendar', () => {
