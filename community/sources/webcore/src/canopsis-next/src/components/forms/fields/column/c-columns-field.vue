3d87b35ce414bf72a36168b0252c50f64a7c4a8e
Dmitry <Dmitriy.Mikhaylishin@noveogroup.com>
2023-12-08 13:03:19 +0600

    fix(#5143): remove flags

--- community/sources/webcore/src/canopsis-next/src/components/forms/fields/column/c-columns-field.vue
+++ community/sources/webcore/src/canopsis-next/src/components/forms/fields/column/c-columns-field.vue
@@ -19,8 +19,6 @@
         :optional-infos-attributes="optionalInfosAttributes",
         :with-simple-template="withSimpleTemplate",
         :without-infos-attributes="withoutInfosAttributes",
-        :with-alarm-display-name="withAlarmDisplayName",
-        :with-alarm-creation-date="withAlarmCreationDate",
         :variables="variables",
         @remove="remove(index)"
       )
@@ -96,14 +94,6 @@ export default {
       required: false,
     },
     withoutInfosAttributes: {
-      type: Boolean,
-      default: false,
-    },
-    withAlarmDisplayName: {
-      type: Boolean,
-      default: false,
-    },
-    withAlarmCreationDate: {
       type: Boolean,
       default: false,
     },
