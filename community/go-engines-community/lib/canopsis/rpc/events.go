71fe4256841110929b41eaf64347fd674042e65c
Zarina Yakubova <Zarina.Yakubova@noveogroup.com>
2023-12-11 10:16:54 +0100

    feature(#5215): Add easyjson for RPC events

--- community/go-engines-community/lib/canopsis/rpc/events.go
+++ community/go-engines-community/lib/canopsis/rpc/events.go
@@ -1,5 +1,7 @@
 package rpc
 
+//go:generate easyjson -no_std_marshalers
+
 import (
 	"encoding/json"
 	"errors"
@@ -110,6 +112,9 @@ type PbehaviorResultEvent struct {
 	Error    *Error        `json:"error"`
 }
 
+// DynamicInfosEvent
+//
+//easyjson:json
 type DynamicInfosEvent struct {
 	// On update rule
 	ID     string `json:"_id"`
@@ -120,6 +125,9 @@ type DynamicInfosEvent struct {
 	AlarmChangeType types.AlarmChangeType `json:"alarm_change"`
 }
 
+// DynamicInfosResultEvent
+//
+//easyjson:json
 type DynamicInfosResultEvent struct {
 	Alarm           *types.Alarm          `json:"alarm"`
 	AlarmChangeType types.AlarmChangeType `json:"alarm_change"`
