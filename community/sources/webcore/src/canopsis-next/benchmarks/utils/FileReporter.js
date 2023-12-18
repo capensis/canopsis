f7e1dc3667f089b7cdcccb6cd5dbcdfe8d94f1b3
Dmitry <Dmitriy.Mikhaylishin@noveogroup.com>
2023-12-18 14:18:47 +0600

    feature(#5220): add expand panel metrics, add console and file reporters

---
+++ community/sources/webcore/src/canopsis-next/benchmarks/utils/FileReporter.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+
+const metricsFolderPath = path.resolve(process.cwd(), 'benchmarks', '__metrics__');
+
+class FileReporter {
+  constructor({ name = 'metrics' } = {}) {
+    this.name = name;
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  report(data) {
+    if (!fs.existsSync(metricsFolderPath)) {
+      fs.mkdirSync(metricsFolderPath);
+    }
+
+    fs.writeFileSync(`${metricsFolderPath}/${this.name}.json`, JSON.stringify(data));
+  }
+}
+
+module.exports = FileReporter;
---
+++ community/sources/webcore/src/canopsis-next/benchmarks/utils/Measure.js
@@ -0,0 +1,29 @@
+const { logInfo } = require('./logger');
+
+class Measure {
+  constructor(name, measureFunc) {
+    this.name = name;
+    this.measureFunc = measureFunc;
+  }
+
+  report = (data) => {
+    this.reportData = data;
+  };
+
+  getReportData() {
+    return this.reportData;
+  }
+
+  async run(options) {
+    const startTime = performance.now();
+    logInfo(`Start metric: ${this.name}`);
+
+    await this.measureFunc(this.report, options);
+
+    const time = performance.now() - startTime;
+
+    logInfo(`Finish metric: ${this.name} (${time.toFixed()} ms)`);
+  }
+}
+
+module.exports = Measure;
