dd5b9f69dc1da27d9b5415f1912b97671d93f397
Dmitry <Dmitriy.Mikhaylishin@noveogroup.com>
2023-12-22 13:10:12 +0600

    feature(#5220): add js doc

--- community/sources/webcore/src/canopsis-next/benchmarks/index.js
+++ community/sources/webcore/src/canopsis-next/benchmarks/index.js
@@ -9,6 +9,13 @@ require('yargs')
   .scriptName('benchmarks')
   .usage('$0 <cmd> [args]')
   .command(
+    /**
+     * Run measure metrics for compare metrics
+     *
+     * @example
+     *
+     * yarn benchmark run --url=https://localhost:8080 --viewId=view-id --tabId=view-tab-id --jsonName=release-23.10
+     */
     'run',
     'run measure metrics',
     (yargs) => {
@@ -33,6 +40,16 @@ require('yargs')
     runBenchmarks,
   )
   .command(
+    /**
+     * Command for compare metrics
+     *
+     * @example
+     * For compare all available metrics
+     * yarn benchmark compare
+     *
+     * For compare two metrics
+     * yarn benchmark compare --target=release-23.04 --source=release-23.10
+     */
     'compare',
     'compare metrics and render images from metrics and export pdf',
     (yargs) => {
