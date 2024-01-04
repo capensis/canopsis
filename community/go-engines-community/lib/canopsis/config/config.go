7932eec159bf948eb1612f1fc8cca835e2c7d32d
Zarina Yakubova <Zarina.Yakubova@noveogroup.com>
2024-01-04 12:47:14 +0100

    feature(#5153): Move message rate to TimescaleDB

--- community/go-engines-community/lib/canopsis/config/config.go
+++ community/go-engines-community/lib/canopsis/config/config.go
@@ -97,6 +97,7 @@ type ConsoleWriter struct {
 }
 
 type SectionMetrics struct {
+	Enabled                bool   `toml:"Enabled"`
 	FlushInterval          string `toml:"FlushInterval"`
 	SliInterval            string `toml:"SliInterval"`
 	UserSessionGapInterval string `toml:"UserSessionGapInterval"`
