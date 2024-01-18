f8347ae926c6b3436fe2186830d0477991705bdc
Dmitry <Dmitriy.Mikhaylishin@noveogroup.com>
2024-01-18 15:40:34 +0600

    feature(#5262): add tests for tree of deps field

---
+++ community/sources/webcore/src/canopsis-next/tests/unit/components/sidebars/form/tree-of-dependencies-settings.spec.js
@@ -0,0 +1,52 @@
+import { TREE_OF_DEPENDENCIES_SHOW_TYPES } from '@/constants';
+import { generateShallowRenderer, generateRenderer } from '@unit/utils/vue';
+import { randomArrayItem } from '@unit/utils/array';
+import { createNumberInputStub } from '@unit/stubs/input';
+
+import TreeOfDependenciesSettings from '@/components/sidebars/form/fields/tree-of-dependencies-settings.vue';
+
+const stubs = {
+  'widget-settings-item': true,
+  'v-radio-group': createNumberInputStub('v-radio-group'),
+};
+
+const snapshotStubs = {
+  'widget-settings-item': true,
+};
+
+const selectRadioGroup = wrapper => wrapper.find('.v-radio-group');
+
+describe('tree-of-dependencies-settings', () => {
+  const factory = generateShallowRenderer(TreeOfDependenciesSettings, { stubs });
+  const snapshotFactory = generateRenderer(TreeOfDependenciesSettings, { stubs: snapshotStubs });
+
+  test('Value changed after trigger radio group field', () => {
+    const wrapper = factory({
+      propsData: {
+        value: TREE_OF_DEPENDENCIES_SHOW_TYPES.custom,
+      },
+    });
+
+    const newValue = randomArrayItem(Object.values(TREE_OF_DEPENDENCIES_SHOW_TYPES));
+
+    selectRadioGroup(wrapper).setValue(newValue);
+
+    expect(wrapper).toEmit('input', newValue);
+  });
+
+  test('Renders `tree-of-dependencies-settings` with default props', () => {
+    const wrapper = snapshotFactory();
+
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  test('Renders `tree-of-dependencies-settings` with custom props', () => {
+    const wrapper = snapshotFactory({
+      propsData: {
+        value: TREE_OF_DEPENDENCIES_SHOW_TYPES.allDependencies,
+      },
+    });
+
+    expect(wrapper).toMatchSnapshot();
+  });
+});
