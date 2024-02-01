5fe387e3c643581a5a579e03459f604680c79b2b
Vladimir Erokhov <vladimir.erokhov@noveogroup.com>
2024-02-01 10:45:53 +0600

    fix(#5265): Added search and search_by parameters to details request

--- community/sources/webcore/src/canopsis-next/src/helpers/query.js
+++ community/sources/webcore/src/canopsis-next/src/helpers/query.js
@@ -1,4 +1,4 @@
-import { isUndefined, isEmpty, omit, isArray, uniq } from 'lodash';
+import { isUndefined, isEmpty, omit, isArray, uniq, map } from 'lodash';
 
 import { PAGINATION_LIMIT, DEFAULT_WEATHER_LIMIT } from '@/config';
 import {
@@ -528,15 +528,18 @@ const convertFiltersToQuery = (filter, lockedFilter) => [
  *
  * @param {Alarm} alarm
  * @param {Widget} widget
+ * @param {string} search
  * @returns {Object}
  */
-export const prepareAlarmDetailsQuery = (alarm, widget) => {
+export const prepareAlarmDetailsQuery = (alarm, widget, search) => {
   const { sort = {}, widgetGroupColumns = [] } = widget.parameters;
   const columns = widgetGroupColumns.length > 0
     ? widgetGroupColumns
     : DEFAULT_ALARMS_WIDGET_GROUP_COLUMNS;
 
   const query = {
+    search,
+    search_by: map(columns, 'value'),
     _id: alarm._id,
     with_instructions: true,
     with_declare_tickets: true,
