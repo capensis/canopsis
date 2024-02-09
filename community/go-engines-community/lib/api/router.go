8528dc6d2abe4f81baa2572ecd8a8343c2880d70
ikalugin <ivan.kalugin@noveogroup.com>
2024-02-09 17:24:35 +0100

    feature(#5270): support metaalarm children in declate ticket rule webhook templates + a new route to get alarm display names

--- community/go-engines-community/lib/api/router.go
+++ community/go-engines-community/lib/api/router.go
@@ -368,6 +368,11 @@ func RegisterRoutes(
 			middleware.Authorize(apisecurity.PermAlarmRead, model.PermissionCan, enforcer),
 			alarmAPI.Count,
 		)
+		protected.GET(
+			"/alarm-display-names",
+			middleware.Authorize(apisecurity.PermAlarmRead, model.PermissionCan, enforcer),
+			alarmAPI.GetDisplayNames,
+		)
 		exportExecutor.RegisterType("alarm", alarmStore.Export)
 		alarmExportRouter := protected.Group("/alarm-export")
 		{
