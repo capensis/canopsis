77ef988821c03fdd3da8235e72ecf0d7cf0d7f3a
Vladimir Erokhov <vladimir.erokhov@noveogroup.com>
2024-02-09 16:28:18 +0600

    fix(#5279): Added schemas checking for linkify

--- community/sources/webcore/src/canopsis-next/src/config.js
+++ community/sources/webcore/src/canopsis-next/src/config.js
@@ -1,11 +1,12 @@
 import { removeTrailingSlashes } from '@/helpers/url';
 
 export const {
-  BASE_URL,
-  VUE_APP_API_HOST,
-  VUE_APP_LOCAL_STORAGE_ACCESS_TOKEN_KEY,
-  VUE_APP_PAGINATION_LIMIT,
-  VUE_APP_OPEN_STREET_LAYER_URL,
+  BASE_URL = '',
+  VUE_APP_API_HOST = '',
+  VUE_APP_LOCAL_STORAGE_ACCESS_TOKEN_KEY = '',
+  VUE_APP_PAGINATION_LIMIT = '',
+  VUE_APP_OPEN_STREET_LAYER_URL = '',
+  VUE_APP_LINKIFY_SCHEMAS = '',
 } = process.env;
 
 export const APP_HOST = removeTrailingSlashes(`${window.location.origin}${BASE_URL}`);
@@ -31,6 +32,12 @@ export const PAGINATION_LIMIT = parseInt(VUE_APP_PAGINATION_LIMIT, 10);
 export const PAGINATION_PER_PAGE_VALUES = [5, 10, 20, 50, 100];
 
 export const PAGINATION_TOTAL_VISIBLE = 7;
+
+export const LINKIFY_SCHEMAS = [
+  ...VUE_APP_LINKIFY_SCHEMAS.split(',').map(schema => schema.trim()).filter(Boolean),
+
+  'ssh',
+];
 
 export const DEFAULT_MAX_MULTI_SORT_COLUMNS_COUNT = 3;
 
