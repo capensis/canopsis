1e1ca14335e7362dde89c82961bdb2328f687ae5
Zarina Yakubova <Zarina.Yakubova@noveogroup.com>
2024-02-21 14:30:16 +0100

    feature(#5294): Fix SLI unique index

--- community/go-engines-community/database/postgres_migrations/15_alter_sli_duration_entity_id_time_index.up.sql
+++ community/go-engines-community/database/postgres_migrations/15_alter_sli_duration_entity_id_time_index.up.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
 DROP INDEX IF EXISTS sli_duration_entity_id_time_idx;
-CREATE UNIQUE INDEX IF NOT EXISTS sli_duration_entity_id_time_idx ON sli_duration (entity_id, time DESC);
+CREATE UNIQUE INDEX IF NOT EXISTS sli_duration_entity_id_time_type_idx ON sli_duration (entity_id, time DESC, type);
 
 END;
