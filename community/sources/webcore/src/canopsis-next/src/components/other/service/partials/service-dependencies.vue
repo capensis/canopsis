847f18a45c6e958d58e069375d19b365809e3a74
Dmitry <Dmitriy.Mikhaylishin@noveogroup.com>
2024-02-21 11:56:31 +0600

    feature(#5259): fix service dependencies column data

--- community/sources/webcore/src/canopsis-next/src/components/other/service/partials/service-dependencies.vue
+++ community/sources/webcore/src/canopsis-next/src/components/other/service/partials/service-dependencies.vue
@@ -161,7 +161,6 @@ export default {
 
         ...this.columns.map(column => ({
           ...column,
-          value: `entity.${column.value}`,
           isState: column.value?.endsWith(ENTITY_FIELDS.state),
         })),
       ];
