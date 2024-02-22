6031461caf9db62c04d5fbbd7cc7ac8cbf8d722a
Zarina Yakubova <Zarina.Yakubova@noveogroup.com>
2024-02-22 10:15:46 +0100

    feature(#5280): Fix merge conflicts

--- community/go-engines-community/mocks/lib/canopsis/alarm/alarm.go
+++ community/go-engines-community/mocks/lib/canopsis/alarm/alarm.go
@@ -482,16 +482,16 @@ func (m *MockActivationService) EXPECT() *MockActivationServiceMockRecorder {
 // Process mocks base method.
 func (m *MockActivationService) Process(arg0 context.Context, arg1 types.Alarm, arg2 datetime.MicroTime, arg3 string, arg4 bool) (bool, error) {
 	m.ctrl.T.Helper()
-	ret := m.ctrl.Call(m, "Process", arg0, arg1, arg2, arg3)
+	ret := m.ctrl.Call(m, "Process", arg0, arg1, arg2, arg3, arg4)
 	ret0, _ := ret[0].(bool)
 	ret1, _ := ret[1].(error)
 	return ret0, ret1
 }
 
 // Process indicates an expected call of Process.
-func (mr *MockActivationServiceMockRecorder) Process(arg0, arg1, arg2, arg3 interface{}) *gomock.Call {
+func (mr *MockActivationServiceMockRecorder) Process(arg0, arg1, arg2, arg3, arg4 interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
-	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Process", reflect.TypeOf((*MockActivationService)(nil).Process), arg0, arg1, arg2, arg3)
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Process", reflect.TypeOf((*MockActivationService)(nil).Process), arg0, arg1, arg2, arg3, arg4)
 }
 
 // MockMetaAlarmEventProcessor is a mock of MetaAlarmEventProcessor interface.
