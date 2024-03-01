65d43ca71db4e2c3e223e6aee5f6b9532e676aa4
Zarina Yakubova <Zarina.Yakubova@noveogroup.com>
2024-03-01 16:32:11 +0100

    fix(#5331): Fix SLI periodical worker

--- community/go-engines-community/lib/axe/event/no_events.go
+++ community/go-engines-community/lib/axe/event/no_events.go
@@ -404,9 +404,14 @@ func (p *noEventsProcessor) postProcess(
 	result Result,
 	updatedServiceStates map[string]statecounters.UpdatedServicesInfo,
 ) {
+	entity := *event.Entity
+	if result.Entity.ID != "" {
+		entity = result.Entity
+	}
+
 	p.metricsSender.SendEventMetrics(
 		result.Alarm,
-		*event.Entity,
+		entity,
 		result.AlarmChange,
 		event.Parameters.Timestamp.Time,
 		event.Parameters.Initiator,
