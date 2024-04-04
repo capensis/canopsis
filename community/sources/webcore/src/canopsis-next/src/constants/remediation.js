2b6cf56c082a22b2e4cfa130f961ac7910d4d5d7
Vladimir Erokhov <vladimir.erokhov@noveogroup.com>
2024-04-04 14:08:13 +0500

    feature(#5304): Finished issue

--- community/sources/webcore/src/canopsis-next/src/constants/remediation.js
+++ community/sources/webcore/src/canopsis-next/src/constants/remediation.js
@@ -1,3 +1,5 @@
+import { COLORS } from '@/config';
+
 import { TRIGGERS_TYPES } from './common';
 
 export const REMEDIATION_TABS = {
@@ -32,6 +34,25 @@ export const REMEDIATION_INSTRUCTION_EXECUTION_STATUSES = {
   completed: 2,
   aborted: 3,
   failed: 4,
+  waitingResult: 5,
+};
+
+export const REMEDIATION_INSTRUCTION_EXECUTION_STATUSES_ICONS = {
+  [REMEDIATION_INSTRUCTION_EXECUTION_STATUSES.running]: 'play_circle_filled',
+  [REMEDIATION_INSTRUCTION_EXECUTION_STATUSES.paused]: 'pause',
+  [REMEDIATION_INSTRUCTION_EXECUTION_STATUSES.completed]: 'check_circle',
+  [REMEDIATION_INSTRUCTION_EXECUTION_STATUSES.aborted]: 'not_interested',
+  [REMEDIATION_INSTRUCTION_EXECUTION_STATUSES.failed]: 'cancel',
+  [REMEDIATION_INSTRUCTION_EXECUTION_STATUSES.waitingResult]: 'play_circle_filled',
+};
+
+export const REMEDIATION_INSTRUCTION_EXECUTION_STATUSES_COLORS = {
+  [REMEDIATION_INSTRUCTION_EXECUTION_STATUSES.running]: COLORS.remediation.executionStatus.running,
+  [REMEDIATION_INSTRUCTION_EXECUTION_STATUSES.paused]: COLORS.remediation.executionStatus.paused,
+  [REMEDIATION_INSTRUCTION_EXECUTION_STATUSES.completed]: COLORS.remediation.executionStatus.completed,
+  [REMEDIATION_INSTRUCTION_EXECUTION_STATUSES.aborted]: COLORS.remediation.executionStatus.aborted,
+  [REMEDIATION_INSTRUCTION_EXECUTION_STATUSES.failed]: COLORS.remediation.executionStatus.failed,
+  [REMEDIATION_INSTRUCTION_EXECUTION_STATUSES.waitingResult]: COLORS.remediation.executionStatus.running,
 };
 
 export const REMEDIATION_JOB_EXECUTION_STATUSES = {
