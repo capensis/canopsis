5438552a2b9323351e84eb675ed510a2f12c9716
Zarina Yakubova <Zarina.Yakubova@noveogroup.com>
2024-04-05 11:00:14 +0200

    feature(#5369): Fix meta alarm resolve

--- community/go-engines-community/lib/axe/meta_alarm_event_processor.go
+++ community/go-engines-community/lib/axe/meta_alarm_event_processor.go
@@ -517,7 +517,7 @@ func (p *metaAlarmEventProcessor) resolveParents(ctx context.Context, childAlarm
 						return nil
 					}
 
-					err = parentAlarm.Alarm.PartialUpdateResolve(types.NewCpsTime())
+					err = parentAlarm.Alarm.PartialUpdateResolve(timestamp)
 					if err != nil {
 						return fmt.Errorf("cannot update alarm: %w", err)
 					}
