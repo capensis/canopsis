8f05c181c29f358e2b5a09765a08c96ddbbd016a
Zarina Yakubova <Zarina.Yakubova@noveogroup.com>
2024-04-10 15:34:23 +0200

    feature(#5247): Fix code style

--- community/go-engines-community/lib/canopsis/entitycounters/calculator/interface.go
+++ community/go-engines-community/lib/canopsis/entitycounters/calculator/interface.go
@@ -9,7 +9,12 @@ import (
 
 type ComponentCountersCalculator interface {
 	// CalculateCounters calculates counters for the component by dependency alarm change and returns true if component state is changed as the first argument, and a new component state as the second one.
-	CalculateCounters(ctx context.Context, entity *types.Entity, alarm *types.Alarm, alarmChange types.AlarmChange) (bool, bool, int, error)
+	CalculateCounters(
+		ctx context.Context,
+		entity *types.Entity,
+		alarm *types.Alarm,
+		alarmChange types.AlarmChange,
+	) (isCountersUpdated bool, isStateUpdated bool, newState int, _ error)
 	// RecomputeCounters recomputes all counters for component's dependencies, return a new calculated component state.
 	RecomputeCounters(ctx context.Context, component *types.Entity) (int, error)
 	// RecomputeAll sends entityupdated events for all components with state settings, which should recompute counters.
@@ -18,7 +23,12 @@ type ComponentCountersCalculator interface {
 
 type EntityServiceCountersCalculator interface {
 	// CalculateCounters calculates counters for the entity service by dependency alarm change and returns a map with updated entity service ids and a new calculated state and output.
-	CalculateCounters(ctx context.Context, entity *types.Entity, alarm *types.Alarm, alarmChange types.AlarmChange) (bool, map[string]entitycounters.UpdatedServicesInfo, error)
+	CalculateCounters(
+		ctx context.Context,
+		entity *types.Entity,
+		alarm *types.Alarm,
+		alarmChange types.AlarmChange,
+	) (isAnyServiceCountersUpdated bool, updatedServicesInfos map[string]entitycounters.UpdatedServicesInfo, _ error)
 	// RecomputeCounters recomputes all counters for service's dependencies, returns a map with updated entity service id and a new calculated state and output.
 	RecomputeCounters(ctx context.Context, service *types.Entity) (map[string]entitycounters.UpdatedServicesInfo, error)
 	// RecomputeAll sends recomputeentityservice events for all entity services, which should recompute counters.
