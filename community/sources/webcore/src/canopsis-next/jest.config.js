876096e0c5b52bb621c389280b19e6cf245c50ad
Dmitry <Dmitriy.Mikhaylishin@noveogroup.com>
2024-04-11 10:31:15 +0500

    feature(#5328): change coverage reporter for local tests

--- community/sources/webcore/src/canopsis-next/jest.config.js
+++ community/sources/webcore/src/canopsis-next/jest.config.js
@@ -48,6 +48,7 @@ module.exports = {
   },
   snapshotSerializers: ['<rootDir>/tests/unit/jest.serializer-vue'],
   collectCoverageFrom: ['<rootDir>/src/**/*.{js,vue}'],
+  coverageProvider: 'v8',
   setupFiles: ['jest-localstorage-mock'],
   setupFilesAfterEnv: ['<rootDir>/tests/unit/jest.setup-test-framework'],
   globalSetup: '<rootDir>/tests/unit/jest.global-setup',
