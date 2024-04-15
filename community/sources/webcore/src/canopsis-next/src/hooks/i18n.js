a35b1d3605a2272805d0995e447fc0cbe3486572
Dmitry <Dmitriy.Mikhaylishin@noveogroup.com>
2024-04-15 15:18:27 +0500

    feature(#5328): fix JSDoc for useI18n

--- community/sources/webcore/src/canopsis-next/src/hooks/i18n.js
+++ community/sources/webcore/src/canopsis-next/src/hooks/i18n.js
@@ -5,9 +5,16 @@ import i18nInstance from '@/i18n';
 import { useComponentInstance } from '@/hooks/vue';
 
 /**
- * USe I18n hoo for composition API
+ * Provides a reactive i18n interface for handling internationalization within Vue components.
+ * This hook utilizes the `useComponentInstance` to access the current Vue instance and sets up
+ * a reactive `locale` property. It also binds the translation methods `$t`, `$tc`, and `$te` from
+ * the Vue instance to ensure they can be used reactively in the component.
  *
- * @return {Object}
+ * @returns {Object} An object containing:
+ * - `locale`: a reactive property for getting and setting the current locale.
+ * - `t`: a method bound to the current Vue instance for translating text.
+ * - `tc`: a method bound to the current Vue instance for translating text with pluralization.
+ * - `te`: a method bound to the current Vue instance for checking if a translation exists.
  */
 export const useI18n = () => {
   const instance = useComponentInstance();
