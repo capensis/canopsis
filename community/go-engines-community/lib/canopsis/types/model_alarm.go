d243a95fc5c3897974a3f90eaf9d832586a5e141
ikalugin <ivan.kalugin@noveogroup.com>
2024-04-17 16:39:27 +0200

    fix(#5398): fix alarm tags values on no events

--- community/go-engines-community/lib/canopsis/types/model_alarm.go
+++ community/go-engines-community/lib/canopsis/types/model_alarm.go
@@ -100,9 +100,9 @@ type Alarm struct {
 	Time     CpsTime `bson:"t" json:"t"`
 	EntityID string  `bson:"d" json:"d"`
 
-	Tags                []string  `bson:"tags" json:"tags"`
-	ExternalTags        []string  `bson:"etags" json:"etags"`
-	InternalTags        []string  `bson:"itags" json:"itags"`
+	Tags                []string  `bson:"tags,omitempty" json:"tags,omitempty"`
+	ExternalTags        []string  `bson:"etags,omitempty" json:"etags,omitempty"`
+	InternalTags        []string  `bson:"itags,omitempty" json:"itags,omitempty"`
 	InternalTagsUpdated MicroTime `bson:"itags_upd" json:"itags_upd"`
 	// todo move all field from Value to Alarm
 	Value AlarmValue `bson:"v" json:"v"`
