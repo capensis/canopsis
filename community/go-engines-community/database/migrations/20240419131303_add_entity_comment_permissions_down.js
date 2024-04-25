c837f2451d5f215435c18cfecfc0d51f35ee37d1
Alexander Pavlov <alexander.pavlov@noveogroup.com>
2024-04-25 11:19:50 +0200

    feature(#5087):add UI permissions

--- community/go-engines-community/database/migrations/20240419131303_add_entity_comment_permissions_down.js
+++ community/go-engines-community/database/migrations/20240419131303_add_entity_comment_permissions_down.js
@@ -1,2 +1,18 @@
-db.permission.deleteMany({_id: "api_entitycomment"});
-db.role.updateMany({}, {$unset: {"permissions.api_entitycomment": ""}});
+db.permission.deleteMany({
+    _id: {
+        $in: [
+            "api_entitycomment",
+            "common_entityCommentsList",
+            "common_createEntityComment",
+            "common_editEntityComment",
+        ]
+    }
+});
+db.role.updateMany({}, {
+    $unset: {
+        "permissions.api_entitycomment": "",
+        "permissions.common_entityCommentsList": "",
+        "permissions.common_createEntityComment": "",
+        "permissions.common_editEntityComment": "",
+    }
+});
