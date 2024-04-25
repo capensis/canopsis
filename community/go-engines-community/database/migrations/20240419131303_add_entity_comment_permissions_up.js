65568a0d7dd66893a01d64f213676aba2dacf384
Alexander Pavlov <alexander.pavlov@noveogroup.com>
2024-04-25 11:19:50 +0200

    feature(#5087):add UI permissions

--- community/go-engines-community/database/migrations/20240419131303_add_entity_comment_permissions_up.js
+++ community/go-engines-community/database/migrations/20240419131303_add_entity_comment_permissions_up.js
@@ -10,3 +10,42 @@ if (!db.permission.findOne({_id: "api_entitycomment"})) {
         }
     });
 }
+
+if (!db.permission.findOne({_id: "common_entityCommentsList"})) {
+    db.permission.insertOne({
+        _id: "common_entityCommentsList",
+        name: "common_entityCommentsList",
+        description: "Entity comments list"
+    });
+    db.role.updateOne({name: "admin"}, {
+        $set: {
+            "permissions.common_entityCommentsList": 1
+        }
+    });
+}
+
+if (!db.permission.findOne({_id: "common_createEntityComment"})) {
+    db.permission.insertOne({
+        _id: "common_createEntityComment",
+        name: "common_createEntityComment",
+        description: "Create entity comment"
+    });
+    db.role.updateOne({name: "admin"}, {
+        $set: {
+            "permissions.common_createEntityComment": 1
+        }
+    });
+}
+
+if (!db.permission.findOne({_id: "common_editEntityComment"})) {
+    db.permission.insertOne({
+        _id: "common_editEntityComment",
+        name: "common_editEntityComment",
+        description: "Edit entity comment"
+    });
+    db.role.updateOne({name: "admin"}, {
+        $set: {
+            "permissions.common_editEntityComment": 1
+        }
+    });
+}
