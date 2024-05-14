e755d0eb61815704c6c13a4b6e946e68a70caa43
Vladimir Erokhov <vladimir.erokhov@noveogroup.com>
2024-05-14 14:48:13 +0500

    fix(#5332): Reduced min columns width

--- community/sources/webcore/src/canopsis-next/src/components/widgets/alarm/headers-formatting/alarm-header-tag.vue
+++ community/sources/webcore/src/canopsis-next/src/components/widgets/alarm/headers-formatting/alarm-header-tag.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-  span
+  span(v-bind="$attrs")
     slot
     c-alarm-action-chip(
       v-if="selectedTag",
@@ -14,6 +14,7 @@ import { entitiesAlarmTagMixin } from '@/mixins/entities/alarm-tag';
 
 export default {
   mixins: [entitiesAlarmTagMixin],
+  inheritAttrs: false,
   props: {
     selectedTag: {
       type: String,
