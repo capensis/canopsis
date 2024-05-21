a7fdb50fee742eba11806b8a2fcde8db123c909e
Vladimir Erokhov <vladimir.erokhov@noveogroup.com>
2024-05-21 16:51:12 +0500

    fix(#5366): Added defer row rendering

---
+++ community/sources/webcore/src/canopsis-next/src/components/common/placeholder/c-placeholder-loader.vue
@@ -0,0 +1,54 @@
+<template functional>
+  <div
+    :style="{ height: `${props.height}px` }"
+    class="placeholder"
+  >
+    <div class="placeholder__background" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    height: {
+      type: [String, Number],
+      default: 24,
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+@keyframes placeholder-glow {
+  50% {
+    opacity: 1;
+  }
+}
+
+.placeholder {
+  width: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 1;
+
+  &__background {
+    opacity: .2;
+    transform: translate3d(-50%, -50%, 0);
+    top: 50%;
+    left: 50%;
+    height: 86%;
+    position: relative;
+    animation: placeholder-glow 2s ease-in-out infinite;
+    animation-iteration-count: 2;
+
+    .v-application.theme--light & {
+      background: darkgray; // TODO: use vars
+    }
+
+    .v-application.theme--dark & {
+      background: darkgray; // TODO: use vars
+    }
+  }
+}
+</style>
