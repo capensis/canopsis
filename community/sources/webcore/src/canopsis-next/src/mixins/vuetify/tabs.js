258004e99a86d12956ac9c55029774b12996d6c8
Vladimir Erokhov <vladimir.erokhov@noveogroup.com>
2024-05-24 15:33:29 +0500

    fix(#5460): Fixed detachable error

--- community/sources/webcore/src/canopsis-next/src/mixins/vuetify/tabs.js
+++ community/sources/webcore/src/canopsis-next/src/mixins/vuetify/tabs.js
@@ -1,16 +1,10 @@
-import { isFunction } from 'lodash';
-
 export const vuetifyTabsMixin = {
   methods: {
     callTabsOnResizeMethod() {
-      if (this.$refs.tabs && isFunction(this.$refs.tabs.onResize)) {
-        this.$refs.tabs.onResize();
-      }
+      this.$refs?.tabs?.onResize?.();
     },
     callTabsUpdateTabsMethod() {
-      if (this.$refs.tabs && isFunction(this.$refs.tabs.updateTabsView)) {
-        this.$refs.tabs.updateTabsView();
-      }
+      this.$refs?.tabs?.updateTabsView?.();
     },
   },
 };
