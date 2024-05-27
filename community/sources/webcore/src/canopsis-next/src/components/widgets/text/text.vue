c41aeabb4d2f3733e0f271df79de881575ddcbdb
Vladimir Erokhov <vladimir.erokhov@noveogroup.com>
2024-05-27 12:23:39 +0500

    feature(#5459): Added possibility to use iframe in text widget

--- community/sources/webcore/src/canopsis-next/src/components/widgets/text/text.vue
+++ community/sources/webcore/src/canopsis-next/src/components/widgets/text/text.vue
@@ -1,10 +1,14 @@
 <template>
   <div class="position-relative">
-    <c-compiled-template :template="template" />
+    <c-compiled-template :template="template" :sanitize-options="sanitizeOptions" />
   </div>
 </template>
 
 <script>
+import { DEFAULT_SANITIZE_OPTIONS } from '@/config';
+
+import { setSeveralFields } from '@/helpers/immutable';
+
 export default {
   props: {
     widget: {
@@ -13,6 +17,25 @@ export default {
     },
   },
   computed: {
+    sanitizeOptions() {
+      return setSeveralFields(DEFAULT_SANITIZE_OPTIONS, {
+        allowedTags: tags => [...tags, 'iframe'],
+        'allowedAttributes.iframe': [
+          'allowtransparency',
+          'frameborder',
+          'hspace',
+          'marginheight',
+          'marginwidth',
+          'sandbox',
+          'scrolling',
+          'seamless',
+          'src',
+          'srcdoc',
+          'vspace',
+        ],
+      });
+    },
+
     template() {
       return this.widget.parameters?.template;
     },
