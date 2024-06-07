73ac2e5082a0fe02c40a674aabd0447d15f3eeca
Vladimir Erokhov <vladimir.erokhov@noveogroup.com>
2024-06-07 12:13:33 +0500

    fix(#5460): Added fix to avoid nativeOn handlers warning

---
+++ community/sources/webcore/src/canopsis-next/src/plugins/vuetify/components/v-tabs/v-tab.vue
@@ -0,0 +1,24 @@
+<script>
+import { omit } from 'lodash';
+import VTab from 'vuetify/es5/components/VTabs/VTab';
+
+export default {
+  extends: VTab,
+  render(h) {
+    const link = this.generateRouteLink(this.classes);
+
+    /**
+     * We've added this code to avoid warning error when `nativeOn` listeners added on `div` element
+     */
+    const [tag, data] = this.disabled
+      ? ['div', omit(link.data, ['on', 'nativeOn'])]
+      : [link.tag, link.data];
+
+    data.ref = 'link';
+
+    return h('div', {
+      staticClass: 'v-tabs__div',
+    }, [h(tag, data, this.$slots.default)]);
+  },
+};
+</script>
