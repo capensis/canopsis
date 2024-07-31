5f37627a46eec3843c47ccbb254fd1a56ee7e917
Doston Norpulatov <doston.norpulatov@noveogroup.com>
2024-07-31 14:30:58 +0500

    fix(#5533): Live report modals title changed

--- community/sources/webcore/src/canopsis-next/src/i18n/messages/en/modals.js
+++ community/sources/webcore/src/canopsis-next/src/i18n/messages/en/modals.js
@@ -173,7 +173,7 @@ export default {
     title: 'Create uncancel event',
   },
   liveReporting: {
-    editLiveReporting: 'Live reporting',
+    editLiveReporting: 'Set a custom date range',
     dateInterval: 'Date interval',
     today: 'Today',
     yesterday: 'Yesterday',
