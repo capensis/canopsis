fd861b50a2899871c3827be356335d3196fc361c
Vladimir Erokhov <vladimir.erokhov@noveogroup.com>
2024-08-12 12:43:18 +0500

    feature(#5098): Added some fixes after self-review

--- community/sources/webcore/src/canopsis-next/src/hooks/store/modules/broadcast-message.js
+++ community/sources/webcore/src/canopsis-next/src/hooks/store/modules/broadcast-message.js
@@ -2,6 +2,35 @@ import { useStoreModuleHooks } from '@/hooks/store';
 
 const useBroadcastMessageStoreModule = () => useStoreModuleHooks('broadcastMessage');
 
+/**
+ * Custom hook for accessing actions related to broadcast messages from Vuex store module.
+ *
+ * @returns {Object} An object containing functions to create, update, remove, and fetch broadcast messages list.
+ *
+ * @example
+ * // Usage example within a Vue component
+ * import { useBroadcastMessages } from './path/to/useBroadcastMessages';
+ *
+ * export default {
+ *   setup() {
+ *     const {
+ *       createBroadcastMessage,
+ *       updateBroadcastMessage,
+ *       removeBroadcastMessage,
+ *       fetchBroadcastMessagesListWithoutStore
+ *     } = useBroadcastMessages();
+ *
+ *     // Access and use the functions as needed
+ *
+ *     return {
+ *       createBroadcastMessage,
+ *       updateBroadcastMessage,
+ *       removeBroadcastMessage,
+ *       fetchBroadcastMessagesListWithoutStore
+ *     };
+ *   }
+ * }
+ */
 export const useBroadcastMessages = () => {
   const { useActions } = useBroadcastMessageStoreModule();
 
