1d71a4780a20473e433e33ae153fd581298ccf98
Vladimir Erokhov <vladimir.erokhov@noveogroup.com>
2024-08-27 14:16:53 +0500

    feature(#5098): Added updateOptions function and added source_type field into table

--- community/sources/webcore/src/canopsis-next/src/hooks/query/options.js
+++ community/sources/webcore/src/canopsis-next/src/hooks/query/options.js
@@ -14,58 +14,68 @@ import { isPickEqual } from '@/helpers/collection';
  */
 export const useQueryOptions = (query, updateQuery) => {
   const unwrappedQuery = unref(query);
-  const options = computed({
+
+  const options = computed(() => {
+    const { page = 1, itemsPerPage = PAGINATION_LIMIT, sortBy = [], sortDesc = [], ...restQuery } = query.value;
+
+    return { page, itemsPerPage, sortBy, sortDesc, ...restQuery };
+  });
+
+  /**
+   * Update options based on the provided value.
+   *
+   * @param {Object} value - The new options values to update.
+   */
+  const updateOptions = (value) => {
+    const otherValues = omit(value, [
+      'search',
+      'page',
+      'itemsPerPage',
+      'totalItems',
+      'groupBy',
+      'groupDesc',
+      'multiSort',
+      'mustSort',
+    ]);
+
+    const isEqualOptions = isPickEqual(
+      options.value,
+      value,
+      ['search', 'page', 'itemsPerPage', 'sortBy', 'sortDesc', ...Object.keys(otherValues)],
+    );
+
+    if (isEqualOptions) {
+      return;
+    }
+
+    updateQuery({
+      ...unwrappedQuery,
+
+      search: value.search || '',
+      page: value.itemsPerPage <= unwrappedQuery.itemsPerPage ? value.page : 1,
+      itemsPerPage: value.itemsPerPage || PAGINATION_LIMIT,
+      sortBy: value.sortBy || [],
+      sortDesc: value.sortDesc || [],
+
+      ...otherValues,
+    });
+  };
+
+  const optionsWithSet = computed({
     /**
      * Get function to extract pagination and sorting options from the query object.
      *
      * @returns {Object} - Object containing page, itemsPerPage, sortBy, sortDesc, and other query parameters.
      */
-    get: () => {
-      const { page = 1, itemsPerPage = PAGINATION_LIMIT, sortBy = [], sortDesc = [], ...restQuery } = query.value;
-
-      return { page, itemsPerPage, sortBy, sortDesc, ...restQuery };
-    },
+    get: () => options.value,
 
     /**
      * Set function to update the query object based on the new options value.
      *
      * @param {Object} value - New options object for pagination and sorting.
      */
-    set: (value) => {
-      const otherValues = omit(value, [
-        'search',
-        'page',
-        'itemsPerPage',
-        'totalItems',
-        'groupBy',
-        'groupDesc',
-        'multiSort',
-        'mustSort',
-      ]);
-
-      const isEqualOptions = isPickEqual(
-        options.value,
-        value,
-        ['search', 'page', 'itemsPerPage', 'sortBy', 'sortDesc', ...Object.keys(otherValues)],
-      );
-
-      if (isEqualOptions) {
-        return;
-      }
-
-      updateQuery({
-        ...unwrappedQuery,
-
-        search: value.search || '',
-        page: value.itemsPerPage <= unwrappedQuery.itemsPerPage ? value.page : 1,
-        itemsPerPage: value.itemsPerPage || PAGINATION_LIMIT,
-        sortBy: value.sortBy || [],
-        sortDesc: value.sortDesc || [],
-
-        ...otherValues,
-      });
-    },
+    set: updateOptions,
   });
 
-  return { options };
+  return { options: optionsWithSet, updateOptions };
 };
