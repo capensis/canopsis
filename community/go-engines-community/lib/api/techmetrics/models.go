f28b712eba83595194b7ab3cf7361a5acdad0924
Zarina Yakubova <Zarina.Yakubova@noveogroup.com>
2024-09-13 14:28:56 +0200

    feature(#5584): Add tech metrics settings

--- community/go-engines-community/lib/api/techmetrics/models.go
+++ community/go-engines-community/lib/api/techmetrics/models.go
@@ -3,7 +3,8 @@ package techmetrics
 import "git.canopsis.net/canopsis/canopsis-community/community/go-engines-community/lib/canopsis/datetime"
 
 type Settings struct {
-	Enabled bool `bson:"enabled" json:"enabled"`
+	Enabled          bool   `bson:"enabled" json:"enabled"`
+	DumpKeepInterval string `bson:"dumpkeepinterval" json:"-"`
 }
 
 type ExportResponse struct {
