b0cced409f89e38e5942a8c171c782793f7da072
Vladimir Erokhov <verokhov@noveogroup.com>
2024-10-01 13:45:20 +0200

    Apply 5 suggestion(s) to 3 file(s)
    Co-authored-by: Mikaël Mourcia <mmourcia@capensis.fr>

--- community/sources/webcore/src/canopsis-next/src/i18n/messages/fr/events-record.js
+++ community/sources/webcore/src/canopsis-next/src/i18n/messages/fr/events-record.js
@@ -1,8 +1,8 @@
 export default {
   recorded: 'Enregistré à',
   eventsCount: 'Nombre d\'événements enregistrés',
-  subheader: 'Enregistrer tous les événements de RabbitMQ',
-  launch: 'Enregistrement des événements de lancement',
+  subheader: 'Enregistrer tous les événements',
+  launch: 'Lancement d\'un enregistrement d\'événements',
   stop: 'Arrêter l\'enregistrement des événements',
   stopResending: 'Arrêter de renvoyer',
   inProgress: 'Enregistrement en cours...',
@@ -12,5 +12,5 @@ export default {
   applyEventFilter: 'Appliquer le filtre d\'événement',
   delayBetweenEvents: 'Délai entre les événements',
   resendEvents: 'Renvoyer l\'événement | Renvoyer les événements',
-  launchEventRecording: 'Enregistrement des événements de lancement',
+  launchEventRecording: 'Lancement d\'un enregistrement d\'événements',
 };
