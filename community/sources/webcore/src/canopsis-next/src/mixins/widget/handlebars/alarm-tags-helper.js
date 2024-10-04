62988c86c97242079fdd9b34a7964ce6158a3476
Vladimir Erokhov <vladimir.erokhov@noveogroup.com>
2024-10-04 11:12:53 +0500

    feature(#5622): Add regex flags support to alarm tags helper and update component props

--- community/sources/webcore/src/canopsis-next/src/mixins/widget/handlebars/alarm-tags-helper.js
+++ community/sources/webcore/src/canopsis-next/src/mixins/widget/handlebars/alarm-tags-helper.js
@@ -8,18 +8,20 @@ import { registerHelper, unregisterHelper } from '@/helpers/handlebars';
 export const alarmHandlebarsTagsHelper = {
   beforeCreate() {
     /**
-     * @example {{tags 'tag1' 'tag2' regex='tag.*'}}
+     * @example {{tags 'tag1' 'tag2' regex='tag.*' flags='i'}}
      */
     registerHelper('tags', (...args) => {
-      const { hash: { regex } = {} } = args.pop() ?? {};
-      const nameFilter = args.filter(Boolean).map(arg => escapeRegExp(arg)).join('|');
+      const { hash: { regex = '', flags = '' } = {} } = args.pop() ?? {};
+      const escapedArgs = args.filter(Boolean).map(arg => escapeRegExp(arg)).join('|');
+      const nameFilter = escapedArgs.length ? `^${escapedArgs.join('|')}$` : '';
 
       return new Handlebars.SafeString(
         `<c-alarm-tags-chips
         :alarm="alarm"
         :selected-tag="selectedTag"
-        name-filter="^${nameFilter}$"
+        name-filter="${nameFilter}"
         regex-filter="${regex}"
+        regex-filter-flags="${flags}"
         inline-count="${MAX_LIMIT}"
         closable-active
         @select="$emit('select:tag', $event)"
