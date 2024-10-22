47ea719f8d498b2e3717aeafe2b6be48e55b498d
Zarina Yakubova <Zarina.Yakubova@noveogroup.com>
2024-10-22 16:58:41 +0200

    feature(#5378): Update migration

--- community/go-engines-community/database/migrations/20241015144704_add_meta_alarm_component_name_field_down.js
+++ community/go-engines-community/database/migrations/20241015144704_add_meta_alarm_component_name_field_down.js
@@ -1,3 +1,10 @@
+const ruleTypesToUpdate = [
+    "attribute",
+    "complex",
+    "timebased",
+    "valuegroup",
+];
+const ruleTypeCorel = "corel"
 db.meta_alarm_states.aggregate([
     {$match: {meta_alarm_component_name: {$ne: null}}},
     {
@@ -9,18 +16,7 @@ db.meta_alarm_states.aggregate([
         }
     },
     {$unwind: "$rule"},
-    {
-        $match: {
-            "rule.type": {
-                $in: [
-                    "attribute",
-                    "complex",
-                    "timebased",
-                    "valuegroup",
-                ]
-            }
-        }
-    }
+    {$match: {"rule.type": {$in: ruleTypesToUpdate}}}
 ]).forEach(function (doc) {
     db.meta_alarm_states.updateOne({_id: doc._id}, {
         $unset: {
@@ -40,11 +36,7 @@ db.meta_alarm_states.aggregate([
         }
     },
     {$unwind: "$rule"},
-    {
-        $match: {
-            "rule.type": "corel"
-        }
-    },
+    {$match: {"rule.type": ruleTypeCorel}},
 ]).forEach(function (doc) {
     db.meta_alarm_states.updateOne({_id: doc._id}, {
         $set: {
@@ -55,3 +47,93 @@ db.meta_alarm_states.aggregate([
         },
     });
 });
+
+(function () {
+    let ruleTypes = {};
+    db.meta_alarm_rules.find().forEach(function (rule) {
+        ruleTypes[rule._id] = rule.type;
+    });
+
+    db.meta_alarm_states.aggregate([
+        {$match: {meta_alarm_component_name: {$ne: null}}},
+        {
+            $lookup: {
+                from: "meta_alarm_rules",
+                localField: "_id",
+                foreignField: "_id",
+                as: "rule"
+            }
+        },
+        {$unwind: {path: "$rule", preserveNullAndEmptyArrays: true}},
+        {$match: {rule: null}}
+    ]).forEach(function (doc) {
+        let ruleType = "";
+        for (const ruleID in ruleTypes) {
+            if (doc._id.startsWith(ruleID)) {
+                ruleType = ruleTypes[ruleID];
+                break;
+            }
+        }
+
+        if (!ruleTypesToUpdate.includes(ruleType)) {
+            return;
+        }
+
+        db.meta_alarm_states.updateOne({_id: doc._id}, {
+            $unset: {
+                meta_alarm_component_name: "",
+            }
+        });
+    });
+
+    db.meta_alarm_states.aggregate([
+        {$match: {meta_alarm_component_name: {$ne: null}}},
+        {
+            $lookup: {
+                from: "meta_alarm_rules",
+                localField: "_id",
+                foreignField: "_id",
+                as: "rule"
+            }
+        },
+        {$unwind: {path: "$rule", preserveNullAndEmptyArrays: true}},
+        {$match: {rule: null}},
+        {
+            $lookup: {
+                from: "default_entities",
+                localField: "meta_alarm_name",
+                foreignField: "name",
+                as: "entity",
+                let: {component: "$meta_alarm_component_name"},
+                pipeline: [
+                    {$match: {
+                        type: "resource",
+                        $expr: {$eq: ["$component", "$$component"]}
+                    }}
+                ]
+            }
+        },
+        {$unwind: "$entity"}
+    ]).forEach(function (doc) {
+        let ruleType = "";
+        for (const ruleID in ruleTypes) {
+            if (doc._id.startsWith(ruleID)) {
+                ruleType = ruleTypes[ruleID];
+                break;
+            }
+        }
+
+        if (ruleType !== ruleTypeCorel) {
+            return;
+        }
+
+        db.meta_alarm_states.updateOne({_id: doc._id}, {
+            $set: {
+                meta_alarm_name: doc.meta_alarm_name + "/" + doc.meta_alarm_component_name,
+            },
+            $unset: {
+                meta_alarm_component_name: ""
+            },
+        });
+    });
+})();
--- community/go-engines-community/database/migrations/20241015144704_add_meta_alarm_component_name_field_up.js
+++ community/go-engines-community/database/migrations/20241015144704_add_meta_alarm_component_name_field_up.js
@@ -1,3 +1,10 @@
+const ruleTypesToUpdate = [
+    "attribute",
+    "complex",
+    "timebased",
+    "valuegroup",
+];
+const ruleTypeCorel = "corel"
 db.meta_alarm_states.aggregate([
     {$match: {meta_alarm_component_name: null}},
     {
@@ -9,18 +16,7 @@ db.meta_alarm_states.aggregate([
         }
     },
     {$unwind: "$rule"},
-    {
-        $match: {
-            "rule.type": {
-                $in: [
-                    "attribute",
-                    "complex",
-                    "timebased",
-                    "valuegroup",
-                ]
-            }
-        }
-    }
+    {$match: {"rule.type": {$in: ruleTypesToUpdate}}}
 ]).forEach(function (doc) {
     db.meta_alarm_states.updateOne({_id: doc._id}, {
         $set: {
@@ -40,11 +36,7 @@ db.meta_alarm_states.aggregate([
         }
     },
     {$unwind: "$rule"},
-    {
-        $match: {
-            "rule.type": "corel"
-        }
-    },
+    {$match: {"rule.type": ruleTypeCorel}},
     {
         $lookup: {
             from: "default_entities",
@@ -54,11 +46,7 @@ db.meta_alarm_states.aggregate([
         }
     },
     {$unwind: "$entity"},
-    {
-        $match: {
-            "entity.type": "resource"
-        }
-    },
+    {$match: {"entity.type": "resource"}},
 ]).forEach(function (doc) {
     db.meta_alarm_states.updateOne({_id: doc._id}, {
         $set: {
@@ -67,3 +55,87 @@ db.meta_alarm_states.aggregate([
         }
     });
 });
+
+(function () {
+    let ruleTypes = {};
+    db.meta_alarm_rules.find().forEach(function (rule) {
+        ruleTypes[rule._id] = rule.type;
+    });
+
+    db.meta_alarm_states.aggregate([
+        {$match: {meta_alarm_component_name: null}},
+        {
+            $lookup: {
+                from: "meta_alarm_rules",
+                localField: "_id",
+                foreignField: "_id",
+                as: "rule"
+            }
+        },
+        {$unwind: {path: "$rule", preserveNullAndEmptyArrays: true}},
+        {$match: {rule: null}}
+    ]).forEach(function (doc) {
+        let ruleType = "";
+        for (const ruleID in ruleTypes) {
+            if (doc._id.startsWith(ruleID)) {
+                ruleType = ruleTypes[ruleID];
+                break;
+            }
+        }
+
+        if (!ruleTypesToUpdate.includes(ruleType)) {
+            return;
+        }
+
+        db.meta_alarm_states.updateOne({_id: doc._id}, {
+            $set: {
+                meta_alarm_component_name: "metaalarm",
+            }
+        });
+    });
+
+    db.meta_alarm_states.aggregate([
+        {$match: {meta_alarm_component_name: null}},
+        {
+            $lookup: {
+                from: "meta_alarm_rules",
+                localField: "_id",
+                foreignField: "_id",
+                as: "rule"
+            }
+        },
+        {$unwind: {path: "$rule", preserveNullAndEmptyArrays: true}},
+        {$match: {rule: null}},
+        {
+            $lookup: {
+                from: "default_entities",
+                localField: "meta_alarm_name",
+                foreignField: "_id",
+                as: "entity",
+                pipeline: [
+                    {$match: {type: "resource"}}
+                ]
+            }
+        },
+        {$unwind: "$entity"}
+    ]).forEach(function (doc) {
+        let ruleType = "";
+        for (const ruleID in ruleTypes) {
+            if (doc._id.startsWith(ruleID)) {
+                ruleType = ruleTypes[ruleID];
+                break;
+            }
+        }
+
+        if (ruleType !== ruleTypeCorel) {
+            return;
+        }
+
+        db.meta_alarm_states.updateOne({_id: doc._id}, {
+            $set: {
+                meta_alarm_component_name: doc.entity.component,
+                meta_alarm_name: doc.entity.name,
+            }
+        });
+    });
+})();
