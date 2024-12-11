5f03e1a0efb1718cb5bfed3a9dc76a4a97fd2820
Zarina Yakubova <Zarina.Yakubova@noveogroup.com>
2024-12-11 15:39:25 +0100

    feature(#5191): Add update role permissions bulk endpoint

--- community/go-engines-community/lib/api/role/models.go
+++ community/go-engines-community/lib/api/role/models.go
@@ -31,6 +31,11 @@ type EditRequest struct {
 	Author     string                  `json:"author" swaggerignore:"true"`
 }
 
+type BulkUpdatePermissionsRequestItem struct {
+	ID          string              `json:"_id" binding:"required"`
+	Permissions map[string][]string `json:"permissions"`
+}
+
 type Response struct {
 	ID          string       `bson:"_id" json:"_id"`
 	Name        string       `bson:"name" json:"name"`
@@ -55,9 +60,9 @@ type Permission struct {
 	Type    string   `bson:"type" json:"type"`
 	Actions []string `bson:"actions" json:"actions"`
 
-	Bitmask               int64                      `bson:"bitmask" json:"-"`
-	ApiPermissions        map[string]int64           `bson:"api_permissions" json:"-"`
-	ApiPermissionsBitmask map[int64]map[string]int64 `bson:"api_permissions_bitmask" json:"-"`
+	Bitmask               int8                     `bson:"bitmask" json:"-"`
+	ApiPermissions        map[string]int8          `bson:"api_permissions" json:"-"`
+	ApiPermissionsBitmask map[int8]map[string]int8 `bson:"api_permissions_bitmask" json:"-"`
 }
 
 type View struct {
