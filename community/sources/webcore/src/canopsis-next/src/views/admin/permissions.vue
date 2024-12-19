96f490d517eab3e84202afc54166f1e31a570467
Vladimir Erokhov <vladimir.erokhov@noveogroup.com>
2024-12-19 16:14:20 +0500

    feature(#5191): extract role permission hooks to improve code organization and reusability

--- community/sources/webcore/src/canopsis-next/src/views/admin/permissions.vue
+++ community/sources/webcore/src/canopsis-next/src/views/admin/permissions.vue
@@ -39,26 +39,12 @@
 </template>
 
 <script>
+import { ref, onMounted } from 'vue';
+
 import {
-  keyBy,
-  omit,
-  isEqual,
-  filter,
-  cloneDeep,
-  sortBy,
-} from 'lodash';
-import { computed, ref, set, onMounted } from 'vue';
-
-import { API_USER_PERMISSIONS_ROOT_GROUPS, MAX_LIMIT, MODALS, ROLE_TYPES } from '@/constants';
-
-import { permissionsToTreeview } from '@/helpers/entities/permissions/list';
-import { formToRolePermissions, roleToPermissionForm } from '@/helpers/entities/role/form';
-import { mapIds } from '@/helpers/array';
-
-import { useModals } from '@/hooks/modals';
-import { useRole } from '@/hooks/store/modules/role';
-import { usePendingHandler } from '@/hooks/query/pending';
-import { usePermissions } from '@/hooks/store/modules/permissions';
+  useRolePermissionActions,
+  useRolePermissionFetching,
+} from '@/components/other/permission/hooks/role-permission';
 
 import PermissionsTable from '@/components/other/permission/permissions-table.vue';
 import PermissionsFabBtn from '@/components/other/permission/permissions-fab-btn.vue';
@@ -67,133 +53,19 @@ export default {
   components: { PermissionsTable, PermissionsFabBtn },
   setup() {
     const activeTab = ref();
-    const originalRolesById = ref({});
-    const rolesById = ref({});
-    const changedRoles = ref({});
-    const permissions = ref([]);
 
-    const modals = useModals();
+    const {
+      pending,
+      roles,
+      treeviewPermissions,
+      hasChanges,
+      resetRolesById,
+      updateRoles,
+      changeRole,
+      fetchList,
+    } = useRolePermissionFetching({ activeTab });
 
-    const { fetchPermissionsListWithoutStore } = usePermissions();
-    const { fetchRolesListWithoutStore, bulkUpdateRolePermissions } = useRole();
-
-    const resetRolesById = () => {
-      rolesById.value = cloneDeep(originalRolesById.value);
-      changedRoles.value = {};
-    };
-
-    const { pending, handler: fetchList } = usePendingHandler(async () => {
-      const [rolesResponse, permissionsResponse] = await Promise.all([
-        fetchRolesListWithoutStore({ params: { limit: MAX_LIMIT, with_flags: true } }),
-        fetchPermissionsListWithoutStore({ params: { limit: MAX_LIMIT } }),
-      ]);
-
-      originalRolesById.value = keyBy(Object.values(rolesResponse.data).map(roleToPermissionForm), '_id');
-      permissions.value = permissionsResponse.data;
-      resetRolesById();
-    });
-
-    const treeviewPermissions = computed(() => (
-      sortBy(Object.values(permissionsToTreeview(permissions.value)), 'position')
-    ));
-    const isApiPermissionsTab = computed(() => API_USER_PERMISSIONS_ROOT_GROUPS.includes(activeTab.value));
-    const uiRoles = computed(() => filter(Object.values(rolesById.value), ['type', ROLE_TYPES.ui]));
-    const apiRoles = computed(() => filter(Object.values(rolesById.value), ['type', ROLE_TYPES.api]));
-    const roles = computed(() => (isApiPermissionsTab.value ? apiRoles.value : uiRoles.value));
-    const hasChanges = computed(() => (
-      Object.entries(changedRoles.value).some(([roleId, rolePermissions]) => (
-        Object.keys(rolePermissions).some(permissionId => (
-          !isEqual(
-            rolesById.value[roleId].permissions[permissionId],
-            originalRolesById.value[roleId].permissions[permissionId],
-          )
-        ))
-      ))
-    ));
-
-    const setChangedRolePermission = (roleId, permissionId) => {
-      if (!changedRoles.value[roleId]) {
-        set(changedRoles.value, roleId, { [permissionId]: true });
-      }
-
-      if (!changedRoles.value[roleId][permissionId]) {
-        set(changedRoles.value[roleId], permissionId, true);
-      }
-    };
-
-    const setChangedRolePermissions = (roleId, permissionsIds) => (
-      permissionsIds.forEach(permissionId => setChangedRolePermission(roleId, permissionId))
-    );
-
-    const changeRole = (value, role, permission, action) => {
-      if (!changedRoles.value[role._id]) {
-        set(changedRoles.value, role._id, { [permission._id]: true });
-      }
-
-      if (!changedRoles.value[role._id][permission._id]) {
-        set(changedRoles.value, permission._id, true);
-      }
-
-      if (permission.allChildren) {
-        const allChildrenIds = mapIds(permission.allChildren);
-        const newPermissions = value
-          ? {
-            ...role.permissions,
-            ...permission.allChildren.reduce((acc, { _id: id, actions }) => {
-              acc[id] = actions;
-
-              return acc;
-            }, {}),
-          }
-          : omit(role.permissions, allChildrenIds);
-
-        setChangedRolePermissions(role._id, allChildrenIds);
-
-        set(rolesById.value[role._id], 'permissions', newPermissions);
-
-        return;
-      }
-
-      setChangedRolePermission(role._id, permission._id);
-
-      const currentActions = role.permissions[permission._id] ?? [];
-
-      const newActions = value
-        ? [...currentActions, action]
-        : currentActions.filter(currentAction => currentAction !== action);
-
-      if (!newActions.length) {
-        set(rolesById.value[role._id], 'permissions', omit(role.permissions, permission._id));
-
-        return;
-      }
-
-      set(rolesById.value[role._id].permissions, permission._id, newActions);
-    };
-
-    const updateRoles = async () => {
-      const rolesForUpdate = Object.keys(changedRoles.value)
-        .map(roleId => rolesById.value[roleId] && formToRolePermissions(rolesById.value[roleId]))
-        .filter(Boolean);
-
-      await bulkUpdateRolePermissions({ data: rolesForUpdate });
-
-      return fetchList();
-    };
-
-    const submit = () => modals.show({
-      name: MODALS.confirmation,
-      config: {
-        action: updateRoles,
-      },
-    });
-
-    const cancel = () => modals.show({
-      name: MODALS.confirmation,
-      config: {
-        action: resetRolesById,
-      },
-    });
+    const { submit, cancel } = useRolePermissionActions({ updateRoles, resetRolesById });
 
     onMounted(fetchList);
 
