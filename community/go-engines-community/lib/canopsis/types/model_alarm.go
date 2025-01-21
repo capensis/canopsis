72839dc02ded366fe54cbfd3ee30c5bfe454c69e
Zarina Yakubova <Zarina.Yakubova@noveogroup.com>
2025-01-21 16:50:55 +0100

    feature(#5768): Add fields to alarm pattern

--- community/go-engines-community/lib/canopsis/types/model_alarm.go
+++ community/go-engines-community/lib/canopsis/types/model_alarm.go
@@ -439,6 +439,8 @@ func (a *Alarm) GetStringField(f string) (string, bool) {
 		return a.Value.Component, true
 	case "v.resource":
 		return a.Value.Resource, true
+	case "v.state.initiator":
+		return a.Value.State.GetInitiator(), true
 	case "v.last_comment.m":
 		if a.Value.LastComment == nil {
 			return "", true
@@ -476,6 +478,14 @@ func (a *Alarm) GetStringField(f string) (string, bool) {
 		return a.Value.ACK.GetInitiator(), true
 	case "v.canceled.initiator":
 		return a.Value.Canceled.GetInitiator(), true
+	case "v.snooze.a":
+		if a.Value.Snooze == nil {
+			return "", true
+		}
+
+		return a.Value.Snooze.Author, true
+	case "v.snooze.initiator":
+		return a.Value.Snooze.GetInitiator(), true
 	default:
 		if n := strings.TrimPrefix(f, "v.ticket.ticket_data."); n != f {
 			if a.Value.Ticket == nil || a.Value.Ticket.TicketData == nil {
