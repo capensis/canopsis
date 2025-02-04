204e73d5ba21d69a1468b072d3d8daf65570cf7d
Mikael Mourcia <mmourcia@capensis.fr>
2025-02-04 11:36:24 +0100

    french review of headers messages

--- community/sources/webcore/src/canopsis-next/src/i18n/messages/fr/page-headers.js
+++ community/sources/webcore/src/canopsis-next/src/i18n/messages/fr/page-headers.js
@@ -9,22 +9,22 @@ export default {
    */
   [USER_PERMISSIONS.technical.exploitation.eventFilter]: {
     title: 'Filtres d\'événements',
-    message: 'Le filtre d\'événements est une fonctionnalité du moteur CHE qui permet la définition de règles de traitement des événements.',
+    message: 'Centre de filtrage et d\'enrichissement des événements et entités.',
   },
 
   [USER_PERMISSIONS.technical.exploitation.dynamicInfo]: {
     title: 'Informations dynamiques',
-    message: 'Les informations dynamiques de Canopsis sont utilisées pour enrichire les alarmes. Ces enrichissements sont définis par des règles indiquant dans quelles conditions les informations doivent être présentées sur une alarme.',
+    message: 'Centre d\'enrichissement des alarmes',
   },
 
   [USER_PERMISSIONS.technical.exploitation.metaAlarmRule]: {
     title: 'Règles de méta-alarme',
-    message: 'Les règles de méta-alarme peuvent être utilisées pour grouper les alarmes par types et critères (relation parent/enfant, intervalle de temps, etc).',
+    message: 'Module de corrélation permettant de regrouper des alarmes en méta-alarmes selon des critères (relation parent/enfant, intervalle de temps, etc).',
   },
 
   [USER_PERMISSIONS.technical.exploitation.idleRules]: {
     title: 'Règles d\'inactivité',
-    message: 'Les règles d\'inactivité des entités ou des alarmes peuvent être utilisées pour surveiller les événements et les états d\'alarme afin de savoir si des événements ne sont pas reçus ou si l\'état d\'alarme n\'est pas modifié pendant une longue période en raison d\'erreurs ou d\'une configuration non valide.',
+    message: 'Règles permettant de réagir en cas de \"non événement\". Utile pour gérer les lignes de vie des sources de données par exemple.',
   },
 
   [USER_PERMISSIONS.technical.exploitation.flappingRules]: {
@@ -39,12 +39,12 @@ export default {
 
   [USER_PERMISSIONS.technical.exploitation.pbehavior]: {
     title: 'Comportements périodiques',
-    message: 'Les comportements périodiques de Canopsis peuvent être utilisés afin de définir une période pendant laquelle le comportement doit être modifié, par exemple pour la maintenance ou le type de services.',
+    message: 'Les comportements périodiques de Canopsis peuvent être utilisés afin de définir une période pendant laquelle un comportement doit être modifié. Utile pour définir des périodes de maintenance, des périodes de service.',
   },
 
   [USER_PERMISSIONS.technical.exploitation.scenario]: {
     title: 'Scénarios',
-    message: 'Les scénarios Canopsis peuvent être utilisés pour déclencher conditionnellement divers types d\'actions sur les alarmes.',
+    message: 'Les scénarios Canopsis peuvent être utilisés pour déclencher conditionnellement divers types d\'actions sur les alarmes.\nExemples : Notifications sur un chat, création d\'un ticket d\'incident, inscription d\'une alarme sur une main courante.',
   },
 
   [USER_PERMISSIONS.technical.exploitation.snmpRule]: {
@@ -54,12 +54,13 @@ export default {
 
   [USER_PERMISSIONS.technical.exploitation.declareTicketRule]: {
     title: 'Règles de déclaration des tickets',
-    // message: '', // TODO: need to put description
+    message: 'Permet de définir les règles de création de tickets d\'incidents à destination de plusieurs outils de gestion d\'incidents. Ces tickets sont créés par un pilote depuis la WebUI, et Canopsis se charge ensuite de contacter l\'outil de gestion d\'incident pour l\'enregistrement du ticket.\n',
+
   },
 
   [USER_PERMISSIONS.technical.exploitation.linkRule]: {
     title: 'Générateur de liens',
-    // message: '', // TODO: need to put description
+    message: 'Permet de définir les règles d\'association de liens aux alarmes', 
   },
 
   /**
@@ -67,7 +68,7 @@ export default {
    */
   [USER_PERMISSIONS.technical.permission]: {
     title: 'Droits',
-    message: 'Les droits Entreprise, Affichage et Technique s\'appliquent uniquement aux utilisateurs de l\'interface utilisateur. Les droits API s\'appliquent uniquement aux utilisateurs API.',
+    message: 'Les droits Widgets, Vues, et Technique s\'appliquent uniquement à l\'interface utilisateur. Les droits API s\'appliquent uniquement aux utilisateurs API.',
   },
   [USER_PERMISSIONS.technical.role]: {
     title: 'Rôles',
@@ -81,11 +82,11 @@ export default {
    */
   [USER_PERMISSIONS.technical.broadcastMessage]: {
     title: 'Diffusion de messages',
-    message: 'La diffusion de messages peut être utilisée pour afficher les bannières et les messages d\'information qui apparaîtront dans l\'interface de Canopsis.',
+    message: 'Permet d\'afficher des bannières et des messages d\'information dans l\'interface de Canopsis.',
   },
   [USER_PERMISSIONS.technical.playlist]: {
     title: 'Listes de lecture',
-    message: 'Les listes de lecture peuvent être utilisées pour la personnalisation des vues qui peuvent être affichées les unes après les autres avec un délai associé.',
+    message: 'Permet de personnaliser l\'affichage des vues en les faisant défiler les unes après les autres avec un délai défini.',
   },
 
   /**
@@ -96,7 +97,7 @@ export default {
   },
   [USER_PERMISSIONS.technical.healthcheck]: {
     title: 'Bilan de santé',
-    message: 'La fonction Healthcheck est le tableau de bord avec des indications d\'états et d\'erreurs de tous les systèmes inclus dans Canopsis.',
+    message: 'Le Healthcheck est un tableau de bord indiquant l\'état et les erreurs de tous les composants inclus dans Canopsis.',
   },
   [USER_PERMISSIONS.technical.engine]: {
     title: 'Engines',
@@ -104,31 +105,31 @@ export default {
   },
   [USER_PERMISSIONS.technical.kpi]: {
     title: 'KPI',
-    // message: '', // TODO: add correct message
+    message: 'Permet de présenter des indicateurs sous forme de graphiques',
   },
   [USER_PERMISSIONS.technical.map]: {
     title: 'Cartographie',
-    // message: '', // TODO: add correct message
+    message: 'Module permettant de définir et d\'afficher des cartes (géographiques, logiques, mermaid, etc.) via le widget "Map".',
   },
   [USER_PERMISSIONS.technical.maintenance]: {
     title: 'Mode de Maintenance',
-    // message: '', // TODO: add correct message
+    message: 'Permet de basculer Canopsis en mode maintenance. Tous les utilisateurs, à l\'exception des administrateurs, seront déconnectés.',
   },
   [USER_PERMISSIONS.technical.tag]: {
     title: 'Gestion des Tags',
-    // message: '', // TODO: add correct message
+    message: 'Permet de définir des règles d\'attribution de tags aux alarmes',
   },
   [USER_PERMISSIONS.technical.storageSettings]: {
     title: 'Paramètres de stockage',
-    // message: '', // TODO: add correct message
+    message: 'Permet de définir les politiques de rétention des données.',
   },
   [USER_PERMISSIONS.technical.stateSetting]: {
     title: 'Paramètres de calcul d\'état/sévérité',
-    // message: '', // TODO: add correct message
+    message: 'Permet de définir des méthodes de calcul d\'état/sévérité de composants et/ou de services.\nUtile pour modéliser des arbres de dépendances.',
   },
   [USER_PERMISSIONS.technical.eventsRecord]: {
     title: 'Enregistrements d\'événements',
-    // message: '', // TODO: add correct message
+    message: 'Permet de définir et de déclencher l\'enregistrement des événements dès leur arrivée dans le bus de données Canopsis.',
   },
 
   /**
@@ -136,11 +137,11 @@ export default {
    */
   [GROUPED_USER_PERMISSIONS_KEYS.planning]: {
     title: 'Planification',
-    message: 'La fonctionnalité d\'administration de la planification de Canopsis peut être utilisée pour la personnalisation des types de comportements périodiques.',
+    message: 'Permet d\'administrer la planification dans Canopsis et de personnaliser les types de comportements périodiques.',
   },
   [GROUPED_USER_PERMISSIONS_KEYS.remediation]: {
     title: 'Consignes',
-    message: 'La fonction de remédiation de Canopsis peut être utilisée pour créer des plans ou des consignes visant à corriger des situations.',
+    message: 'Permet de créer des consignes de remédiation, avec ou sans job, pour corriger des situations.',
   },
 
   /**
@@ -148,7 +149,8 @@ export default {
    */
   [USER_PERMISSIONS.technical.notification.instructionStats]: {
     title: 'Évaluation des consignes',
-    message: 'Cette page contient les statistiques sur l\'exécution des consignes. Les utilisateurs peuvent noter les consignes en fonction de leurs performances.',
+    message: 'Affiche les statistiques sur l\'exécution des consignes. Les utilisateurs peuvent les évaluer en fonction de leur efficacité.',
+
   },
 
   /**
@@ -156,6 +158,6 @@ export default {
    */
   [USER_PERMISSIONS.technical.profile.theme]: {
     title: 'Thèmes graphiques',
-    // message: '', // TODO: add correct message
+     message: 'Permet de définir des thèmes graphiques pour l\'interface graphique.',
   },
 };
