c44d527ffbbb32a71f6ffd10b7318fcf3d40bc14
Alexander Pavlov <alexander.pavlov@noveogroup.com>
2025-02-27 15:08:15 +0100

    fix(#5745):invert lookups for tags without alarm patterns

--- community/go-engines-community/lib/axe/default.go
+++ community/go-engines-community/lib/axe/default.go
@@ -405,6 +405,7 @@ func NewEngine(
 			Logger:             logger,
 			TagCollection:      dbClient.Collection(mongo.AlarmTagCollection),
 			AlarmCollection:    dbClient.Collection(mongo.AlarmMongoCollection),
+			EntityCollection:   dbClient.Collection(mongo.EntityMongoCollection),
 		},
 		logger,
 	))
