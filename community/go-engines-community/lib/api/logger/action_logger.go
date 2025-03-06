179a4b330be23ab6256e919f8500378c726c713c
Zarina Yakubova <Zarina.Yakubova@noveogroup.com>
2025-03-06 14:57:12 +0100

    feature(#5612): Fix err handling

--- community/go-engines-community/lib/api/logger/action_logger.go
+++ community/go-engines-community/lib/api/logger/action_logger.go
@@ -378,7 +378,7 @@ func (l *logger) runWatcher(ctx context.Context, g *errgroup.Group) (<-chan Acti
 		for stream.Next(ctx) {
 			var event ActionLogEvent
 
-			err = stream.Decode(&event)
+			err := stream.Decode(&event)
 			if err != nil {
 				l.zLog.Err(err).Msg("failed to decode change stream event")
 				continue
