6fae90152968615abda6ae088fb12a2dcbd5e5ad
Vladimir Erokhov <vladimir.erokhov@noveogroup.com>
2025-05-06 11:54:35 +0500

    feature(#5850): Migrate to Composition API and enhance form handling
    - Refactored `create-tag.vue` to use Composition API, replacing mixins with hooks for modal and form handling.
    - Added `isNew` prop to manage form state for new tags.
    - Updated `tag-form.vue` to disable the input field based on `isNew` and `isImported` states.
    - Improved test assertions in `create-tag.spec.js` for better clarity and consistency.
    - Updated snapshots to reflect changes in the component structure and props.
    
    This refactor enhances maintainability and aligns with the current coding standards.

--- community/sources/webcore/src/canopsis-next/src/components/modals/tag/create-tag.vue
+++ community/sources/webcore/src/canopsis-next/src/components/modals/tag/create-tag.vue
@@ -8,13 +8,14 @@
         <tag-form
           v-model="form"
           :is-imported="isImported"
+          :is-new="isNew"
         />
       </template>
       <template #actions="">
         <v-btn
           depressed
           text
-          @click="$modals.hide"
+          @click="close"
         >
           {{ $t('common.cancel') }}
         </v-btn>
@@ -32,13 +33,16 @@
 </template>
 
 <script>
+import { ref, computed } from 'vue';
+
 import { MODALS, VALIDATION_DELAY } from '@/constants';
 
 import { tagToForm, formToTag } from '@/helpers/entities/tag/form';
 
-import { modalInnerMixin } from '@/mixins/modal/inner';
-import { submittableMixinCreator } from '@/mixins/submittable';
-import { confirmableModalMixinCreator } from '@/mixins/confirmable-modal';
+import { useI18n } from '@/hooks/i18n';
+import { useInnerModal } from '@/hooks/modals';
+import { useSubmittableForm } from '@/hooks/submittable-form';
+import { useFormConfirmableCloseModal } from '@/hooks/confirmable-modal';
 
 import TagForm from '@/components/other/tag/form/tag-form.vue';
 
@@ -51,37 +55,47 @@ export default {
     delay: VALIDATION_DELAY,
   },
   components: { TagForm, ModalWrapper },
-  mixins: [
-    modalInnerMixin,
-    submittableMixinCreator(),
-    confirmableModalMixinCreator(),
-  ],
-  data() {
-    return {
-      form: tagToForm(this.modal.config.tag),
-    };
-  },
-  computed: {
-    title() {
-      return this.config.title || this.$t('modals.createTag.create.title');
-    },
-
-    isImported() {
-      return this.config.isImported;
+  props: {
+    modal: {
+      type: Object,
+      required: true,
     },
   },
-  methods: {
-    async submit() {
-      const isFormValid = await this.$validator.validateAll();
+  setup(props) {
+    const { t } = useI18n();
+    const { config, close } = useInnerModal(props);
 
-      if (isFormValid) {
-        if (this.config.action) {
-          await this.config.action(formToTag(this.form));
+    const form = ref(tagToForm(config.value.tag));
+
+    const isNew = computed(() => !config.value.tag?._id);
+    const title = computed(() => (
+      config.value.title || t('modals.createTag.create.title')
+    ));
+
+    const isImported = computed(() => config.value.isImported);
+
+    const { submit, isDisabled, submitting } = useSubmittableForm({
+      form,
+      method: async () => {
+        if (config.value.action) {
+          await config.value.action(formToTag(form.value));
         }
+        close();
+      },
+    });
 
-        this.$modals.hide();
-      }
-    },
+    useFormConfirmableCloseModal({ form, submit, close });
+
+    return {
+      form,
+      title,
+      isNew,
+      isImported,
+      isDisabled,
+      submitting,
+      submit,
+      close,
+    };
   },
 };
 </script>
