6d76c38be101be3593df2df56a7a1ce4f5e4fac6
Vladimir Erokhov <vladimir.erokhov@noveogroup.com>
2025-06-18 10:47:24 +0500

    feature(#5444): Update pbehavior patterns handling and introduce new API route
    - Added new API route for fetching all pbehavior patterns.
    - Removed unused modal for pbehavior patterns.
    - Enhanced c-patterns-field component to support counter methods and updated related logic.
    - Updated create-pbehavior modal to include pbehavior ID and counter type.
    - Refactored pbehavior patterns form to utilize new props and methods.
    - Improved tests for create-pbehavior and added new tests for pbehaviors-list-expand-item component.

--- community/sources/webcore/src/canopsis-next/src/bootstrap/plugins.js
+++ community/sources/webcore/src/canopsis-next/src/bootstrap/plugins.js
@@ -78,7 +78,6 @@ export const bootstrapApplicationPlugins = (Vue) => {
       [MODALS.createEventFilter]: { maxWidth: 1280 },
       [MODALS.testSuite]: { maxWidth: 920 },
       [MODALS.createPattern]: { maxWidth: 1280 },
-      [MODALS.pbehaviorPatterns]: { maxWidth: 1280 },
       [MODALS.createIdleRule]: { maxWidth: 1280 },
       [MODALS.createScenario]: { maxWidth: 1280 },
       [MODALS.createKpiFilter]: { maxWidth: 1280 },
