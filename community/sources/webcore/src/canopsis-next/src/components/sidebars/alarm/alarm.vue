4d7c41e8beb39f790ece455dd26915e3822d92f9
Vladimir Erokhov <vladimir.erokhov@noveogroup.com>
2025-07-23 08:37:18 +0500

    feature(#5918): Enhance alarm components with filter functionality and styling updates
    - Added `withFilterOnClick` prop to `c-columns-field.vue`, `columns.vue`, and `alarm.vue` for improved filtering capabilities.
    - Updated `column-field-expand-panel.vue` to handle filter state based on `withFilterOnClick`.
    - Refactored `c-alarm-state-chip.vue` to include a specific class for better styling.
    - Adjusted CSS in `alarm-column-cell.vue` for hover effects on alarm state chips.
    - Updated snapshots to reflect changes in component rendering and functionality.
    - Added new constants in `alarm.js` for additional alarm fields related to filtering.

--- community/sources/webcore/src/canopsis-next/src/components/sidebars/alarm/alarm.vue
+++ community/sources/webcore/src/canopsis-next/src/components/sidebars/alarm/alarm.vue
@@ -29,6 +29,7 @@
         with-template
         with-html
         with-color-indicator
+        with-filter-on-click
         @update:template="updateWidgetColumnsTemplate"
       />
       <field-columns
@@ -41,6 +42,7 @@
         with-template
         with-html
         with-color-indicator
+        with-filter-on-click
         @update:template="updateWidgetGroupColumnsTemplate"
       />
       <field-columns
