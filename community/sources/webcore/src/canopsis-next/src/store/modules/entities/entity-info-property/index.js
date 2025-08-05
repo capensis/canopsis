d849b41a08fe308cd72ecc540614ca00c0a1f285
Vladimir Erokhov <vladimir.erokhov@noveogroup.com>
2025-08-05 14:38:46 +0500

    feature(#5793): Enhance entity info properties and update related components
    - Integrated `entitiesEntityInfoPropertyMixin` into `c-entity-patterns-field.vue` for improved management of entity info properties.
    - Updated `pattern-rule-field.vue` to handle timestamp type in date rules, enhancing validation logic.
    - Refactored internationalization messages in French for entity info properties, streamlining user prompts.
    - Added new action in the Vuex store for fetching entity info properties with error handling.
    - Improved payload variables mixin to include entity aliases, enhancing data representation in forms.

--- community/sources/webcore/src/canopsis-next/src/store/modules/entities/entity-info-property/index.js
+++ community/sources/webcore/src/canopsis-next/src/store/modules/entities/entity-info-property/index.js
@@ -1,6 +1,10 @@
 import { API_ROUTES } from '@/config';
 
-import { createCRUDModule } from '@/store/plugins/entities';
+import request, { useRequestCancelling } from '@/services/request';
+
+import i18n from '@/i18n';
+
+import { createCRUDModule, DEFAULT_ENTITY_MODULE_TYPES } from '@/store/plugins/entities';
 
 export default createCRUDModule({
   route: API_ROUTES.entityInfosProperties,
@@ -9,5 +13,30 @@ export default createCRUDModule({
   getters: {
     itemsWithAlias: state => state.items.filter(item => !!item.alias),
     itemsWithoutAlias: state => state.items.filter(item => !item.alias),
+  },
+  actions: {
+    async fetchList({ commit, dispatch }, { params } = {}) {
+      try {
+        await useRequestCancelling(async (source) => {
+          commit(DEFAULT_ENTITY_MODULE_TYPES.FETCH_LIST, { params });
+
+          const { data, meta } = await request.get(
+            API_ROUTES.entityInfosProperties,
+            { params, cancelToken: source.token },
+          );
+
+          commit(DEFAULT_ENTITY_MODULE_TYPES.FETCH_LIST_COMPLETED, {
+            data,
+            meta,
+          });
+        }, 'entity-infos-properties-list');
+      } catch (err) {
+        console.error(err);
+
+        await dispatch('popups/error', { text: i18n.t('errors.default') }, { root: true });
+
+        commit(DEFAULT_ENTITY_MODULE_TYPES.FETCH_LIST_FAILED, {});
+      }
+    },
   },
 });
