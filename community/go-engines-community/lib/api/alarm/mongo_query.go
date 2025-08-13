dc7109fc6e1dcbe9d6e510b617b8d4f8f897a064
Zarina Yakubova <Zarina.Yakubova@noveogroup.com>
2025-08-13 18:21:41 +0200

    feature(#5913): Remove deprecated code

--- community/go-engines-community/lib/api/alarm/mongo_query.go
+++ community/go-engines-community/lib/api/alarm/mongo_query.go
@@ -948,9 +948,6 @@ func (q *MongoQueryBuilder) addCategoryFilter(r FilterRequest, match *[]bson.M) 
 func (q *MongoQueryBuilder) addTagsFilter(r FilterRequest, match *[]bson.M) {
 	if len(r.Tags) != 0 {
 		*match = append(*match, bson.M{"tags": bson.M{"$in": r.Tags}})
-	} else if r.Tag != "" {
-		// @todo: backward compatibility, tag parameter is deprecated, should be removed in 25.04.
-		*match = append(*match, bson.M{"tags": r.Tag})
 	}
 }
 
