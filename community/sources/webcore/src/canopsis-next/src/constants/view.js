b4c16be001d0867e502709e1263b09e7593cc01d
Vladimir Erokhov <vladimir.erokhov@noveogroup.com>
2025-08-14 10:58:10 +0500

    feature(#4735): Enhance fullscreen mode handling in view-fab-btns component
    - Added `FULLSCREEN_MODES_TO_DEFAULT_SCREEN_MODES` constant to map fullscreen modes to their default screen modes.
    - Updated `view-fab-btns` component to set the active view screen mode when exiting fullscreen.
    - Refactored imports in `view-fab-btns` to include the new constant for improved functionality.

--- community/sources/webcore/src/canopsis-next/src/constants/view.js
+++ community/sources/webcore/src/canopsis-next/src/constants/view.js
@@ -5,6 +5,11 @@ export const VIEW_SCREEN_MODES = {
   kioskFullscreen: 'kioskFullscreen',
 };
 
+export const FULLSCREEN_MODES_TO_DEFAULT_SCREEN_MODES = {
+  [VIEW_SCREEN_MODES.fullscreen]: VIEW_SCREEN_MODES.default,
+  [VIEW_SCREEN_MODES.kioskFullscreen]: VIEW_SCREEN_MODES.kiosk,
+};
+
 export const KEYS_TO_VIEW_SCREEN_MODES = {
   Digit1: VIEW_SCREEN_MODES.default,
   Digit2: VIEW_SCREEN_MODES.fullscreen,
