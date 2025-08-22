ac0a980225a7deb63fbd369dda4ce7b8cecb0da7
Zarina Yakubova <Zarina.Yakubova@noveogroup.com>
2025-08-22 18:09:11 +0200

    feature(#5988): Add logger cmd option

--- community/go-engines-community/cmd/canopsis-api-community/main.go
+++ community/go-engines-community/cmd/canopsis-api-community/main.go
@@ -39,7 +39,7 @@ func main() {
 		gin.SetMode(gin.ReleaseMode)
 	}
 
-	logger := log.NewLogger(ctx, flags.Debug)
+	logger := log.NewLogger(ctx, flags.Options)
 
 	// Retrieve config.
 	dbClient, err := mongo.NewClient(ctx, 0, 0, logger)
