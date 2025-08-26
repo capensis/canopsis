5c80e699cc60041a680d68a5a64a731742b97456
Vladimir Erokhov <vladimir.erokhov@noveogroup.com>
2025-08-26 16:09:26 +0500

    feature(#5905): Enhance external data table components and improve validation
    - Added disabled prop to c-select-chip and other related components for better user control.
    - Updated import-external-data-table-records.vue to improve form submission handling and validation.
    - Refactored external data table import hooks to streamline form management and error handling.
    - Enhanced i18n messages for improved user experience, including new translations for data type selection and loading states.
    - Adjusted external data table column handling to utilize updated structure and improve data processing.

--- community/sources/webcore/src/canopsis-next/src/components/other/external-data-table/form/fields/external-data-table-column-data-type-field.vue
+++ community/sources/webcore/src/canopsis-next/src/components/other/external-data-table/form/fields/external-data-table-column-data-type-field.vue
@@ -4,40 +4,89 @@
       <c-select-chip
         v-field="value.type"
         :items="types"
+        :disabled="disabled"
         class="px-2"
         color="grey"
         text-color="blue darken-1"
         outlined
       >
         <template #selection-empty>
-          <span class="grey--text">Select data type</span>
+          <span class="grey--text">{{ $t('externalData.selectDataType') }}</span>
         </template>
       </c-select-chip>
     </v-flex>
-    <v-flex v-if="additionalComponent">
-      <component
-        :is="additionalComponent.is"
-        v-bind="additionalComponent.bind"
-        v-on="additionalComponent.on"
+    <v-flex v-if="isNumber || isStringArray">
+      <external-data-table-column-data-type-field-number-form
+        v-if="isNumber"
+        v-field="value"
+        :table-separator="tableSeparator"
+        :disabled="disabled"
       />
+      <external-data-table-column-data-type-field-string-array-menu
+        v-if="isStringArray"
+        v-field="value"
+        :table-separator="tableSeparator"
+        :disabled="disabled"
+      >
+        <template #selection="{ on }">
+          <c-chip
+            :color="stringArrayChip.color"
+            :text-color="stringArrayChip.textColor"
+            class="px-2"
+            outlined
+            v-on="on"
+          >
+            <span v-if="!value.string_array_type" :class="stringArrayChip.prefixColorClass">
+              {{ $t('externalData.tableColumnDataTypesAdditionalChips.stringArray.selectSeparator') }}
+            </span>
+            <span v-else-if="isCustomValueTypeSelected">
+              <span :class="stringArrayChip.prefixColorClass" class="mr-2">
+                {{ stringArrayChip.prefix }}:
+              </span>
+              <span>
+                {{ value.string_array_separator }}
+              </span>
+            </span>
+            <span v-else>
+              <span :class="stringArrayChip.prefixColorClass">
+                {{ stringArrayChip.prefix }}
+              </span>
+            </span>
+          </c-chip>
+        </template>
+      </external-data-table-column-data-type-field-string-array-menu>
     </v-flex>
   </v-layout>
 </template>
 
 <script>
 import { mapValues } from 'lodash';
-import { computed } from 'vue';
+import { computed, onBeforeMount, onBeforeUnmount } from 'vue';
+import { Validator } from 'vee-validate';
 
-import { EXTERNAL_DATA_TABLE_COLUMN_DATA_TYPES } from '@/constants';
+import {
+  EXTERNAL_DATA_TABLE_COLUMN_DATA_TYPES,
+  EXTERNAL_DATA_TABLE_COLUMN_STRING_ARRAY_DATA_TYPE_TYPES,
+} from '@/constants';
 
 import { useI18n } from '@/hooks/i18n';
-import { useModelField } from '@/hooks/form';
+import { useValidator } from '@/hooks/validator/validator';
+import { useValidationAttachRequired } from '@/hooks/validator/validation-attach-required';
 
 import ExternalDataTableColumnDataTypeFieldNumberForm from './partials/external-data-table-column-data-type-field-number-form.vue';
 import ExternalDataTableColumnDataTypeFieldStringArrayMenu
   from './partials/external-data-table-column-data-type-field-string-array-menu.vue';
 
 export default {
+  inject: {
+    $validator: {
+      default: new Validator(),
+    },
+  },
+  components: {
+    ExternalDataTableColumnDataTypeFieldNumberForm,
+    ExternalDataTableColumnDataTypeFieldStringArrayMenu,
+  },
   model: {
     prop: 'value',
     event: 'input',
@@ -51,10 +100,15 @@ export default {
       type: String,
       required: false,
     },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
   },
-  setup(props, { emit }) {
+  setup(props) {
     const { t, te } = useI18n();
-    const { updateModel } = useModelField(props, emit);
+
+    const validator = useValidator();
 
     const typesMap = computed(() => mapValues(EXTERNAL_DATA_TABLE_COLUMN_DATA_TYPES, (type) => {
       const key = `externalData.tableColumnDataTypes.${type}`;
@@ -71,32 +125,47 @@ export default {
     }));
 
     const types = computed(() => Object.values(typesMap.value));
-    const additionalComponent = computed(() => {
-      if ([
-        EXTERNAL_DATA_TABLE_COLUMN_DATA_TYPES.number,
-        EXTERNAL_DATA_TABLE_COLUMN_DATA_TYPES.stringArray,
-      ].includes(props.value.type)) {
-        return {
-          is: props.value.type === EXTERNAL_DATA_TABLE_COLUMN_DATA_TYPES.number
-            ? ExternalDataTableColumnDataTypeFieldNumberForm
-            : ExternalDataTableColumnDataTypeFieldStringArrayMenu,
-          bind: {
-            value: props.value,
-            tableSeparator: props.tableSeparator,
-          },
-          on: {
-            input: value => updateModel(value),
-          },
-        };
+    const isNumber = computed(() => props.value.type === EXTERNAL_DATA_TABLE_COLUMN_DATA_TYPES.number);
+    const isStringArray = computed(() => props.value.type === EXTERNAL_DATA_TABLE_COLUMN_DATA_TYPES.stringArray);
+
+    const isCustomValueTypeSelected = computed(() => (
+      props.value.string_array_type === EXTERNAL_DATA_TABLE_COLUMN_STRING_ARRAY_DATA_TYPE_TYPES.custom
+    ));
+
+    const stringArrayChip = computed(() => {
+      const result = {
+        prefix: isCustomValueTypeSelected.value
+          ? t('externalData.tableColumnDataTypesAdditionalChips.stringArray.separator')
+          : t(`externalData.tableColumnDataTypesAdditionalChips.stringArray.types.${props.value.string_array_type}.text`),
+        color: 'grey',
+        textColor: 'blue darken-1',
+        prefixColorClass: 'grey--text',
+      };
+
+      if (validator.errors.has(props.value.name)) {
+        result.color = 'error';
+        result.textColor = 'error';
+        result.prefixColorClass = 'error--text';
       }
 
-      return null;
+      return result;
     });
+
+    const {
+      attachRequiredRule,
+      detachRequiredRule,
+    } = useValidationAttachRequired(props.value.name);
+
+    onBeforeMount(() => attachRequiredRule(() => !isStringArray.value || !!props.value.string_array_type));
+    onBeforeUnmount(detachRequiredRule);
 
     return {
       typesMap,
       types,
-      additionalComponent,
+      isNumber,
+      isStringArray,
+      isCustomValueTypeSelected,
+      stringArrayChip,
     };
   },
 };
