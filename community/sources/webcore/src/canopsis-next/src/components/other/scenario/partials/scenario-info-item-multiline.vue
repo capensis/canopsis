72b6675d0a04d78eae23dec27dde0f903ba99817
Vladimir Erokhov <vladimir.erokhov@noveogroup.com>
2025-08-29 16:37:37 +0500

    feature(#5774): Enhance request form and scenario components with multiple URL support
    - Added a toggle for allowing multiple URLs in the request form component.
    - Updated the webhook form to handle multiple URLs and emit changes accordingly.
    - Introduced a new multiline info item component for better display of scenario action details.
    - Refactored existing components to integrate new features and improve layout.
    - Updated translations for new UI elements related to multiple URLs.

--- community/sources/webcore/src/canopsis-next/src/components/other/scenario/partials/scenario-info-item-multiline.vue
+++ community/sources/webcore/src/canopsis-next/src/components/other/scenario/partials/scenario-info-item-multiline.vue
@@ -1,14 +1,17 @@
 <template>
   <v-list-item class="text-subtitle-1">
-    <v-list-item-avatar v-if="!hideIcon && !multiline">
-      <v-icon>{{ icon }}</v-icon>
-    </v-list-item-avatar>
     <v-list-item-content>
-      <v-layout align-center>
+      <v-layout class="gap-2" column>
         <div class="text-subtitle-1">
           <span class="font-weight-bold">{{ label }}</span>
-          <span v-if="value">: {{ value }}</span>
+          <span v-if="value">:</span>
         </div>
+        <v-layout v-if="value" class="gap-2 grey--text" align-center>
+          <v-icon v-if="!hideIcon" color="grey">
+            {{ icon }}
+          </v-icon>
+          <span v-if="value">{{ value }}</span>
+        </v-layout>
       </v-layout>
     </v-list-item-content>
   </v-list-item>
@@ -30,10 +33,6 @@ export default {
       default: '',
     },
     hideIcon: {
-      type: Boolean,
-      default: false,
-    },
-    multiline: {
       type: Boolean,
       default: false,
     },
