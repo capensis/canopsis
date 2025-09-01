2cd47600d214c69c31467d842c7a9c2b8dccc568
Vladimir Erokhov <vladimir.erokhov@noveogroup.com>
2025-09-01 16:20:48 +0500

    feature(#5923): Add default alarm more info template and update form handling
    - Introduced `DEFAULT_ALARM_MORE_INFO_TEMPLATE` for consistent alarm information display.
    - Updated `alarmListBaseParametersToForm` and `alarmListWidgetDefaultParametersToForm` to utilize the new template.
    - Adjusted tests to accommodate changes in alarm parameters and ensure proper functionality.

--- community/sources/webcore/src/canopsis-next/src/helpers/entities/widget/forms/alarm.js
+++ community/sources/webcore/src/canopsis-next/src/helpers/entities/widget/forms/alarm.js
@@ -21,6 +21,7 @@ import {
   SORT_ORDERS,
   TIME_UNITS,
   WIDGET_TYPES,
+  DEFAULT_ALARM_MORE_INFO_TEMPLATE,
 } from '@/constants';
 import { EXPAND_DEFAULT_MAX_LETTERS, PAGINATION_LIMIT } from '@/config';
 
@@ -301,7 +302,7 @@ export const widgetSortToForm = (sort = {}) => ({
 export const alarmListBaseParametersToForm = (alarmListParameters = {}) => ({
   sort: widgetSortToForm(alarmListParameters.sort),
   itemsPerPage: alarmListParameters.itemsPerPage ?? PAGINATION_LIMIT,
-  moreInfoTemplate: alarmListParameters.moreInfoTemplate ?? '',
+  moreInfoTemplate: alarmListParameters.moreInfoTemplate ?? DEFAULT_ALARM_MORE_INFO_TEMPLATE,
   moreInfoTemplateTemplate: widgetTemplateValueToForm(alarmListParameters.moreInfoTemplateTemplate),
   infoPopups: infoPopupsToForm(alarmListParameters.infoPopups),
   widgetColumnsTemplate: widgetTemplateValueToForm(alarmListParameters.widgetColumnsTemplate),
@@ -361,7 +362,7 @@ export const alarmListChartToForm = (chart = {}) => {
 export const alarmListWidgetDefaultParametersToForm = (parameters = {}) => ({
   itemsPerPage: parameters.itemsPerPage ?? PAGINATION_LIMIT,
   infoPopups: infoPopupsToForm(parameters.infoPopups),
-  moreInfoTemplate: parameters.moreInfoTemplate ?? '',
+  moreInfoTemplate: parameters.moreInfoTemplate ?? DEFAULT_ALARM_MORE_INFO_TEMPLATE,
   moreInfoTemplateTemplate: widgetTemplateValueToForm(parameters.moreInfoTemplateTemplate),
   isAckNoteRequired: !!parameters.isAckNoteRequired,
   isSnoozeNoteRequired: !!parameters.isSnoozeNoteRequired,
