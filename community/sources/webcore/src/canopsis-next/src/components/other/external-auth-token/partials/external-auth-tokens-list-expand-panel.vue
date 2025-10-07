f9302ecc8fa5dbcf36d76497b52ddbe04622d518
Vladimir Erokhov <vladimir.erokhov@noveogroup.com>
2025-10-08 00:14:18 +0500

    feature(#5847): Enhance request forms with authentication options
    - Added support for authentication tokens in request forms.
    - Introduced new components for handling authentication with and without tokens.
    - Updated existing request form to conditionally render authentication fields based on the selected method.
    - Created a new request-with-token-form component for managing requests that require tokens.
    - Refactored request-auth-field to streamline the authentication process.
    - Added constants for request authentication types and token types.
    - Updated internationalization files to include new labels for authentication fields.

---
+++ community/sources/webcore/src/canopsis-next/src/components/other/external-auth-token/partials/external-auth-tokens-list-expand-panel.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="secondary pa-3">
+    <v-card>
+      <v-card-text>
+        <v-data-table
+          :headers="headers"
+          :items="items"
+        >
+          <template #item.date="{ item }">
+            {{ item.date | date }}
+          </template>
+        </v-data-table>
+      </v-card-text>
+    </v-card>
+  </div>
+</template>
+
+<script>
+import { computed } from 'vue';
+
+import { useI18n } from '@/hooks/i18n';
+
+export default {
+  props: {
+    record: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+  setup() {
+    const { t } = useI18n();
+
+    const headers = computed(() => [
+      { text: t('common.requestDate'), value: 'date' },
+      { text: t('common.output'), value: 'output' },
+    ]);
+
+    return {
+      headers,
+    };
+  },
+};
+</script>
--- community/sources/webcore/src/canopsis-next/src/i18n/messages/en/external-auth-token.js
+++ community/sources/webcore/src/canopsis-next/src/i18n/messages/en/external-auth-token.js
@@ -3,4 +3,6 @@ export default {
   tokenExpirationTime: 'Token expiration time',
   lastUsedDate: 'Last used date',
   lastUpdateDate: 'Last update date',
+  allowVariables: 'Allow variables in the Token',
+  tokenExpirationHelpText: 'API Response field in JSON format where “Token” is taken from',
 };
