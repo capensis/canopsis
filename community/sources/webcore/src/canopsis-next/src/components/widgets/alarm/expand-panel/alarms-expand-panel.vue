aada5b4c83f69f7e8ce85b4d0142f7c3a5b27336
Vladimir Erokhov <vladimir.erokhov@noveogroup.com>
2025-10-14 16:49:14 +0500

    fix(#5879): Update search handler to prevent unnecessary updates, add checks before joining socket room, and ensure state updates only if widget exists.

--- community/sources/webcore/src/canopsis-next/src/components/widgets/alarm/expand-panel/alarms-expand-panel.vue
+++ community/sources/webcore/src/canopsis-next/src/components/widgets/alarm/expand-panel/alarms-expand-panel.vue
@@ -411,12 +411,14 @@ export default {
 
     search: {
       immediate: true,
-      handler(search) {
-        this.query = {
-          ...this.query,
+      handler(search, prevSearch = '') {
+        if (search !== prevSearch) {
+          this.query = {
+            ...this.query,
 
-          search,
-        };
+            search,
+          };
+        }
       },
     },
   },
