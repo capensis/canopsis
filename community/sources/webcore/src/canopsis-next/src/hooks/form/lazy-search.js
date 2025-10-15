8ca2592639f7caaeabbd23b7b2ef021111e644e5
Vladimir Erokhov <vladimir.erokhov@noveogroup.com>
2025-10-15 17:12:03 +0500

    fix(#5871): Enhance alarm management features by adding a new meta alarm field component, updating existing components to utilize it, and improving the lazy search functionality. Adjust styles for required labels and refine form submission logic in the link-to-meta-alarm modal.

--- community/sources/webcore/src/canopsis-next/src/hooks/form/lazy-search.js
+++ community/sources/webcore/src/canopsis-next/src/hooks/form/lazy-search.js
@@ -5,7 +5,10 @@ import {
   isString,
   isNumber,
   isUndefined,
+  isEmpty,
+  isObject,
   debounce,
+  uniq,
   uniqBy,
 } from 'lodash';
 import {
@@ -13,6 +16,7 @@ import {
   ref,
   unref,
   watch,
+  set,
   onMounted,
 } from 'vue';
 
@@ -35,11 +39,12 @@ import { useModelField } from '@/hooks/form';
  * @param {string} options.isKey - The key used to identify each item uniquely.
  * @param {string} options.childrenKey - The key used for children checking.
  * @param {string} options.idParamsKey - The key used for query parameters when fetching selected items.
- * @param {number} [options.limit = PAGINATION_LIMIT] - The limit for pagination, determining how many items to
- * fetch per page.
  * @param {Function} options.fetchHandler - The asynchronous function used to fetch data from the server.
  * @param {boolean} options.addable - The flag for indicating possibility to add new item.
  * @param {boolean} options.multiple - The flag for indicating possibility to choose more than one item.
+ * @param {boolean} options.returnObject - The flag for indicating possibility to return object instead of id.
+ * @param {boolean} options.attachValue - The flag for indicating possibility to attach the value to the items.
+ * @param {Object} options.initialQuery - The initial query object.
  * @param {number} options.delay - The delay number value for debouncing.
  * @param {Function} emit - The emit function for Vue events, used to update the model.
  * @returns {Object} An object containing methods and properties for managing search and selection:
@@ -57,14 +62,17 @@ export const useLazySearch = ({
   idKey,
   idParamsKey,
   childrenKey = 'items',
-  limit = PAGINATION_LIMIT,
   fetchHandler,
   addable,
   multiple,
+  returnObject,
+  attachValue,
+  initialQuery = { page: 1, limit: PAGINATION_LIMIT, search: '' },
   delay = 100,
 }, emit) => {
   const pageCount = ref(1);
   const itemsByValue = ref({});
+  const itemsValues = ref([]);
   const selectedItems = ref([]);
 
   const { updateModel } = useModelField({}, emit);
@@ -73,7 +81,7 @@ export const useLazySearch = ({
    * Computed property to get the list of items from the itemsByValue map.
    * @type {ComputedRef<Array>}
    */
-  const items = computed(() => Object.values(itemsByValue.value));
+  const items = computed(() => itemsValues.value.map(itemValue => itemsByValue.value[itemValue]));
 
   /**
    * Computed property to convert the value into an array format.
@@ -82,7 +90,7 @@ export const useLazySearch = ({
   const arrayValue = computed(() => {
     const unwrappedValue = unref(value);
 
-    if (!unwrappedValue && !isNumber(unwrappedValue)) {
+    if ((!unwrappedValue || isEmpty(unwrappedValue)) && !isNumber(unwrappedValue)) {
       return [];
     }
 
@@ -100,15 +108,16 @@ export const useLazySearch = ({
     pending: valuesPending,
     handler: initializeSelectedItems,
   } = usePendingHandler(async () => {
+    if (!arrayValue.value.length && selectedItems.value.length) {
+      selectedItems.value = [];
+      return;
+    }
+
     const selectedItemsFromItemsByValue = arrayValue.value.map(item => itemsByValue.value[item]).filter(Boolean);
 
     if (selectedItemsFromItemsByValue.length === arrayValue.value.length) {
       selectedItems.value = selectedItemsFromItemsByValue;
 
-      return;
-    }
-
-    if (!arrayValue.value.length) {
       return;
     }
 
@@ -124,13 +133,24 @@ export const useLazySearch = ({
     const dataById = deepKeyBy(data, unwrappedIdKey, unwrappedChildrenKey);
 
     selectedItems.value = arrayValue.value.map(item => (
-      dataById[item[unwrappedIdKey] ?? item] ?? ({ [unwrappedIdKey]: item, noData: true })
+      dataById[item[unwrappedIdKey] ?? item]
+      ?? (isObject(item) ? item : ({ [unwrappedIdKey]: item, noData: true }))
     ));
+
+    if (attachValue) {
+      selectedItems.value.forEach((item) => {
+        const id = item[unwrappedIdKey];
+
+        if (id && !itemsByValue.value[id]) {
+          set(itemsByValue.value, id, item);
+        }
+      });
+    }
   }, true);
 
   /**
-   * MAIN FETCH LOGIC
-   */
+     * MAIN FETCH LOGIC
+     */
   const {
     pending,
     query,
@@ -138,21 +158,27 @@ export const useLazySearch = ({
     updateQuery,
     updateQueryPage,
   } = usePendingWithLocalQuery({
-    initialQuery: {
-      page: 1,
-      limit: unref(limit),
-      search: '',
-    },
+    initialQuery: unref(initialQuery),
     fetchHandler: async (params) => {
+      const unwrappedIdKey = unref(idKey);
+
       const { data, meta } = await unref(fetchHandler)({
         params,
       });
 
       pageCount.value = meta.page_count;
 
+      /**
+      * We need to use it for saving order of items
+      */
+      itemsValues.value = uniq([
+        ...(params.page && params.page !== 1 ? itemsValues.value : []),
+        ...data.map(item => item[unwrappedIdKey]),
+      ]);
+
       itemsByValue.value = {
-        ...(params.page !== 1 ? itemsByValue.value : {}),
-        ...keyBy(data, unref(idKey)),
+        ...(params.page && params.page !== 1 ? itemsByValue.value : {}),
+        ...keyBy(data, unwrappedIdKey),
         ...pick(itemsByValue.value, arrayValue.value),
       };
     },
@@ -202,6 +228,7 @@ export const useLazySearch = ({
   const changeSelectedItems = (newSelectedItems) => {
     if (!newSelectedItems) {
       selectedItems.value = [];
+
       updateModel('');
 
       return;
@@ -216,14 +243,30 @@ export const useLazySearch = ({
     if (!isArray(newSelectedItems)) {
       preparedNewSelectedItems = [newSelectedItems];
     } else {
-      preparedNewSelectedItems = unwrappedMultiple ? newSelectedItems : newSelectedItems.slice(1);
+      preparedNewSelectedItems = unwrappedMultiple ? newSelectedItems : newSelectedItems.slice(-1);
     }
 
-    selectedItems.value = uniqBy((
-      unwrappedAddable
-        ? preparedNewSelectedItems
-        : preparedNewSelectedItems.filter(item => !isString(item))
-    ).map(item => (isUndefined(item[unwrappedIdKey]) ? { [unwrappedIdKey]: item } : item)), unwrappedIdKey);
+    selectedItems.value = uniqBy(
+      (
+        unwrappedAddable
+          ? preparedNewSelectedItems
+          : preparedNewSelectedItems.filter(item => !isString(item))
+      ).map(item => (
+        isUndefined(item[unwrappedIdKey])
+          ? { [unwrappedIdKey]: item, noData: true }
+          : item)),
+      unwrappedIdKey,
+    );
+
+    if (returnObject) {
+      updateModel(
+        unwrappedMultiple
+          ? selectedItems.value
+          : selectedItems.value[0],
+      );
+
+      return;
+    }
 
     updateModel(
       unwrappedMultiple
