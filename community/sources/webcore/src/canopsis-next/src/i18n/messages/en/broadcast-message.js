2fc8ea25fefe44a81849d18c2e2189eb90c87e38
Vladimir Erokhov <vladimir.erokhov@noveogroup.com>
2025-10-17 15:59:29 +0500

    feature(#5937): Refactor broadcast message forms and enhance view selection
    - Replaced `broadcast-message-pages-form.vue` with `broadcast-message-views-form.vue` for improved view selection.
    - Updated `create-broadcast-message.vue` to integrate new tree structure for views and playlists.
    - Enhanced form handling in `form.js` to support optimized view selection and validation.
    - Added error handling for required views in localization files.
    - Removed obsolete `broadcast-message-pages-form.vue` to streamline component structure.

--- community/sources/webcore/src/canopsis-next/src/i18n/messages/en/broadcast-message.js
+++ community/sources/webcore/src/canopsis-next/src/i18n/messages/en/broadcast-message.js
@@ -4,6 +4,9 @@ export default {
   viewsAndPages: 'Views and pages',
   allViews: 'All views',
   allPlaylists: 'All playlists',
+  errors: {
+    viewsRequired: 'At least one page must be selected',
+  },
   statuses: {
     [BROADCAST_MESSAGES_STATUSES.active]: 'Active',
     [BROADCAST_MESSAGES_STATUSES.pending]: 'Pending',
