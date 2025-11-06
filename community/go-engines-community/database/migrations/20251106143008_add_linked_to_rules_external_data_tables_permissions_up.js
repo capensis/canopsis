066b428370bc9a01dc2a8befc4975a5790084c9d
ikalugin <ivan.kalugin@noveogroup.com>
2025-11-06 15:49:42 +0100

    fix(#6058): fixes

--- community/go-engines-community/database/migrations/20251106143008_add_linked_to_rules_external_data_tables_permissions_up.js
+++ community/go-engines-community/database/migrations/20251106143008_add_linked_to_rules_external_data_tables_permissions_up.js
@@ -6,3 +6,9 @@ db.role.updateMany({"$or":[
         "permissions.api_external_data_table": 4
     }
 });
+
+db.permission.updateMany({_id: {$in: ["models_exploitation_linkRule", "models_exploitation_eventFilter"]}}, {
+    $set: {
+        "api_permissions.api_external_data_table": 4
+    }
+});
