892f8a4be0396a74198fcfeed52badbf3d5d360e
Vladimir Erokhov <vladimir.erokhov@noveogroup.com>
2025-11-18 13:58:37 +0500

    fix(#5509): Update french translation for weekly

--- community/sources/webcore/src/canopsis-next/src/i18n/messages/fr/common.js
+++ community/sources/webcore/src/canopsis-next/src/i18n/messages/fr/common.js
@@ -354,7 +354,7 @@ export default {
     minutely: 'Par minute',
     hourly: 'Par heure',
     daily: 'Quotidien',
-    weekly: 'Hebdomadiare',
+    weekly: 'Hebdomadaire',
     monthly: 'Mensuel',
     yearly: 'Annuel',
   },
