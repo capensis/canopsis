76bdea772b7d133b504b19fc029529091b73284c
Vladimir Erokhov <vladimir.erokhov@noveogroup.com>
2025-12-11 11:52:36 +0500

    fix(#5509): Update French translation for 'view management' to 'groupes de vues'

--- community/sources/webcore/src/canopsis-next/src/i18n/messages/fr/permission.js
+++ community/sources/webcore/src/canopsis-next/src/i18n/messages/fr/permission.js
@@ -911,7 +911,7 @@ export default {
       description: 'Accès à la route API pour gérer les icônes',
     },
     [USERS_PERMISSIONS.api.general.view]: {
-      name: 'Gestion des vues',
+      name: 'Groupes de vues',
       description: 'Accès à la route API pour gérer les vues',
     },
 
