89ff6dcd766285c93a43b225f2b8fc609e246421
Zarina Yakubova <Zarina.Yakubova@noveogroup.com>
2026-02-04 17:37:44 +0100

    feature(#6000): Add permission

--- community/go-engines-community/database/migrations/20260123152123_add_failed_ticket_to_alarms_down.js
+++ community/go-engines-community/database/migrations/20260123152123_add_failed_ticket_to_alarms_down.js
@@ -1,3 +1,15 @@
+db.permission.deleteOne({_id: "listalarm_removeAssociatedTicket"});
+db.role.updateMany({}, {
+    $unset: {
+        "permissions.listalarm_removeAssociatedTicket": "",
+    }
+});
+db.role_template.updateMany({}, {
+    $unset: {
+        "permissions.listalarm_removeAssociatedTicket": "",
+    }
+});
+
 db.periodical_alarm.updateMany(
     {"v.failed_ticket": {$ne: null}},
     {$unset: {"v.failed_ticket": null}},
