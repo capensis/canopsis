79a6a560e0759adc84387f18a68e5986875c46b0
Zarina Yakubova <Zarina.Yakubova@noveogroup.com>
2026-02-23 16:48:06 +0100

    feature(#6062): Fix code style

--- community/go-engines-community/lib/api/docs/handler.go
+++ community/go-engines-community/lib/api/docs/handler.go
@@ -1,6 +1,8 @@
 package docs
 
 import (
+	"bytes"
+	"maps"
 	"net/http"
 	"strings"
 
@@ -26,7 +28,7 @@ func GetHandler(errorResponder httperror.Responder, generatedSchemasContents [][
 			return
 		}
 
-		if info, ok := mergedContent["info"].(map[string]interface{}); ok {
+		if info, ok := mergedContent["info"].(map[string]any); ok {
 			buildInfo := canopsis.GetBuildInfo()
 
 			info["version"] = buildInfo.Version
@@ -40,9 +42,13 @@ func GetHandler(errorResponder httperror.Responder, generatedSchemasContents [][
 func mergeContent(contents [][]byte) (map[string]any, error) {
 	var mergedContent map[string]any
 	for _, content := range contents {
-		replacedSchemasContent := strings.ReplaceAll(string(content), "schemas_swagger.yaml#/definitions/", "#/components/schemas/")
-		var parsed map[string]interface{}
-		err := yaml.Unmarshal([]byte(replacedSchemasContent), &parsed)
+		replacedSchemasContent := bytes.ReplaceAll(
+			content,
+			[]byte(`schemas_swagger.yaml#/definitions/`),
+			[]byte(`#/components/schemas/`),
+		)
+		var parsed map[string]any
+		err := yaml.Unmarshal(replacedSchemasContent, &parsed)
 		if err != nil {
 			return nil, err
 		}
@@ -52,23 +58,19 @@ func mergeContent(contents [][]byte) (map[string]any, error) {
 			continue
 		}
 
-		if paths, ok := parsed["paths"].(map[string]interface{}); ok {
-			if mergedPaths, ok := mergedContent["paths"].(map[string]interface{}); ok {
-				for k, v := range paths {
-					mergedPaths[k] = v
-				}
+		if paths, ok := parsed["paths"].(map[string]any); ok {
+			if mergedPaths, ok := mergedContent["paths"].(map[string]any); ok {
+				maps.Copy(mergedPaths, paths)
 			} else {
 				mergedContent["paths"] = paths
 			}
 		}
 
-		if components, ok := parsed["components"].(map[string]interface{}); ok {
-			if schemas, ok := components["schemas"].(map[string]interface{}); ok {
-				if mergedComponents, ok := mergedContent["components"].(map[string]interface{}); ok {
-					if mergedSchemas, ok := mergedComponents["schemas"].(map[string]interface{}); ok {
-						for k, v := range schemas {
-							mergedSchemas[k] = v
-						}
+		if components, ok := parsed["components"].(map[string]any); ok {
+			if schemas, ok := components["schemas"].(map[string]any); ok {
+				if mergedComponents, ok := mergedContent["components"].(map[string]any); ok {
+					if mergedSchemas, ok := mergedComponents["schemas"].(map[string]any); ok {
+						maps.Copy(mergedSchemas, schemas)
 					} else {
 						mergedComponents["schemas"] = schemas
 					}
@@ -83,18 +85,16 @@ func mergeContent(contents [][]byte) (map[string]any, error) {
 func mergeSchemas(generatedSchemasContents [][]byte, mergedContent map[string]any) (map[string]any, error) {
 	for _, generatedSchemasContent := range generatedSchemasContents {
 		replacedSchemasContent := strings.ReplaceAll(string(generatedSchemasContent), "#/definitions/", "#/components/schemas/")
-		var parsed map[string]interface{}
+		var parsed map[string]any
 		err := yaml.Unmarshal([]byte(replacedSchemasContent), &parsed)
 		if err != nil {
 			return nil, err
 		}
 
-		if schemas, ok := parsed["definitions"].(map[string]interface{}); ok {
-			if mergedComponents, ok := mergedContent["components"].(map[string]interface{}); ok {
-				if mergedSchemas, ok := mergedComponents["schemas"].(map[string]interface{}); ok {
-					for k, v := range schemas {
-						mergedSchemas[k] = v
-					}
+		if schemas, ok := parsed["definitions"].(map[string]any); ok {
+			if mergedComponents, ok := mergedContent["components"].(map[string]any); ok {
+				if mergedSchemas, ok := mergedComponents["schemas"].(map[string]any); ok {
+					maps.Copy(mergedSchemas, schemas)
 				} else {
 					mergedComponents["schemas"] = schemas
 				}
