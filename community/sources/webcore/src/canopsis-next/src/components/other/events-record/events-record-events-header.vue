13c490e2a389a32b3d7b8638d3712369c092e889
Vladimir Erokhov <Vladimir.Erokhov@noveogroup.com>
2026-03-02 09:35:44 +0500

    feature(#5857): Refactor events record modal and introduce event filtering
    - Added a new events record filter component to enhance user interaction with event records.
    - Updated the events record header to display recording status and improved button functionalities for stopping recordings and applying filters.
    - Refactored the events record modal to integrate the new filter component and streamline the display of recording counts and statuses.
    
    These changes improve the usability and functionality of the events record management interface.

--- community/sources/webcore/src/canopsis-next/src/components/other/events-record/events-record-events-header.vue
+++ community/sources/webcore/src/canopsis-next/src/components/other/events-record/events-record-events-header.vue
@@ -1,77 +1,42 @@
 <template>
-  <v-layout class="gap-2" column>
-    <v-layout class="gap-3 my-4" justify-center align-center>
-      <span class="text-subtitle-2">{{ $t('modals.eventsRecord.subtitle', { count }) }}</span>
-      <c-action-btn
-        :tooltip="$t('modals.eventsRecord.buttonTooltip')"
-        type="delete"
-        @click="remove"
-      />
-    </v-layout>
-    <v-layout class="gap-3 my-4" justify-space-between align-center>
-      <v-layout class="gap-2" align-center>
-        <v-btn
-          v-if="hasFilterApplied"
-          color="secondary"
-          @click="applyEventFilter"
-        >
-          <v-icon class="mr-2">
-            filter_list
-          </v-icon>
-          {{ $t('eventsRecord.eventFilterApplied') }}
-        </v-btn>
-        <v-btn
-          v-else
-          color="secondary"
-          outlined
-          @click="applyEventFilter"
-        >
-          {{ $t('eventsRecord.applyEventFilter') }}
-        </v-btn>
-
-        <v-btn
-          v-if="hasFilterApplied"
-          color="secondary"
-          outlined
-          @click="resetFilter"
-        >
-          {{ $t('common.reset') }}
-        </v-btn>
-      </v-layout>
-      <v-spacer />
-      <events-record-download-btn :events-record-id="eventsRecordId" />
-    </v-layout>
+  <v-layout class="gap-3 my-4" justify-center align-center>
+    <v-progress-circular
+      v-if="isRecording"
+      size="24"
+      width="2"
+      indeterminate
+    />
+    <span class="text-subtitle-2">
+      {{ $t('modals.eventsRecord.subtitle', { count }) }}
+    </span>
+    <c-action-btn
+      v-if="isRecording"
+      :tooltip="$t('eventsRecord.stop')"
+      icon="stop"
+      color="blue darken-3"
+      @click="$emit('stop:recording')"
+    />
+    <c-action-btn
+      v-else
+      :tooltip="$t('modals.eventsRecord.buttonTooltip')"
+      icon="delete"
+      color="error"
+      @click="$emit('remove')"
+    />
   </v-layout>
 </template>
-<script>
-import EventsRecordDownloadBtn from './partials/events-record-download-btn.vue';
 
+<script>
 export default {
-  components: { EventsRecordDownloadBtn },
   props: {
-    eventsRecordId: {
-      type: String,
-      required: true,
-    },
     count: {
       type: Number,
       default: 0,
     },
-    hasFilterApplied: {
+    isRecording: {
       type: Boolean,
       default: false,
     },
-  },
-  setup(props, { emit }) {
-    const remove = () => emit('remove');
-    const applyEventFilter = () => emit('apply:filter');
-    const resetFilter = () => emit('reset:filter');
-
-    return {
-      remove,
-      applyEventFilter,
-      resetFilter,
-    };
   },
 };
 </script>
