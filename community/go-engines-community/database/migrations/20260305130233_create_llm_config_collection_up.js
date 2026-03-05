102e58c59f159bb1a7bd43a58b48d9c7359f68c8
Zarina Yakubova <Zarina.Yakubova@noveogroup.com>
2026-03-05 17:13:21 +0100

    feature(#6175): Add LLM CRUD API

---
+++ community/go-engines-community/database/migrations/20260305130233_create_llm_config_collection_up.js
@@ -0,0 +1,22 @@
+if (db.getCollectionNames().includes("llm_config")) {
+    db.runCommand({collMod: "llm_config", changeStreamPreAndPostImages: {enabled: true}})
+} else {
+    db.createCollection("llm_config", {changeStreamPreAndPostImages: {enabled: true}})
+}
+
+db.llm_config.createIndex({name: 1}, {name: "name_1", unique: true});
+
+if (!db.permission.findOne({_id: "api_llm_config"})) {
+    db.permission.insertOne({
+        _id: "api_llm_config",
+        name: "api_llm_config",
+        type: "CRUD",
+        description: "LLM config",
+        groups: ["api", "api_rules"]
+    });
+    db.role.updateOne({name: "admin"}, {
+        $set: {
+            "permissions.api_llm_config": 15
+        }
+    });
+}
