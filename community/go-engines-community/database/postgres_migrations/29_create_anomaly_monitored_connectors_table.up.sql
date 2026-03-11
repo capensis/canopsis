4638881ea82212f8d37d8d68161e7376768b659c
Alexander Pavlov <alexander.pavlov@noveogroup.com>
2026-03-11 12:59:47 +0100

    feat(#5356): update anomaly_monitored_connectors table and adjust queries in Store

--- community/go-engines-community/database/postgres_migrations/29_create_anomaly_monitored_connectors_table.up.sql
+++ community/go-engines-community/database/postgres_migrations/29_create_anomaly_monitored_connectors_table.up.sql
@@ -1,9 +1,6 @@
 BEGIN;
-
-CREATE TABLE IF NOT EXISTS anomaly_monitored_connectors
-(
-    id            BIGSERIAL PRIMARY KEY,
-    connector_name   VARCHAR(255) UNIQUE NOT NULL
+CREATE TABLE IF NOT EXISTS anomaly_monitored_connectors (
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(255) UNIQUE NOT NULL
 );
-
 COMMIT;
