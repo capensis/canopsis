33220411b5b99351876df6ecfd77d0dec0817ebf
Vladimir Erokhov <Vladimir.Erokhov@noveogroup.com>
2026-03-13 14:30:27 +0500

    feature(#5950): Enhance comment template forms with validation and UI improvements
    - Added sortable columns for name, last modified date, and last modified by in the comment templates list.
    - Implemented validation for required fields in the comment template fields form, displaying an alert when no fields are present.
    - Introduced a tooltip for the add field button to improve user experience.
    - Set a maximum length for the name field in the comment template form.
    - Added English and French translations for the new validation error message.

--- community/sources/webcore/src/canopsis-next/src/components/other/comment-template/form/comment-template-fields-form.vue
+++ community/sources/webcore/src/canopsis-next/src/components/other/comment-template/form/comment-template-fields-form.vue
@@ -1,6 +1,14 @@
 <template>
   <v-layout class="gap-2" column>
     <span class="text-subtitle-2">{{ $t('common.fields') }}</span>
+    <v-flex xs12>
+      <v-alert
+        :value="!form.length"
+        :color="errors.has('fields') ? 'error' : 'info'"
+      >
+        {{ $t('commentTemplate.errors.fieldsRequired') }}
+      </v-alert>
+    </v-flex>
     <c-card-iterator-field
       v-field="form"
       :handle="`.${dragItemHandleClass}`"
@@ -18,6 +26,7 @@
                 v-field="form[index].name"
                 :label="$t('common.fieldName')"
                 :name="`fields[${index}].name`"
+                :max-length="255"
                 hide-details
                 required
               />
@@ -35,22 +44,31 @@
       class="mt-2"
       justify-end
     >
-      <v-btn
-        color="primary"
-        fab
-        small
-        @click="addField"
-      >
-        <v-icon>add</v-icon>
-      </v-btn>
+      <v-tooltip left>
+        <template #activator="{ on }">
+          <v-btn
+            color="primary"
+            fab
+            small
+            v-on="on"
+            @click="addField"
+          >
+            <v-icon>add</v-icon>
+          </v-btn>
+        </template>
+        <span>{{ $t('common.add') }}</span>
+      </v-tooltip>
     </v-layout>
   </v-layout>
 </template>
 
 <script>
+import { watch, nextTick, onMounted, onBeforeUnmount } from 'vue';
+
 import { uid } from '@/helpers/uid';
 
 import { useArrayModelField } from '@/hooks/form/array-model-field';
+import { useValidationAttachRequired } from '@/hooks/validator/validation-attach-required';
 
 export default {
   inject: ['$validator'],
@@ -68,6 +86,10 @@ export default {
     const dragItemHandleClass = 'field-drag-handler';
 
     const { addItemIntoArray, removeItemFromArray } = useArrayModelField(props, emit);
+    const { attachRequiredRule, detachRequiredRule, validateRequiredRule } = useValidationAttachRequired('fields');
+
+    const getter = () => !!props.form.length;
+    const validate = () => nextTick(validateRequiredRule);
 
     /**
      * Add new field to the fields array
@@ -85,9 +107,22 @@ export default {
      */
     const removeField = index => removeItemFromArray(index);
 
+    watch(() => props.form, () => validate());
+
+    onMounted(() => {
+      if (!props.form.length) {
+        addField();
+      }
+
+      attachRequiredRule(getter);
+    });
+
+    onBeforeUnmount(detachRequiredRule);
+
     return {
       dragItemHandleClass,
       addField,
+      validate,
       removeField,
     };
   },
