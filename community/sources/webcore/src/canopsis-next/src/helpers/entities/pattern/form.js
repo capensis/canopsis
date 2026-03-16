90eb2d0ef2a46b5f6cb4b122e08154030d493d51
Vladimir Erokhov <Vladimir.Erokhov@noveogroup.com>
2026-03-16 16:37:15 +0500

    fix(#6192): Update patternRuleToForm to include array operator support for stringArray field type

--- community/sources/webcore/src/canopsis-next/src/helpers/entities/pattern/form.js
+++ community/sources/webcore/src/canopsis-next/src/helpers/entities/pattern/form.js
@@ -1063,8 +1063,7 @@ export const patternRuleToForm = (rule = {}) => {
   }
 
   if (
-    (isExtraInfos || isInfos)
-    && form.fieldType === PATTERN_FIELD_TYPES.stringArray
+    (form.fieldType === PATTERN_FIELD_TYPES.stringArray || isArrayOperator(form.operator))
     && isArray(form.value)
     && (!form.value.length || !form.value[0]?.key)
   ) {
