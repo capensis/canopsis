b44c52e36bbb1886baa3025daedd7b68e923b05a
Zarina Yakubova <Zarina.Yakubova@noveogroup.com>
2026-06-09 11:59:39 +0200

    fix(#6279): Fix migration

--- community/go-engines-community/database/migrations/20260608180029_fix_ui_permissions_up.js
+++ community/go-engines-community/database/migrations/20260608180029_fix_ui_permissions_up.js
@@ -1,31 +1,31 @@
 db.permission.updateOne({_id: "listalarm_pbehavior"}, {
     $set: {
-        api_pbehaviortype: 4,
-        api_pbehaviorreason: 4,
-        api_pbehaviorexception: 4,
+        "api_permissions.api_pbehaviortype": 4,
+        "api_permissions.api_pbehaviorreason": 4,
+        "api_permissions.api_pbehaviorexception": 4,
     }
 });
 
 db.role.updateMany({"permissions.listalarm_pbehavior": 1}, {
-    $set: {
-        "permissions.api_pbehaviortype": 4,
-        "permissions.api_pbehaviorreason": 4,
-        "permissions.api_pbehaviorexception": 4,
+    $bit: {
+        "permissions.api_pbehaviortype": {or: 4},
+        "permissions.api_pbehaviorreason": {or: 4},
+        "permissions.api_pbehaviorexception": {or: 4},
     }
 });
 
 db.permission.updateOne({_id: "listalarm_fastPbehavior"}, {
     $set: {
-        api_pbehavior: 9,
-        api_pbehaviortype: 4,
-        api_pbehaviorreason: 4,
+        "api_permissions.api_pbehavior": 9,
+        "api_permissions.api_pbehaviortype": 4,
+        "api_permissions.api_pbehaviorreason": 4,
     }
 });
 
 db.role.updateMany({"permissions.listalarm_fastPbehavior": 1}, {
-    $set: {
-        "permissions.api_pbehavior": 9,
-        "permissions.api_pbehaviortype": 4,
-        "permissions.api_pbehaviorreason": 4,
+    $bit: {
+        "permissions.api_pbehavior": {or: 9},
+        "permissions.api_pbehaviortype": {or: 4},
+        "permissions.api_pbehaviorreason": {or: 4},
     }
 });
