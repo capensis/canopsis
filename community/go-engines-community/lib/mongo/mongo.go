d189ae299e15c0804ce0c71a4a3f24d58d14fbd6
Alexander Pavlov <alexander.pavlov@noveogroup.com>
2026-06-15 20:14:19 +0200

    fix(#6294): revert url.ParseRequestURI to get MongoDB database name

--- community/go-engines-community/lib/mongo/mongo.go
+++ community/go-engines-community/lib/mongo/mongo.go
@@ -6,7 +6,9 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"net/url"
 	"os"
+	"strings"
 	"time"
 
 	"go.mongodb.org/mongo-driver/v2/bson"
@@ -375,9 +377,12 @@ func NewClient(ctx context.Context, opts ...ClientOptions) (_ DbClient, err erro
 		return nil, errors.New("only one ClientOptions is allowed")
 	}
 
-	mongoURL, err := getURL()
+	mongoURL, dbName, err := getURL()
 	if err != nil {
 		return nil, err
+	}
+	if dbName == "*" {
+		dbName = DB
 	}
 
 	bsonOpts := &options.BSONOptions{
@@ -439,7 +444,7 @@ func NewClient(ctx context.Context, opts ...ClientOptions) (_ DbClient, err erro
 
 	return &dbClient{
 		Client:          mongoClient,
-		Database:        mongoClient.Database(DB),
+		Database:        mongoClient.Database(dbName),
 		RetryCount:      clientOptions.RetryCount,
 		MinRetryTimeout: clientOptions.MinRetryTimeout,
 	}, nil
@@ -564,13 +569,17 @@ func isMongoDistributed(ctx context.Context, client *mongo.Client) (bool, error)
 }
 
 // getURL parses URL value in EnvURL environment variable
-func getURL() (string, error) {
-	mongoURL := os.Getenv(EnvURL)
+func getURL() (mongoURL, dbName string, err error) {
+	mongoURL = os.Getenv(EnvURL)
 	if mongoURL == "" {
-		return "", fmt.Errorf("environment variable %s empty", EnvURL)
+		return "", "", fmt.Errorf("environment variable %s empty", EnvURL)
 	}
-
-	return mongoURL, nil
+	parsed, err := url.ParseRequestURI(mongoURL)
+	if err != nil {
+		return "", "", err
+	}
+	dbName = strings.TrimPrefix(parsed.EscapedPath(), "/")
+	return mongoURL, dbName, nil
 }
 
 func retry(ctx context.Context, retryCount int, retryTimeout time.Duration, f func(context.Context) error) {
