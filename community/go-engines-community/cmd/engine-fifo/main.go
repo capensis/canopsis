a607d037f59b918636a0fa535970a89bd0aacc8a
ikalugin <ivan.kalugin@noveogroup.com>
2026-08-20 17:46:26 +0200

    fix(#5904): add SIGTERM to signal.NotifyContext signals

--- community/go-engines-community/cmd/engine-fifo/main.go
+++ community/go-engines-community/cmd/engine-fifo/main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"os"
 	"os/signal"
+	"syscall"
 
 	"git.canopsis.net/canopsis/canopsis-community/community/go-engines-community/lib/canopsis"
 	"git.canopsis.net/canopsis/canopsis-community/community/go-engines-community/lib/canopsis/debug"
@@ -14,7 +15,7 @@ import (
 
 func main() {
 	// Graceful shutdown.
-	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
 	defer stop()
 
 	opts, deprecatedFlags := fifo.ParseOptions()
